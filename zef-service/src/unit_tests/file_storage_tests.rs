b62ac4a925080d8d669ee7334e8e81438a7defea
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2022-02-14 00:11:03 +0000

    [zef] storage: add unit tests

---
+++ zef-service/src/unit_tests/file_storage_tests.rs
@@ -0,0 +1,22 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use super::*;
+use zef_core::base_types::*;
+
+#[tokio::test]
+async fn test_file_storage_for_accounts() {
+    let dir = tempfile::TempDir::new().unwrap();
+    let mut client = FileStoreClient::new(dir.path().to_path_buf());
+    let id = AccountId(vec![SequenceNumber(1)]);
+    {
+        let mut account = client.read_account_or_default(id.clone()).await.unwrap();
+        assert_eq!(account.next_sequence_number, SequenceNumber(0));
+        account.next_sequence_number = SequenceNumber(3);
+        client.write_account(id.clone(), account).await.unwrap();
+    }
+    {
+        let account = client.read_account_or_default(id.clone()).await.unwrap();
+        assert_eq!(account.next_sequence_number, SequenceNumber(3));
+    }
+}
