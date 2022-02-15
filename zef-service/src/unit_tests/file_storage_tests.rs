cd6436ddbf32caf9ff62e7fac8dba302c4d513e4
laurent <laurent.mazare@gmail.com>
2022-02-15 11:40:04 +0000

    Add certificate test.

--- zef-service/src/unit_tests/file_storage_tests.rs
+++ zef-service/src/unit_tests/file_storage_tests.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use super::*;
-use zef_core::base_types::*;
+use zef_core::{base_types::*, messages::*};
 
 #[tokio::test]
 async fn test_file_storage_for_accounts() {
@@ -19,4 +19,28 @@ async fn test_file_storage_for_accounts() {
         let account = client.read_account_or_default(&id).await.unwrap();
         assert_eq!(account.next_sequence_number, SequenceNumber(3));
     }
+}
+
+#[tokio::test]
+async fn test_file_storage_for_certificates() {
+    let dir = tempfile::TempDir::new().unwrap();
+    let mut client = FileStoreClient::new(dir.path().to_path_buf());
+    let proposal = ConsensusProposal {
+        instance_id: AccountId::default(),
+        round: SequenceNumber::default(),
+        decision: ConsensusDecision::Confirm,
+    };
+    let value = Value::PreCommit {
+        proposal,
+        requests: vec![],
+    };
+    let certificate = Certificate::new(value, vec![]);
+    let certificate_clone = certificate.clone();
+    let certificate_hash = certificate.hash.clone();
+    client.write_certificate(certificate).await.unwrap();
+    let read_certificate = client
+        .read_certificate(certificate_hash.clone())
+        .await
+        .unwrap();
+    assert_eq!(read_certificate.hash, certificate_clone.hash);
 }
