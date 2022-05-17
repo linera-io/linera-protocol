e9554843395b17101b1eb28ee31d0fda9d28713b
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-05-17 22:35:57 +0000

    Implement `From<*>` for `SerializedMessage`
    Make it easier to wrap existing instances of message variants.
    
    PR #42: Implement a `Codec` for the network protocol

--- zef-base/src/serialize.rs
+++ zef-base/src/serialize.rs
@@ -41,3 +41,45 @@ where
 {
     bincode::deserialize_from(reader)
 }
+
+impl From<BlockProposal> for SerializedMessage {
+    fn from(block_proposal: BlockProposal) -> Self {
+        SerializedMessage::BlockProposal(Box::new(block_proposal))
+    }
+}
+
+impl From<Certificate> for SerializedMessage {
+    fn from(certificate: Certificate) -> Self {
+        SerializedMessage::Certificate(Box::new(certificate))
+    }
+}
+
+impl From<ChainInfoQuery> for SerializedMessage {
+    fn from(chain_info_query: ChainInfoQuery) -> Self {
+        SerializedMessage::ChainInfoQuery(Box::new(chain_info_query))
+    }
+}
+
+impl From<Vote> for SerializedMessage {
+    fn from(vote: Vote) -> Self {
+        SerializedMessage::Vote(Box::new(vote))
+    }
+}
+
+impl From<ChainInfoResponse> for SerializedMessage {
+    fn from(chain_info_response: ChainInfoResponse) -> Self {
+        SerializedMessage::ChainInfoResponse(Box::new(chain_info_response))
+    }
+}
+
+impl From<Error> for SerializedMessage {
+    fn from(error: Error) -> Self {
+        SerializedMessage::Error(Box::new(error))
+    }
+}
+
+impl From<CrossChainRequest> for SerializedMessage {
+    fn from(cross_chain_request: CrossChainRequest) -> Self {
+        SerializedMessage::CrossChainRequest(Box::new(cross_chain_request))
+    }
+}
