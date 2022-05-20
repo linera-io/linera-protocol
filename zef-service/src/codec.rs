70c540d41dc96aa91dc52dd854a6f01fc6261678
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-05-20 17:08:01 +0000

    Use a new `codec::Error` type
    Implement `Error` and `From` using `thiserror`. The new error type
    should make it easier to handle the errors that rise from the transport.
    
    PR #42: Implement a `Codec` for the network protocol

--- zef-service/src/codec.rs
+++ zef-service/src/codec.rs
@@ -1,4 +1,6 @@
 use bytes::{Buf, BufMut, BytesMut};
+use std::io;
+use thiserror::Error;
 use tokio_util::codec::{Decoder, Encoder};
 use zef_base::serialize::SerializedMessage;
 
@@ -6,7 +8,7 @@ use zef_base::serialize::SerializedMessage;
 pub struct Codec;
 
 impl Encoder<SerializedMessage> for Codec {
-    type Error = Box<bincode::ErrorKind>;
+    type Error = Error;
 
     fn encode(
         &mut self,
@@ -14,14 +16,27 @@ impl Encoder<SerializedMessage> for Codec {
         buffer: &mut BytesMut,
     ) -> Result<(), Self::Error> {
         bincode::serialize_into(&mut buffer.writer(), &message)
+            .map_err(|error| Error::Serialization(*error))
     }
 }
 
 impl Decoder for Codec {
     type Item = SerializedMessage;
-    type Error = Box<bincode::ErrorKind>;
+    type Error = Error;
 
     fn decode(&mut self, buffer: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         Ok(bincode::deserialize_from(buffer.reader()).ok())
     }
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("IO error in the underlying transport")]
+    Io(#[from] io::Error),
+
+    #[error("Failed to deserialize an incoming message")]
+    Deserialization(#[source] bincode::ErrorKind),
+
+    #[error("Failed to serialize outgoing message")]
+    Serialization(#[source] bincode::ErrorKind),
 }
