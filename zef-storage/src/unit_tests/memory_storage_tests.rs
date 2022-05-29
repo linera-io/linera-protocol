35cc51cc092700f14af3ee4a7a4213d9514f71f3
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2022-05-29 11:54:41 -0700

    move committee subcribers out of the authenticated chain states

--- zef-storage/src/unit_tests/memory_storage_tests.rs
+++ zef-storage/src/unit_tests/memory_storage_tests.rs
@@ -17,9 +17,7 @@ async fn test_read_write() {
         .committees
         .insert(Epoch::from(0), Committee::make_simple(Vec::new()));
     chain.state.epoch = Some(Epoch::from(0));
-    chain.state.status = Some(ChainStatus::Managing {
-        subscribers: Vec::new(),
-    });
+    chain.state.status = Some(ChainStatus::Managing);
     chain.state.manager = ChainManager::single(Owner(PublicKey::debug(2)));
     store.write_chain(chain).await.unwrap();
     store
