1cbdce3ace12b008ba161d80c01ce7fb771edf84
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2022-07-17 19:33:05 -0700

    Add an integration test variant using S3 storage
    Use the same integration test in the `README`, but with the server
    storage changed to use an S3 LocalStack instance.
    
    As part of the test, a `LocalStackTestContext` guard is obtained in
    order to avoid running the test in parallel with another test that also
    uses the same LocalStack instance.

--- linera-service/tests/test_readme.rs
+++ linera-service/tests/test_readme.rs
@@ -2,6 +2,7 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use linera_storage::LocalStackTestContext;
 use std::{io::Write, process::Command};
 use tempfile::tempdir;
 
@@ -12,13 +13,13 @@ fn test_examples_in_readme() -> std::io::Result<()> {
     let mut quotes = get_bash_quotes(file)?;
     // Check that we have the expected number of examples starting with "```bash".
     assert_eq!(quotes.len(), 1);
+    let quote = quotes.pop().unwrap();
 
     let mut test_script = std::fs::File::create(dir.path().join("test.sh"))?;
-    write!(&mut test_script, "{}", quotes.pop().unwrap())?;
+    write!(&mut test_script, "{}", quote)?;
 
     let status = Command::new("bash")
         .current_dir("..") // root of the repo
-        .env("DEST", dir.path())
         .arg("-e")
         .arg("-x")
         .arg(dir.path().join("test.sh"))
@@ -52,4 +53,33 @@ where
     }
 
     Ok(result)
+}
+
+const ROCKSDB_STORAGE: &str = "--storage rocksdb:server_\"$I\"_\"$J\".db";
+const S3_STORAGE: &str = "--storage s3:localstack:server-\"$I\"";
+
+#[test]
+#[ignore]
+fn test_examples_in_readme_s3() -> std::io::Result<()> {
+    let _localstack_guard = LocalStackTestContext::new();
+    let dir = tempdir().unwrap();
+    let file = std::io::BufReader::new(std::fs::File::open("../README.md")?);
+    let mut quotes = get_bash_quotes(file)?;
+    // Check that we have the expected number of examples starting with "```bash".
+    assert_eq!(quotes.len(), 1);
+    let quote = quotes.pop().unwrap();
+    assert_eq!(quote.matches(ROCKSDB_STORAGE).count(), 3);
+    let quote = quote.replace(ROCKSDB_STORAGE, S3_STORAGE);
+
+    let mut test_script = std::fs::File::create(dir.path().join("test.sh"))?;
+    write!(&mut test_script, "{}", quote)?;
+
+    let status = Command::new("bash")
+        .current_dir("..") // root of the repo
+        .arg("-e")
+        .arg("-x")
+        .arg(dir.path().join("test.sh"))
+        .status()?;
+    assert!(status.success());
+    Ok(())
 }
