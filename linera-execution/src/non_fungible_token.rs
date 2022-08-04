4954a271181f994409578cf4b99f5d0f187ec072
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-08-04 00:59:32 +0000

    WIP Non-fungible token

--- linera-execution/src/non_fungible_token.rs
+++ linera-execution/src/non_fungible_token.rs
@@ -1,22 +1,35 @@
 use crate::{ExecutionContext, SmartContract};
 use async_trait::async_trait;
-use linera_base::execution::ApplicationResult;
+use linera_base::{ensure, execution::ApplicationResult, messages::ChainId};
+use linera_views::views::{RegisterOperations, RegisterView, View};
+use serde::Serialize;
+use thiserror::Error;
 
 pub struct NonFungibleToken;
 
 #[async_trait]
-impl<C: Send> SmartContract<C> for NonFungibleToken {
-    type Parameters = ();
-    type Operation = ();
-    type Effect = ();
-    type Query = ();
-    type Response = ();
+impl<C> SmartContract<C> for NonFungibleToken
+where
+    C: RegisterOperations<bool> + Clone + Send + Sync,
+{
+    type Parameters = InitialOwner;
+    type Operation = Operation;
+    type Effect = Effect;
+    type Query = Query;
+    type Response = QueryResponse;
 
     async fn instantiate(
         execution: &ExecutionContext,
-        parameters: Self::Parameters,
+        initial_owner: Self::Parameters,
     ) -> ApplicationResult {
-        todo!()
+        ApplicationResult {
+            effects: vec![Effect::Receive.to_system_effect(execution)],
+            operations: vec![],
+            recipients: vec![initial_owner.0],
+            subscribe: None,
+            unsubscribe: None,
+            need_channel_broadcast: Vec::new(),
+        }
     }
 
     async fn apply_operation(
@@ -24,22 +37,132 @@ impl<C: Send> SmartContract<C> for NonFungibleToken {
         storage: &mut C,
         operation: Self::Operation,
     ) -> ApplicationResult {
-        todo!()
+        match operation {
+            Operation::Transfer { recipient } => Self::transfer(recipient, execution, storage)
+                .await
+                .unwrap_or_else(|error| {
+                    log::error!("{error}");
+                    ApplicationResult::default()
+                }),
+        }
     }
 
     async fn apply_effect(
-        execution: &ExecutionContext,
+        _execution: &ExecutionContext,
         storage: &mut C,
         effect: Self::Effect,
     ) -> ApplicationResult {
-        todo!()
+        match effect {
+            Effect::Receive => Self::receive(storage).await.unwrap_or_else(|_| {
+                log::error!("Failed to store received token");
+                ApplicationResult::default()
+            }),
+        }
     }
 
     async fn run_query(
-        execution: &ExecutionContext,
+        _execution: &ExecutionContext,
         storage: &mut C,
         query: Self::Query,
     ) -> Self::Response {
-        todo!()
+        match query {
+            Query::HasToken => {
+                QueryResponse::HasToken(Self::has_token(storage).await.unwrap_or_else(|_| {
+                    log::error!("Failed to query token ownership");
+                    false
+                }))
+            }
+        }
     }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct InitialOwner(ChainId);
+
+#[derive(Clone, Copy, Debug)]
+pub enum Operation {
+    Transfer { recipient: Option<ChainId> },
+}
+
+#[derive(Clone, Copy, Debug, Serialize)]
+pub enum Effect {
+    Receive,
+}
+
+impl Effect {
+    pub fn to_system_effect(&self, context: &ExecutionContext) -> linera_base::execution::Effect {
+        linera_base::execution::Effect::application_specific(context.application_id, self)
+            .expect("Receive effect is serializable")
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum Query {
+    HasToken,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum QueryResponse {
+    HasToken(bool),
+}
+
+impl NonFungibleToken {
+    async fn transfer<S>(
+        recipient: Option<ChainId>,
+        execution: &ExecutionContext,
+        storage: &mut S,
+    ) -> Result<ApplicationResult, TransferError>
+    where
+        S: RegisterOperations<bool> + Clone + Send + Sync,
+    {
+        let mut ownership = RegisterView::<_, bool>::load(storage.clone(), vec![].into())
+            .await
+            .map_err(|_| TransferError::StorageError)?;
+        let is_current_owner = *ownership.get();
+        ensure!(is_current_owner, TransferError::NotCurrentOwner);
+        ownership.set(false);
+        ownership
+            .commit()
+            .await
+            .map_err(|_| TransferError::StorageError)?;
+        let application = match recipient {
+            None => ApplicationResult::default(),
+            Some(recipient) => ApplicationResult {
+                effects: vec![Effect::Receive.to_system_effect(execution)],
+                operations: vec![],
+                recipients: vec![recipient],
+                subscribe: None,
+                unsubscribe: None,
+                need_channel_broadcast: Vec::new(),
+            },
+        };
+        Ok(application)
+    }
+
+    async fn receive<S>(storage: &mut S) -> Result<ApplicationResult, S::Error>
+    where
+        S: RegisterOperations<bool> + Clone + Send + Sync,
+    {
+        let mut ownership = RegisterView::<_, bool>::load(storage.clone(), vec![].into()).await?;
+        ownership.set(true);
+        ownership.commit().await?;
+        Ok(ApplicationResult::default())
+    }
+
+    async fn has_token<S>(storage: &mut S) -> Result<bool, S::Error>
+    where
+        S: RegisterOperations<bool> + Clone + Send + Sync,
+    {
+        let ownership = RegisterView::<_, bool>::load(storage.clone(), vec![].into()).await?;
+        Ok(*ownership.get())
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum TransferError {
+    #[error("Can't transfer if the chain is not the current owner of the token")]
+    NotCurrentOwner,
+
+    #[error("Failed to load or store balance")]
+    StorageError,
 }
