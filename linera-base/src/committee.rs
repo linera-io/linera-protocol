eb57db8228090cc1e9b8950a16f0fad298361b4e
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2022-08-20 12:23:07 -0700

    create API for clients to force consistency and use it to make the client "benchmark" command more robust

--- linera-base/src/committee.rs
+++ linera-base/src/committee.rs
@@ -22,35 +22,44 @@ pub struct Committee {
     pub validators: BTreeMap<ValidatorName, ValidatorState>,
     /// The sum of all voting rights.
     pub total_votes: u64,
+    /// The threshold to form a quorum.
+    pub quorum_threshold: u64,
+    /// The threshold to prove the validity of a statement.
+    pub validity_threshold: u64,
 }
 
 impl Committee {
     pub fn new(validators: BTreeMap<ValidatorName, ValidatorState>) -> Self {
         let total_votes = validators.values().fold(0, |sum, state| sum + state.votes);
+        // Let N = 3f + 1 + k (0 <= k < 3).
+        // * (2 N + 3) / 3 = 2f + 1 + (2k + 2)/3 = 2f + 1 + k = N - f
+        // * (N + 2) / 3 = f + 1 + k/3 = f + 1
+        let quorum_threshold = 2 * total_votes / 3 + 1;
+        let validity_threshold = (total_votes + 2) / 3;
+
         Committee {
             validators,
             total_votes,
+            quorum_threshold,
+            validity_threshold,
         }
     }
 
     #[cfg(any(test, feature = "test"))]
     pub fn make_simple(keys: Vec<ValidatorName>) -> Self {
-        let total_votes = keys.len() as u64;
-        Committee {
-            validators: keys
-                .into_iter()
-                .map(|k| {
-                    (
-                        k,
-                        ValidatorState {
-                            network_address: k.to_string(),
-                            votes: 1,
-                        },
-                    )
-                })
-                .collect(),
-            total_votes,
-        }
+        let map = keys
+            .into_iter()
+            .map(|k| {
+                (
+                    k,
+                    ValidatorState {
+                        network_address: k.to_string(),
+                        votes: 1,
+                    },
+                )
+            })
+            .collect();
+        Committee::new(map)
     }
 
     pub fn weight(&self, author: &ValidatorName) -> u64 {
@@ -67,15 +76,11 @@ impl Committee {
     }
 
     pub fn quorum_threshold(&self) -> u64 {
-        // If N = 3f + 1 + k (0 <= k < 3)
-        // then (2 N + 3) / 3 = 2f + 1 + (2k + 2)/3 = 2f + 1 + k = N - f
-        2 * self.total_votes / 3 + 1
+        self.quorum_threshold
     }
 
     pub fn validity_threshold(&self) -> u64 {
-        // If N = 3f + 1 + k (0 <= k < 3)
-        // then (N + 2) / 3 = f + 1 + k/3 = f + 1
-        (self.total_votes + 2) / 3
+        self.validity_threshold
     }
 
     /// Find the highest value than is supported by a certain subset of validators.
