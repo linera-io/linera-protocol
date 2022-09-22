6c1e1acf34ae3155fd7a70e89ae4e3cdf245fbf3
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2022-09-22 16:55:10 +0000

    Stub initial `WasmApplication` type
    Only available if one of the WASM runtime features (`wasmer` or
    `wasmtime`) is enabled.

--- linera-execution/src/lib.rs
+++ linera-execution/src/lib.rs
@@ -5,6 +5,7 @@ mod execution;
 mod ownership;
 mod runtime;
 pub mod system;
+mod wasm;
 
 pub use execution::{ExecutionStateView, ExecutionStateViewContext};
 pub use ownership::ChainOwnership;
@@ -14,6 +15,8 @@ pub use system::{
     SystemEffect, SystemExecutionStateView, SystemExecutionStateViewContext, SystemOperation,
     SystemQuery, SystemResponse,
 };
+#[cfg(any(feature = "wasmer", feature = "wasmtime"))]
+pub use wasm::WasmApplication;
 
 use async_trait::async_trait;
 use dashmap::DashMap;
---
+++ linera-execution/src/wasm/mod.rs
@@ -0,0 +1,86 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Support for user applications compiled as WebAssembly (WASM) modules.
+//!
+//! Requires a WebAssembly runtime to be selected and enabled using one of the following features:
+//!
+//! - `wasmer` enables the [Wasmer](https://wasmer.io/) runtime
+//! - `wasmtime` enables the [Wasmtime](https://wasmtime.dev/) runtime
+
+#![cfg(any(feature = "wasmer", feature = "wasmtime"))]
+
+use crate::{
+    ApplicationCallResult, CalleeContext, EffectContext, ExecutionError, OperationContext,
+    QueryContext, QueryableStorage, RawExecutionResult, SessionCallResult, SessionId,
+    UserApplication, WritableStorage,
+};
+use async_trait::async_trait;
+use std::{io, path::Path};
+use tokio::fs;
+
+/// A user application in a compiled WebAssembly module.
+pub struct WasmApplication {
+    bytecode: Vec<u8>,
+}
+
+impl WasmApplication {
+    /// Create a new [`WasmApplication`] using the WebAssembly module in `bytecode_file`.
+    pub async fn from_file(bytecode_file: impl AsRef<Path>) -> Result<Self, io::Error> {
+        Ok(WasmApplication {
+            bytecode: fs::read(bytecode_file).await?,
+        })
+    }
+}
+
+#[async_trait]
+impl UserApplication for WasmApplication {
+    async fn execute_operation(
+        &self,
+        context: &OperationContext,
+        storage: &dyn WritableStorage,
+        operation: &[u8],
+    ) -> Result<RawExecutionResult<Vec<u8>>, ExecutionError> {
+        todo!();
+    }
+
+    async fn execute_effect(
+        &self,
+        context: &EffectContext,
+        storage: &dyn WritableStorage,
+        effect: &[u8],
+    ) -> Result<RawExecutionResult<Vec<u8>>, ExecutionError> {
+        todo!();
+    }
+
+    async fn call_application(
+        &self,
+        context: &CalleeContext,
+        storage: &dyn WritableStorage,
+        argument: &[u8],
+        forwarded_sessions: Vec<SessionId>,
+    ) -> Result<ApplicationCallResult, ExecutionError> {
+        todo!();
+    }
+
+    async fn call_session(
+        &self,
+        context: &CalleeContext,
+        storage: &dyn WritableStorage,
+        session_kind: u64,
+        session_data: &mut Vec<u8>,
+        argument: &[u8],
+        forwarded_sessions: Vec<SessionId>,
+    ) -> Result<SessionCallResult, ExecutionError> {
+        todo!();
+    }
+
+    async fn query_application(
+        &self,
+        context: &QueryContext,
+        storage: &dyn QueryableStorage,
+        argument: &[u8],
+    ) -> Result<Vec<u8>, ExecutionError> {
+        todo!();
+    }
+}
