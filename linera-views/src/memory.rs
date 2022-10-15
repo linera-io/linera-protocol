fe9c890b39d8376fa8696205daf3451013dcf12c
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-10-15 20:02:28 +0200

    More correction to the memory model.

--- linera-views/src/memory.rs
+++ linera-views/src/memory.rs
@@ -238,10 +238,11 @@ where
 
     async fn delete_front(
         &mut self,
-        _stored_indices: &mut Range<usize>,
+        stored_indices: &mut Range<usize>,
         _batch: &mut Self::Batch,
         count: usize,
     ) -> Result<(), Self::Error> {
+        stored_indices.start += count;
         self.with_mut(|v: &mut VecDeque<T>| {
             v.drain(..count);
         })
@@ -251,10 +252,11 @@ where
 
     async fn append_back(
         &mut self,
-        _stored_indices: &mut Range<usize>,
+        stored_indices: &mut Range<usize>,
         _batch: &mut Self::Batch,
         values: Vec<T>,
     ) -> Result<(), Self::Error> {
+        stored_indices.end += values.len();
         self.with_mut(|v: &mut VecDeque<T>| {
             for value in values {
                 v.push_back(value);
