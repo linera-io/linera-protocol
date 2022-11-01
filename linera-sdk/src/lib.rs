3f0c3e0eb6e4c294a20d01b111d9737d96cc0bc7
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2022-11-01 18:28:14 +0000

    Create a `HashValue` type
    Equivalent to `linera_base::crypto::HashValue`, but without the need for
    the `generic_array` dependency.

--- linera-sdk/src/lib.rs
+++ linera-sdk/src/lib.rs
@@ -1,2 +1,6 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
+
+/// A Sha512 value.
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
+pub struct HashValue([u8; 64]);
