eeff3827e7818505a16d645a0587e1a3bc622d5c
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2022-11-01 18:30:52 +0000

    Copy some types from `linera_base::messages`
    The types that are required inside a contract.

--- linera-sdk/src/lib.rs
+++ linera-sdk/src/lib.rs
@@ -1,6 +1,35 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+/// The index of an effect in a chain.
+#[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Debug)]
+pub struct EffectId {
+    pub chain_id: ChainId,
+    pub height: BlockHeight,
+    pub index: u64,
+}
+
+/// The unique identifier (UID) of a chain. This is the hash value of a ChainDescription.
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash)]
+pub struct ChainId(pub HashValue);
+
+/// A block height to identify blocks in a chain.
+#[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Default, Debug)]
+pub struct BlockHeight(pub u64);
+
 /// A Sha512 value.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
 pub struct HashValue([u8; 64]);
+
+/// The destination of a message, relative to a particular application.
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+pub enum Destination {
+    /// Direct message to a chain.
+    Recipient(ChainId),
+    /// Broadcast to the current subscribers of our channel.
+    Subscribers(String),
+}
+
+// FIXME: placeholder
+#[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Default, Debug)]
+pub struct ApplicationId(pub u64);
