a80d998b6e611495a72fc3b055dae34031767e5d
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2022-11-01 19:59:00 +0000

    Copy some types from `linera-execution`
    Copy the types necessary inside an application's implementation.
    
    Derive `Default` for `RawExecutionResult` instead of copying the custom
    `Default` implementation because the plan is to remove the generic type
    parameter (and therefore the inferred generic constraint) later.

--- linera-sdk/src/lib.rs
+++ linera-sdk/src/lib.rs
@@ -1,6 +1,54 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+#[derive(Debug, Clone)]
+pub struct OperationContext {
+    /// The current chain id.
+    pub chain_id: ChainId,
+    /// The current block height.
+    pub height: BlockHeight,
+    /// The current index of the operation.
+    pub index: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct EffectContext {
+    /// The current chain id.
+    pub chain_id: ChainId,
+    /// The current block height.
+    pub height: BlockHeight,
+    /// The id of the effect (based on the operation height and index in the remote
+    /// chain that created the effect).
+    pub effect_id: EffectId,
+}
+
+#[derive(Debug, Clone)]
+pub struct CalleeContext {
+    /// The current chain id.
+    pub chain_id: ChainId,
+    /// `None` if the caller doesn't want this particular call to be authenticated (e.g.
+    /// for safety reasons).
+    pub authenticated_caller_id: Option<ApplicationId>,
+}
+
+#[derive(Debug, Clone)]
+pub struct QueryContext {
+    /// The current chain id.
+    pub chain_id: ChainId,
+}
+
+/// Externally visible results of an execution. These results are meant in the context of
+/// the application that created them.
+#[derive(Debug, Default)]
+pub struct RawExecutionResult<Effect> {
+    /// Send messages to the given destinations.
+    pub effects: Vec<(Destination, Effect)>,
+    /// Subscribe chains to channels.
+    pub subscribe: Vec<(String, ChainId)>,
+    /// Unsubscribe chains to channels.
+    pub unsubscribe: Vec<(String, ChainId)>,
+}
+
 /// The index of an effect in a chain.
 #[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Debug)]
 pub struct EffectId {
@@ -34,3 +82,43 @@ pub enum Destination {
 // FIXME: placeholder
 #[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Default, Debug)]
 pub struct ApplicationId(pub u64);
+
+/// The identifier of a session.
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
+pub struct SessionId {
+    /// The application that runs the session.
+    pub application_id: ApplicationId,
+    /// User-defined tag.
+    pub kind: u64,
+    /// Unique index set by the runtime.
+    pub index: u64,
+}
+
+/// Syscall to request creating a new session.
+#[derive(Default)]
+pub struct NewSession {
+    /// A kind provided by the creator (meant to be visible to other applications).
+    pub kind: u64,
+    /// The data associated to the session.
+    pub data: Vec<u8>,
+}
+
+/// The result of calling into a user application.
+#[derive(Default)]
+pub struct ApplicationCallResult {
+    /// The return value.
+    pub value: Vec<u8>,
+    /// The externally-visible result.
+    pub execution_result: RawExecutionResult<Vec<u8>>,
+    /// The new sessions that were just created by the callee for us.
+    pub create_sessions: Vec<NewSession>,
+}
+
+/// The result of calling into a session.
+#[derive(Default)]
+pub struct SessionCallResult {
+    /// The application result.
+    pub inner: ApplicationCallResult,
+    /// If `call_session` was called, this tells the system to clean up the session.
+    pub data: Option<Vec<u8>>,
+}
