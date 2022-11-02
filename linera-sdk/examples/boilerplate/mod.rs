9a3811e556758dcca49b264080ba1eddd6d072d5
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2022-11-02 17:54:03 +0000

    Create a `boilerplate` module
    The goal is to allow this module to be shared between all application
    examples, at least until the SDK is able to generate the boilerplate
    code.

---
+++ linera-sdk/examples/boilerplate/mod.rs
@@ -0,0 +1,13 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Contains boilerplate necessary for the application to interface with the host runtime.
+//!
+//! Ideally, this code should be exported from [`linera-sdk`], but that's currently impossible due
+//! to how [`wit_bindgen_guest_rust`] works. It expects concrete types to be available in its parent
+//! module (which in this case is this module), so it has to exist in every application
+//! implementation.
+//!
+//! This should be fixable with a few changes to [`wit-bindgen`], but an alternative is to generate
+//! the code with a procedural macro. For now, this module should be included by all implemented
+//! applications.
