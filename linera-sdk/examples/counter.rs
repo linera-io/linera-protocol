5bee3e54766feb878b7e7e386c1d10f6635482df
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2022-11-02 13:37:32 +0000

    Create a simple counter application example
    A trivially simple application implementation. Supports operations and
    cross-application calls that increment the counter by a value, and
    supports queries to get the counter value.

---
+++ linera-sdk/examples/counter.rs
@@ -0,0 +1,96 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+#![cfg(target_arch = "wasm32")]
+
+use async_trait::async_trait;
+use linera_sdk::{
+    Application, ApplicationCallResult, CalleeContext, EffectContext, ExecutionResult,
+    OperationContext, QueryContext, Session, SessionCallResult, SessionId,
+};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+/// The application state.
+#[derive(Clone, Copy, Debug, Default, Deserialize, Serialize)]
+pub struct Counter {
+    value: u128,
+}
+
+#[async_trait]
+impl Application for Counter {
+    type Error = Error;
+
+    async fn execute_operation(
+        &mut self,
+        _context: &OperationContext,
+        operation: &[u8],
+    ) -> Result<ExecutionResult, Self::Error> {
+        let increment: u128 = bcs::from_bytes(operation)?;
+        self.value += increment;
+        Ok(ExecutionResult::default())
+    }
+
+    async fn execute_effect(
+        &mut self,
+        _context: &EffectContext,
+        _effect: &[u8],
+    ) -> Result<ExecutionResult, Self::Error> {
+        Err(Error::EffectsNotSupported)
+    }
+
+    async fn call_application(
+        &mut self,
+        _context: &CalleeContext,
+        argument: &[u8],
+        _forwarded_sessions: Vec<SessionId>,
+    ) -> Result<ApplicationCallResult, Self::Error> {
+        let increment: u128 = bcs::from_bytes(argument)?;
+        self.value += increment;
+        Ok(ApplicationCallResult {
+            value: bcs::to_bytes(&self.value).expect("Serialization should not fail"),
+            ..ApplicationCallResult::default()
+        })
+    }
+
+    async fn call_session(
+        &mut self,
+        _context: &CalleeContext,
+        _session: Session,
+        _argument: &[u8],
+        _forwarded_sessions: Vec<SessionId>,
+    ) -> Result<SessionCallResult, Self::Error> {
+        Err(Error::SessionsNotSupported)
+    }
+
+    async fn query_application(
+        &self,
+        _context: &QueryContext,
+        argument: &[u8],
+    ) -> Result<Vec<u8>, Self::Error> {
+        match argument {
+            &[] => Ok(bcs::to_bytes(&self.value).expect("Serialization should not fail")),
+            _ => Err(Error::InvalidQuery),
+        }
+    }
+}
+
+/// An error that can occur during the contract execution.
+#[derive(Debug, Error)]
+pub enum Error {
+    /// Counter application doesn't support any cross-chain effects.
+    #[error("Counter application doesn't support any cross-chain effects")]
+    EffectsNotSupported,
+
+    /// Counter application doesn't support any cross-application sessions.
+    #[error("Counter application doesn't support any cross-application sessions")]
+    SessionsNotSupported,
+
+    /// Invalid serialized increment value.
+    #[error("Invalid serialized increment value")]
+    InvalidIncrement(#[from] bcs::Error),
+
+    /// Invalid query argument; Counter application only supports a single (empty) query.
+    #[error("Invalid query argument; Counter application only supports a single (empty) query")]
+    InvalidQuery,
+}
