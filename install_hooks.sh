c94e64e453bf5ba2bc34a947b5a3af2066c1781d
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2022-11-08 16:47:32 +0200

    chore: pre-push git hook installation script (#187)

---
+++ install_hooks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if cat <<EOF > $SCRIPT_DIR/.git/hooks/pre-push
+#!/bin/sh
+#
+# This pre-push hook runs a cargo-clippy and cargo +nightly fmt before pushing code to remote.
+# This is to prevent commits being pushed which will fail CI.
+
+cargo clippy --all-targets || { echo "Error: clippy did not pass - aborting push. Please run 'cargo clippy --all-targets'." ; exit 1 ; }
+
+cargo +nightly fmt -- --check --config unstable_features=true --config imports_granularity=Crate || { echo "Error: format check failed - aborting push. Please run 'cargo +nightly fmt'." ; exit 1 ; }
+EOF
+then
+	chmod +x $SCRIPT_DIR/.git/hooks/pre-push
+else
+	echo "Failed to write pre-push hook"
+	exit 1
+fi
