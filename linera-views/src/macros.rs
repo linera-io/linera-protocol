7cbf700e56aa633ed77efabc5d3282e0448e78e5
Mathieu Dutour Sikiric <mathieu.dutour@linera.io>
2022-11-08 21:42:31 +0100

    Correction for compilation.

--- linera-views/src/macros.rs
+++ linera-views/src/macros.rs
@@ -96,7 +96,6 @@ where
 
     pub async fn write_delete(self) -> Result<(), $crate::views::ViewError> {
         use $crate::views::View;
-        use $crate::views::View;
         use $crate::common::build_batch;
 
         let context = self.context().clone();
