jvff/linera-protocol-archive

Implement `GuestFutureInterface` for WIT types
Prepare to make the futures implemented as WIT resources into futures
that can be called from the host.

linera-execution/src/wasm/common.rs
@@ -3,8 +3,15 @@
 
 //! Runtime independent code for interfacing with user applications in WebAssembly modules.
 
-use super::{async_boundary::ContextForwarder, runtime::application};
-use crate::ExecutionError;
+use super::{
+    async_boundary::{ContextForwarder, GuestFutureInterface},
+    runtime::application::{
+        self, CallApplication, CallSession, ExecuteEffect, ExecuteOperation, PollCallApplication,
+        PollCallSession, PollExecutionResult, PollQuery, QueryApplication,
+    },
+};
+use crate::{ApplicationCallResult, ExecutionError, RawExecutionResult, SessionCallResult};
+use std::task::Poll;
 
 /// Types that are specific to a WebAssembly runtime.
 pub trait Runtime: Sized {
@@ -120,4 +127,45 @@ where
 
     /// Guard type to clean up any host state after the call to the WASM application finishes.
     pub(crate) _storage_guard: R::StorageGuard,
+}
+
+/// Implement [`GuestFutureInterface`] for a `future` type implemented by a guest WASM module.
+///
+/// The future is then polled by calling the guest `poll_func`. The return type of that function is
+/// a `poll_type` that must be convertible into the `output` type wrapped in a
+/// `Poll<Result<_, _>>`.
+macro_rules! impl_guest_future_interface {
+    ( $( $future:ident : $poll_func:ident -> $poll_type:ident => $output:ty ),* $(,)* ) => {
+        $(
+            impl<'storage, R> GuestFutureInterface<R> for $future
+            where
+                R: Runtime,
+                ExecutionError: From<R::Error>,
+            {
+                type Output = $output;
+
+                fn poll(
+                    &self,
+                    application: &R::Application,
+                    store: &mut R::Store,
+                ) -> Poll<Result<Self::Output, ExecutionError>> {
+                    match application.$poll_func(store, self)? {
+                        $poll_type::Ready(Ok(result)) => Poll::Ready(Ok(result.into())),
+                        $poll_type::Ready(Err(message)) => {
+                            Poll::Ready(Err(ExecutionError::UserApplication(message)))
+                        }
+                        $poll_type::Pending => Poll::Pending,
+                    }
+                }
+            }
+        )*
+    }
+}
+
+impl_guest_future_interface! {
+    ExecuteOperation: execute_operation_poll -> PollExecutionResult => RawExecutionResult<Vec<u8>>,
+    ExecuteEffect: execute_effect_poll -> PollExecutionResult => RawExecutionResult<Vec<u8>>,
+    CallApplication: call_application_poll -> PollCallApplication => ApplicationCallResult,
+    CallSession: call_session_poll -> PollCallSession => SessionCallResult,
+    QueryApplication: query_application_poll -> PollQuery => Vec<u8>,
 }

linera-io/linera-protocol

linera-execution/src/wasm/common.rs
@@ -3,8 +3,15 @@
 
 //! Runtime independent code for interfacing with user applications in WebAssembly modules.
 
-use super::{async_boundary::ContextForwarder, runtime::application};
-use crate::WasmExecutionError;
+use super::{
+    async_boundary::{ContextForwarder, GuestFutureInterface},
+    runtime::application::{
+        self, CallApplication, CallSession, ExecuteEffect, ExecuteOperation, PollCallApplication,
+        PollCallSession, PollExecutionResult, PollQuery, QueryApplication,
+    },
+};
+use crate::{ApplicationCallResult, RawExecutionResult, SessionCallResult, WasmExecutionError};
+use std::task::Poll;
 
 /// Types that are specific to a WebAssembly runtime.
 pub trait Runtime: Sized {
@@ -120,4 +127,45 @@ where
 
     /// Guard type to clean up any host state after the call to the WASM application finishes.
     pub(crate) _storage_guard: R::StorageGuard,
+}
+
+/// Implement [`GuestFutureInterface`] for a `future` type implemented by a guest WASM module.
+///
+/// The future is then polled by calling the guest `poll_func`. The return type of that function is
+/// a `poll_type` that must be convertible into the `output` type wrapped in a
+/// `Poll<Result<_, _>>`.
+macro_rules! impl_guest_future_interface {
+    ( $( $future:ident : $poll_func:ident -> $poll_type:ident => $output:ty ),* $(,)* ) => {
+        $(
+            impl<'storage, R> GuestFutureInterface<R> for $future
+            where
+                R: Runtime,
+                WasmExecutionError: From<R::Error>,
+            {
+                type Output = $output;
+
+                fn poll(
+                    &self,
+                    application: &R::Application,
+                    store: &mut R::Store,
+                ) -> Poll<Result<Self::Output, WasmExecutionError>> {
+                    match application.$poll_func(store, self)? {
+                        $poll_type::Ready(Ok(result)) => Poll::Ready(Ok(result.into())),
+                        $poll_type::Ready(Err(message)) => {
+                            Poll::Ready(Err(WasmExecutionError::UserApplication(message)))
+                        }
+                        $poll_type::Pending => Poll::Pending,
+                    }
+                }
+            }
+        )*
+    }
+}
+
+impl_guest_future_interface! {
+    ExecuteOperation: execute_operation_poll -> PollExecutionResult => RawExecutionResult<Vec<u8>>,
+    ExecuteEffect: execute_effect_poll -> PollExecutionResult => RawExecutionResult<Vec<u8>>,
+    CallApplication: call_application_poll -> PollCallApplication => ApplicationCallResult,
+    CallSession: call_session_poll -> PollCallSession => SessionCallResult,
+    QueryApplication: query_application_poll -> PollQuery => Vec<u8>,
 }