0a8e75ddb3faa839054d22e9f258edb9cc828b65
Mathieu Dutour Sikiric <mathieu.dutour@linera.io>
2022-11-17 16:51:13 +0100

    A better scheme for the iteration over the keys.

--- linera-views/src/memory.rs
+++ linera-views/src/memory.rs
@@ -9,6 +9,8 @@ use serde::de::DeserializeOwned;
 use std::{collections::BTreeMap, fmt::Debug, sync::Arc};
 use thiserror::Error;
 use tokio::sync::{OwnedMutexGuard, RwLock};
+use std::ops::Bound::{Excluded, Included, Unbounded};
+use std::ops::Bound;
 
 /// The data is serialized in memory just like for rocksdb / dynamodb
 /// The analogue of the database is the BTreeMap
@@ -18,6 +20,25 @@ pub type MemoryContainer = Arc<RwLock<OwnedMutexGuard<MemoryStoreMap>>>;
 
 /// A context that stores all values in memory.
 pub type MemoryContext<E> = ContextFromDb<E, MemoryContainer>;
+
+/// When wanting to find the entries in a BTreeMap with a specific prefix,
+/// one option is to iterate over all keys. Another is to select an interval
+/// that represents exactly the keys having that prefix. Which fortunately
+/// is possible with the way the comparison operators for vectors is built.
+fn get_interval(key_prefix: Vec<u8>) -> (Bound<Vec<u8>>, Bound<Vec<u8>>) {
+    let len = key_prefix.len();
+    for i in (0..len).rev() {
+        let val = key_prefix[i];
+        if val < 255 {
+            let mut upper_bound = key_prefix.clone();
+            upper_bound[i] += 1;
+            return (Included(key_prefix), Excluded(upper_bound));
+        }
+    }
+    return (Included(key_prefix.clone()), Unbounded)
+}
+
+
 
 impl<E> MemoryContext<E> {
     pub fn new(guard: OwnedMutexGuard<MemoryStoreMap>, extra: E) -> Self {
@@ -54,10 +75,8 @@ impl KeyValueOperations for MemoryContainer {
     ) -> Result<Vec<Vec<u8>>, MemoryContextError> {
         let map = self.read().await;
         let mut vals = Vec::new();
-        for key in map.keys() {
-            if key.starts_with(key_prefix) {
-                vals.push(key.clone())
-            }
+        for (key, _value) in map.range(get_interval(key_prefix.to_vec())) {
+            vals.push(key.clone())
         }
         Ok(vals)
     }
@@ -69,10 +88,8 @@ impl KeyValueOperations for MemoryContainer {
         let map = self.read().await;
         let mut keys = Vec::new();
         let len = key_prefix.len();
-        for key in map.keys() {
-            if key.starts_with(key_prefix) {
-                keys.push(bcs::from_bytes(&key[len..])?);
-            }
+        for (key, _value) in map.range(get_interval(key_prefix.to_vec())) {
+            keys.push(bcs::from_bytes(&key[len..])?);
         }
         Ok(keys)
     }
