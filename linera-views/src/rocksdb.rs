0653396ea90b8dd431f0b51f2d770025a1bbe966
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-11-26 18:37:33 +0100

    Applying reformatting.

--- linera-views/src/rocksdb.rs
+++ linera-views/src/rocksdb.rs
@@ -2,8 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::common::{
-    get_upper_bound, Batch, ContextFromDb, KeyValueOperations,
-    SimpleKeyIterator, WriteOperation,
+    get_upper_bound, Batch, ContextFromDb, KeyValueOperations, SimpleKeyIterator, WriteOperation,
 };
 use async_trait::async_trait;
 use std::sync::Arc;
--- linera-views/tests/upper_bound.rs
+++ linera-views/tests/upper_bound.rs
@@ -5,10 +5,9 @@ use linera_views::common::get_upper_bound;
 
 #[test]
 fn test_upper_bound() {
-    assert_eq!(get_upper_bound(&vec![255]), None);
-    assert_eq!(get_upper_bound(&vec![255, 255, 255, 255]), None);
-    assert_eq!(get_upper_bound(&vec![0,2]), Some(vec![0,3]));
-    assert_eq!(get_upper_bound(&vec![0,255]), Some(vec![1]));
-    assert_eq!(get_upper_bound(&vec![255,0]), Some(vec![255,1]));
+    assert_eq!(get_upper_bound(&[255]), None);
+    assert_eq!(get_upper_bound(&[255, 255, 255, 255]), None);
+    assert_eq!(get_upper_bound(&[0, 2]), Some(vec![0, 3]));
+    assert_eq!(get_upper_bound(&[0, 255]), Some(vec![1]));
+    assert_eq!(get_upper_bound(&[255, 0]), Some(vec![255, 1]));
 }
-
