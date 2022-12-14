81f19a5eb147a9a4209e3e7858870873477b57ce
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2022-12-14 19:55:23 +0000

    Stub fungible token application contract
    Implement `Contract` stubbing all methods.

---
+++ linera-sdk/examples/fungible/contract.rs
@@ -0,0 +1,69 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+#![cfg(target_arch = "wasm32")]
+
+mod state;
+
+use self::state::{ApplicationState, FungibleToken};
+use async_trait::async_trait;
+use linera_sdk::{
+    ApplicationCallResult, CalleeContext, Contract, EffectContext, ExecutionResult,
+    OperationContext, Session, SessionCallResult, SessionId,
+};
+use thiserror::Error;
+
+#[async_trait]
+impl Contract for FungibleToken {
+    type Error = Error;
+
+    async fn initialize(
+        &mut self,
+        _context: &OperationContext,
+        _argument: &[u8],
+    ) -> Result<ExecutionResult, Self::Error> {
+        todo!();
+    }
+
+    async fn execute_operation(
+        &mut self,
+        _context: &OperationContext,
+        _operation: &[u8],
+    ) -> Result<ExecutionResult, Self::Error> {
+        todo!();
+    }
+
+    async fn execute_effect(
+        &mut self,
+        _context: &EffectContext,
+        _effect: &[u8],
+    ) -> Result<ExecutionResult, Self::Error> {
+        todo!();
+    }
+
+    async fn call_application(
+        &mut self,
+        _context: &CalleeContext,
+        _argument: &[u8],
+        _forwarded_sessions: Vec<SessionId>,
+    ) -> Result<ApplicationCallResult, Self::Error> {
+        todo!();
+    }
+
+    async fn call_session(
+        &mut self,
+        _context: &CalleeContext,
+        _session: Session,
+        _argument: &[u8],
+        _forwarded_sessions: Vec<SessionId>,
+    ) -> Result<SessionCallResult, Self::Error> {
+        todo!();
+    }
+}
+
+/// An error that can occur during the contract execution.
+#[derive(Debug, Error)]
+pub enum Error {}
+
+#[path = "../boilerplate/contract/mod.rs"]
+mod boilerplate;
