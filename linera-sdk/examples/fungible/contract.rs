4599c84d8dba2aaa943986dd7ee3885fedb7db2e
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2022-12-14 21:13:18 +0000

    Implement transfers started by applications
    Requires no signatures. Either credits an account on the same chain, or
    creates a credit effect for an account on a different chain.

--- linera-sdk/examples/fungible/contract.rs
+++ linera-sdk/examples/fungible/contract.rs
@@ -8,8 +8,8 @@ mod state;
 use self::state::{AccountOwner, ApplicationState, FungibleToken};
 use async_trait::async_trait;
 use linera_sdk::{
-    ApplicationCallResult, CalleeContext, Contract, EffectContext, ExecutionResult, FromBcsBytes,
-    OperationContext, Session, SessionCallResult, SessionId,
+    ApplicationCallResult, CalleeContext, ChainId, Contract, EffectContext, ExecutionResult,
+    FromBcsBytes, OperationContext, Session, SessionCallResult, SessionId,
 };
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
@@ -49,11 +49,22 @@ impl Contract for FungibleToken {
 
     async fn call_application(
         &mut self,
-        _context: &CalleeContext,
-        _argument: &[u8],
+        context: &CalleeContext,
+        argument: &[u8],
         _forwarded_sessions: Vec<SessionId>,
     ) -> Result<ApplicationCallResult, Self::Error> {
-        todo!();
+        let transfer = Transfer::from_bcs_bytes(argument).map_err(Error::InvalidArgument)?;
+        let caller = context
+            .authenticated_caller_id
+            .ok_or(Error::MissingSourceApplication)?;
+        let source = AccountOwner::Application(caller);
+
+        self.debit(source, transfer.amount)?;
+
+        Ok(ApplicationCallResult {
+            execution_result: self.finish_transfer(transfer),
+            ..ApplicationCallResult::default()
+        })
     }
 
     async fn call_session(
@@ -67,11 +78,41 @@ impl Contract for FungibleToken {
     }
 }
 
+impl FungibleToken {
+    /// Credit an account or forward it to another micro-chain.
+    fn finish_transfer(&mut self, transfer: Transfer) -> ExecutionResult {
+        if transfer.destination_chain == Self::current_chain_id() {
+            self.credit(transfer.destination_account, transfer.amount);
+            ExecutionResult::default()
+        } else {
+            ExecutionResult::default()
+                .with_effect(transfer.destination_chain, &Credit::from(transfer))
+        }
+    }
+}
+
+/// A transfer request from an application.
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Transfer {
+    destination_account: AccountOwner,
+    destination_chain: ChainId,
+    amount: u128,
+}
+
 /// The credit effect.
 #[derive(Deserialize, Serialize)]
 pub struct Credit {
     destination: AccountOwner,
     amount: u128,
+}
+
+impl From<Transfer> for Credit {
+    fn from(transfer: Transfer) -> Self {
+        Credit {
+            destination: transfer.destination_account,
+            amount: transfer.amount,
+        }
+    }
 }
 
 /// An error that can occur during the contract execution.
@@ -84,6 +125,18 @@ pub enum Error {
     /// Invalid serialized [`Credit`].
     #[error("Effect is not a valid serialized credit operation")]
     InvalidEffect(#[source] bcs::Error),
+
+    /// Cross-application call without a source application ID.
+    #[error("Applications must identify themselves to perform transfers")]
+    MissingSourceApplication,
+
+    /// Invalid serialized [`Transfer`].
+    #[error("Cross-application call argument is not a valid serialized transfer")]
+    InvalidArgument(#[source] bcs::Error),
+
+    /// Insufficient balance in source account.
+    #[error("Source account does not have sufficient balance for transfer")]
+    InsufficientBalance(#[from] state::InsufficientBalanceError),
 }
 
 #[path = "../boilerplate/contract/mod.rs"]
