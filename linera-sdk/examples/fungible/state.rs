11a09231e210f915feee62b5ad61581e6f63448b
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2022-12-16 18:45:52 +0000

    Add helper methods to manage nonces
    Prepare to protect against replay attacks on the same chain, by allowing
    to mark a used nonce and to check if a nonce is valid.

--- linera-sdk/examples/fungible/state.rs
+++ linera-sdk/examples/fungible/state.rs
@@ -26,6 +26,15 @@ pub enum AccountOwner {
 #[derive(Debug, Default, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct Nonce(u64);
 
+impl Nonce {
+    /// Obtain the next number to use, if there is one.
+    fn next(&self) -> Option<Self> {
+        let next_number = self.0.checked_add(1)?;
+
+        Some(Nonce(next_number))
+    }
+}
+
 #[allow(dead_code)]
 impl FungibleToken {
     /// Initialize the application state with some accounts with initial balances.
@@ -59,6 +68,29 @@ impl FungibleToken {
         *balance -= amount;
 
         Ok(())
+    }
+
+    /// Obtain the minimum allowed [`Nonce`] for an `account`.
+    ///
+    /// The minimum allowed nonce is the value of the previously used nonce plus one, or zero if
+    /// this is the first transaction for the `account` on the current chain.
+    ///
+    /// If the increment to obtain the next nonce overflows, `None` is returned.
+    pub(crate) fn minimum_nonce(&self, account: &AccountOwner) -> Option<Nonce> {
+        self.nonces
+            .get(account)
+            .map(Nonce::next)
+            .unwrap_or(Some(Nonce::default()))
+    }
+
+    /// Mark the provided [`Nonce`] as used for the `account`.
+    pub(crate) fn mark_nonce_as_used(&mut self, account: AccountOwner, nonce: Nonce) {
+        let minimum_nonce = self.minimum_nonce(&account);
+
+        assert!(minimum_nonce.is_some());
+        assert!(nonce >= minimum_nonce.unwrap());
+
+        self.nonces.insert(account, nonce);
     }
 }
 
