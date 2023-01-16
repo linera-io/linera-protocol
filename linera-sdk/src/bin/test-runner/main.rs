3c1d6604532304ada3ffba1b0463d12f589ec5d6
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-01-16 16:57:26 +0000

    Document the test runner binary
    Provide a brief description of its purpose and how to use it.

--- linera-sdk/src/bin/test-runner/main.rs
+++ linera-sdk/src/bin/test-runner/main.rs
@@ -1,9 +1,32 @@
+// Copyright (c) Zefchain Labs, Inc.
 // Copyright (c) Alex Kladov
 // SPDX-License-Identifier: Apache-2.0
+
+//! A binary for running unit tests for Linera applications implemented in WebAssembly.
+//!
+//! The tests must be compiled for the `wasm32-unknown-unknown` target, and each unit test should
+//! use the [webassembly_test attribute][webassembly_test].
+//!
+//! # Using With Cargo
+//!
+//! It's possible to make Cargo use this test-runner for the `wasm32-unknown-unknown` target
+//! automatically by adding the following line to one of [Cargo's configuration
+//! files](https://doc.rust-lang.org/cargo/reference/config.html#hierarchical-structure) or setting
+//! the `CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUNNER` environment variable.
+//!
+//! ```ignore
+//! [target.wasm32-unknown-unknown]
+//! runner = "/path/to/test-runner"
+//! ```
+//!
+//! [webassembly_test]: https://docs.rs/webassembly-test/latest/webassembly_test/
 
 use anyhow::{bail, Result};
 use wasmtime::*;
 
+/// Load a test WASM module and run all test functions annotated by [webassembly_test].
+///
+/// Prints out a summary of executed tests and their results.
 fn main() -> Result<()> {
     let file = match std::env::args().nth(1) {
         Some(it) => it,
