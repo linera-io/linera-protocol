jvff/linera-protocol-archive

Export the `writable_system` API from `linera-sdk`
Copy the relevant parts of the code from the
`examples/boilerplate/contract` module.

linera-sdk/src/contract/conversions_from_wit.rs
@@ -1,0 +1,94 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Conversions from types generated by [`wit_bindgen_rust`] to types declared in [`linera_sdk`].
+
+use super::writable_system::{self as system, PollCallResult, PollLoad};
+use crate::{
+    ApplicationId, BlockHeight, BytecodeId, ChainId, EffectId, HashValue, SessionId, SystemBalance,
+};
+use std::task::Poll;
+
+impl From<system::EffectId> for EffectId {
+    fn from(effect_id: system::EffectId) -> Self {
+        EffectId {
+            chain_id: ChainId(effect_id.chain_id.into()),
+            height: BlockHeight(effect_id.height),
+            index: effect_id.index,
+        }
+    }
+}
+
+impl From<system::ApplicationId> for ApplicationId {
+    fn from(application_id: system::ApplicationId) -> Self {
+        ApplicationId {
+            bytecode: BytecodeId(application_id.bytecode_id.into()),
+            creation: application_id.creation.into(),
+        }
+    }
+}
+
+impl From<system::HashValue> for HashValue {
+    fn from(hash_value: system::HashValue) -> Self {
+        HashValue::from([
+            hash_value.part1,
+            hash_value.part2,
+            hash_value.part3,
+            hash_value.part4,
+            hash_value.part5,
+            hash_value.part6,
+            hash_value.part7,
+            hash_value.part8,
+        ])
+    }
+}
+
+impl From<system::SystemBalance> for SystemBalance {
+    fn from(balance: system::SystemBalance) -> Self {
+        let value = ((balance.upper_half as u128) << 64) | (balance.lower_half as u128);
+        SystemBalance(value)
+    }
+}
+
+impl From<PollLoad> for Poll<Result<Vec<u8>, String>> {
+    fn from(poll_get: PollLoad) -> Poll<Result<Vec<u8>, String>> {
+        match poll_get {
+            PollLoad::Ready(bytes) => Poll::Ready(bytes),
+            PollLoad::Pending => Poll::Pending,
+        }
+    }
+}
+
+impl From<PollCallResult> for Poll<Result<(Vec<u8>, Vec<SessionId>), String>> {
+    fn from(poll_call_result: PollCallResult) -> Poll<Result<(Vec<u8>, Vec<SessionId>), String>> {
+        match poll_call_result {
+            PollCallResult::Ready(Ok(result)) => Poll::Ready(Ok(result.into())),
+            PollCallResult::Ready(Err(message)) => Poll::Ready(Err(message)),
+            PollCallResult::Pending => Poll::Pending,
+        }
+    }
+}
+
+impl From<system::CallResult> for (Vec<u8>, Vec<SessionId>) {
+    fn from(call_result: system::CallResult) -> (Vec<u8>, Vec<SessionId>) {
+        let value = call_result.value;
+
+        let sessions = call_result
+            .sessions
+            .into_iter()
+            .map(SessionId::from)
+            .collect();
+
+        (value, sessions)
+    }
+}
+
+impl From<system::SessionId> for SessionId {
+    fn from(session_id: system::SessionId) -> SessionId {
+        SessionId {
+            application_id: session_id.application_id.into(),
+            kind: session_id.kind,
+            index: session_id.index,
+        }
+    }
+}

linera-sdk/src/contract/conversions_to_wit.rs
@@ -1,0 +1,53 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Conversions from types declared in [`linera_sdk`] to types generated by [`wit_bindgen_rust`].
+
+use super::writable_system as system;
+use crate::{ApplicationId, EffectId, HashValue, SessionId};
+
+impl From<HashValue> for system::HashValue {
+    fn from(hash_value: HashValue) -> Self {
+        let parts = <[u64; 8]>::from(hash_value);
+
+        system::HashValue {
+            part1: parts[0],
+            part2: parts[1],
+            part3: parts[2],
+            part4: parts[3],
+            part5: parts[4],
+            part6: parts[5],
+            part7: parts[6],
+            part8: parts[7],
+        }
+    }
+}
+
+impl From<ApplicationId> for system::ApplicationId {
+    fn from(application_id: ApplicationId) -> system::ApplicationId {
+        system::ApplicationId {
+            bytecode_id: application_id.bytecode.0.into(),
+            creation: application_id.creation.into(),
+        }
+    }
+}
+
+impl From<SessionId> for system::SessionId {
+    fn from(session_id: SessionId) -> Self {
+        system::SessionId {
+            application_id: session_id.application_id.into(),
+            kind: session_id.kind,
+            index: session_id.index,
+        }
+    }
+}
+
+impl From<EffectId> for system::EffectId {
+    fn from(effect_id: EffectId) -> Self {
+        system::EffectId {
+            chain_id: effect_id.chain_id.0.into(),
+            height: effect_id.height.0,
+            index: effect_id.index,
+        }
+    }
+}

linera-io/linera-protocol

linera-sdk/src/contract/conversions_from_wit.rs
@@ -1,0 +1,94 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Conversions from types generated by [`wit_bindgen_rust`] to types declared in [`linera_sdk`].
+
+use super::writable_system::{self as system, PollCallResult, PollLoad};
+use crate::{
+    ApplicationId, BlockHeight, BytecodeId, ChainId, CryptoHash, EffectId, SessionId, SystemBalance,
+};
+use std::task::Poll;
+
+impl From<system::EffectId> for EffectId {
+    fn from(effect_id: system::EffectId) -> Self {
+        EffectId {
+            chain_id: ChainId(effect_id.chain_id.into()),
+            height: BlockHeight(effect_id.height),
+            index: effect_id.index,
+        }
+    }
+}
+
+impl From<system::ApplicationId> for ApplicationId {
+    fn from(application_id: system::ApplicationId) -> Self {
+        ApplicationId {
+            bytecode: BytecodeId(application_id.bytecode_id.into()),
+            creation: application_id.creation.into(),
+        }
+    }
+}
+
+impl From<system::CryptoHash> for CryptoHash {
+    fn from(hash_value: system::CryptoHash) -> Self {
+        CryptoHash::from([
+            hash_value.part1,
+            hash_value.part2,
+            hash_value.part3,
+            hash_value.part4,
+            hash_value.part5,
+            hash_value.part6,
+            hash_value.part7,
+            hash_value.part8,
+        ])
+    }
+}
+
+impl From<system::SystemBalance> for SystemBalance {
+    fn from(balance: system::SystemBalance) -> Self {
+        let value = ((balance.upper_half as u128) << 64) | (balance.lower_half as u128);
+        SystemBalance(value)
+    }
+}
+
+impl From<PollLoad> for Poll<Result<Vec<u8>, String>> {
+    fn from(poll_get: PollLoad) -> Poll<Result<Vec<u8>, String>> {
+        match poll_get {
+            PollLoad::Ready(bytes) => Poll::Ready(bytes),
+            PollLoad::Pending => Poll::Pending,
+        }
+    }
+}
+
+impl From<PollCallResult> for Poll<Result<(Vec<u8>, Vec<SessionId>), String>> {
+    fn from(poll_call_result: PollCallResult) -> Poll<Result<(Vec<u8>, Vec<SessionId>), String>> {
+        match poll_call_result {
+            PollCallResult::Ready(Ok(result)) => Poll::Ready(Ok(result.into())),
+            PollCallResult::Ready(Err(message)) => Poll::Ready(Err(message)),
+            PollCallResult::Pending => Poll::Pending,
+        }
+    }
+}
+
+impl From<system::CallResult> for (Vec<u8>, Vec<SessionId>) {
+    fn from(call_result: system::CallResult) -> (Vec<u8>, Vec<SessionId>) {
+        let value = call_result.value;
+
+        let sessions = call_result
+            .sessions
+            .into_iter()
+            .map(SessionId::from)
+            .collect();
+
+        (value, sessions)
+    }
+}
+
+impl From<system::SessionId> for SessionId {
+    fn from(session_id: system::SessionId) -> SessionId {
+        SessionId {
+            application_id: session_id.application_id.into(),
+            kind: session_id.kind,
+            index: session_id.index,
+        }
+    }
+}

linera-sdk/src/contract/conversions_to_wit.rs
@@ -1,0 +1,53 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Conversions from types declared in [`linera_sdk`] to types generated by [`wit_bindgen_rust`].
+
+use super::writable_system as system;
+use crate::{ApplicationId, CryptoHash, EffectId, SessionId};
+
+impl From<CryptoHash> for system::CryptoHash {
+    fn from(hash_value: CryptoHash) -> Self {
+        let parts = <[u64; 8]>::from(hash_value);
+
+        system::CryptoHash {
+            part1: parts[0],
+            part2: parts[1],
+            part3: parts[2],
+            part4: parts[3],
+            part5: parts[4],
+            part6: parts[5],
+            part7: parts[6],
+            part8: parts[7],
+        }
+    }
+}
+
+impl From<ApplicationId> for system::ApplicationId {
+    fn from(application_id: ApplicationId) -> system::ApplicationId {
+        system::ApplicationId {
+            bytecode_id: application_id.bytecode.0.into(),
+            creation: application_id.creation.into(),
+        }
+    }
+}
+
+impl From<SessionId> for system::SessionId {
+    fn from(session_id: SessionId) -> Self {
+        system::SessionId {
+            application_id: session_id.application_id.into(),
+            kind: session_id.kind,
+            index: session_id.index,
+        }
+    }
+}
+
+impl From<EffectId> for system::EffectId {
+    fn from(effect_id: EffectId) -> Self {
+        system::EffectId {
+            chain_id: effect_id.chain_id.0.into(),
+            height: effect_id.height.0,
+            index: effect_id.index,
+        }
+    }
+}