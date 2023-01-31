dbd775d85357a55ba12dda156e2b973c64c91a27
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2023-01-31 18:15:42 +0000

    Can now properly query confirmed_log and received_log in ChainStateView

--- linera-chain/src/graphql.rs
+++ linera-chain/src/graphql.rs
@@ -1,13 +1,33 @@
 use crate::{chain::ChainTipState, ChainManager, ChainStateView};
-use async_graphql::scalar;
+use async_graphql::{scalar, Error};
 use linera_base::crypto::CryptoHash;
 use linera_execution::ExecutionStateView;
-use linera_views::common::Context;
+use linera_views::{common::Context, views::ViewError};
+use serde::{Deserialize, Serialize};
 
 scalar!(ChainManager);
+scalar!(Range);
+
+#[derive(Serialize, Deserialize)]
+pub struct Range {
+    start: usize,
+    end: usize,
+}
+
+impl From<Range> for std::ops::Range<usize> {
+    fn from(range: Range) -> Self {
+        std::ops::Range {
+            start: range.start,
+            end: range.end,
+        }
+    }
+}
 
 #[async_graphql::Object]
-impl<C: Sync + Send + Context> ChainStateView<C> {
+impl<C: Sync + Send + Context> ChainStateView<C>
+where
+    ViewError: From<C::Error>,
+{
     async fn execution_state_view(&self) -> &ExecutionStateView<C> {
         &self.execution_state
     }
@@ -24,11 +44,19 @@ impl<C: Sync + Send + Context> ChainStateView<C> {
         self.manager.get()
     }
 
-    async fn confirmed_log(&self) -> usize {
-        self.confirmed_log.count()
+    async fn confirmed_log(&self, range: Option<Range>) -> Result<Vec<CryptoHash>, Error> {
+        let range = range.unwrap_or(Range {
+            start: 0,
+            end: self.confirmed_log.count(),
+        });
+        Ok(self.confirmed_log.read(range.into()).await?)
     }
 
-    async fn received_log(&self) -> usize {
-        self.received_log.count()
+    async fn received_log(&self, range: Option<Range>) -> Result<Vec<CryptoHash>, Error> {
+        let range = range.unwrap_or(Range {
+            start: 0,
+            end: self.received_log.count(),
+        });
+        Ok(self.received_log.read(range.into()).await?)
     }
 }
