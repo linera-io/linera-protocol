efd95326f7e2109e8c058ec918c5789464dada52
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-02-02 14:38:28 +0000

    Use exported contract futures from `linera-sdk`
    Simplify the code in the `boilerplate` module.

--- linera-sdk/examples/boilerplate/contract/exported_futures.rs
+++ linera-sdk/examples/boilerplate/contract/exported_futures.rs
@@ -8,92 +8,52 @@
 //! contract type that implements [`linera_sdk::Contract`].
 
 use super::super::ApplicationState;
-use linera_sdk::{
-    contract, ApplicationCallResult, Contract, ContractLogger, ExecutionResult, ExportedFuture,
-    SessionCallResult, SessionId,
-};
+use linera_sdk::contract::{self, exported_futures};
 use wit_bindgen_guest_rust::Handle;
 
-pub struct Initialize {
-    future: ExportedFuture<Result<ExecutionResult, String>>,
-}
+pub struct Initialize(exported_futures::Initialize<ApplicationState>);
 
 impl contract::Initialize for Initialize {
     fn new(context: contract::OperationContext, argument: Vec<u8>) -> Handle<Self> {
-        ContractLogger::install();
-
-        Handle::new(Initialize {
-            future: ExportedFuture::new(async move {
-                let mut application = ApplicationState::load_and_lock().await;
-                let result = application.initialize(&context.into(), &argument).await;
-                if result.is_ok() {
-                    application.store_and_unlock().await;
-                }
-                result.map_err(|error| error.to_string())
-            }),
-        })
+        Handle::new(Initialize(exported_futures::Initialize::new(
+            context, argument,
+        )))
     }
 
     fn poll(&self) -> contract::PollExecutionResult {
-        self.future.poll()
+        self.0.poll()
     }
 }
 
-pub struct ExecuteOperation {
-    future: ExportedFuture<Result<ExecutionResult, String>>,
-}
+pub struct ExecuteOperation(exported_futures::ExecuteOperation<ApplicationState>);
 
 impl contract::ExecuteOperation for ExecuteOperation {
     fn new(context: contract::OperationContext, operation: Vec<u8>) -> Handle<Self> {
-        ContractLogger::install();
-
-        Handle::new(ExecuteOperation {
-            future: ExportedFuture::new(async move {
-                let mut application = ApplicationState::load_and_lock().await;
-                let result = application
-                    .execute_operation(&context.into(), &operation)
-                    .await;
-                if result.is_ok() {
-                    application.store_and_unlock().await;
-                }
-                result.map_err(|error| error.to_string())
-            }),
-        })
+        Handle::new(ExecuteOperation(exported_futures::ExecuteOperation::new(
+            context, operation,
+        )))
     }
 
     fn poll(&self) -> contract::PollExecutionResult {
-        self.future.poll()
+        self.0.poll()
     }
 }
 
-pub struct ExecuteEffect {
-    future: ExportedFuture<Result<ExecutionResult, String>>,
-}
+pub struct ExecuteEffect(exported_futures::ExecuteEffect<ApplicationState>);
 
 impl contract::ExecuteEffect for ExecuteEffect {
     fn new(context: contract::EffectContext, effect: Vec<u8>) -> Handle<Self> {
-        ContractLogger::install();
-
-        Handle::new(ExecuteEffect {
-            future: ExportedFuture::new(async move {
-                let mut application = ApplicationState::load_and_lock().await;
-                let result = application.execute_effect(&context.into(), &effect).await;
-                if result.is_ok() {
-                    application.store_and_unlock().await;
-                }
-                result.map_err(|error| error.to_string())
-            }),
-        })
+        Handle::new(ExecuteEffect(exported_futures::ExecuteEffect::new(
+            context, effect,
+        )))
     }
 
     fn poll(&self) -> contract::PollExecutionResult {
-        self.future.poll()
+        self.0.poll()
     }
 }
 
-pub struct CallApplication {
-    future: ExportedFuture<Result<ApplicationCallResult, String>>,
-}
+pub struct CallApplication(exported_futures::CallApplication<ApplicationState>);
 
 impl contract::CallApplication for CallApplication {
     fn new(
@@ -101,36 +61,19 @@ impl contract::CallApplication for CallApplication {
         argument: Vec<u8>,
         forwarded_sessions: Vec<contract::SessionId>,
     ) -> Handle<Self> {
-        ContractLogger::install();
-
-        Handle::new(CallApplication {
-            future: ExportedFuture::new(async move {
-                let mut application = ApplicationState::load_and_lock().await;
-
-                let forwarded_sessions = forwarded_sessions
-                    .into_iter()
-                    .map(SessionId::from)
-                    .collect();
-
-                let result = application
-                    .call_application(&context.into(), &argument, forwarded_sessions)
-                    .await;
-                if result.is_ok() {
-                    application.store_and_unlock().await;
-                }
-                result.map_err(|error| error.to_string())
-            }),
-        })
+        Handle::new(CallApplication(exported_futures::CallApplication::new(
+            context,
+            argument,
+            forwarded_sessions,
+        )))
     }
 
     fn poll(&self) -> contract::PollCallApplication {
-        self.future.poll()
+        self.0.poll()
     }
 }
 
-pub struct CallSession {
-    future: ExportedFuture<Result<SessionCallResult, String>>,
-}
+pub struct CallSession(exported_futures::CallSession<ApplicationState>);
 
 impl contract::CallSession for CallSession {
     fn new(
@@ -139,34 +82,15 @@ impl contract::CallSession for CallSession {
         argument: Vec<u8>,
         forwarded_sessions: Vec<contract::SessionId>,
     ) -> Handle<Self> {
-        ContractLogger::install();
-
-        Handle::new(CallSession {
-            future: ExportedFuture::new(async move {
-                let mut application = ApplicationState::load_and_lock().await;
-
-                let forwarded_sessions = forwarded_sessions
-                    .into_iter()
-                    .map(SessionId::from)
-                    .collect();
-
-                let result = application
-                    .call_session(
-                        &context.into(),
-                        session.into(),
-                        &argument,
-                        forwarded_sessions,
-                    )
-                    .await;
-                if result.is_ok() {
-                    application.store_and_unlock().await;
-                }
-                result.map_err(|error| error.to_string())
-            }),
-        })
+        Handle::new(CallSession(exported_futures::CallSession::new(
+            context,
+            session,
+            argument,
+            forwarded_sessions,
+        )))
     }
 
     fn poll(&self) -> contract::PollCallSession {
-        self.future.poll()
+        self.0.poll()
     }
 }
