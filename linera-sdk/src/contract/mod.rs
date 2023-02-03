642e80614907af9b96527bfaa49b2dfaafcba4fb
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-02-03 14:10:29 +0000

    Create a `contract!` macro to generate boilerplate
    Generates the necessary boilerplate to export the contract application's
    endpoints.

--- linera-sdk/src/contract/mod.rs
+++ linera-sdk/src/contract/mod.rs
@@ -13,3 +13,63 @@ wit_bindgen_guest_rust::export!(
     reexported_crate_path = "wit_bindgen_guest_rust"
     "contract.wit"
 );
+
+/// Declares an implementation of the [`Contract`][`crate::Contract`] trait, exporting it from the
+/// WASM module.
+///
+/// Generates the necessary boilerplate for implementing the contract WIT interface, exporting the
+/// necessary resource types and functions so that the host can call the contract application.
+#[macro_export]
+macro_rules! contract {
+    ($application:ident) => {
+        // Export the contract interface.
+        $crate::export_contract!($application);
+
+        /// Mark the contract type to be exported.
+        impl $crate::contract::Contract for $application {
+            type CallApplication = CallApplication;
+            type CallSession = CallSession;
+            type ExecuteEffect = ExecuteEffect;
+            type ExecuteOperation = ExecuteOperation;
+            type Initialize = Initialize;
+        }
+
+        $crate::instance_exported_future! {
+            contract::Initialize<$application>(
+                context: $crate::contract::OperationContext,
+                argument: Vec<u8>,
+            ) -> PollExecutionResult
+        }
+
+        $crate::instance_exported_future! {
+            contract::ExecuteOperation<$application>(
+                context: $crate::contract::OperationContext,
+                operation: Vec<u8>,
+            ) -> PollExecutionResult
+        }
+
+        $crate::instance_exported_future! {
+            contract::ExecuteEffect<$application>(
+                context: $crate::contract::EffectContext,
+                effect: Vec<u8>,
+            ) -> PollExecutionResult
+        }
+
+        $crate::instance_exported_future! {
+            contract::CallApplication<$application>(
+                context: $crate::contract::CalleeContext,
+                argument: Vec<u8>,
+                forwarded_sessions: Vec<$crate::contract::SessionId>,
+            ) -> PollCallApplication
+        }
+
+        $crate::instance_exported_future! {
+            contract::CallSession<$application>(
+                context: $crate::contract::CalleeContext,
+                session: $crate::contract::Session,
+                argument: Vec<u8>,
+                forwarded_sessions: Vec<$crate::contract::SessionId>,
+            ) -> PollCallSession
+        }
+    };
+}
