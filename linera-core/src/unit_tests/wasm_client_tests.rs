ec311390a69a4d51fb1b88a6ae91f316004650bd
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2023-02-08 20:05:25 -0800

    Create example application "meta-counter"

--- linera-core/src/unit_tests/wasm_client_tests.rs
+++ linera-core/src/unit_tests/wasm_client_tests.rs
@@ -97,3 +97,104 @@ where
     assert!(matches!(response, Response::User(bytes) if bytes == expected_bytes));
     Ok(())
 }
+
+#[test(tokio::test)]
+async fn test_memory_run_application_with_dependency() -> Result<(), anyhow::Error> {
+    run_test_run_application_with_dependency(MakeMemoryStoreClient).await
+}
+
+#[test(tokio::test)]
+async fn test_rocksdb_run_application_with_dependency() -> Result<(), anyhow::Error> {
+    let _lock = GUARD.lock().await;
+    run_test_run_application_with_dependency(MakeRocksdbStoreClient::default()).await
+}
+
+#[test(tokio::test)]
+#[ignore]
+async fn test_dynamo_db_run_application_with_dependency() -> Result<(), anyhow::Error> {
+    run_test_run_application_with_dependency(MakeDynamoDbStoreClient::default()).await
+}
+
+async fn run_test_run_application_with_dependency<B>(store_builder: B) -> Result<(), anyhow::Error>
+where
+    B: StoreBuilder,
+    ViewError: From<<B::Store as Store>::ContextError>,
+{
+    let mut builder = TestBuilder::new(store_builder, 4, 1).await?;
+    let mut publisher = builder
+        .add_initial_chain(ChainDescription::Root(0), Balance::from(3))
+        .await?;
+    let mut creator = builder
+        .add_initial_chain(ChainDescription::Root(1), Balance::from(0))
+        .await?;
+
+    let cert = creator
+        .subscribe_to_published_bytecodes(publisher.chain_id)
+        .await
+        .unwrap();
+    publisher.receive_certificate(cert).await.unwrap();
+    publisher.process_inbox().await.unwrap();
+
+    let (bytecode_id1, cert1) = {
+        let (contract_path, service_path) =
+            linera_execution::wasm_test::get_example_bytecode_paths("counter")?;
+        publisher
+            .publish_bytecode(
+                Bytecode::load_from_file(contract_path).await?,
+                Bytecode::load_from_file(service_path).await?,
+            )
+            .await
+            .unwrap()
+    };
+    let (bytecode_id2, cert2) = {
+        let (contract_path, service_path) =
+            linera_execution::wasm_test::get_example_bytecode_paths("meta_counter")?;
+        publisher
+            .publish_bytecode(
+                Bytecode::load_from_file(contract_path).await?,
+                Bytecode::load_from_file(service_path).await?,
+            )
+            .await
+            .unwrap()
+    };
+    // Receive our own certs to broadcast the bytecode locations.
+    publisher.receive_certificate(cert1).await.unwrap();
+    publisher.receive_certificate(cert2).await.unwrap();
+    publisher.process_inbox().await.unwrap();
+
+    creator.synchronize_and_recompute_balance().await.unwrap();
+    creator.process_inbox().await.unwrap();
+
+    let initial_value = 10_u128;
+    let (application_id1, _) = creator
+        .create_application(bytecode_id1, bcs::to_bytes(&initial_value)?, vec![])
+        .await
+        .unwrap();
+    let (application_id2, _) = creator
+        .create_application(
+            bytecode_id2,
+            bcs::to_bytes(&application_id1)?,
+            vec![application_id1],
+        )
+        .await
+        .unwrap();
+
+    let increment = 5_u128;
+    let user_operation = bcs::to_bytes(&increment)?;
+    creator
+        .execute_operation(
+            ApplicationId::User(application_id2),
+            Operation::User(user_operation),
+        )
+        .await
+        .unwrap();
+    let response = creator
+        .query_application(ApplicationId::User(application_id2), &Query::User(vec![]))
+        .await
+        .unwrap();
+
+    let expected = 15_u128;
+    let expected_bytes = bcs::to_bytes(&expected)?;
+    assert!(matches!(response, Response::User(bytes) if bytes == expected_bytes));
+    Ok(())
+}
---
+++ linera-examples/meta-counter/src/contract.rs
@@ -0,0 +1,88 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+#![no_main]
+
+mod state;
+
+use self::state::MetaCounter;
+use async_trait::async_trait;
+use linera_sdk::{
+    contract::system_api, ApplicationCallResult, CalleeContext, Contract, EffectContext,
+    ExecutionResult, OperationContext, Session, SessionCallResult, SessionId, SimpleStateStorage,
+};
+use thiserror::Error;
+
+linera_sdk::contract!(MetaCounter);
+
+#[async_trait]
+impl Contract for MetaCounter {
+    type Error = Error;
+    type Storage = SimpleStateStorage<Self>;
+
+    async fn initialize(
+        &mut self,
+        _context: &OperationContext,
+        argument: &[u8],
+    ) -> Result<ExecutionResult, Self::Error> {
+        self.counter_id = Some(bcs::from_bytes(argument).map_err(|_| Error::Initialization)?);
+        Ok(ExecutionResult::default())
+    }
+
+    async fn execute_operation(
+        &mut self,
+        _context: &OperationContext,
+        operation: &[u8],
+    ) -> Result<ExecutionResult, Self::Error> {
+        system_api::call_application(true, self.counter_id.unwrap(), operation, vec![])
+            .await
+            .map_err(|_| Error::InternalCall)?;
+        Ok(ExecutionResult::default())
+    }
+
+    async fn execute_effect(
+        &mut self,
+        _context: &EffectContext,
+        _effect: &[u8],
+    ) -> Result<ExecutionResult, Self::Error> {
+        Err(Error::EffectsNotSupported)
+    }
+
+    async fn call_application(
+        &mut self,
+        _context: &CalleeContext,
+        _argument: &[u8],
+        _forwarded_sessions: Vec<SessionId>,
+    ) -> Result<ApplicationCallResult, Self::Error> {
+        Err(Error::CallsNotSupported)
+    }
+
+    async fn call_session(
+        &mut self,
+        _context: &CalleeContext,
+        _session: Session,
+        _argument: &[u8],
+        _forwarded_sessions: Vec<SessionId>,
+    ) -> Result<SessionCallResult, Self::Error> {
+        Err(Error::SessionsNotSupported)
+    }
+}
+
+/// An error that can occur during the contract execution.
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("MetaCounter application doesn't support any cross-chain effects")]
+    EffectsNotSupported,
+
+    #[error("MetaCounter application doesn't support any cross-application calls")]
+    CallsNotSupported,
+
+    #[error("MetaCounter application doesn't support any cross-application sessions")]
+    SessionsNotSupported,
+
+    #[error("Error with the internal call to Counter")]
+    InternalCall,
+
+    #[error("Error with the initialization")]
+    Initialization,
+}
