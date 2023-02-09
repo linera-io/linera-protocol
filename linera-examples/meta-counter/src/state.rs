e4ff7f679ec63e74d46125bbb28b261ea1829799
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2023-02-08 20:05:25 -0800

    Create example application "meta-counter"

---
+++ linera-examples/meta-counter/src/state.rs
@@ -0,0 +1,11 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use linera_sdk::ApplicationId;
+use serde::{Deserialize, Serialize};
+
+/// The application state.
+#[derive(Clone, Copy, Debug, Default, Deserialize, Serialize)]
+pub struct MetaCounter {
+    pub counter_id: Option<ApplicationId>,
+}
