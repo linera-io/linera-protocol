d88ae4fc2bf20211b70967d7c94b69e1d38441cd
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2023-02-16 00:39:47 +0000

    Added 'run_local.sh' to run a local network
    Local testing sometimes requires running the entire network locally. Historically this has
    been done by running `cargo test` and modifying the README script to stop at a specific
    point in time. This shell script is a convenience tool to avoid doing this.

---
+++ scripts/run_local.sh
@@ -0,0 +1,44 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR/..
+
+# For debug builds:
+cargo build && cd target/debug
+# For release builds:
+# cargo build --release && cd target/release
+
+# Clean up data files
+rm -rf *.json *.txt *.db
+
+# Make sure to clean up child processes on exit.
+trap 'kill $(jobs -p)' EXIT
+
+# Create configuration files for 4 validators with 4 shards each.
+# * Private server states are stored in `server*.json`.
+# * `committee.json` is the public description of the FastPay committee.
+./server generate --validators \
+   server_1.json:grpc:127.0.0.1:9100:grpc:127.0.0.1:10100:127.0.0.1:9101:127.0.0.1:9102:127.0.0.1:9103:127.0.0.1:9104 \
+   server_2.json:grpc:127.0.0.1:9200:grpc:127.0.0.1:10200:127.0.0.1:9201:127.0.0.1:9202:127.0.0.1:9203:127.0.0.1:9204 \
+   server_3.json:grpc:127.0.0.1:9300:grpc:127.0.0.1:10300:127.0.0.1:9301:127.0.0.1:9302:127.0.0.1:9303:127.0.0.1:9304 \
+   server_4.json:grpc:127.0.0.1:9400:grpc:127.0.0.1:10400:127.0.0.1:9401:127.0.0.1:9402:127.0.0.1:9403:127.0.0.1:9404 \
+--committee committee.json
+
+# Create configuration files for 10 user chains.
+# * Private chain states are stored in one local wallet `wallet.json`.
+# * `genesis.json` will contain the initial balances of chains as well as the initial committee.
+./client --wallet wallet.json --genesis genesis.json create_genesis_config 10 --initial-funding 10 --committee committee.json
+
+# Start servers and create initial chains in DB
+for I in 1 2 3 4
+do
+    ./proxy server_"$I".json &
+
+    for J in $(seq 0 3)
+    do
+        ./server run --storage rocksdb:server_"$I"_"$J".db --server server_"$I".json --shard "$J" --genesis genesis.json &
+    done
+done
+
+while :; do
+    sleep 5
+done
