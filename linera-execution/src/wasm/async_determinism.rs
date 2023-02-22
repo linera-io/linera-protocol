021206cc86e0ae3066efb66f8ebf646ef0f152ef
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-02-22 21:32:17 +0000

    Create the `QueuedHostFutureFactory` type
    Responsible for sending futures to the `HostFutureQueue`, wrapped with
    the correct code to delay the completion of the returned `HostFuture`.

--- linera-execution/src/wasm/async_determinism.rs
+++ linera-execution/src/wasm/async_determinism.rs
@@ -28,3 +28,78 @@
 //! The [`HostFutureQueue`] implements [`Stream`], and produces a marker `()` item every time the
 //! next future in the queue is ready for completion. Therefore, the [`GuestFuture`] is responsible
 //! for always polling the [`HostFutureQueue`] before polling the guest WASM module.
+
+use super::async_boundary::HostFuture;
+use futures::{
+    channel::{mpsc, oneshot},
+    future::{BoxFuture, FutureExt},
+    sink::SinkExt,
+};
+use std::future::Future;
+
+/// A factory of [`HostFuture`]s that enforces determinism of the host futures they represent.
+///
+/// This type is created by [`HostFutureQueue::new`], and is associated to the [`HostFutureQueue`]
+/// returned with it. Both must be used together in the correct manner as described by the module
+/// documentation. The summary is that the [`HostFutureQueue`] should be polled until it returns an
+/// item before the guest WASM module is polled, so that the created [`HostFuture`]s are only polled
+/// deterministically.
+#[derive(Clone)]
+pub struct QueuedHostFutureFactory<'futures> {
+    sender: mpsc::Sender<BoxFuture<'futures, Box<dyn FnOnce() + Send>>>,
+}
+
+impl<'futures> QueuedHostFutureFactory<'futures> {
+    /// Enqueues a `future` in the associated [`HostFutureQueue`].
+    ///
+    /// Returns a [`HostFuture`] that can be passed to the guest WASM module, and that will only be
+    /// ready when the inner `future` is ready and all previous futures added to the queue are
+    /// ready.
+    ///
+    /// The `future` itself is only executed when the associated [`HostFutureQueue`] is polled.
+    /// When the `future` is complete, the result is paired inside a closure with a
+    /// [`oneshot::Sender`] that's connected to the [`oneshot::Receiver`] inside the returned
+    /// [`HostFuture`]. The [`HostFutureQueue`] runs the closure when it's time to complete the
+    /// [`HostFuture`].
+    ///
+    /// # Panics
+    ///
+    /// The returned [`HostFuture`] may panic if it is polled after the [`HostFutureQueue`] is
+    /// dropped.
+    pub fn enqueue<Output>(
+        &mut self,
+        future: impl Future<Output = Output> + Send + 'futures,
+    ) -> HostFuture<'futures, Output>
+    where
+        Output: Send + 'static,
+    {
+        let (result_sender, result_receiver) = oneshot::channel();
+        let mut future_sender = self.sender.clone();
+
+        HostFuture::new(async move {
+            future_sender
+                .send(
+                    future
+                        .map(move |result| -> Box<dyn FnOnce() + Send> {
+                            Box::new(move || {
+                                // An error when sending the result indicates that the user
+                                // application dropped the `HostFuture`, and no longer needs the
+                                // result
+                                let _ = result_sender.send(result);
+                            })
+                        })
+                        .boxed(),
+                )
+                .await
+                .expect(
+                    "`HostFutureQueue` should not be dropped while `QueuedHostFutureFactory` is \
+                    still enqueuing futures",
+                );
+
+            result_receiver.await.expect(
+                "`HostFutureQueue` should not be dropped while the `HostFuture`s of the queued \
+                futures are still alive",
+            )
+        })
+    }
+}
