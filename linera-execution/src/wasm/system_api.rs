4e3b73cfdaf0e7cdba9ba38b80b154cfc3e47942
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-03-08 00:29:56 +0000

    Add a `block_on` helper method
    Common code to run asynchronous code in a blocking manner.

--- linera-execution/src/wasm/system_api.rs
+++ linera-execution/src/wasm/system_api.rs
@@ -303,6 +303,24 @@ macro_rules! impl_writable_system {
                 Ok(())
             }
         }
+
+        impl$(<$param>)? $contract_system_api {
+            /// Calls a `future` in a blocking manner.
+            fn block_on<F>(future: F) -> F::Output
+            where
+                F: std::future::Future + Send,
+                F::Output: Send,
+            {
+                let runtime = tokio::runtime::Handle::current();
+
+                std::thread::scope(|scope| {
+                    scope
+                        .spawn(|| runtime.block_on(future))
+                        .join()
+                        .expect("Panic when running a future in a blocking manner")
+                })
+            }
+        }
     };
 }
 
