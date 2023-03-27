686e6c7690b8b42006fd5249767da056dd677c87
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-03-27 17:32:08 +0000

    Refactor to add a `benchmark` feature
    The benchmark CLI command requires the `linera-base/test` feature
    enabled, so a new `benchmark` feature was added to the `linera-service`
    crate.

--- linera-service/tests/integration_tests.rs
+++ linera-service/tests/integration_tests.rs
@@ -163,6 +163,8 @@ impl Client {
             .kill_on_drop(true)
             .env("RUST_LOG", "ERROR")
             .arg("run")
+            .arg("--features")
+            .arg("benchmark")
             .arg("--manifest-path")
             .arg(env::current_dir().unwrap().join("Cargo.toml"))
             .args(["--bin", "client"])
@@ -411,7 +413,9 @@ impl TestRunner {
             .kill_on_drop(true)
             .arg("run")
             .arg("--manifest-path")
-            .arg(env::current_dir().unwrap().join("Cargo.toml"));
+            .arg(env::current_dir().unwrap().join("Cargo.toml"))
+            .arg("--features")
+            .arg("benchmark");
         command
     }
 
