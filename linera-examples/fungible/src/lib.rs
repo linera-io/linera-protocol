9df8ca7ae2e9dec634a8cdafa4840eb21ed6afb0
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-03-28 18:31:13 +0000

    Create a `InitialStateBuilder` helper type
    Makes it simpler to create the serialized initialization argument of the
    application using a builder pattern.

--- linera-examples/fungible/src/lib.rs
+++ linera-examples/fungible/src/lib.rs
@@ -3,6 +3,7 @@
 
 use linera_sdk::base::{Amount, ApplicationId, ChainId, Owner};
 use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
 
 /// An operation.
 #[derive(Deserialize, Serialize)]
@@ -71,4 +72,24 @@ pub struct Account {
 pub enum Destination {
     Account(Account),
     NewSession,
+}
+
+/// A builder type for constructing the initial state of the application.
+#[derive(Debug, Default)]
+pub struct InitialStateBuilder {
+    account_balances: BTreeMap<AccountOwner, Amount>,
+}
+
+impl InitialStateBuilder {
+    /// Adds an account to the initial state of the application.
+    pub fn with_account(mut self, account: AccountOwner, balance: impl Into<Amount>) -> Self {
+        self.account_balances.insert(account, balance.into());
+        self
+    }
+
+    /// Returns the serialized initial state of the application, ready to used as the
+    /// initialization argument.
+    pub fn build(&self) -> Vec<u8> {
+        bcs::to_bytes(&self.account_balances).expect("Failed to serialize initial state")
+    }
 }
