21f44b036d7f338f1746cfa20cd05a5f5aff5deb
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-03-28 12:19:36 +0200

    Reformatting.

--- linera-storage/src/memory.rs
+++ linera-storage/src/memory.rs
@@ -3,10 +3,10 @@
 
 use crate::{chain_guards::ChainGuards, DbStore, DbStoreClient};
 use async_lock::{Mutex, RwLock};
+use futures::FutureExt;
 use linera_execution::WasmRuntime;
 use linera_views::memory::MemoryClient;
 use std::{collections::BTreeMap, sync::Arc};
-use futures::FutureExt;
 
 type MemoryStore = DbStore<MemoryClient>;
 
@@ -23,7 +23,10 @@ impl MemoryStoreClient {
 impl MemoryStore {
     pub fn new(wasm_runtime: Option<WasmRuntime>) -> Self {
         let state = Arc::new(Mutex::new(BTreeMap::new()));
-        let guard = state.lock_arc().now_or_never().expect("We should be able to acquire what we just created");
+        let guard = state
+            .lock_arc()
+            .now_or_never()
+            .expect("We should be able to acquire what we just created");
         let client = Arc::new(RwLock::new(guard));
         Self {
             client,
