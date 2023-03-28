9cc779ac7a0c12f132c20240257218c62257e29e
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-03-28 02:07:21 +0000

    Document the `linera_sdk::extensions` module
    And the `FromBcsBytes` it contains.

--- linera-sdk/src/extensions.rs
+++ linera-sdk/src/extensions.rs
@@ -1,9 +1,13 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+//! Extension traits with some common functionality.
+
 use serde::de::DeserializeOwned;
 
+/// Extension trait to deserialize a type from a vector of bytes using [`bcs`].
 pub trait FromBcsBytes: Sized {
+    /// Deserializes itself from a vector of bytes using [`bcs`].
     fn from_bcs_bytes(bytes: &[u8]) -> Result<Self, bcs::Error>;
 }
 
