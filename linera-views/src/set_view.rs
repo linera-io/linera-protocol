2ca1246e2185164ff7f5801ac8c7993c56d25739
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-03-29 20:31:52 +0200

    Update linera-views/src/set_view.rs
    Co-authored-by: Andreas Fackler <afck@users.noreply.github.com>

--- linera-views/src/set_view.rs
+++ linera-views/src/set_view.rs
@@ -546,7 +546,7 @@ where
         Ok(indices)
     }
 
-    /// Execute a function on each index. Indices are visited in a stable, yet unspecified
+    /// Executes a function on each index. Indices are visited in a stable, yet unspecified
     /// order determined by the custom serialization. If the function does return false, then the iteration exits.
     pub async fn for_each_index_while<F>(&self, mut f: F) -> Result<(), ViewError>
     where
