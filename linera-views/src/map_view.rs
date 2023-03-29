5450cf879934ba089ea62e62d7d683aa202bb4e7
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-03-29 20:36:46 +0200

    Update linera-views/src/map_view.rs
    Co-authored-by: Andreas Fackler <afck@users.noreply.github.com>

--- linera-views/src/map_view.rs
+++ linera-views/src/map_view.rs
@@ -747,7 +747,7 @@ where
         Ok(indices)
     }
 
-    /// Execute a function on each index. Indices are visited in a stable, yet unspecified
+    /// Executes a function on each index. Indices are visited in a stable, yet unspecified
     /// order determined by the custom serialization.
     pub async fn for_each_index_while<F>(&self, mut f: F) -> Result<(), ViewError>
     where
