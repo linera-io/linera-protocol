d65803408a03009418c85ab743f7f1d774ce9053
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-03-31 13:54:13 +0000

    Add initial crate-level documentation
    Describe Linera applications and how to write them.

--- linera-sdk/src/lib.rs
+++ linera-sdk/src/lib.rs
@@ -1,6 +1,36 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+//! An SDK for developing Linera applications using Rust.
+//!
+//! A Linera application consists of two WebAssembly binaries: a contract and a service. In both
+//! binaries, there should be a shared application state. The state is a type that represents what
+//! the application would like to persist in storage across blocks, and must implement the
+//! [`Contract`] trait in the contract binary and the [`Service`] trait in the service binary.
+//!
+//! The application can select between two storage backends to use. Selecting the storage backend
+//! is done by specifying both the [`Contract::Storage`] and the [`Service::Storage`] associated
+//! types.
+//!
+//! The [`SimpleStateStorage`] backend stores the application's state type by serializing it into
+//! binary blob. This allows the entire contents of the state to be persisted and made available to
+//! the application when it is executed.
+//!
+//! The [`ViewStateStorage`] backend stores the application's state using the [`linera-views`], a
+//! framework that allows loading selected parts of the state. This is useful if the application's
+//! state is large and doesn't need to be loaded in its entirety for every execution.
+//!
+//! The contract binary should use the [`contract!`] macro to export the application's contract
+//! endpoints implemented via the [`Contract`] trait implementation.
+//!
+//! The service binary should use the [`service!`] macro to export the application's service
+//! endpoints implemented via the [`Service`] trait implementation.
+//!
+//! # Examples
+//!
+//! The [`linera-examples`](https://github.com/linera-io/linera-protocol/tree/main/linera-examples)
+//! directory contains some example applications.
+
 pub mod base;
 pub mod contract;
 mod exported_future;
