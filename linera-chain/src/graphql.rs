1d34bccadeb56f3c8989fe8231e968d623b56fa9
Andreas Fackler <andreas.fackler@linera.io>
2023-04-18 15:40:18 +0200

    Add description strings to scalars.

--- linera-chain/src/graphql.rs
+++ linera-chain/src/graphql.rs
@@ -5,13 +5,27 @@ use crate::{
     data_types::{Certificate, ChannelFullName, Event, Medium, Origin, Target},
     ChainManager,
 };
-use async_graphql::scalar;
-use linera_base::bcs_scalar;
+use linera_base::{bcs_scalar, doc_scalar};
 
-bcs_scalar!(Certificate);
-scalar!(ChainManager);
-scalar!(ChannelFullName);
-scalar!(Event);
-scalar!(Medium);
-scalar!(Origin);
-scalar!(Target);
+bcs_scalar!(Certificate, "A certified statement from the committee");
+doc_scalar!(ChainManager, "How to produce new blocks");
+doc_scalar!(
+    ChannelFullName,
+    "A channel name together with its application id"
+);
+doc_scalar!(
+    Event,
+    "An effect together with non replayable information to ensure uniqueness in a particular inbox"
+);
+doc_scalar!(
+    Medium,
+    "The origin of a message coming from a particular chain. Used to identify each inbox."
+);
+doc_scalar!(
+    Origin,
+    "The origin of a message, relative to a particular application. Used to identify each inbox."
+);
+doc_scalar!(
+    Target,
+    "The target of a message, relative to a particular application. Used to identify each outbox."
+);
