66af6dc377974bdf54ac4df6eedfa4b85061a196
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2023-04-20 19:49:54 -0700

    use singlepass for wasmer services as well

--- linera-execution/src/wasm/wasmer.rs
+++ linera-execution/src/wasm/wasmer.rs
@@ -143,7 +143,8 @@ impl WasmApplication {
         &self,
         runtime: &'runtime dyn ServiceRuntime,
     ) -> Result<WasmRuntimeContext<'static, Service<'runtime>>, WasmExecutionError> {
-        let mut store = Store::default();
+        let compiler_config = Singlepass::default();
+        let mut store = Store::new(EngineBuilder::new(compiler_config));
         let module = Module::new(&store, &self.service_bytecode)
             .map_err(wit_bindgen_host_wasmer_rust::anyhow::Error::from)?;
         let mut imports = imports! {};
