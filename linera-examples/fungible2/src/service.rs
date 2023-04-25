fad2ec44b69a67918d6b34a81d9d73c4a54ca147
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2023-04-25 14:16:49 +0100

    fungible2 now works with graphql

--- linera-examples/fungible2/src/service.rs
+++ linera-examples/fungible2/src/service.rs
@@ -6,9 +6,12 @@
 mod state;
 
 use self::state::FungibleToken;
+use async_graphql::{EmptySubscription, Object, Schema};
 use async_trait::async_trait;
+use fungible::{Account, AccountOwner, Operation};
 use linera_sdk::{
-    service::system_api::ReadOnlyViewStorageContext, QueryContext, Service, ViewStateStorage,
+    base::Amount, service::system_api::ReadOnlyViewStorageContext, QueryContext, Service,
+    ViewStateStorage,
 };
 use std::sync::Arc;
 use thiserror::Error;
@@ -25,17 +28,40 @@ impl Service for FungibleToken<ReadOnlyViewStorageContext> {
         _context: &QueryContext,
         argument: &[u8],
     ) -> Result<Vec<u8>, Self::Error> {
-        let account = bcs::from_bytes(argument)?;
-        let bytes = bcs::to_bytes(&self.balance(&account).await)
-            .expect("Serialization of amounts should not fail");
-        Ok(bytes)
+        let graphql_request: async_graphql::Request =
+            serde_json::from_slice(argument).map_err(|_| Error::InvalidQuery)?;
+        let schema = Schema::build(self.clone(), MutationRoot {}, EmptySubscription).finish();
+        let res = schema.execute(graphql_request).await;
+        Ok(serde_json::to_vec(&res).unwrap())
+    }
+}
+
+struct MutationRoot;
+
+#[Object]
+impl MutationRoot {
+    #[allow(unused)]
+    async fn transfer(
+        &self,
+        owner: AccountOwner,
+        amount: Amount,
+        target_account: Account,
+    ) -> Vec<u8> {
+        bcs::to_bytes(&Operation::Transfer {
+            owner,
+            amount,
+            target_account,
+        })
+        .unwrap()
     }
 }
 
 /// An error that can occur during the contract execution.
-#[derive(Debug, Error)]
+#[derive(Debug, Error, Eq, PartialEq)]
 pub enum Error {
-    /// Invalid account query.
-    #[error("Invalid account specified in query parameter")]
-    InvalidAccount(#[from] bcs::Error),
+    /// Invalid query argument; could not deserialize GraphQL request.
+    #[error(
+        "Invalid query argument; Counter application only supports JSON encoded GraphQL queries"
+    )]
+    InvalidQuery,
 }
