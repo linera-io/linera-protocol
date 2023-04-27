1c42c734d45df0fe7bb040a67b6f1fe79dbf5093
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2023-04-27 00:31:45 -0500

    share wit files using symlinks

--- linera-execution/src/wasm/wasmtime.rs
+++ linera-execution/src/wasm/wasmtime.rs
@@ -6,17 +6,17 @@
 // Export the system interface used by a user contract.
 wit_bindgen_host_wasmtime_rust::export!({
     custom_error: true,
-    paths: ["../linera-sdk/writable_system.wit"],
+    paths: ["writable_system.wit"],
 });
 
 // Export the system interface used by a user service.
-wit_bindgen_host_wasmtime_rust::export!("../linera-sdk/queryable_system.wit");
+wit_bindgen_host_wasmtime_rust::export!("queryable_system.wit");
 
 // Import the interface implemented by a user contract.
-wit_bindgen_host_wasmtime_rust::import!("../linera-sdk/contract.wit");
+wit_bindgen_host_wasmtime_rust::import!("contract.wit");
 
 // Import the interface implemented by a user service.
-wit_bindgen_host_wasmtime_rust::import!("../linera-sdk/service.wit");
+wit_bindgen_host_wasmtime_rust::import!("service.wit");
 
 #[path = "conversions_from_wit.rs"]
 mod conversions_from_wit;
