6a6c89b4d83d66fa32782299bb9c6df492142fd8
Christos Hadjiaslanis <chrishadjiaslanis@gmail.com>
2023-05-09 20:06:24 +0300

    linera-service tests use JSON for initialization arguments

--- examples/fungible/src/lib.rs
+++ examples/fungible/src/lib.rs
@@ -3,8 +3,8 @@
 
 use async_graphql::{scalar, InputObject};
 use linera_sdk::base::{Amount, ApplicationId, ChainId, Owner};
-use serde::{Deserialize, Serialize};
-use std::collections::BTreeMap;
+use serde::{de::Error, Deserialize, Deserializer, Serialize, Serializer};
+use std::{collections::BTreeMap, str::FromStr};
 
 /// An operation.
 #[derive(Deserialize, Serialize)]
@@ -73,12 +73,80 @@ pub enum SessionCall {
 scalar!(AccountOwner);
 
 /// An account owner.
-#[derive(Clone, Copy, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub enum AccountOwner {
     /// An account owned by a user.
     User(Owner),
     /// An account for an application.
     Application(ApplicationId),
+}
+
+impl Serialize for AccountOwner {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            AccountOwner::User(owner) => {
+                let key = format!("User:{}", owner);
+                serializer.serialize_str(&key)
+            }
+            AccountOwner::Application(app_id) => {
+                let key = format!("Application:{}", app_id);
+                serializer.serialize_str(&key)
+            }
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for AccountOwner {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct AccountOwnerVisitor;
+
+        impl<'de> serde::de::Visitor<'de> for AccountOwnerVisitor {
+            type Value = AccountOwner;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("a string representing an AccountOwner")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                let parts: Vec<&str> = value.splitn(2, ':').collect();
+                if parts.len() != 2 {
+                    return Err(Error::custom("string does not contain colon"));
+                }
+
+                match parts[0] {
+                    "User" => {
+                        let owner = Owner::from_str(parts[1]).map_err(|_| {
+                            Error::custom(format!(
+                                "failed to parse Owner from string: {}",
+                                parts[1]
+                            ))
+                        })?;
+                        Ok(AccountOwner::User(owner))
+                    }
+                    "Application" => {
+                        let app_id = ApplicationId::from_str(parts[1]).map_err(|_| {
+                            Error::custom(format!(
+                                "failed to parse ApplicationId from string: {}",
+                                parts[1]
+                            ))
+                        })?;
+                        Ok(AccountOwner::Application(app_id))
+                    }
+                    _ => Err(Error::unknown_variant(parts[0], &["User", "Application"])),
+                }
+            }
+        }
+        deserializer.deserialize_str(AccountOwnerVisitor)
+    }
 }
 
 impl<T> From<T> for AccountOwner
