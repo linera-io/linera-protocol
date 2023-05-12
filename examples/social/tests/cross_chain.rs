cdf68f4e6a5829716f5f3b31c363ab42037012ce
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-05-12 22:06:19 +0200

    Some correction.

--- examples/social/tests/cross_chain.rs
+++ examples/social/tests/cross_chain.rs
@@ -1,18 +1,17 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-//! Integration tests for the Fungible Token application.
+//! Integration tests for the social network application.
 
 #![cfg(not(target_arch = "wasm32"))]
 
-//use async_graphql::InputType;
 use linera_sdk::test::TestValidator;
 use social::Operation;
 
 /// Test posting messages across microchains.
 ///
-/// Creates the application on chain2 indirectrly from the subscription, then
-/// send a message to chain2 and see it receiced on chain1.
+/// Creates the application on chain2 indirectly from the subscription, then
+/// send a message to chain2 and see it received on chain1.
 #[tokio::test]
 async fn cross_chain_transfer() {
     let (validator, bytecode_id) = TestValidator::with_current_bytecode().await;
@@ -26,7 +25,6 @@ async fn cross_chain_transfer() {
     let chain2 = validator.new_chain().await;
 
     // Subscribe chain1 to chain2
-
     chain1
         .add_block(|block| {
             block.with_operation(application_id, Operation::RequestSubscribe(chain2.id()));
@@ -34,11 +32,9 @@ async fn cross_chain_transfer() {
         .await;
 
     // Make chain2 handle that fact.
-
     chain2.handle_received_effects().await;
 
     // Post on chain2
-
     chain2
         .add_block(|block| {
             block.with_operation(
@@ -49,18 +45,15 @@ async fn cross_chain_transfer() {
         .await;
 
     // Now make chain1 handle that fact.
-
     chain1.handle_received_effects().await;
 
     // Querying the own posts
-
     let query_string = "query { ownPosts(start: 0, end:1) { timestamp, text } }";
     let value: serde_json::Value = chain2.query(application_id, query_string).await;
     let value = value["data"]["ownPosts"][0]["text"].clone();
     assert_eq!(value, "Linera is the new Mastodon".to_string());
 
     // Now handling the received messages
-
     let query_string = "query { receivedPostsKeys { timestamp, author, index } }";
     let value: serde_json::Value = chain1.query(application_id, query_string).await;
     let author = value["data"]["receivedPostsKeys"][0]["author"].clone();
