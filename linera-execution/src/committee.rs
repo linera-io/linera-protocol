43652671ec7c87429394f345477014ac0f32c7a6
peanut <firedpeanut@foxmail.com>
2023-05-23 16:27:45 +0800

    optimize doc

--- linera-execution/src/committee.rs
+++ linera-execution/src/committee.rs
@@ -99,10 +99,10 @@ impl Epoch {
 impl Committee {
     pub fn new(validators: BTreeMap<ValidatorName, ValidatorState>) -> Self {
         let total_votes = validators.values().fold(0, |sum, state| sum + state.votes);
-        // Let N = 3f + 1 + k (0 <= k <= 2).
-        // * k / 3 = 0, (2 - k) /3 = 0 (0 <= 2 - k <= 2)
-        // * 2 N / 3 + 1 = 2f + 1 + (2k + 2 + k - k)/3 = 2f + 1 + k = N - f
-        // * (N + 2) / 3 = f + 1 + k/3 = f + 1
+        // Let N = 3f + 1 + k such that 0 <= k <= 2. (Notably ⌊k / 3⌋ = 0 and ⌊(2 - k) / 3⌋ = 0.)
+        // The following thresholds verify:
+        // * ⌊2 N / 3⌋ + 1 = ⌊(6f + 2 + 2k) / 3⌋ + 1 = 2f + 1 + k + ⌊(2 - k) / 3⌋ = N - f
+        // * ⌊(N + 2) / 3⌋= ⌊(3f + 3 + k) / 3⌋ = f + 1 + ⌊k / 3⌋ = f + 1
         let quorum_threshold = 2 * total_votes / 3 + 1;
         let validity_threshold = (total_votes + 2) / 3;
 
