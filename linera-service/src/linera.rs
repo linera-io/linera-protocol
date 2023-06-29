a02eaa1c19bd0b99159a36324d943be83ae48cee
Max <maxime.levillain@functori.com>
2023-06-29 15:45:23 +0200

    chains rpc

--- linera-service/src/linera.rs
+++ linera-service/src/linera.rs
@@ -30,7 +30,7 @@ use linera_rpc::node_provider::{NodeOptions, NodeProvider};
 use linera_service::{
     chain_listener::ChainListenerConfig,
     config::{CommitteeConfig, Export, GenesisConfig, Import, UserChain, WalletState},
-    node_service::NodeService,
+    node_service::{ChainInfo, NodeService},
     project::{self, Project},
     storage::{Runnable, StorageConfig},
 };
@@ -1301,7 +1301,20 @@ where
             } => {
                 let mut chain_client = context.make_chain_client(storage, chain_id);
                 chain_client.synchronize_from_validators().await?;
-                let service = NodeService::new(chain_client, config, port);
+                let default_chain = chain_client.chain_id();
+                let chains = context
+                    .wallet_state
+                    .chain_ids()
+                    .iter()
+                    .map(|id| {
+                        let id = *id;
+                        ChainInfo {
+                            id,
+                            default: id == default_chain,
+                        }
+                    })
+                    .collect();
+                let service = NodeService::new(chain_client, config, port, chains);
                 service
                     .run(context, |context, client| {
                         Box::pin(async {
