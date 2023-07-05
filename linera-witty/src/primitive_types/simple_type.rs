580661a9ab8f607d87224db70553e7aaf1ae4289
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-05 18:18:01 +0000

    Map `SimpleType`s to `FlatType`s
    Ensure all `SimpleType`s can be flattened into a `FlatType`.

--- linera-witty/src/primitive_types/simple_type.rs
+++ linera-witty/src/primitive_types/simple_type.rs
@@ -9,6 +9,8 @@
 //! [Component Model]:
 //! https://github.com/WebAssembly/component-model/blob/main/design/mvp/Explainer.md#type-definitions
 
+use super::FlatType;
+
 /// Marker trait to prevent [`SimpleType`] to be implemented for other types.
 pub trait Sealed {}
 
@@ -18,27 +20,32 @@ pub trait SimpleType: Default + Sealed + Sized {
     ///
     /// [canonical ABI]: https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#alignment
     const ALIGNMENT: u32;
+
+    /// The underlying WebAssembly type used when flattening this type.
+    type Flat: FlatType;
 }
 
 macro_rules! simple_type {
-    ($impl_type:ident, $alignment:expr) => {
+    ($impl_type:ident -> $flat:ty, $alignment:expr) => {
         impl Sealed for $impl_type {}
 
         impl SimpleType for $impl_type {
             const ALIGNMENT: u32 = $alignment;
+
+            type Flat = $flat;
         }
     };
 }
 
-simple_type!(bool, 1);
-simple_type!(i8, 1);
-simple_type!(i16, 2);
-simple_type!(i32, 4);
-simple_type!(i64, 8);
-simple_type!(u8, 1);
-simple_type!(u16, 2);
-simple_type!(u32, 4);
-simple_type!(u64, 8);
-simple_type!(f32, 4);
-simple_type!(f64, 8);
-simple_type!(char, 4);
+simple_type!(bool -> i32, 1);
+simple_type!(i8 -> i32, 1);
+simple_type!(i16 -> i32, 2);
+simple_type!(i32 -> i32, 4);
+simple_type!(i64 -> i64, 8);
+simple_type!(u8 -> i32, 1);
+simple_type!(u16 -> i32, 2);
+simple_type!(u32 -> i32, 4);
+simple_type!(u64 -> i64, 8);
+simple_type!(f32 -> f32, 4);
+simple_type!(f64 -> f64, 8);
+simple_type!(char -> i32, 4);
