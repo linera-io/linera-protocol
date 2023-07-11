c0ec6a05aaf04574e32e8322bf0dbc2dfdc4cfdc
Max <maxime.levillain@functori.com>
2023-07-11 11:12:44 +0200

    corrections

--- linera-explorer/src/entrypoint.rs
+++ linera-explorer/src/entrypoint.rs
@@ -8,14 +8,18 @@ use serde_json::Value;
 use serde_wasm_bindgen::from_value;
 use wasm_bindgen::prelude::*;
 
-/// Recursively forge query argument
+/// Auxiliary recursive function for forge_arg.
 fn forge_arg_type(arg: &Value, non_null: bool) -> Option<String> {
+    let deprecated = matches!(arg.get("isDeprecated"), Some(Value::Bool(true)));
+    if deprecated {
+        return None;
+    };
     match arg["kind"].as_str() {
         Some("SCALAR") => {
             if non_null {
-                Some(format!("{}", arg["_input"]))
+                Some(arg["_input"].to_string())
             } else {
-                arg.get("_input").map(|input| format!("{}", input))
+                arg.get("_input").map(Value::to_string)
             }
         }
         Some("NON_NULL") => forge_arg_type(&arg["ofType"], true),
@@ -45,6 +49,7 @@ fn forge_arg_type(arg: &Value, non_null: bool) -> Option<String> {
     }
 }
 
+/// Forges a query argument.
 fn forge_arg(arg: &Value) -> Option<String> {
     forge_arg_type(&arg["type"], false).map(|s| {
         format!(
@@ -55,6 +60,7 @@ fn forge_arg(arg: &Value) -> Option<String> {
     })
 }
 
+/// Forges query arguments.
 fn forge_args(args: Vec<Value>) -> String {
     let args: Vec<String> = args.iter().filter_map(forge_arg).collect();
     if !args.is_empty() {
@@ -64,49 +70,53 @@ fn forge_args(args: Vec<Value>) -> String {
     }
 }
 
-/// Recursively forge query response
-fn forge_response_type(t: &Value, name: Option<&str>, root: bool) -> String {
-    let is_non_null_or_list = matches!(t["kind"].as_str(), Some("NON_NULL") | Some("LIST"));
-    let incl = matches!(t.get("_include"), Some(Value::Bool(true)));
-    if !(incl || root || is_non_null_or_list) {
-        "".to_string()
-    } else {
-        match t["kind"].as_str().unwrap() {
-            "SCALAR" | "ENUM" => name
-                .unwrap_or_else(|| t["name"].as_str().unwrap())
+/// Auxiliary recursive function for forge_response.
+fn forge_response_type(output: &Value, name: Option<&str>, root: bool) -> Option<String> {
+    let is_non_null_or_list = matches!(output["kind"].as_str(), Some("NON_NULL") | Some("LIST"));
+    let incl = matches!(output.get("_include"), Some(Value::Bool(true)));
+    let deprecated = matches!(output.get("isDeprecated"), Some(Value::Bool(true)));
+    if !(incl || root || is_non_null_or_list) || deprecated {
+        return None;
+    }
+    match output["kind"].as_str().unwrap() {
+        "SCALAR" | "ENUM" => Some(
+            name.unwrap_or_else(|| output["name"].as_str().unwrap())
                 .to_string(),
-            "NON_NULL" | "LIST" => forge_response_type(&t["ofType"], name, root),
-            "OBJECT" => {
-                let fields: Vec<String> = t["fields"]
-                    .as_array()
-                    .unwrap()
-                    .iter()
-                    .map(|x: &Value| forge_response_type(&x["type"], x["name"].as_str(), false))
-                    .collect();
-                if root {
-                    format!("{{ {} }}", fields.join(" "))
-                } else {
-                    format!(
-                        "{} {{ {} }}",
-                        name.unwrap_or_else(|| t["name"].as_str().unwrap()),
-                        fields.join(" ")
-                    )
-                }
+        ),
+        "NON_NULL" | "LIST" => forge_response_type(&output["ofType"], name, root),
+        "OBJECT" => {
+            let fields: Vec<String> = output["fields"]
+                .as_array()
+                .unwrap()
+                .iter()
+                .filter_map(|elt: &Value| {
+                    forge_response_type(&elt["type"], elt["name"].as_str(), false)
+                })
+                .collect();
+            if root {
+                Some(format!("{{ {} }}", fields.join(" ")))
+            } else {
+                Some(format!(
+                    "{} {{ {} }}",
+                    name.unwrap_or_else(|| output["name"].as_str().unwrap()),
+                    fields.join(" ")
+                ))
             }
-            _ => "".to_string(),
         }
+        _ => None,
     }
 }
 
-fn forge_response(t: &Value) -> String {
-    if empty_output_aux(t) {
+/// Forges query response.
+fn forge_response(output: &Value) -> String {
+    if empty_response_aux(output) {
         "".to_string()
     } else {
-        forge_response_type(t, None, true)
+        forge_response_type(output, None, true).unwrap_or("".to_string())
     }
 }
 
-/// Query mutations or queries for applications
+/// Queries mutations or queries for applications.
 #[wasm_bindgen]
 pub async fn query(app: JsValue, query: JsValue, kind: String) {
     let link =
@@ -120,46 +130,109 @@ pub async fn query(app: JsValue, query: JsValue, kind: String) {
     let body =
         serde_json::json!({ "query": format!("{} {{{} {}}}", kind, input, response) }).to_string();
     let client = reqwest::Client::new();
-    let res = client
-        .post(&link)
-        .body(body)
-        .send()
-        .await
-        .expect("fail query send")
-        .text()
-        .await
-        .expect("cannot get text of query response");
-    match serde_json::from_str::<Value>(&res) {
-        Ok(res_json) => {
-            setf(&app, "result", &res_json["data"].serialize(&SER).unwrap());
-            setf(
-                &app,
-                "errors",
-                &res_json
-                    .get("errors")
-                    .unwrap_or(&Value::Null)
-                    .serialize(&SER)
-                    .unwrap(),
-            );
-        }
-        Err(_) => setf(&app, "errors", &JsValue::from_str(&res)),
+    match client.post(&link).body(body).send().await {
+        Err(e) => setf(&app, "errors", &JsValue::from_str(&e.to_string())),
+        Ok(response) => match response.text().await {
+            Err(e) => setf(&app, "errors", &JsValue::from_str(&e.to_string())),
+            Ok(response_txt) => match serde_json::from_str::<Value>(&response_txt) {
+                Ok(res_json) => {
+                    setf(&app, "result", &res_json["data"].serialize(&SER).unwrap());
+                    setf(
+                        &app,
+                        "errors",
+                        &res_json
+                            .get("errors")
+                            .unwrap_or(&Value::Null)
+                            .serialize(&SER)
+                            .unwrap(),
+                    );
+                }
+                Err(_) => setf(&app, "errors", &JsValue::from_str(&response_txt)),
+            },
+        },
     }
 }
 
-fn empty_output_aux(v: &Value) -> bool {
-    match v.get("kind") {
+/// Checks if response fields are not needed.
+#[wasm_bindgen]
+pub fn empty_response(output: JsValue) -> bool {
+    let output = js_to_json(&output);
+    empty_response_aux(&output)
+}
+
+/// Auxiliary recursive function for empty_response
+fn empty_response_aux(output: &Value) -> bool {
+    match output.get("kind") {
         None => true,
         Some(s) => match s.as_str() {
             Some("SCALAR") => true,
-            Some("LIST") | Some("NON_NULL") => empty_output_aux(&v["ofType"]),
+            Some("LIST") | Some("NON_NULL") => empty_response_aux(&output["ofType"]),
             _ => false,
         },
     }
 }
 
-/// Check if response fields are not needed
-#[wasm_bindgen]
-pub fn empty_output(t: JsValue) -> bool {
-    let t = js_to_json(&t);
-    empty_output_aux(&t)
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    #[test]
+    fn test_forge_response() {
+        let json = json!(
+            { "_include":true, "args":null, "kind":"OBJECT", "name":"OutputObject", "ofType":null, "type":null,
+                "fields":[
+                    { "args":[], "name":"field1", "ofType":null,
+                       "type": { "_include":true, "args":null, "isDeprecated":false, "kind":"SCALAR", "name":"String", "ofType":null, "type":null } },
+                    { "args":[], "name": "field2", "ofType":null,
+                       "type": { "_include":false, "args":null, "isDeprecated":false, "kind":"SCALAR", "name":"Int", "ofType":null, "type":null } },
+                    { "args":[], "name":"field3", "ofType":null,
+                       "type": { "_include":true, "args":null, "isDeprecated":false, "kind":"OBJECT", "name":"field3Output", "ofType":null, "type":null,
+                                   "fields":[
+                                       { "args":[], "isDeprecated":false, "name":"field31", "ofType":null,
+                                          "type": { "_include":true, "args":null, "kind":"SCALAR", "name":"Boolean", "ofType":null, "type":null } },
+
+                                   ]
+                       }
+                    }
+                ]
+            }
+        );
+        let result = super::forge_response(&json);
+        assert_eq!(&result, "{ field1 field3 { field31 } }")
+    }
+
+    #[test]
+    fn test_forge_args() {
+        let json = json!(
+            { "args":null, "name":"arg", "ofType":null,
+               "type":{ "args":null, "kind":"NON_NULL", "name":null, "type":null,
+                         "ofType":{ "args":null, "kind":"INPUT_OBJECT", "name":"InputObject", "ofType":null, "type":null, "inputFields":[
+                             { "args":null, "name":"field1", "ofType":null,
+                                "type":{ "_input":[{ "args":null, "kind":"NON_NULL", "name":null,
+                                                      "ofType":{ "_include":true, "args":null, "kind":"SCALAR", "name":"String", "ofType":null, "type":null, "_input":"foo"} ,"type":null }],
+                                           "args":null, "kind":"LIST", "name":null,
+                                           "ofType":{ "args":null, "kind":"NON_NULL", "name":null,
+                                                       "ofType":{ "_include":true, "args":null, "kind":"SCALAR", "name":"String", "ofType":null, "type":null }, "type":null}, "type":null } },
+                             { "args":null, "name":"field2", "ofType":null,
+                                "type":{ "_include":true, "args":null, "kind":"ENUM", "name":"Enum", "ofType":null, "type":null, "_input":"E2",
+                                           "enumValues":[
+                                               { "args":null, "isDeprecated":false, "name":"E1", "ofType":null, "type":null },
+                                               { "args":null, "isDeprecated":false, "name":"E2", "ofType":null,"type":null} ] } },
+                             { "args":null, "name":"field3", "ofType":null,
+                                "type":{ "args":null, "kind":"INPUT_OBJECT","name":"InputObject0","ofType":null,"type":null,
+                                          "inputFields":[
+                                              { "args":null, "name":"field31",
+                                                 "ofType":null,"type":{ "_include":true, "args":null, "kind":"SCALAR", "name":"Boolean", "ofType":null,"type":null,"_input":true} },
+                                              { "args":null, "name":"field32", "ofType":null, "type":{ "_include":true, "args":null, "kind":"SCALAR", "name":"Int", "ofType":null, "type":null, "_input":42 } }]
+                                } }
+                         ] }
+               }
+            }
+        );
+        let result = super::forge_args(vec![json]);
+        assert_eq!(
+            &result,
+            "(arg: {field1: [\"foo\"], field2: E2, field3: {field31: true, field32: 42}})"
+        )
+    }
 }
--- linera-explorer/src/input_type.rs
+++ linera-explorer/src/input_type.rs
@@ -4,19 +4,19 @@
 use super::js_utils::{getf, unproxy};
 use wasm_bindgen::prelude::*;
 
-/// add a input line for lists
+/// Adds an input line for lists.
 #[wasm_bindgen]
 pub fn append_input(component: JsValue) {
-    let t = getf(&component, "t");
-    let input: js_sys::Array = getf(&t, "_input").into();
-    let child = unproxy(&getf(&t, "ofType"));
+    let element = getf(&component, "elt");
+    let input: js_sys::Array = getf(&element, "_input").into();
+    let child = unproxy(&getf(&element, "ofType"));
     input.splice(input.length(), 0, &child);
 }
 
-/// remove an input line
+/// Removes an input line.
 #[wasm_bindgen]
 pub fn remove_input(component: JsValue, index: u32) {
-    let t = getf(&component, "t");
-    let input: js_sys::Array = getf(&t, "_input").into();
+    let element = getf(&component, "elt");
+    let input: js_sys::Array = getf(&element, "_input").into();
     input.splice(index, 1, &JsValue::undefined());
 }
