f8661bd20f29e2fddb945e30d666517d3bea52a7
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-12 21:31:47 +0000

    Allow guest exports to be loaded
    The main thing an active guest module must provide is a way to load an
    export, represented by a runtime-specific type.

--- linera-witty/src/runtime/traits.rs
+++ linera-witty/src/runtime/traits.rs
@@ -6,10 +6,16 @@
 /// A Wasm runtime.
 ///
 /// Shared types between different guest instances that use the same runtime.
-pub trait Runtime: Sized {}
+pub trait Runtime: Sized {
+    /// A handle to something exported from a guest Wasm module.
+    type Export;
+}
 
 /// An active guest Wasm module.
 pub trait Instance: Sized {
     /// The runtime this instance is running in.
     type Runtime: Runtime;
+
+    /// Loads an export from the guest module.
+    fn load_export(&mut self, name: &str) -> Option<<Self::Runtime as Runtime>::Export>;
 }
