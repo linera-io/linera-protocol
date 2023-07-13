a4f474cd490b03193855acc38750ea15d15dc7a8
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-13 15:41:28 +0000

    Implement loading of functions from instances
    Use a trait with generic parameters to represent the function signature.

--- linera-witty/src/runtime/error.rs
+++ linera-witty/src/runtime/error.rs
@@ -7,4 +7,12 @@ use thiserror::Error;
 
 /// Errors that can occur when using a Wasm runtime.
 #[derive(Debug, Error)]
-pub enum RuntimeError {}
+pub enum RuntimeError {
+    /// Attempt to load a function not exported from a module.
+    #[error("Function `{_0}` could not be found in the module's exports")]
+    FunctionNotFound(String),
+
+    /// Attempt to load a function with a name that's used for a different import in the module.
+    #[error("Export `{_0}` is not a function")]
+    NotAFunction(String),
+}
--- linera-witty/src/runtime/traits.rs
+++ linera-witty/src/runtime/traits.rs
@@ -3,6 +3,9 @@
 
 //! Abstractions over different Wasm runtime implementations.
 
+use super::RuntimeError;
+use crate::memory_layout::FlatLayout;
+
 /// A Wasm runtime.
 ///
 /// Shared types between different guest instances that use the same runtime.
@@ -18,4 +21,37 @@ pub trait Instance: Sized {
 
     /// Loads an export from the guest module.
     fn load_export(&mut self, name: &str) -> Option<<Self::Runtime as Runtime>::Export>;
+}
+
+/// How a runtime supports a function signature.
+pub trait InstanceWithFunction<Parameters, Results>: Instance
+where
+    Parameters: FlatLayout,
+    Results: FlatLayout,
+{
+    /// The runtime-specific type to represent the function.
+    type Function;
+
+    /// Converts an export into a function, if it is one.
+    fn function_from_export(
+        &mut self,
+        export: <Self::Runtime as Runtime>::Export,
+    ) -> Result<Option<Self::Function>, RuntimeError>;
+
+    /// Calls the `function` from this instance using the specified `parameters`.
+    fn call(
+        &mut self,
+        function: &Self::Function,
+        parameters: Parameters,
+    ) -> Result<Results, RuntimeError>;
+
+    /// Loads a function from the guest Wasm instance.
+    fn load_function(&mut self, name: &str) -> Result<Self::Function, RuntimeError> {
+        let export = self
+            .load_export(name)
+            .ok_or_else(|| RuntimeError::FunctionNotFound(name.to_string()))?;
+
+        self.function_from_export(export)?
+            .ok_or_else(|| RuntimeError::NotAFunction(name.to_string()))
+    }
 }
