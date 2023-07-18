f41e121133f440921f1b6ed703a1a0bd03bf6c51
Andreas Fackler <afck@users.noreply.github.com>
2023-07-18 18:38:45 +0200

    Revert "Make it unnecessary to share the wallet state in a mutex."
    This reverts commit 8933ce0bb53455a7c83f3c8995baa1d61a1e3787.

--- linera-service/src/chain_listener.rs
+++ linera-service/src/chain_listener.rs
@@ -1,19 +1,17 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use futures::{future, StreamExt};
+use futures::{lock::Mutex, StreamExt};
 use linera_core::{
     client::{ChainClient, ValidatorNodeProvider},
     tracker::NotificationTracker,
-    worker::{Notification, Reason},
+    worker::Reason,
 };
 use linera_storage::Store;
 use linera_views::views::ViewError;
-use std::{collections::HashMap, time::Duration};
+use std::{ops::DerefMut, sync::Arc, time::Duration};
 use structopt::StructOpt;
 use tracing::{info, warn};
-
-use crate::node_service::ClientMap;
 
 #[derive(Debug, Clone, StructOpt)]
 pub struct ChainListenerConfig {
@@ -30,7 +28,7 @@ pub struct ChainListenerConfig {
 /// appropriately.
 pub struct ChainListener<P, S> {
     config: ChainListenerConfig,
-    clients: ClientMap<P, S>,
+    client: Arc<Mutex<ChainClient<P, S>>>,
 }
 
 impl<P, S> ChainListener<P, S>
@@ -40,87 +38,60 @@ where
     ViewError: From<S::ContextError>,
 {
     /// Creates a new chain listener given a client chain.
-    pub(crate) fn new(config: ChainListenerConfig, clients: ClientMap<P, S>) -> Self {
-        Self { config, clients }
+    pub fn new(config: ChainListenerConfig, client: Arc<Mutex<ChainClient<P, S>>>) -> Self {
+        Self { config, client }
     }
 
     /// Runs the chain listener.
-    pub async fn run<C, F>(self, mut context: C, wallet_updater: F) -> Result<(), anyhow::Error>
+    pub async fn run<C, F>(
+        self,
+        context: Arc<Mutex<C>>,
+        wallet_updater: F,
+    ) -> Result<(), anyhow::Error>
     where
         for<'a> F:
             (Fn(&'a mut C, &'a mut ChainClient<P, S>) -> futures::future::BoxFuture<'a, ()>) + Send,
     {
-        let mut streams = HashMap::new();
-        let clients: Vec<_> = {
-            let map_guard = self.clients.map_lock().await;
-            map_guard
-                .iter()
-                .map(|(chain_id, client)| (*chain_id, client.clone()))
-                .collect()
-        };
-        for (chain_id, client) in clients {
-            let notification_stream = ChainClient::listen(client.clone()).await?;
-            let mut tracker = NotificationTracker::default();
-            streams.insert(
-                chain_id,
-                notification_stream
-                    .filter(move |notification| future::ready(tracker.insert(notification.clone())))
-                    .then(move |notification| {
-                        info!("Received new notification: {:?}", notification);
-                        let client = client.clone();
-                        Box::pin(async move {
-                            if self.config.delay_before_ms > 0 {
-                                tokio::time::sleep(Duration::from_millis(
-                                    self.config.delay_before_ms,
-                                ))
-                                .await;
+        let mut notification_stream = ChainClient::listen(self.client.clone()).await?;
+        let mut tracker = NotificationTracker::default();
+        while let Some(notification) = notification_stream.next().await {
+            if tracker.insert(notification.clone()) {
+                info!("Received new notification: {:?}", notification);
+                if self.config.delay_before_ms > 0 {
+                    tokio::time::sleep(Duration::from_millis(self.config.delay_before_ms)).await;
+                }
+                {
+                    let mut client = self.client.lock().await;
+                    match &notification.reason {
+                        Reason::NewBlock { .. } => {
+                            if let Err(e) = client.update_validators().await {
+                                warn!(
+                                    "Failed to update validators about the local chain after \
+                                    receiving notification {:?} with error: {:?}",
+                                    notification, e
+                                );
                             }
-                            {
-                                let mut client = client.lock().await;
-                                Self::handle_notification(&mut *client, notification).await;
+                        }
+                        Reason::NewIncomingMessage { .. } => {
+                            if let Err(e) = client.process_inbox().await {
+                                warn!(
+                                    "Failed to process inbox after receiving new message: {:?} \
+                                    with error: {:?}",
+                                    notification, e
+                                );
                             }
-                            if self.config.delay_after_ms > 0 {
-                                tokio::time::sleep(Duration::from_millis(
-                                    self.config.delay_after_ms,
-                                ))
-                                .await;
-                            }
-                            client.clone()
-                        })
-                    }),
-            );
-        }
+                        }
+                    }
+                    let mut context_guard = context.lock().await;
+                    wallet_updater(&mut *context_guard, client.deref_mut()).await;
+                }
 
-        while let (Some(client), _, _) =
-            future::select_all(streams.values_mut().map(StreamExt::next)).await
-        {
-            let mut client = client.lock().await;
-            wallet_updater(&mut context, &mut *client).await;
+                if self.config.delay_after_ms > 0 {
+                    tokio::time::sleep(Duration::from_millis(self.config.delay_after_ms)).await;
+                }
+            }
         }
 
         Ok(())
-    }
-
-    async fn handle_notification(client: &mut ChainClient<P, S>, notification: Notification) {
-        match &notification.reason {
-            Reason::NewBlock { .. } => {
-                if let Err(e) = client.update_validators().await {
-                    warn!(
-                        "Failed to update validators about the local chain after \
-                        receiving notification {:?} with error: {:?}",
-                        notification, e
-                    );
-                }
-            }
-            Reason::NewIncomingMessage { .. } => {
-                if let Err(e) = client.process_inbox().await {
-                    warn!(
-                        "Failed to process inbox after receiving new message: {:?} \
-                        with error: {:?}",
-                        notification, e
-                    );
-                }
-            }
-        }
     }
 }
