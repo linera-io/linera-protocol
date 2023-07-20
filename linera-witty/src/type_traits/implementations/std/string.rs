c6a8b18208d176e5f51580b780dfaeb88b6a9e39
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-20 14:20:23 +0000

    Implement `WitLoad` for `String`
    Allow loading WIT strings.

--- linera-witty/src/type_traits/implementations/std/string.rs
+++ linera-witty/src/type_traits/implementations/std/string.rs
@@ -3,11 +3,48 @@
 
 //! Implementations of the custom traits for the [`String`] type.
 
-use crate::WitType;
-use frunk::HList;
+use crate::{
+    GuestPointer, InstanceWithMemory, Layout, Memory, Runtime, RuntimeError, RuntimeMemory,
+    WitLoad, WitType,
+};
+use frunk::{hlist_pat, HList};
 
 impl WitType for String {
     const SIZE: u32 = 8;
 
     type Layout = HList![i32, i32];
+}
+
+impl WitLoad for String {
+    fn load<Instance>(
+        memory: &Memory<'_, Instance>,
+        location: GuestPointer,
+    ) -> Result<Self, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let address = GuestPointer::load(memory, location)?;
+        let length = u32::load(memory, location.after::<GuestPointer>())?;
+
+        let bytes = memory.read(address, length)?.to_vec();
+
+        Ok(String::from_utf8(bytes)?)
+    }
+
+    fn lift_from<Instance>(
+        hlist_pat![address, length]: <Self::Layout as Layout>::Flat,
+        memory: &Memory<'_, Instance>,
+    ) -> Result<Self, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let address = GuestPointer(address.try_into()?);
+        let length = length as u32;
+
+        let bytes = memory.read(address, length)?.to_vec();
+
+        Ok(String::from_utf8(bytes)?)
+    }
 }
