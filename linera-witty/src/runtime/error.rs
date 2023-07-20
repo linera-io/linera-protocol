ea545882dbda355662788c71af2c4a13ac6a44a6
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-20 00:51:09 +0000

    Implement `WitLoad` for `GuestPointer`
    Make it simple to load pointers to implement `WitLoad` for types that
    have dynamically allocated data.

--- linera-witty/src/runtime/error.rs
+++ linera-witty/src/runtime/error.rs
@@ -3,6 +3,7 @@
 
 //! Common error type for usage of different Wasm runtimes.
 
+use std::{num::TryFromIntError, string::FromUtf8Error};
 use thiserror::Error;
 
 /// Errors that can occur when using a Wasm runtime.
@@ -27,4 +28,12 @@ pub enum RuntimeError {
     /// Attempt to load a function with a name that's used for a different import in the module.
     #[error("Export `{_0}` is not a function")]
     NotAFunction(String),
+
+    /// Attempt to load a string from a sequence of bytes that doesn't contain a UTF-8 string.
+    #[error("Failed to load string from non-UTF-8 bytes")]
+    InvalidString(#[from] FromUtf8Error),
+
+    /// Attempt to create a `GuestPointer` from an invalid address representation.
+    #[error("Invalid address read")]
+    InvalidNumber(#[from] TryFromIntError),
 }
--- linera-witty/src/type_traits/implementations/custom_types.rs
+++ linera-witty/src/type_traits/implementations/custom_types.rs
@@ -3,11 +3,38 @@
 
 //! Implementations of the custom traits for types declared in this crate.
 
-use crate::{GuestPointer, WitType};
-use frunk::HList;
+use crate::{
+    GuestPointer, InstanceWithMemory, Layout, Memory, Runtime, RuntimeError, RuntimeMemory,
+    WitLoad, WitType,
+};
+use frunk::{hlist_pat, HList};
 
 impl WitType for GuestPointer {
     const SIZE: u32 = u32::SIZE;
 
     type Layout = HList![i32];
+}
+
+impl WitLoad for GuestPointer {
+    fn load<Instance>(
+        memory: &Memory<'_, Instance>,
+        location: GuestPointer,
+    ) -> Result<Self, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        Ok(GuestPointer(u32::load(memory, location)?))
+    }
+
+    fn lift_from<Instance>(
+        hlist_pat![value]: <Self::Layout as Layout>::Flat,
+        _memory: &Memory<'_, Instance>,
+    ) -> Result<Self, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        Ok(GuestPointer(value.try_into()?))
+    }
 }
