db5beebc67304489071da19bb0eb0b3650f692a2
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-25 21:20:15 +0000

    Implement `WitStore` for tuples
    Reuse the `frunk` implementations, with a small workaround to avoid an
    overflow in constraint resolution for the unit type.

--- linera-witty/src/type_traits/implementations/std/tuples.rs
+++ linera-witty/src/type_traits/implementations/std/tuples.rs
@@ -5,12 +5,41 @@
 
 use crate::{
     GuestPointer, InstanceWithMemory, Layout, Memory, Runtime, RuntimeError, RuntimeMemory,
-    WitLoad, WitType,
+    WitLoad, WitStore, WitType,
 };
-use frunk::{hlist_pat, HList};
+use frunk::{hlist, hlist_pat, HList};
 
+/// Implement [`WitType`], [`WitLoad`] and [`WitStore`].
+///
+/// When implementing [`WitStore`] for tuples, it's necessary to deconstruct the tuple and rebuild
+/// it as an heterogeneous list. However, because the methods receive `&self`, the deconstruction
+/// leads to references to the elements. Therefore an extra constraint is necessary, which is that
+/// the reference also implements [`WitStore`] and that the layout is the same as the referenced
+/// type.
+///
+/// Using this clause for the unit type leads to a compiler error, because it tries to match the
+/// layout type to itself, which the compiler doesn't handle correctly. The solution to this is to
+/// only add the clause for the implementations that have elements.
 macro_rules! impl_wit_traits {
+    () => {
+        impl_wit_traits_with_borrow_store_clause!(;);
+    };
+
     ($( $names:ident : $types:ident ),*) => {
+        impl_wit_traits_with_borrow_store_clause!(
+            $( $names: $types ),* ;
+            for<'a> HList![$( &'a $types ),*]:
+                WitType<Layout = <HList![$( $types ),*] as WitType>::Layout> + WitStore,
+        );
+    };
+}
+
+/// Implement [`WitType`], [`WitLoad`] and [`WitStore`], using the optional extra where clause.
+///
+/// See [`impl_wit_traits`] above for why the extra clause is optional and can't be used with the
+/// implementation for the unit type.
+macro_rules! impl_wit_traits_with_borrow_store_clause {
+    ($( $names:ident : $types:ident ),* ; $( $borrow_store_clause:tt )*) => {
         impl<$( $types ),*> WitType for ($( $types, )*)
         where
             $( $types: WitType, )*
@@ -52,6 +81,42 @@ macro_rules! impl_wit_traits {
                     <HList![$( $types, )*] as WitLoad>::lift_from(layout, memory)?;
 
                 Ok(($( $names, )*))
+            }
+        }
+
+        impl<$( $types ),*> WitStore for ($( $types, )*)
+        where
+            $( $types: WitStore, )*
+            HList![$( $types ),*]: WitStore,
+            $( $borrow_store_clause )*
+        {
+            fn store<Instance>(
+                &self,
+                memory: &mut Memory<'_, Instance>,
+                location: GuestPointer,
+            ) -> Result<(), RuntimeError>
+            where
+                Instance: InstanceWithMemory,
+                <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+            {
+                let ($( $names, )*) = self;
+
+                hlist![$( $names ),*].store(memory, location)?;
+
+                Ok(())
+            }
+
+            fn lower<Instance>(
+                &self,
+                memory: &mut Memory<'_, Instance>,
+            ) -> Result<<Self::Layout as Layout>::Flat, RuntimeError>
+            where
+                Instance: InstanceWithMemory,
+                <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+            {
+                let ($( $names, )*) = self;
+
+                hlist![$( $names ),*].lower(memory)
             }
         }
     };
