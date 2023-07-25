a6ca12ae7c4ff2a860939808cfabc415ec5e73be
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-25 21:34:48 +0000

    Implement `WitStore` for `Vec<_>`
    Allow storing WIT lists.

--- linera-witty/src/type_traits/implementations/std/vec.rs
+++ linera-witty/src/type_traits/implementations/std/vec.rs
@@ -5,9 +5,9 @@
 
 use crate::{
     GuestPointer, InstanceWithMemory, Layout, Memory, Runtime, RuntimeError, RuntimeMemory,
-    WitLoad, WitType,
+    WitLoad, WitStore, WitType,
 };
-use frunk::{hlist_pat, HList};
+use frunk::{hlist, hlist_pat, HList};
 
 impl<T> WitType for Vec<T>
 where
@@ -52,5 +52,52 @@ where
         (0..length)
             .map(|index| T::load(memory, address.index::<T>(index)))
             .collect()
+    }
+}
+
+impl<T> WitStore for Vec<T>
+where
+    T: WitStore,
+{
+    fn store<Instance>(
+        &self,
+        memory: &mut Memory<'_, Instance>,
+        location: GuestPointer,
+    ) -> Result<(), RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let length = u32::try_from(self.len())?;
+        let size = length * T::SIZE;
+
+        let destination = memory.allocate(size)?;
+
+        destination.store(memory, location)?;
+        length.store(memory, location.after::<GuestPointer>())?;
+
+        self.iter()
+            .zip(0..)
+            .try_for_each(|(element, index)| element.store(memory, destination.index::<T>(index)))
+    }
+
+    fn lower<Instance>(
+        &self,
+        memory: &mut Memory<'_, Instance>,
+    ) -> Result<Self::Layout, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let length = u32::try_from(self.len())?;
+        let size = length * T::SIZE;
+
+        let destination = memory.allocate(size)?;
+
+        self.iter().zip(0..).try_for_each(|(element, index)| {
+            element.store(memory, destination.index::<T>(index))
+        })?;
+
+        Ok(destination.lower(memory)? + hlist![length as i32])
     }
 }
