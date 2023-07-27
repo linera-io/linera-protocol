719d72c0e98748b4ccaf161269ae402c6fa2c722
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-27 22:19:51 +0000

    Implement splitting of joined flat layouts
    Allow extracting a variant's flat layout from the joined flat layout of
    its `variant` type.

--- linera-witty/src/memory_layout/join_flat_layouts.rs
+++ linera-witty/src/memory_layout/join_flat_layouts.rs
@@ -8,19 +8,26 @@
 //! represent the flat type of any of the variants. See [`crate::primitive_types::JoinFlatTypes`]
 //! for more information on how flat types are joined.
 
-use crate::primitive_types::JoinFlatTypes;
+use crate::primitive_types::{FlatType, JoinFlatTypes};
 use either::Either;
 use frunk::{HCons, HNil};
 
-/// Converts the current flat layout into the join `Target` flat layout, which may be longer or have
-/// some elements wider than the current elements.
+/// Allows converting between the current flat layout and the joined `Target` flat layout, which
+/// may be longer or have some elements wider than the current elements.
 pub trait JoinFlatLayouts<Target> {
     /// Converts the current flat layout into a the joined `Target` flat layout.
     fn into_joined(self) -> Target;
+
+    /// Converts from the joined `Target` flat layout into the current flat layout.
+    fn from_joined(joined: Target) -> Self;
 }
 
 impl JoinFlatLayouts<HNil> for HNil {
     fn into_joined(self) -> HNil {
+        HNil
+    }
+
+    fn from_joined(_joined: HNil) -> Self {
         HNil
     }
 }
@@ -36,11 +43,17 @@ where
             tail: HNil.into_joined(),
         }
     }
+
+    fn from_joined(_joined: HCons<TargetHead, TargetTail>) -> Self {
+        HNil
+    }
 }
 
 impl<SourceHead, SourceTail, TargetHead, TargetTail> JoinFlatLayouts<HCons<TargetHead, TargetTail>>
     for HCons<SourceHead, SourceTail>
 where
+    SourceHead: FlatType,
+    TargetHead: FlatType,
     Either<SourceHead, TargetHead>: JoinFlatTypes<Flat = TargetHead>,
     SourceTail: JoinFlatLayouts<TargetTail>,
 {
@@ -48,6 +61,13 @@ where
         HCons {
             head: Either::Left(self.head).join(),
             tail: self.tail.into_joined(),
+        }
+    }
+
+    fn from_joined(joined: HCons<TargetHead, TargetTail>) -> Self {
+        HCons {
+            head: joined.head.split_into(),
+            tail: SourceTail::from_joined(joined.tail),
         }
     }
 }
