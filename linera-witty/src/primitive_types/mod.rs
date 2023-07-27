888339ed4b117edd5b0953507c3fb7e73aeaff90
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-27 20:23:50 +0000

    Implement the `join` operator for flat types
    The first step to support `variant` types is to allow joining flat types
    from each variant's layouts.

--- linera-witty/src/primitive_types/mod.rs
+++ linera-witty/src/primitive_types/mod.rs
@@ -4,7 +4,11 @@
 //! Primitive WebAssembly and WIT types.
 
 mod flat_type;
+mod join_flat_types;
 mod maybe_flat_type;
 mod simple_type;
 
-pub use self::{flat_type::FlatType, maybe_flat_type::MaybeFlatType, simple_type::SimpleType};
+pub use self::{
+    flat_type::FlatType, join_flat_types::JoinFlatTypes, maybe_flat_type::MaybeFlatType,
+    simple_type::SimpleType,
+};
