9bdc9c2fc68d566cec4c399270c03f4b0669e9f2
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-07-27 22:22:13 +0000

    Implement `WitLoad` for `Option<T>`
    Allow loading optional values from Wasm guest modules.

--- linera-witty/src/type_traits/implementations/std/option.rs
+++ linera-witty/src/type_traits/implementations/std/option.rs
@@ -3,8 +3,11 @@
 
 //! Implementations of the custom traits for the [`Option`] type.
 
-use crate::{Layout, Merge, WitType};
-use frunk::{HCons, HNil};
+use crate::{
+    GuestPointer, InstanceWithMemory, JoinFlatLayouts, Layout, Memory, Merge, Runtime,
+    RuntimeError, RuntimeMemory, WitLoad, WitType,
+};
+use frunk::{hlist, hlist_pat, HCons, HNil};
 
 impl<T> WitType for Option<T>
 where
@@ -19,4 +22,52 @@ where
     };
 
     type Layout = HCons<i8, <HNil as Merge<T::Layout>>::Output>;
+}
+
+impl<T> WitLoad for Option<T>
+where
+    T: WitLoad,
+    HNil: Merge<T::Layout>,
+    <HNil as Merge<T::Layout>>::Output: Layout,
+    <T::Layout as Layout>::Flat:
+        JoinFlatLayouts<<<HNil as Merge<T::Layout>>::Output as Layout>::Flat>,
+{
+    fn load<Instance>(
+        memory: &Memory<'_, Instance>,
+        location: GuestPointer,
+    ) -> Result<Self, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let is_some = bool::load(memory, location)?;
+
+        match is_some {
+            true => Ok(Some(T::load(
+                memory,
+                location.after::<bool>().after_padding_for::<T>(),
+            )?)),
+            false => Ok(None),
+        }
+    }
+
+    fn lift_from<Instance>(
+        hlist_pat![is_some, ...value_layout]: <Self::Layout as Layout>::Flat,
+        memory: &Memory<'_, Instance>,
+    ) -> Result<Self, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        let is_some = bool::lift_from(hlist![is_some], memory)?;
+
+        if is_some {
+            Ok(Some(T::lift_from(
+                JoinFlatLayouts::from_joined(value_layout),
+                memory,
+            )?))
+        } else {
+            Ok(None)
+        }
+    }
 }
