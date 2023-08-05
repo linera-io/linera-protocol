cc105c733775cd7783e43681cc16e1ab416676b5
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-08-05 00:14:36 +0000

    Refactor `repeat_macro!` to support lone elements
    Don't require a name and type pair, allowing macros to be applied on
    lists of either just names or just types as well.

--- linera-witty/src/macro_utils.rs
+++ linera-witty/src/macro_utils.rs
@@ -3,32 +3,32 @@
 
 //! Common macros used by other macros.
 
-/// Repeats a `macro` call with a growing list of `name: Type` elements.
+/// Repeats a `macro` call with a growing list of `name: Type` or just `name` elements.
 ///
 /// Calls the `macro` repeatedly, starting with list of the elements until the first `|`, then
 /// adding the next element for each successive call.
 macro_rules! repeat_macro {
     (
         $macro:tt =>
-        $( $current_names:ident : $current_types:ident ),*
-        | $next_name:ident : $next_type:ident
-        $(, $queued_names:ident : $queued_types:ident )* $(,)?
+        $( $current_names:ident $( : $current_types:ident )? ),*
+        | $next_name:ident $( : $next_type:ident )?
+        $(, $queued_names:ident $( : $queued_types:ident )? )* $(,)?
     ) => {
-        $macro!($( $current_names: $current_types ),*);
+        $macro!($( $current_names $(: $current_types)? ),*);
 
         repeat_macro!(
             $macro =>
-            $( $current_names: $current_types, )*
-            $next_name: $next_type
-            | $($queued_names: $queued_types ),*
+            $( $current_names $(: $current_types)?, )*
+            $next_name $(: $next_type)?
+            | $($queued_names $(: $queued_types)? ),*
         );
     };
 
-    ( $macro:tt => $( $current_names:ident : $current_types:ident ),* |) => {
-        $macro!($( $current_names: $current_types ),*);
+    ( $macro:tt => $( $current_names:ident $( : $current_types:ident )? ),* |) => {
+        $macro!($( $current_names $(: $current_types)? ),*);
     };
 
-    ( $macro:tt => $( $current_names:ident : $current_types:ident ),* $(,)*) => {
-        repeat_macro!($macro => | $( $current_names: $current_types ),*);
+    ( $macro:tt => $( $current_names:ident $( : $current_types:ident )? ),* $(,)*) => {
+        repeat_macro!($macro => | $( $current_names $(: $current_types)? ),*);
     }
 }
