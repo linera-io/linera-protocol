c8798ec22fd3c19b4acc20f2df2c64a92e4887f3
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-08-05 14:13:56 +0000

    Create a `TokensSetItem` helper type
    A type to allow `TokenStream`s to be used as an element in a `HashSet`.

--- linera-witty-macros/src/util.rs
+++ linera-witty-macros/src/util.rs
@@ -6,7 +6,8 @@
 use heck::ToKebabCase;
 use proc_macro2::{Span, TokenStream};
 use proc_macro_error::abort;
-use quote::quote;
+use quote::{quote, ToTokens};
+use std::hash::{Hash, Hasher};
 use syn::{
     parse::{self, Parse, ParseStream},
     punctuated::Punctuated,
@@ -87,5 +88,44 @@ impl AttributeParameters {
 
                 lit_str.value()
             })
+    }
+}
+
+/// A helper type to allow comparing [`TokenStream`] instances, allowing it to be used in a
+/// [`HashSet`].
+pub struct TokensSetItem<'input> {
+    string: String,
+    tokens: &'input TokenStream,
+}
+
+impl<'input> From<&'input TokenStream> for TokensSetItem<'input> {
+    fn from(tokens: &'input TokenStream) -> Self {
+        TokensSetItem {
+            string: tokens.to_string(),
+            tokens,
+        }
+    }
+}
+
+impl PartialEq for TokensSetItem<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.string.eq(&other.string)
+    }
+}
+
+impl Eq for TokensSetItem<'_> {}
+
+impl Hash for TokensSetItem<'_> {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: Hasher,
+    {
+        self.string.hash(state)
+    }
+}
+
+impl ToTokens for TokensSetItem<'_> {
+    fn to_tokens(&self, stream: &mut TokenStream) {
+        self.tokens.to_tokens(stream)
     }
 }
