1a4d9806044c9461d01231633efe46b04ff34fb2
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-08-09 02:30:39 +0000

    Allow mock exported functions to be called
    Implement `InstanceWithFunction` for all valid parameters and results
    types.

--- linera-witty/src/runtime/test.rs
+++ linera-witty/src/runtime/test.rs
@@ -10,6 +10,7 @@ use super::{
     GuestPointer, Instance, InstanceWithFunction, InstanceWithMemory, Runtime, RuntimeError,
     RuntimeMemory,
 };
+use crate::memory_layout::FlatLayout;
 use frunk::{hlist, hlist_pat, HList};
 use std::{
     any::Any,
@@ -130,60 +131,35 @@ impl Instance for MockInstance {
     }
 }
 
-// Support for `cabi_free`.
-impl InstanceWithFunction<HList![i32], HList![]> for MockInstance {
-    type Function = ();
+impl<Parameters, Results> InstanceWithFunction<Parameters, Results> for MockInstance
+where
+    Parameters: FlatLayout + 'static,
+    Results: FlatLayout + 'static,
+{
+    type Function = String;
 
     fn function_from_export(
         &mut self,
-        _: <Self::Runtime as Runtime>::Export,
+        name: <Self::Runtime as Runtime>::Export,
     ) -> Result<Option<Self::Function>, RuntimeError> {
-        Ok(Some(()))
+        Ok(Some(name))
     }
 
     fn call(
         &mut self,
-        _function: &Self::Function,
-        _: HList![i32],
-    ) -> Result<HList![], RuntimeError> {
-        Ok(hlist![])
-    }
-}
+        function: &Self::Function,
+        parameters: Parameters,
+    ) -> Result<Results, RuntimeError> {
+        let handler = self
+            .exported_functions
+            .get(function)
+            .ok_or_else(|| RuntimeError::FunctionNotFound(function.clone()))?;
 
-// Support for `cabi_realloc`.
-impl InstanceWithFunction<HList![i32, i32, i32, i32], HList![i32]> for MockInstance {
-    type Function = ();
+        let results = handler(Box::new(parameters))?;
 
-    fn function_from_export(
-        &mut self,
-        _: <Self::Runtime as Runtime>::Export,
-    ) -> Result<Option<Self::Function>, RuntimeError> {
-        Ok(Some(()))
-    }
-
-    fn call(
-        &mut self,
-        _function: &Self::Function,
-        hlist_pat![_old_address, _old_size, alignment, new_size]: HList![i32, i32, i32, i32],
-    ) -> Result<HList![i32], RuntimeError> {
-        let allocation_size =
-            usize::try_from(new_size).expect("Failed to allocate a negative amount of memory");
-
-        let mut memory = self
-            .memory
-            .lock()
-            .expect("Panic while holding a lock to a `MockInstance`'s memory");
-
-        let address = GuestPointer(memory.len().try_into()?).aligned_at(alignment as u32);
-
-        memory.resize(address.0 as usize + allocation_size, 0);
-
-        assert!(
-            memory.len() <= i32::MAX as usize,
-            "No more memory for allocations"
-        );
-
-        Ok(hlist![address.0 as i32])
+        Ok(*results
+            .downcast()
+            .expect("Incorrect results type expected from handler of expected function"))
     }
 }
 
