87c89773767b9c6b2011613fc4d9e2a9ecddf824
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-08-12 02:25:18 +0000

    Create a type to implement `Instance` for Wasmtime
    Keep the `Store` together with the actual guest `Instance`.

--- linera-witty/src/runtime/wasmtime/mod.rs
+++ linera-witty/src/runtime/wasmtime/mod.rs
@@ -3,8 +3,8 @@
 
 //! Support for the [Wasmtime](https://wasmtime.dev) runtime.
 
-use super::traits::Runtime;
-use wasmtime::{Extern, Memory};
+use super::traits::{Instance, Runtime};
+use wasmtime::{AsContext, AsContextMut, Extern, Memory, Store, StoreContext, StoreContextMut};
 
 /// Representation of the [Wasmtime](https://wasmtime.dev) runtime.
 pub struct Wasmtime;
@@ -12,4 +12,40 @@ pub struct Wasmtime;
 impl Runtime for Wasmtime {
     type Export = Extern;
     type Memory = Memory;
+}
+
+/// Necessary data for implementing an entrypoint [`Instance`].
+pub struct EntrypointInstance {
+    instance: wasmtime::Instance,
+    store: Store<()>,
+}
+
+impl EntrypointInstance {
+    /// Creates a new [`EntrypointInstance`] with the guest module
+    /// [`Instance`][`wasmtime::Instance`] and [`Store`].
+    pub fn new(instance: wasmtime::Instance, store: Store<()>) -> Self {
+        EntrypointInstance { instance, store }
+    }
+}
+
+impl AsContext for EntrypointInstance {
+    type Data = ();
+
+    fn as_context(&self) -> StoreContext<()> {
+        self.store.as_context()
+    }
+}
+
+impl AsContextMut for EntrypointInstance {
+    fn as_context_mut(&mut self) -> StoreContextMut<()> {
+        self.store.as_context_mut()
+    }
+}
+
+impl Instance for EntrypointInstance {
+    type Runtime = Wasmtime;
+
+    fn load_export(&mut self, name: &str) -> Option<Extern> {
+        self.instance.get_export(&mut self.store, name)
+    }
 }
