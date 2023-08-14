jvff/linera-protocol-archive

Add an `Instance` implementation for reentrancy
Allow the type that exported host functions receive to be able to call
imported guest functions reentrantly.

linera-witty/src/runtime/wasmer/memory.rs
@@ -3,45 +3,52 @@
 
 //! How to access the memory of a Wasmer guest instance.
 
-use super::{super::traits::InstanceWithMemory, EntrypointInstance};
+use super::{super::traits::InstanceWithMemory, EntrypointInstance, ReentrantInstance};
 use crate::{GuestPointer, RuntimeError, RuntimeMemory};
 use std::borrow::Cow;
 use wasmer::{Extern, Memory};
 
-impl InstanceWithMemory for EntrypointInstance {
-    fn memory_from_export(&self, export: Extern) -> Result<Option<Memory>, RuntimeError> {
-        Ok(match export {
-            Extern::Memory(memory) => Some(memory),
-            _ => None,
-        })
-    }
+macro_rules! impl_memory_traits {
+    ($instance:ty) => {
+        impl InstanceWithMemory for $instance {
+            fn memory_from_export(&self, export: Extern) -> Result<Option<Memory>, RuntimeError> {
+                Ok(match export {
+                    Extern::Memory(memory) => Some(memory),
+                    _ => None,
+                })
+            }
+        }
+
+        impl RuntimeMemory<$instance> for Memory {
+            fn read<'instance>(
+                &self,
+                runtime: &'instance $instance,
+                location: GuestPointer,
+                length: u32,
+            ) -> Result<Cow<'instance, [u8]>, RuntimeError> {
+                let mut buffer = vec![0u8; length as usize];
+                let start = location.0 as u64;
+
+                self.view(runtime).read(start, &mut buffer)?;
+
+                Ok(Cow::Owned(buffer))
+            }
+
+            fn write(
+                &mut self,
+                runtime: &mut $instance,
+                location: GuestPointer,
+                bytes: &[u8],
+            ) -> Result<(), RuntimeError> {
+                let start = location.0 as u64;
+
+                self.view(&*runtime).write(start, bytes)?;
+
+                Ok(())
+            }
+        }
+    };
 }
 
-impl RuntimeMemory<EntrypointInstance> for Memory {
-    fn read<'instance>(
-        &self,
-        runtime: &'instance EntrypointInstance,
-        location: GuestPointer,
-        length: u32,
-    ) -> Result<Cow<'instance, [u8]>, RuntimeError> {
-        let mut buffer = vec![0u8; length as usize];
-        let start = location.0 as u64;
-
-        self.view(runtime).read(start, &mut buffer)?;
-
-        Ok(Cow::Owned(buffer))
-    }
-
-    fn write(
-        &mut self,
-        runtime: &mut EntrypointInstance,
-        location: GuestPointer,
-        bytes: &[u8],
-    ) -> Result<(), RuntimeError> {
-        let start = location.0 as u64;
-
-        self.view(&*runtime).write(start, bytes)?;
-
-        Ok(())
-    }
-}
+impl_memory_traits!(EntrypointInstance);
+impl_memory_traits!(ReentrantInstance<'_>);

linera-io/linera-protocol

linera-witty/src/runtime/wasmer/memory.rs
@@ -3,45 +3,52 @@
 
 //! How to access the memory of a Wasmer guest instance.
 
-use super::{super::traits::InstanceWithMemory, EntrypointInstance};
+use super::{super::traits::InstanceWithMemory, EntrypointInstance, ReentrantInstance};
 use crate::{GuestPointer, RuntimeError, RuntimeMemory};
 use std::borrow::Cow;
 use wasmer::{Extern, Memory};
 
-impl InstanceWithMemory for EntrypointInstance {
-    fn memory_from_export(&self, export: Extern) -> Result<Option<Memory>, RuntimeError> {
-        Ok(match export {
-            Extern::Memory(memory) => Some(memory),
-            _ => None,
-        })
-    }
+macro_rules! impl_memory_traits {
+    ($instance:ty) => {
+        impl InstanceWithMemory for $instance {
+            fn memory_from_export(&self, export: Extern) -> Result<Option<Memory>, RuntimeError> {
+                Ok(match export {
+                    Extern::Memory(memory) => Some(memory),
+                    _ => None,
+                })
+            }
+        }
+
+        impl RuntimeMemory<$instance> for Memory {
+            fn read<'instance>(
+                &self,
+                instance: &'instance $instance,
+                location: GuestPointer,
+                length: u32,
+            ) -> Result<Cow<'instance, [u8]>, RuntimeError> {
+                let mut buffer = vec![0u8; length as usize];
+                let start = location.0 as u64;
+
+                self.view(instance).read(start, &mut buffer)?;
+
+                Ok(Cow::Owned(buffer))
+            }
+
+            fn write(
+                &mut self,
+                instance: &mut $instance,
+                location: GuestPointer,
+                bytes: &[u8],
+            ) -> Result<(), RuntimeError> {
+                let start = location.0 as u64;
+
+                self.view(&*instance).write(start, bytes)?;
+
+                Ok(())
+            }
+        }
+    };
 }
 
-impl RuntimeMemory<EntrypointInstance> for Memory {
-    fn read<'instance>(
-        &self,
-        instance: &'instance EntrypointInstance,
-        location: GuestPointer,
-        length: u32,
-    ) -> Result<Cow<'instance, [u8]>, RuntimeError> {
-        let mut buffer = vec![0u8; length as usize];
-        let start = location.0 as u64;
-
-        self.view(instance).read(start, &mut buffer)?;
-
-        Ok(Cow::Owned(buffer))
-    }
-
-    fn write(
-        &mut self,
-        instance: &mut EntrypointInstance,
-        location: GuestPointer,
-        bytes: &[u8],
-    ) -> Result<(), RuntimeError> {
-        let start = location.0 as u64;
-
-        self.view(&*instance).write(start, bytes)?;
-
-        Ok(())
-    }
-}
+impl_memory_traits!(EntrypointInstance);
+impl_memory_traits!(ReentrantInstance<'_>);