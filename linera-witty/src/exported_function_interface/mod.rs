30c5a1741e3b2376e26dd211c83600a40c85d0d5
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-08-21 15:48:52 +0000

    Create an `ExportedFunctionInterface` helper trait
    Use the `GuestInterface` and `ResultStorage` traits in a single trait
    that can be used to determine the interface to an exported function from
    the host to the guest Wasm instance.

--- linera-witty/src/exported_function_interface/mod.rs
+++ linera-witty/src/exported_function_interface/mod.rs
@@ -10,7 +10,10 @@ mod guest_interface;
 mod result_storage;
 
 use self::{guest_interface::GuestInterface, result_storage::ResultStorage};
-use crate::RuntimeError;
+use crate::{
+    InstanceWithMemory, Layout, Memory, Runtime, RuntimeError, RuntimeMemory, WitLoad, WitStore,
+    WitType,
+};
 
 /// A type that can register some functions as exports for the target `Instance`.
 pub trait ExportTo<Instance> {
@@ -31,4 +34,97 @@ pub trait ExportFunction<Handler, Parameters, Results> {
         function_name: &str,
         handler: Handler,
     ) -> Result<(), RuntimeError>;
+}
+
+/// Representation of an exported host function's interface.
+///
+/// Implemented for a tuple pair of the host parameters type and the host results type, and allows
+/// converting to the signature the guest Wasm instance uses for that host function.
+pub trait ExportedFunctionInterface {
+    /// The type representing the host-side parameters.
+    type HostParameters: WitType;
+
+    /// The type representing the host-side results.
+    type HostResults: WitStore;
+
+    /// The representation of the guest-side function interface.
+    type GuestInterface: GuestInterface<
+        FlatHostParameters = <<Self::HostParameters as WitType>::Layout as Layout>::Flat,
+        ResultStorage = Self::ResultStorage,
+    >;
+
+    /// The type representing the guest-side parameters.
+    type GuestParameters;
+
+    /// The type representing the guest-side results.
+    type GuestResults;
+
+    /// How the results from the exported host function should be sent back to the guest.
+    type ResultStorage: ResultStorage<OutputFor<Self::HostResults> = Self::GuestResults>;
+
+    /// Converts the guest-side parameters into the host-side parameters.
+    fn lift_parameters<Instance>(
+        guest_parameters: Self::GuestParameters,
+        memory: &Memory<'_, Instance>,
+    ) -> Result<(Self::HostParameters, Self::ResultStorage), RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>;
+
+    /// Converts the host-side results into the guest-side results.
+    fn lower_results<Instance>(
+        results: Self::HostResults,
+        result_storage: Self::ResultStorage,
+        memory: &mut Memory<'_, Instance>,
+    ) -> Result<Self::GuestResults, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>;
+}
+
+impl<Parameters, Results> ExportedFunctionInterface for (Parameters, Results)
+where
+    Parameters: WitLoad,
+    Results: WitStore,
+    (
+        <Parameters::Layout as Layout>::Flat,
+        <Results::Layout as Layout>::Flat,
+    ): GuestInterface<FlatHostParameters = <Parameters::Layout as Layout>::Flat>,
+    <() as WitType>::Layout: Layout<Flat = frunk::HNil>,
+{
+    type HostParameters = Parameters;
+    type HostResults = Results;
+    type GuestInterface = (
+        <Parameters::Layout as Layout>::Flat,
+        <Results::Layout as Layout>::Flat,
+    );
+    type GuestParameters = <Self::GuestInterface as GuestInterface>::FlatGuestParameters;
+    type GuestResults =
+        <<Self::GuestInterface as GuestInterface>::ResultStorage as ResultStorage>::OutputFor<
+            Self::HostResults,
+        >;
+    type ResultStorage = <Self::GuestInterface as GuestInterface>::ResultStorage;
+
+    fn lift_parameters<Instance>(
+        guest_parameters: Self::GuestParameters,
+        memory: &Memory<'_, Instance>,
+    ) -> Result<(Self::HostParameters, Self::ResultStorage), RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        Self::GuestInterface::lift_parameters(guest_parameters, memory)
+    }
+
+    fn lower_results<Instance>(
+        results: Self::HostResults,
+        result_storage: Self::ResultStorage,
+        memory: &mut Memory<'_, Instance>,
+    ) -> Result<Self::GuestResults, RuntimeError>
+    where
+        Instance: InstanceWithMemory,
+        <Instance::Runtime as Runtime>::Memory: RuntimeMemory<Instance>,
+    {
+        result_storage.lower_result(results, memory)
+    }
 }
