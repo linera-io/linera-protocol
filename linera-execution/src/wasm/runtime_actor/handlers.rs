b4ce33441f49b098b4eaef8ac9101092bba29fed
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-09-07 02:38:36 +0000

    Implement `RequestHandler` for `BaseRuntime`
    Handle application requests common for contracts and services.

--- linera-execution/src/wasm/runtime_actor/handlers.rs
+++ linera-execution/src/wasm/runtime_actor/handlers.rs
@@ -3,7 +3,8 @@
 
 //! Implementations of how requests should be handled inside a [`RuntimeActor`].
 
-use crate::ExecutionError;
+use super::requests::BaseRequest;
+use crate::{BaseRuntime, ExecutionError};
 use async_trait::async_trait;
 
 /// A type that is able to handle incoming `Request`s.
@@ -14,6 +15,53 @@ pub trait RequestHandler<Request> {
     /// Returns an error if the request could not be handled and no further requests should be sent
     /// to this handler.
     async fn handle_request(&self, request: Request) -> Result<(), ExecutionError>;
+}
+
+#[async_trait]
+impl<Runtime> RequestHandler<BaseRequest> for &Runtime
+where
+    Runtime: BaseRuntime + ?Sized,
+{
+    async fn handle_request(&self, request: BaseRequest) -> Result<(), ExecutionError> {
+        match request {
+            BaseRequest::ChainId { response_sender } => response_sender.respond(self.chain_id()),
+            BaseRequest::ApplicationId { response_sender } => {
+                response_sender.respond(self.application_id())
+            }
+            BaseRequest::ApplicationParameters { response_sender } => {
+                response_sender.respond(self.application_parameters())
+            }
+            BaseRequest::ReadSystemBalance { response_sender } => {
+                response_sender.respond(self.read_system_balance())
+            }
+            BaseRequest::ReadSystemTimestamp { response_sender } => {
+                response_sender.respond(self.read_system_timestamp())
+            }
+            BaseRequest::TryReadMyState { response_sender } => {
+                response_sender.respond(self.try_read_my_state().await?)
+            }
+            BaseRequest::LockViewUserState { response_sender } => {
+                response_sender.respond(self.lock_view_user_state().await?)
+            }
+            BaseRequest::UnlockViewUserState { response_sender } => {
+                response_sender.respond(self.unlock_view_user_state().await?)
+            }
+            BaseRequest::ReadKeyBytes {
+                key,
+                response_sender,
+            } => response_sender.respond(self.read_key_bytes(key).await?),
+            BaseRequest::FindKeysByPrefix {
+                key_prefix,
+                response_sender,
+            } => response_sender.respond(self.find_keys_by_prefix(key_prefix).await?),
+            BaseRequest::FindKeyValuesByPrefix {
+                key_prefix,
+                response_sender,
+            } => response_sender.respond(self.find_key_values_by_prefix(key_prefix).await?),
+        }
+
+        Ok(())
+    }
 }
 
 /// Helper trait to send a response and log on failure.
