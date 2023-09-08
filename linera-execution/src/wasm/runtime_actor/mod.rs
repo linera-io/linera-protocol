fac9e553542e250bea62543ba0c8ce8442221232
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-09-08 00:58:21 +0000

    Create a `RuntimeActor` type
    Is able to run asynchronously receiving messages from an MPSC channel
    and handling requests.

--- linera-execution/src/wasm/runtime_actor/mod.rs
+++ linera-execution/src/wasm/runtime_actor/mod.rs
@@ -6,4 +6,65 @@
 mod handlers;
 mod requests;
 
+use self::handlers::RequestHandler;
 pub use self::requests::{BaseRequest, ContractRequest};
+use crate::ExecutionError;
+use futures::{
+    channel::mpsc,
+    select,
+    stream::{FuturesUnordered, StreamExt},
+};
+
+/// A handler of application system APIs that runs as a separate actor.
+///
+/// Receives `Request`s from the application and handles them using the `Runtime`.
+pub struct RuntimeActor<Runtime, Request> {
+    runtime: Runtime,
+    requests: mpsc::UnboundedReceiver<Request>,
+}
+
+impl<Runtime, Request> RuntimeActor<Runtime, Request>
+where
+    Runtime: RequestHandler<Request>,
+    Request: std::fmt::Debug,
+{
+    /// Creates a new [`RuntimeActor`] using the provided `Runtime` to handle `Request`s.
+    ///
+    /// Returns the new [`RuntimeActor`] so that it can be executed later with the
+    /// [`RuntimeActor::run`] method and the endpoint to send `Request`s to the actor.
+    pub fn new(runtime: Runtime) -> (Self, mpsc::UnboundedSender<Request>) {
+        let (sender, receiver) = mpsc::unbounded();
+
+        let actor = RuntimeActor {
+            runtime,
+            requests: receiver,
+        };
+
+        (actor, sender)
+    }
+
+    /// Runs the [`RuntimeActor`], handling `Request`s until all the sender endpoints are closed.
+    pub async fn run(mut self) -> Result<(), ExecutionError> {
+        let mut active_requests = FuturesUnordered::new();
+
+        loop {
+            select! {
+                maybe_result = active_requests.next() => if let Some(result) = maybe_result {
+                    result?;
+                },
+                maybe_request = self.requests.next() => match maybe_request {
+                    Some(request) => active_requests.push(self.runtime.handle_request(request)),
+                    None => break,
+                },
+            }
+        }
+
+        while !active_requests.is_empty() {
+            if let Some(result) = active_requests.next().await {
+                result?;
+            }
+        }
+
+        Ok(())
+    }
+}
