3c228b03b9902c4b26173f71ae52bbd54dfa5cb2
Maxime Levillain <maxime.levillain@functori.com>
2023-09-13 11:08:49 +0200

    Scylla DB runner for indexer

---
+++ linera-indexer/lib/src/scylla_db.rs
@@ -0,0 +1,50 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{
+    common::IndexerError,
+    runner::{IndexerConfig, Runner},
+};
+use linera_views::{common::CommonStoreConfig, scylla_db::ScyllaDbClient};
+use structopt::StructOpt;
+
+#[derive(StructOpt, Clone, Debug)]
+pub struct ScyllaDbConfig {
+    /// RocksDB storage path
+    #[structopt(long, default_value = "localhost:9042")]
+    pub uri: String,
+    #[structopt(long, default_value = "linera")]
+    pub table: String,
+    /// The maximal number of simultaneous queries to the database
+    #[structopt(long)]
+    max_concurrent_queries: Option<usize>,
+    /// The maximal number of simultaneous stream queries to the database
+    #[structopt(long, default_value = "10")]
+    pub max_stream_queries: usize,
+    /// The maximal number of entries in the storage cache.
+    #[structopt(long, default_value = "1000")]
+    cache_size: usize,
+    /// Do not create a table if one is missing
+    #[structopt(long = "do not create a database if missing")]
+    skip_table_creation_when_missing: bool,
+}
+
+pub type ScyllaDbRunner = Runner<ScyllaDbClient, ScyllaDbConfig>;
+
+impl ScyllaDbRunner {
+    pub async fn load() -> Result<Self, IndexerError> {
+        let config = IndexerConfig::<ScyllaDbConfig>::from_args();
+        let (client, _) = ScyllaDbClient::new(
+            &config.client.uri,
+            config.client.table.clone(),
+            CommonStoreConfig {
+                max_concurrent_queries: config.client.max_concurrent_queries,
+                max_stream_queries: config.client.max_stream_queries,
+                cache_size: config.client.cache_size,
+                create_if_missing: !config.client.skip_table_creation_when_missing,
+            },
+        )
+        .await?;
+        Self::new(config, client).await
+    }
+}
