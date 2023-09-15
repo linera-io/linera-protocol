5901836bc9708ec70fc8c149091f81e0afed37c5
Maxime Levillain <maxime.levillain@functori.com>
2023-09-15 11:45:33 +0200

    Fix indexer codegen path

--- linera-explorer/codegen_operations.ts
+++ linera-explorer/codegen_operations.ts
@@ -2,8 +2,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "../linera-indexer-graphql-client/gql/operations_schema.graphql",
-  documents: "../linera-indexer-graphql-client/gql/operations_requests.graphql",
+  schema: "../linera-indexer/graphql-client/gql/operations_schema.graphql",
+  documents: "../linera-indexer/graphql-client/gql/operations_requests.graphql",
   generates: {
     "gql/operations.d.ts": {
       plugins: ['typescript']
