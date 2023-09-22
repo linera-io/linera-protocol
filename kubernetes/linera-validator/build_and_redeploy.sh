c03db5ac3cba7ebc68c301b7bb62500fdae35648
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2023-09-22 16:51:17 +0100

    Moved Dockerfiles to docker directory

--- kubernetes/linera-validator/build_and_redeploy.sh
+++ kubernetes/linera-validator/build_and_redeploy.sh
@@ -87,9 +87,9 @@ else
     docker_image="linera-test:latest"
     if [ "$do_build" = true ]; then
         if [ "$(uname -m)" = "x86_64" ]; then
-            docker build -f ../../Dockerfile.local ../../ -t $docker_image || exit 1
+            docker build -f ../../docker/Dockerfile.local ../../ -t $docker_image || exit 1
         else
-            docker build -f ../../Dockerfile.local-aarch64 ../../ -t $docker_image || exit 1
+            docker build -f ../../docker/Dockerfile.local-aarch64 ../../ -t $docker_image || exit 1
         fi
     fi
 fi
