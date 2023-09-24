029ca51782d11db20fad092785304878cbc37f78
Andreas Fackler <andreas.fackler@linera.io>
2023-09-24 11:04:03 +0200

    Take locked block into account in next_round.

--- linera-chain/src/manager/multi.rs
+++ linera-chain/src/manager/multi.rs
@@ -457,15 +457,23 @@ impl MultiOwnerManagerInfo {
     }
 
     pub fn next_round(&self) -> Option<RoundNumber> {
-        match &self.requested_proposed {
-            Some(proposal) if proposal.content.round == self.current_round => {
-                if self.current_round >= self.multi_leader_rounds {
-                    None // There's already a proposal in the current round.
-                } else {
-                    self.current_round.try_add_one().ok()
-                }
-            }
-            _ => Some(self.current_round),
+        let proposal_round = self
+            .requested_proposed
+            .as_ref()
+            .map(|proposal| proposal.content.round);
+        let locked_round = self
+            .requested_locked
+            .as_ref()
+            .map(|certificate| certificate.round);
+        if proposal_round != Some(self.current_round) && locked_round != Some(self.current_round) {
+            // There's no proposal or locked block yet in the current round.
+            Some(self.current_round)
+        } else if self.current_round < self.multi_leader_rounds {
+            // We're still in multi-leader mode, so we can propose in the next round at any time.
+            self.current_round.try_add_one().ok()
+        } else {
+            // We're in single-leader mode, so the next proposal can only be made after the timeout.
+            None
         }
     }
 }
