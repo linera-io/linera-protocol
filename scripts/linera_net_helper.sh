a6108eca0b2961e8d448e868c52f22386d2e57b1
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2023-10-13 18:42:22 -0700

    scripts/linera_net_helper.sh

---
+++ scripts/linera_net_helper.sh
@@ -0,0 +1,22 @@
+# Copyright (c) Zefchain Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# Runs a command such as `linera net up` in the background.
+# - Records stdout
+# - Waits for the background process to print READY! on stderr
+# - Then executes the bash command recorded from stdout
+# - Returns without killing the process
+function spawn_and_set_wallet_env_vars() {
+    DIR=$(mktemp -d "${TMPDIR:-.}tmp-XXXXX") || exit 1
+    OUT="$DIR/out"
+    ERR="$DIR/err"
+    mkfifo "$ERR" || exit 1
+
+    trap 'jobs -p | xargs -r kill && rm -rf "$DIR"' EXIT
+
+    "$@" 2> >(tee "$ERR") 1>"$OUT" &
+
+    sed -n '/^READY!/q' <"$ERR" || exit 1
+
+    source "$OUT"
+}
