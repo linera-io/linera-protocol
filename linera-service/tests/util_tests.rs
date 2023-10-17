afa92e36d532f50d32538d48a91ffe62eeb14d5e
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2023-10-16 18:47:36 -0700

    Fix resolve_binary (again) (#1130)
    * make resolve_binary more robust + add unit tests
    
    * assert exact path in util_tests.rs

---
+++ linera-service/tests/util_tests.rs
@@ -0,0 +1,49 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+#![cfg(feature = "rocksdb")]
+
+use linera_service::util;
+use std::path::Path;
+
+#[test_log::test(tokio::test)]
+async fn test_resolve_binary_with_test_default() {
+    let path = util::resolve_binary("linera", "linera-service")
+        .await
+        .unwrap();
+    assert!(path.exists());
+    // Since we're in a test, we can use the environment variables `CARGO_BIN_EXE_*`.
+    assert_eq!(path, Path::new(env!("CARGO_BIN_EXE_linera")));
+}
+
+#[test_log::test(tokio::test)]
+async fn test_resolve_binary_from_relative_path() {
+    let debug_or_release = Path::new(env!("CARGO_BIN_EXE_linera"))
+        .parent()
+        .unwrap()
+        .file_name()
+        .unwrap();
+    let path = util::resolve_binary_in_same_directory_as(
+        Path::new("../target").join(debug_or_release).join("linera"),
+        "linera-proxy",
+        "linera-service",
+    )
+    .await
+    .unwrap();
+    assert!(path.exists());
+    assert_eq!(path, Path::new(env!("CARGO_BIN_EXE_linera-proxy")));
+}
+
+#[test_log::test(tokio::test)]
+async fn test_resolve_binary_from_absolute_path() {
+    let path = util::resolve_binary_in_same_directory_as(
+        env!("CARGO_BIN_EXE_linera"),
+        "linera-proxy",
+        "linera-service",
+    )
+    .await
+    .unwrap();
+    assert!(path.exists());
+    assert_eq!(path, Path::new(env!("CARGO_BIN_EXE_linera-proxy")));
+}
