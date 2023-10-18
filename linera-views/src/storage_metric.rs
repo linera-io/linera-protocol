cc648afd4d9d733ae8ba4b47062a94a58e193117
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-10-18 21:39:30 +0200

    Address the problem with the metric_stat.

--- linera-views/src/storage_metric.rs
+++ linera-views/src/storage_metric.rs
@@ -6,9 +6,30 @@ use crate::{
     common::{KeyIterable, KeyValueIterable, KeyValueStoreClient},
     memory::{create_memory_client, MemoryClient},
 };
-use async_lock::RwLock;
 use async_trait::async_trait;
 use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+/// Array containing metric data for this client
+/// This can be used for storage fees or for other
+/// benchmarking purposes.
+#[derive(Clone, Default, Debug)]
+pub struct AtomicMetricStat {
+    /// The total number of keys read in calls to `read_key` and `read_multi_key`
+    pub n_reads: Arc<AtomicUsize>,
+    /// The number of missed cases in read_key and read_multi_key
+    pub n_miss_reads: Arc<AtomicUsize>,
+    /// The number of Put in the batches
+    pub n_puts: Arc<AtomicUsize>,
+    /// The total number of Delete in the batches
+    pub n_deletes: Arc<AtomicUsize>,
+    /// The total number of DeletePrefix in the batches
+    pub n_delete_prefix: Arc<AtomicUsize>,
+    /// The total data that went into the Batches
+    pub size_writes: Arc<AtomicUsize>,
+    /// The total data being read from
+    pub size_reads: Arc<AtomicUsize>,
+}
 
 /// Array containing metric data for this client
 /// This can be used for storage fees or for other
@@ -31,6 +52,10 @@ pub struct MetricStat {
     pub size_reads: usize,
 }
 
+
+
+
+
 /// The `MetricKeyValueClient` encapsulates a client and creates a new one that
 /// measures the operations being done
 #[derive(Clone)]
@@ -38,7 +63,7 @@ pub struct MetricKeyValueClient<K> {
     /// The inner client that is called by the metric one
     pub client: K,
     /// The data contained in the running of this container
-    pub metric_stat: Arc<RwLock<MetricStat>>,
+    pub metric_stat: AtomicMetricStat,
 }
 
 #[async_trait]
@@ -57,14 +82,13 @@ where
 
     async fn read_key_bytes(&self, key: &[u8]) -> Result<Option<Vec<u8>>, Self::Error> {
         let read = self.client.read_key_bytes(key).await?;
-        let mut metric_stat = self.metric_stat.write().await;
-        metric_stat.n_reads += 1;
+        self.metric_stat.n_reads.fetch_add(1, Ordering::Relaxed);
         match &read {
             None => {
-                metric_stat.n_miss_reads += 1;
+                self.metric_stat.n_miss_reads.fetch_add(1, Ordering::Relaxed);
             }
             Some(value) => {
-                metric_stat.size_reads += value.len();
+                self.metric_stat.size_reads.fetch_add(value.len(), Ordering::Relaxed);
             }
         }
         Ok(read)
@@ -76,15 +100,14 @@ where
     ) -> Result<Vec<Option<Vec<u8>>>, Self::Error> {
         let n_keys = keys.len();
         let multi_read = self.client.read_multi_key_bytes(keys).await?;
-        let mut metric_stat = self.metric_stat.write().await;
-        metric_stat.n_reads += n_keys;
+        self.metric_stat.n_reads.fetch_add(n_keys, Ordering::Relaxed);
         for read in &multi_read {
             match read {
                 None => {
-                    metric_stat.n_miss_reads += 1;
+                    self.metric_stat.n_miss_reads.fetch_add(1, Ordering::Relaxed);
                 }
                 Some(value) => {
-                    metric_stat.size_reads += value.len();
+                    self.metric_stat.size_reads.fetch_add(value.len(), Ordering::Relaxed);
                 }
             }
         }
@@ -92,10 +115,9 @@ where
     }
 
     async fn find_keys_by_prefix(&self, key_prefix: &[u8]) -> Result<Self::Keys, Self::Error> {
-        let mut metric_stat = self.metric_stat.write().await;
         let keys = self.client.find_keys_by_prefix(key_prefix).await?;
         for key in keys.iterator() {
-            metric_stat.size_reads += key?.len();
+            self.metric_stat.size_reads.fetch_add(key?.len(), Ordering::Relaxed);
         }
         Ok(keys)
     }
@@ -104,30 +126,28 @@ where
         &self,
         key_prefix: &[u8],
     ) -> Result<Self::KeyValues, Self::Error> {
-        let mut metric_stat = self.metric_stat.write().await;
         let key_values = self.client.find_key_values_by_prefix(key_prefix).await?;
         for key_value in key_values.iterator() {
             let key_value = key_value?;
-            metric_stat.size_reads += key_value.0.len() + key_value.1.len();
+            self.metric_stat.size_reads.fetch_add(key_value.0.len() + key_value.1.len(), Ordering::Relaxed);
         }
         Ok(key_values)
     }
 
     async fn write_batch(&self, batch: Batch, base_key: &[u8]) -> Result<(), Self::Error> {
-        let mut metric_stat = self.metric_stat.write().await;
         for operation in &batch.operations {
             match operation {
                 WriteOperation::Delete { key } => {
-                    metric_stat.n_deletes += 1;
-                    metric_stat.size_writes += key.len();
+                    self.metric_stat.n_deletes.fetch_add(1, Ordering::Relaxed);
+                    self.metric_stat.size_writes.fetch_add(key.len(), Ordering::Relaxed);
                 }
                 WriteOperation::Put { key, value } => {
-                    metric_stat.n_puts += 1;
-                    metric_stat.size_writes += key.len() + value.len();
+                    self.metric_stat.n_puts.fetch_add(1, Ordering::Relaxed);
+                    self.metric_stat.size_writes.fetch_add(key.len() + value.len(), Ordering::Relaxed);
                 }
                 WriteOperation::DeletePrefix { key_prefix } => {
-                    metric_stat.n_delete_prefix += 1;
-                    metric_stat.size_writes += key_prefix.len();
+                    self.metric_stat.n_deletes.fetch_add(1, Ordering::Relaxed);
+                    self.metric_stat.size_writes.fetch_add(key_prefix.len(), Ordering::Relaxed);
                 }
             }
         }
@@ -139,22 +159,32 @@ where
     }
 }
 
+impl<K> MetricKeyValueClient<K>
+where
+    K: KeyValueStoreClient + Send + Sync,
+{
+    /// Returning the current metric information
+    pub fn metric(&self) -> MetricStat {
+        let n_reads = self.metric_stat.n_reads.load(Ordering::Relaxed);
+        let n_miss_reads = self.metric_stat.n_miss_reads.load(Ordering::Relaxed);
+        let n_puts = self.metric_stat.n_puts.load(Ordering::Relaxed);
+        let n_deletes = self.metric_stat.n_deletes.load(Ordering::Relaxed);
+        let n_delete_prefix = self.metric_stat.n_delete_prefix.load(Ordering::Relaxed);
+        let size_writes = self.metric_stat.size_writes.load(Ordering::Relaxed);
+        let size_reads = self.metric_stat.size_reads.load(Ordering::Relaxed);
+        MetricStat { n_reads, n_miss_reads, n_puts, n_deletes, n_delete_prefix, size_writes, size_reads }
+    }
+}
+
+
 /// Create a new client
 pub fn get_metric_memory_client() -> MetricKeyValueClient<MemoryClient> {
     let client = create_memory_client();
-    let metric_stat = MetricStat::default();
-    let metric_stat = Arc::new(RwLock::new(metric_stat));
+    let metric_stat = AtomicMetricStat::default();
     MetricKeyValueClient {
         client,
         metric_stat,
     }
-}
-
-/// Returning the current metric information
-pub async fn metric(client: &MetricKeyValueClient<MemoryClient>) -> MetricStat {
-    let metric_stat = &client.metric_stat;
-    let metric_stat = metric_stat.read().await;
-    *metric_stat
 }
 
 #[cfg(test)]
