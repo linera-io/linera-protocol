0b6b17e036d9b51afbf9abe29caa1364daa07e4d
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2023-10-21 17:48:18 -0700

    Create helper to extract bash script from a readme file

--- linera-service/tests/readme_test.rs
+++ linera-service/tests/readme_test.rs
@@ -7,25 +7,13 @@
 mod common;
 
 use common::INTEGRATION_TEST_GUARD;
-use std::io::Write;
-use tempfile::tempdir;
+use linera_service::util::QuotedBashScript;
 use tokio::process::Command;
 
 #[test_log::test(tokio::test)]
 async fn test_examples_in_readme() -> std::io::Result<()> {
     let _guard = INTEGRATION_TEST_GUARD.lock().await;
-
-    let dir = tempdir().unwrap();
-    let file = std::io::BufReader::new(std::fs::File::open("../README.md")?);
-    let mut quotes = get_bash_quotes(file)?;
-    // Check that we have the expected number of examples starting with "```bash".
-    assert_eq!(quotes.len(), 1);
-    let quote = quotes.pop().unwrap();
-
-    let test_file = dir.path().join("test.sh");
-    let mut test_script = std::fs::File::create(test_file)?;
-    write!(&mut test_script, "{}", quote)?;
-
+    let script = QuotedBashScript::from_markdown("../README.md")?;
     let status = Command::new("bash")
         // Run from the root of the repo.
         .current_dir("..")
@@ -33,34 +21,10 @@ async fn test_examples_in_readme() -> std::io::Result<()> {
         .env("RUST_LOG", "linera_service=debug")
         .arg("-e")
         .arg("-x")
-        .arg(dir.path().join("test.sh"))
+        .arg(script.path())
         .status()
         .await?;
 
     assert!(status.success());
     Ok(())
-}
-
-#[allow(clippy::while_let_on_iterator)]
-fn get_bash_quotes(reader: impl std::io::BufRead) -> std::io::Result<Vec<String>> {
-    let mut result = Vec::new();
-    let mut lines = reader.lines();
-
-    while let Some(line) = lines.next() {
-        let line = line?;
-        if line.starts_with("```bash") {
-            let mut quote = String::new();
-            while let Some(line) = lines.next() {
-                let line = line?;
-                if line.starts_with("```") {
-                    break;
-                }
-                quote += &line;
-                quote += "\n";
-            }
-            result.push(quote);
-        }
-    }
-
-    Ok(result)
 }
