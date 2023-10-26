eff9b4d0d1c8a40aeeaba5bc17e8c1c2760a8f41
James Kay <james.kay@linera.io>
2023-10-26 14:13:37 +0100

    Simplify linera_net_helper script

--- scripts/linera_net_helper.sh
+++ scripts/linera_net_helper.sh
@@ -9,23 +9,12 @@ function linera_spawn_and_read_wallet_variables() {
     # Clean up the temporary directory in case we exit.
     trap 'rm -rf "$LINERA_TMP_DIR"' EXIT
 
-    LINERA_TMP_PID="$LINERA_TMP_DIR/pid"
     LINERA_TMP_OUT="$LINERA_TMP_DIR/out"
     LINERA_TMP_ERR="$LINERA_TMP_DIR/err"
     mkfifo "$LINERA_TMP_ERR" || exit 1
 
-    (
-        # Ignoring SIGPIPE to keep `tee` alive after `sed` exits below, closing
-        # LINERA_TMP_ERR.
-        trap '' PIPE
-
-        # Start the main process.
-        "$@" 2> >(tee "$LINERA_TMP_ERR" 2>/dev/null) 1>"$LINERA_TMP_OUT" &
-
-        # Remember the PID of the service for later.
-        jobs -p > "$LINERA_TMP_PID"
-    )
-    LINERA_NET_PID=$(cat "$LINERA_TMP_PID")
+    "$@" >"$LINERA_TMP_OUT" 2>"$LINERA_TMP_ERR" &
+    LINERA_NET_PID=$!
 
     # When the shell exits, we will clean up the top-level jobs (if any), the temporary
     # directory, and the main process. Handling future top-level jobs here is useful
@@ -33,8 +22,11 @@ function linera_spawn_and_read_wallet_variables() {
     trap 'jobs -p | xargs -r kill && rm -rf "$LINERA_TMP_DIR" && kill "$LINERA_NET_PID"' EXIT
 
     # Read from LINERA_TMP_ERR until the string "READY!" is found.
-    sed -n '/^READY!/q' <"$LINERA_TMP_ERR" || exit 1
+    sed '/^READY!/q' <"$LINERA_TMP_ERR" || exit 1
 
     # Source the Bash commands output by the server.
     source "$LINERA_TMP_OUT"
+
+    # Continue to output the command's stderr onto stdout.
+    cat "$LINERA_TMP_ERR" &
 }
