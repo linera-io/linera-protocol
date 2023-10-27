3ac3beec574d794057f8c8bd4067a0c3b6b79b9f
James Kay <james.kay@linera.io>
2023-10-27 11:18:18 +0100

    Nix flake and `rustup` toolchain format update (#1162)
    * Add newer-format rust-toolchain with WASM support
    
    The newer TOML `rust-toolchain.toml` format allows specifying more
    things about the compiler.  Importantly, it allows us to require
    support for particular targets, like `wasm32-unknown-unknown`.
    
    We keep the legacy `rust-toolchain` file, in the legacy format, as it
    is depended upon by CI.
    
    rust-toolchain documentation:
    https://rust-lang.github.io/rustup/overrides.html#the-toolchain-file
    
    * Add Nix develop environment with Rust and WASM support
    
    Reads the Rust toolchain specification from the rustup
    `rust-toolchain.toml` file, and uses it to provide a development
    environment for `linera-protocol` with required non-Rust dependencies
    and standard development tools (`rustfmt`).

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    # Dev tools
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import inputs.systems;
+    imports = [
+      inputs.treefmt-nix.flakeModule
+    ];
+    perSystem = { config, self', pkgs, lib, system, rust-overlay, ... }:
+      let
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        nonRustDeps = with pkgs; [
+          libclang.lib
+          libiconv
+          openssl
+          protobuf
+          pkg-config
+          nodejs
+        ];
+        rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile
+          ./rust-toolchain.toml);
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = rustToolchain;
+          cargo = rustToolchain;
+        };
+      in {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ (import inputs.rust-overlay) ];
+        };
+
+        # Rust dev environment
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            config.treefmt.build.devShell
+          ];
+          shellHook = ''
+            # For rust-analyzer 'hover' tooltips to work.
+            export RUST_SRC_PATH=${rustToolchain.availableComponents.rust-src}
+            export LIBCLANG_PATH=${pkgs.libclang.lib}/lib
+            export PATH=$PWD/target/release:~/.cargo/bin:$PATH
+          '';
+          buildInputs = nonRustDeps;
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            rust-analyzer
+          ];
+        };
+
+        # Add your auto-formatters here.
+        # cf. https://numtide.github.io/treefmt/
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixpkgs-fmt.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+      };
+  };
+}
