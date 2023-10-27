ba73dde0eef81228de58d4c0a75279aad68a31d6
James Kay <james.kay@linera.io>
2023-10-27 12:05:47 +0100

    Unify error handling
    With this simplification, the child process no longer has to be
    treated specially by the exit handler.
    
    Co-authored-by: Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>

--- scripts/linera_net_helper.sh
+++ scripts/linera_net_helper.sh
@@ -6,20 +6,16 @@
 function linera_spawn_and_read_wallet_variables() {
     LINERA_TMP_DIR=$(mktemp -d "${TMPDIR:-.}tmp-XXXXX") || exit 1
 
-    # Clean up the temporary directory in case we exit.
-    trap 'rm -rf "$LINERA_TMP_DIR"' EXIT
+    # When the shell exits, we will clean up the top-level jobs (if any), the temporary
+    # directory, and the main process. Handling future top-level jobs here is useful
+    # because calling `trap` again will be replace this handler.
+    trap 'jobs -p | xargs -r kill && rm -rf "$LINERA_TMP_DIR"' EXIT
 
     LINERA_TMP_OUT="$LINERA_TMP_DIR/out"
     LINERA_TMP_ERR="$LINERA_TMP_DIR/err"
     mkfifo "$LINERA_TMP_ERR" || exit 1
 
     "$@" >"$LINERA_TMP_OUT" 2>"$LINERA_TMP_ERR" &
-    LINERA_NET_PID=$!
-
-    # When the shell exits, we will clean up the top-level jobs (if any), the temporary
-    # directory, and the main process. Handling future top-level jobs here is useful
-    # because calling `trap` again will be replace this handler.
-    trap 'jobs -p | xargs -r kill && rm -rf "$LINERA_TMP_DIR" && kill "$LINERA_NET_PID"' EXIT
 
     # Read from LINERA_TMP_ERR until the string "READY!" is found.
     sed '/^READY!/q' <"$LINERA_TMP_ERR" || exit 1
