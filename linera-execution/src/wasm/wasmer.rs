jvff/linera-protocol-archive

Configure contract fuel in guest actor thread
Configuring the fuel requires calling the application runtime actor,
which involves a blocking request so it shouldn't run in the
asynchronous thread.

linera-execution/src/wasm/wasmer.rs
@@ -84,6 +84,20 @@ impl ApplicationRuntimeContext for Contract {
     type Error = RuntimeError;
     type Extra = WasmerContractExtra;
 
+    fn initialize(context: &mut WasmRuntimeContext<Self>) {
+        let remaining_points = context
+            .extra
+            .runtime
+            .sync_request(|response| ContractRequest::RemainingFuel { response })
+            .unwrap_or(0);
+
+        metering::set_remaining_points(
+            &mut context.store,
+            &context.extra.instance,
+            remaining_points,
+        );
+    }
+
     fn finalize(context: &mut WasmRuntimeContext<Self>) {
         let remaining_fuel =
             match metering::get_remaining_points(&mut context.store, &context.extra.instance) {
@@ -224,20 +238,6 @@ impl common::Contract for Contract {
     type PollExecutionResult = contract::PollExecutionResult;
     type PollCallApplication = contract::PollCallApplication;
     type PollCallSession = contract::PollCallSession;
-
-    fn configure_fuel(context: &mut WasmRuntimeContext<Self>) {
-        let remaining_points = context
-            .extra
-            .runtime
-            .sync_request(|response| ContractRequest::RemainingFuel { response })
-            .unwrap_or(0);
-
-        metering::set_remaining_points(
-            &mut context.store,
-            &context.extra.instance,
-            remaining_points,
-        );
-    }
 
     fn initialize_new(
         &self,

linera-execution/src/wasm/wasmtime.rs
@@ -92,6 +92,18 @@ impl ApplicationRuntimeContext for Contract {
     type Error = Trap;
     type Extra = ();
 
+    fn initialize(context: &mut WasmRuntimeContext<Self>) {
+        let runtime = &context.store.data().system_api.runtime;
+        let fuel = runtime
+            .sync_request(|response| ContractRequest::RemainingFuel { response })
+            .unwrap_or(0);
+
+        context
+            .store
+            .add_fuel(fuel)
+            .expect("Fuel consumption wasn't properly enabled");
+    }
+
     fn finalize(context: &mut WasmRuntimeContext<Self>) {
         let runtime = &context.store.data().system_api.runtime;
         let initial_fuel = runtime
@@ -312,18 +324,6 @@ impl common::Contract for Contract {
     type PollExecutionResult = contract::PollExecutionResult;
     type PollCallApplication = contract::PollCallApplication;
     type PollCallSession = contract::PollCallSession;
-
-    fn configure_fuel(context: &mut WasmRuntimeContext<Self>) {
-        let runtime = &context.store.data().system_api.runtime;
-        let fuel = runtime
-            .sync_request(|response| ContractRequest::RemainingFuel { response })
-            .unwrap_or(0);
-
-        context
-            .store
-            .add_fuel(fuel)
-            .expect("Fuel consumption wasn't properly enabled");
-    }
 
     fn initialize_new(
         &self,

linera-io/linera-protocol

linera-execution/src/wasm/wasmer.rs
@@ -81,6 +81,20 @@ impl ApplicationRuntimeContext for Contract {
     type Error = RuntimeError;
     type Extra = WasmerContractExtra;
 
+    fn initialize(context: &mut WasmRuntimeContext<Self>) {
+        let remaining_points = context
+            .extra
+            .runtime
+            .sync_request(|response_sender| ContractRequest::RemainingFuel { response_sender })
+            .unwrap_or(0);
+
+        metering::set_remaining_points(
+            &mut context.store,
+            &context.extra.instance,
+            remaining_points,
+        );
+    }
+
     fn finalize(context: &mut WasmRuntimeContext<Self>) {
         let remaining_fuel =
             match metering::get_remaining_points(&mut context.store, &context.extra.instance) {
@@ -220,20 +234,6 @@ impl common::Contract for Contract {
     type PollExecutionResult = contract::PollExecutionResult;
     type PollApplicationCallResult = contract::PollApplicationCallResult;
     type PollSessionCallResult = contract::PollSessionCallResult;
-
-    fn configure_fuel(context: &mut WasmRuntimeContext<Self>) {
-        let remaining_points = context
-            .extra
-            .runtime
-            .sync_request(|response_sender| ContractRequest::RemainingFuel { response_sender })
-            .unwrap_or(0);
-
-        metering::set_remaining_points(
-            &mut context.store,
-            &context.extra.instance,
-            remaining_points,
-        );
-    }
 
     fn initialize_new(
         &self,

linera-execution/src/wasm/wasmtime.rs
@@ -89,6 +89,18 @@ impl ApplicationRuntimeContext for Contract {
     type Error = Trap;
     type Extra = ();
 
+    fn initialize(context: &mut WasmRuntimeContext<Self>) {
+        let runtime = &context.store.data().system_api.runtime;
+        let fuel = runtime
+            .sync_request(|response_sender| ContractRequest::RemainingFuel { response_sender })
+            .unwrap_or(0);
+
+        context
+            .store
+            .add_fuel(fuel)
+            .expect("Fuel consumption wasn't properly enabled");
+    }
+
     fn finalize(context: &mut WasmRuntimeContext<Self>) {
         let runtime = &context.store.data().system_api.runtime;
         let initial_fuel = runtime
@@ -309,18 +321,6 @@ impl common::Contract for Contract {
     type PollExecutionResult = contract::PollExecutionResult;
     type PollApplicationCallResult = contract::PollApplicationCallResult;
     type PollSessionCallResult = contract::PollSessionCallResult;
-
-    fn configure_fuel(context: &mut WasmRuntimeContext<Self>) {
-        let runtime = &context.store.data().system_api.runtime;
-        let fuel = runtime
-            .sync_request(|response_sender| ContractRequest::RemainingFuel { response_sender })
-            .unwrap_or(0);
-
-        context
-            .store
-            .add_fuel(fuel)
-            .expect("Fuel consumption wasn't properly enabled");
-    }
 
     fn initialize_new(
         &self,