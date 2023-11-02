jvff/linera-protocol-archive

Create guest futures inside the actor thread
Run all the guest Wasm code in the non-asynchronous actor thread.

linera-execution/src/wasm/async_boundary.rs
@@ -12,6 +12,7 @@ use super::{
 use futures::{channel::oneshot, ready, stream::StreamExt, FutureExt};
 use std::{
     future::Future,
+    marker::PhantomData,
     mem,
     pin::Pin,
     task::{Context, Poll},
@@ -27,9 +28,9 @@ where
     Application: ApplicationRuntimeContext,
     Future: GuestFutureInterface<Application>,
 {
-    future: Future,
     context: WasmRuntimeContext<Application>,
     poll_requests: std::sync::mpsc::Receiver<oneshot::Sender<PollResponse<Future::Output>>>,
+    _future_type: PhantomData<Future>,
 }
 
 impl<Future, Application> GuestFutureActor<Future, Application>
@@ -43,41 +44,49 @@ where
     /// Returns the [`PollSender`] which can be used in an asynchronous context to `await` the
     /// result.
     pub fn spawn(
-        future: Future,
+        parameters: Future::Parameters,
         context: WasmRuntimeContext<Application>,
     ) -> PollSender<Future::Output> {
-        let (actor, poll_sender) = Self::new(future, context);
+        let (actor, poll_sender) = Self::new(context);
 
-        thread::spawn(|| actor.run());
+        thread::spawn(|| actor.run(parameters));
 
         poll_sender
     }
 
     /// Creates a new [`GuestFutureActor`] to run `future` using a Wasm runtime `context`.
-    pub fn new(
-        future: Future,
-        mut context: WasmRuntimeContext<Application>,
-    ) -> (Self, PollSender<Future::Output>) {
+    pub fn new(mut context: WasmRuntimeContext<Application>) -> (Self, PollSender<Future::Output>) {
         let (dummy_future_queue, _) = HostFutureQueue::new();
         let host_future_queue = mem::replace(&mut context.future_queue, dummy_future_queue);
 
         let (poll_sender, poll_requests) = PollSender::new(host_future_queue);
 
         let actor = GuestFutureActor {
-            future,
             context,
             poll_requests,
+            _future_type: PhantomData,
         };
 
         (actor, poll_sender)
     }
 
-    /// Executes the future, polling it as requested by the [`PollSender`] until it completes.
-    pub fn run(mut self) {
+    /// Creates and executes the `Future` instance, polling it as requested by the [`PollSender`]
+    /// until it completes.
+    pub fn run(mut self, parameters: Future::Parameters) {
+        match Future::new(
+            parameters,
+            &self.context.application,
+            &mut self.context.store,
+        ) {
+            Ok(future) => self.run_poll_loop(future),
+            Err(error) => self.notify_creation_error(error),
+        }
+    }
+
+    /// Executes the `future`, polling it as requested by the [`PollSender`] until it completes.
+    fn run_poll_loop(mut self, future: Future) {
         while let Ok(response_sender) = self.poll_requests.recv() {
-            let response = self
-                .future
-                .poll(&self.context.application, &mut self.context.store);
+            let response = future.poll(&self.context.application, &mut self.context.store);
             let finished = response.is_ready();
 
             let _ = response_sender.send(PollResponse(response));
@@ -85,6 +94,14 @@ where
             if finished {
                 break;
             }
+        }
+    }
+
+    /// Waits for the first poll request sent by the [`PollSender`], and immediately responds with
+    /// the error obtained when attempting to create the `Future` instance.
+    fn notify_creation_error(self, error: ExecutionError) {
+        if let Ok(response_sender) = self.poll_requests.recv() {
+            let _ = response_sender.send(PollResponse(Poll::Ready(Err(error))));
         }
     }
 }
@@ -210,7 +227,7 @@ where
     Application: ApplicationRuntimeContext,
 {
     /// The parameters necessary to initialize the future.
-    type Parameters;
+    type Parameters: Send;
 
     /// The output of the guest future.
     type Output;

linera-execution/src/wasm/common.rs
@@ -12,7 +12,6 @@ use crate::{
     ApplicationCallResult, CalleeContext, MessageContext, OperationContext, QueryContext,
     RawExecutionResult, SessionCallResult, SessionId,
 };
-use futures::{future, TryFutureExt};
 use std::future::Future;
 
 /// Types that are specific to the context of an application ready to be executedy by a WebAssembly
@@ -239,13 +238,7 @@ where
     ) -> impl Future<Output = Result<RawExecutionResult<Vec<u8>>, ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.initialize_new(
-            &mut self.store,
-            *context,
-            argument.to_owned(),
-        ))
-        .err_into()
-        .and_then(move |future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::Initialize, A>::spawn((*context, argument.to_owned()), self)
     }
 
     /// Calls the guest Wasm module's implementation of
@@ -267,13 +260,7 @@ where
     ) -> impl Future<Output = Result<RawExecutionResult<Vec<u8>>, ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.execute_operation_new(
-            &mut self.store,
-            *context,
-            operation.to_owned(),
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::ExecuteOperation, A>::spawn((*context, operation.to_owned()), self)
     }
 
     /// Calls the guest Wasm module's implementation of
@@ -295,13 +282,7 @@ where
     ) -> impl Future<Output = Result<RawExecutionResult<Vec<u8>>, ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.execute_message_new(
-            &mut self.store,
-            *context,
-            message.to_owned(),
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::ExecuteMessage, A>::spawn((*context, message.to_owned()), self)
     }
 
     /// Calls the guest Wasm module's implementation of
@@ -325,14 +306,10 @@ where
     ) -> impl Future<Output = Result<ApplicationCallResult, ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.handle_application_call_new(
-            &mut self.store,
-            *context,
-            argument.to_owned(),
-            forwarded_sessions,
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::HandleApplicationCall, A>::spawn(
+            (*context, argument.to_owned(), forwarded_sessions),
+            self,
+        )
     }
 
     /// Calls the guest Wasm module's implementation of
@@ -358,15 +335,15 @@ where
     ) -> impl Future<Output = Result<(SessionCallResult, Vec<u8>), ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.handle_session_call_new(
-            &mut self.store,
-            *context,
-            session_state.to_owned(),
-            argument.to_owned(),
-            forwarded_sessions,
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::HandleSessionCall, A>::spawn(
+            (
+                *context,
+                session_state.to_owned(),
+                argument.to_owned(),
+                forwarded_sessions,
+            ),
+            self,
+        )
     }
 }
 
@@ -387,17 +364,11 @@ where
     /// ) -> Result<Vec<u8>, ExecutionError>
     /// ```
     pub fn query_application(
-        mut self,
+        self,
         context: &QueryContext,
         argument: &[u8],
     ) -> impl Future<Output = Result<Vec<u8>, ExecutionError>> {
-        future::ready(self.application.query_application_new(
-            &mut self.store,
-            *context,
-            argument.to_owned(),
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::QueryApplication, A>::spawn((*context, argument.to_owned()), self)
     }
 }
 

linera-io/linera-protocol

linera-execution/src/wasm/async_boundary.rs
@@ -12,6 +12,7 @@ use super::{
 use futures::{channel::oneshot, ready, stream::StreamExt, FutureExt};
 use std::{
     future::Future,
+    marker::PhantomData,
     mem,
     pin::Pin,
     task::{Context, Poll},
@@ -27,9 +28,9 @@ where
     Application: ApplicationRuntimeContext,
     Future: GuestFutureInterface<Application>,
 {
-    future: Future,
     context: WasmRuntimeContext<Application>,
     poll_request_receiver: std::sync::mpsc::Receiver<PollRequest<Future::Output>>,
+    _future_type: PhantomData<Future>,
 }
 
 impl<Future, Application> GuestFutureActor<Future, Application>
@@ -43,7 +44,7 @@ where
     /// Returns the [`PollSender`] which can be used in an asynchronous context to `await` the
     /// result.
     pub fn spawn(
-        future: Future,
+        parameters: Future::Parameters,
         mut context: WasmRuntimeContext<Application>,
     ) -> PollSender<Future::Output> {
         let (dummy_future_queue, _) = HostFutureQueue::new();
@@ -51,32 +52,42 @@ where
 
         let (poll_request_sender, poll_request_receiver) = std::sync::mpsc::channel();
         let poll_sender = PollSender::new(host_future_queue, poll_request_sender);
-        let actor = Self::new(future, context, poll_request_receiver);
+        let actor = Self::new(context, poll_request_receiver);
 
-        thread::spawn(|| actor.run());
+        thread::spawn(|| actor.run(parameters));
 
         poll_sender
     }
 
     /// Creates a new [`GuestFutureActor`] to run `future` using a Wasm runtime `context`.
     pub fn new(
-        future: Future,
         context: WasmRuntimeContext<Application>,
         poll_request_receiver: std::sync::mpsc::Receiver<PollRequest<Future::Output>>,
     ) -> Self {
         GuestFutureActor {
-            future,
             context,
             poll_request_receiver,
+            _future_type: PhantomData,
         }
     }
 
-    /// Executes the future, polling it as requested by the [`PollSender`] until it completes.
-    pub fn run(mut self) {
+    /// Creates and executes the `Future` instance, polling it as requested by the [`PollSender`]
+    /// until it completes.
+    pub fn run(mut self, parameters: Future::Parameters) {
+        match Future::new(
+            parameters,
+            &self.context.application,
+            &mut self.context.store,
+        ) {
+            Ok(future) => self.run_poll_loop(future),
+            Err(error) => self.notify_creation_error(error),
+        }
+    }
+
+    /// Executes the `future`, polling it as requested by the [`PollSender`] until it completes.
+    fn run_poll_loop(mut self, future: Future) {
         while let Ok(request) = self.poll_request_receiver.recv() {
-            let response = self
-                .future
-                .poll(&self.context.application, &mut self.context.store);
+            let response = future.poll(&self.context.application, &mut self.context.store);
             let mut finished = response.is_ready();
 
             if request
@@ -90,6 +101,22 @@ where
 
             if finished {
                 break;
+            }
+        }
+    }
+
+    /// Waits for the first poll request sent by the [`PollSender`], and immediately responds with
+    /// the error obtained when attempting to create the `Future` instance.
+    fn notify_creation_error(self, error: ExecutionError) {
+        if let Ok(request) = self.poll_request_receiver.recv() {
+            if request
+                .response_sender
+                .send(PollResponse(Poll::Ready(Err(error))))
+                .is_err()
+            {
+                tracing::debug!(
+                    "Wasm guest future cancelled before constructor error was reported"
+                );
             }
         }
     }
@@ -220,7 +247,7 @@ where
     Application: ApplicationRuntimeContext,
 {
     /// The parameters necessary to initialize the future.
-    type Parameters;
+    type Parameters: Send;
 
     /// The output of the guest future.
     type Output;

linera-execution/src/wasm/common.rs
@@ -12,7 +12,6 @@ use crate::{
     ApplicationCallResult, CalleeContext, MessageContext, OperationContext, QueryContext,
     RawExecutionResult, SessionCallResult, SessionId,
 };
-use futures::{future, TryFutureExt};
 use std::future::Future;
 
 /// Types that are specific to the context of an application ready to be executedy by a WebAssembly
@@ -239,13 +238,7 @@ where
     ) -> impl Future<Output = Result<RawExecutionResult<Vec<u8>>, ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.initialize_new(
-            &mut self.store,
-            *context,
-            argument.to_owned(),
-        ))
-        .err_into()
-        .and_then(move |future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::Initialize, A>::spawn((*context, argument.to_owned()), self)
     }
 
     /// Calls the guest Wasm module's implementation of
@@ -267,13 +260,7 @@ where
     ) -> impl Future<Output = Result<RawExecutionResult<Vec<u8>>, ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.execute_operation_new(
-            &mut self.store,
-            *context,
-            operation.to_owned(),
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::ExecuteOperation, A>::spawn((*context, operation.to_owned()), self)
     }
 
     /// Calls the guest Wasm module's implementation of
@@ -295,13 +282,7 @@ where
     ) -> impl Future<Output = Result<RawExecutionResult<Vec<u8>>, ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.execute_message_new(
-            &mut self.store,
-            *context,
-            message.to_owned(),
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::ExecuteMessage, A>::spawn((*context, message.to_owned()), self)
     }
 
     /// Calls the guest Wasm module's implementation of
@@ -325,14 +306,10 @@ where
     ) -> impl Future<Output = Result<ApplicationCallResult, ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.handle_application_call_new(
-            &mut self.store,
-            *context,
-            argument.to_owned(),
-            forwarded_sessions,
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::HandleApplicationCall, A>::spawn(
+            (*context, argument.to_owned(), forwarded_sessions),
+            self,
+        )
     }
 
     /// Calls the guest Wasm module's implementation of
@@ -358,15 +335,15 @@ where
     ) -> impl Future<Output = Result<(SessionCallResult, Vec<u8>), ExecutionError>> {
         A::configure_fuel(&mut self);
 
-        future::ready(self.application.handle_session_call_new(
-            &mut self.store,
-            *context,
-            session_state.to_owned(),
-            argument.to_owned(),
-            forwarded_sessions,
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::HandleSessionCall, A>::spawn(
+            (
+                *context,
+                session_state.to_owned(),
+                argument.to_owned(),
+                forwarded_sessions,
+            ),
+            self,
+        )
     }
 }
 
@@ -387,17 +364,11 @@ where
     /// ) -> Result<Vec<u8>, ExecutionError>
     /// ```
     pub fn handle_query(
-        mut self,
+        self,
         context: &QueryContext,
         argument: &[u8],
     ) -> impl Future<Output = Result<Vec<u8>, ExecutionError>> {
-        future::ready(self.application.handle_query_new(
-            &mut self.store,
-            *context,
-            argument.to_owned(),
-        ))
-        .err_into()
-        .and_then(|future| GuestFutureActor::spawn(future, self))
+        GuestFutureActor::<A::HandleQuery, A>::spawn((*context, argument.to_owned()), self)
     }
 }
 