jvff/linera-protocol-archive

Refactor `impl_guest_future_interface!` macro
Increase verbosity of the macro to make it easier to find and understand
the macro parameters.

linera-execution/src/wasm/guest_futures.rs
@@ -27,7 +27,14 @@ use std::task::Poll;
 /// a `poll_type` that must be convertible into the `output` type wrapped in a
 /// `Poll<Result<_, _>>`.
 macro_rules! impl_guest_future_interface {
-    ( $( $future:ident : $poll_func:ident -> $poll_type:ident -> $trait:ident => $output:ty ),* $(,)* ) => {
+    (
+        $( $future:ident : {
+            application_trait = $trait:ident,
+            poll_function = $poll_func:ident,
+            poll_type = $poll_type:ident,
+            output_type = $output:ty $(,)*
+        } ),* $(,)*
+    ) => {
         $(
             impl<'storage, A> GuestFutureInterface<A> for $future
             where
@@ -57,10 +64,45 @@ macro_rules! impl_guest_future_interface {
 }
 
 impl_guest_future_interface! {
-    Initialize: initialize_poll -> PollExecutionResult -> Contract => RawExecutionResult<Vec<u8>>,
-    ExecuteOperation: execute_operation_poll -> PollExecutionResult -> Contract => RawExecutionResult<Vec<u8>>,
-    ExecuteMessage: execute_message_poll -> PollExecutionResult -> Contract => RawExecutionResult<Vec<u8>>,
-    HandleApplicationCall: handle_application_call_poll -> PollCallApplication -> Contract => ApplicationCallResult,
-    HandleSessionCall: handle_session_call_poll -> PollCallSession -> Contract => (SessionCallResult, Vec<u8>),
-    QueryApplication: query_application_poll -> PollQuery -> Service => Vec<u8>,
+    Initialize: {
+        application_trait = Contract,
+        poll_function = initialize_poll,
+        poll_type = PollExecutionResult,
+        output_type = RawExecutionResult<Vec<u8>>,
+    },
+
+    ExecuteOperation: {
+        application_trait = Contract,
+        poll_function = execute_operation_poll,
+        poll_type = PollExecutionResult,
+        output_type = RawExecutionResult<Vec<u8>>,
+    },
+
+    ExecuteMessage: {
+        application_trait = Contract,
+        poll_function = execute_message_poll,
+        poll_type = PollExecutionResult,
+        output_type = RawExecutionResult<Vec<u8>>,
+    },
+
+    HandleApplicationCall: {
+        application_trait = Contract,
+        poll_function = handle_application_call_poll,
+        poll_type = PollCallApplication,
+        output_type = ApplicationCallResult,
+    },
+
+    HandleSessionCall: {
+        application_trait = Contract,
+        poll_function = handle_session_call_poll,
+        poll_type = PollCallSession,
+        output_type = (SessionCallResult, Vec<u8>),
+    },
+
+    QueryApplication: {
+        application_trait = Service,
+        poll_function = query_application_poll,
+        poll_type = PollQuery,
+        output_type = Vec<u8>,
+    },
 }

linera-io/linera-protocol

linera-execution/src/wasm/guest_futures.rs
@@ -27,7 +27,14 @@ use std::task::Poll;
 /// a `poll_type` that must be convertible into the `output` type wrapped in a
 /// `Poll<Result<_, _>>`.
 macro_rules! impl_guest_future_interface {
-    ( $( $future:ident : $poll_func:ident -> $poll_type:ident -> $trait:ident => $output:ty ),* $(,)* ) => {
+    (
+        $( $future:ident : {
+            application_trait = $trait:ident,
+            poll_function = $poll_func:ident,
+            poll_type = $poll_type:ident,
+            output_type = $output:ty $(,)*
+        } ),* $(,)*
+    ) => {
         $(
             impl<'storage, A> GuestFutureInterface<A> for $future
             where
@@ -57,10 +64,45 @@ macro_rules! impl_guest_future_interface {
 }
 
 impl_guest_future_interface! {
-    Initialize: initialize_poll -> PollExecutionResult -> Contract => RawExecutionResult<Vec<u8>>,
-    ExecuteOperation: execute_operation_poll -> PollExecutionResult -> Contract => RawExecutionResult<Vec<u8>>,
-    ExecuteMessage: execute_message_poll -> PollExecutionResult -> Contract => RawExecutionResult<Vec<u8>>,
-    HandleApplicationCall: handle_application_call_poll -> PollApplicationCallResult -> Contract => ApplicationCallResult,
-    HandleSessionCall: handle_session_call_poll -> PollSessionCallResult -> Contract => (SessionCallResult, Vec<u8>),
-    HandleQuery: handle_query_poll -> PollApplicationQueryResult -> Service => Vec<u8>,
+    Initialize: {
+        application_trait = Contract,
+        poll_function = initialize_poll,
+        poll_type = PollExecutionResult,
+        output_type = RawExecutionResult<Vec<u8>>,
+    },
+
+    ExecuteOperation: {
+        application_trait = Contract,
+        poll_function = execute_operation_poll,
+        poll_type = PollExecutionResult,
+        output_type = RawExecutionResult<Vec<u8>>,
+    },
+
+    ExecuteMessage: {
+        application_trait = Contract,
+        poll_function = execute_message_poll,
+        poll_type = PollExecutionResult,
+        output_type = RawExecutionResult<Vec<u8>>,
+    },
+
+    HandleApplicationCall: {
+        application_trait = Contract,
+        poll_function = handle_application_call_poll,
+        poll_type = PollApplicationCallResult,
+        output_type = ApplicationCallResult,
+    },
+
+    HandleSessionCall: {
+        application_trait = Contract,
+        poll_function = handle_session_call_poll,
+        poll_type = PollSessionCallResult,
+        output_type = (SessionCallResult, Vec<u8>),
+    },
+
+    HandleQuery: {
+        application_trait = Service,
+        poll_function = handle_query_poll,
+        poll_type = PollApplicationQueryResult,
+        output_type = Vec<u8>,
+    },
 }