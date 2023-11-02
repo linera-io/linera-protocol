jvff/linera-protocol-archive

Create a `SendRequestExt` extension trait
Extensions to the `mpsc::UnboundedSender` endpoint to allow sending
requests with a response channel properly set up.

linera-execution/src/wasm/runtime_actor/mod.rs
@@ -7,11 +7,15 @@ mod handlers;
 mod requests;
 mod responses;
 
-use self::handlers::RequestHandler;
 pub use self::requests::{BaseRequest, ContractRequest};
+use self::{handlers::RequestHandler, responses::SyncResponse};
 use crate::ExecutionError;
 use futures::stream::{FuturesUnordered, StreamExt};
-use tokio::{select, sync::mpsc};
+use std::sync::Arc;
+use tokio::{
+    select,
+    sync::{mpsc, oneshot},
+};
 
 /// A handler of application system APIs that runs as a separate actor.
 ///
@@ -62,5 +66,66 @@ where
         }
 
         Ok(())
+    }
+}
+
+/// Extension trait to help with sending requests to [`RuntimeActor`]s.
+///
+/// Prepares a channel for the actor to send a response back to the sender of the request.
+pub trait SendRequestExt<Request> {
+    /// Sends a synchronous request built by `builder`, blocking until the `Response` is received.
+    fn sync_request<Response>(
+        &self,
+        builder: impl FnOnce(Arc<SyncResponse<Response>>) -> Request,
+    ) -> Response
+    where
+        Response: Send;
+
+    /// Sends an asynchronous request built by `builder`, returning a [`oneshot::Receiver`] for
+    /// awaiting the `Response`.
+    fn async_request<Response>(
+        &self,
+        builder: impl FnOnce(oneshot::Sender<Response>) -> Request,
+    ) -> oneshot::Receiver<Response>
+    where
+        Response: Send;
+}
+
+impl<Request> SendRequestExt<Request> for mpsc::UnboundedSender<Request>
+where
+    Request: Send,
+{
+    fn sync_request<Response>(
+        &self,
+        builder: impl FnOnce(Arc<SyncResponse<Response>>) -> Request,
+    ) -> Response
+    where
+        Response: Send,
+    {
+        let response = Arc::new(SyncResponse::default());
+        let request = builder(response.clone());
+
+        self.send(request).unwrap_or_else(|error| {
+            panic!("Failed to send request because receiver has stopped listening: {error}")
+        });
+
+        response.wait()
+    }
+
+    fn async_request<Response>(
+        &self,
+        builder: impl FnOnce(oneshot::Sender<Response>) -> Request,
+    ) -> oneshot::Receiver<Response>
+    where
+        Response: Send,
+    {
+        let (response_sender, response_receiver) = oneshot::channel();
+        let request = builder(response_sender);
+
+        self.send(request).unwrap_or_else(|error| {
+            panic!("Failed to send request because receiver has stopped listening: {error}")
+        });
+
+        response_receiver
     }
 }

linera-io/linera-protocol

linera-execution/src/wasm/runtime_actor/mod.rs
@@ -10,7 +10,7 @@ use self::handlers::RequestHandler;
 pub use self::requests::{BaseRequest, ContractRequest};
 use crate::ExecutionError;
 use futures::{
-    channel::mpsc,
+    channel::{mpsc, oneshot},
     select,
     stream::{FuturesUnordered, StreamExt},
 };
@@ -66,5 +66,41 @@ where
         }
 
         Ok(())
+    }
+}
+
+/// Extension trait to help with sending requests to [`RuntimeActor`]s.
+///
+/// Prepares a channel for the actor to send a response back to the sender of the request.
+pub trait SendRequestExt<Request> {
+    /// Sends a request built by `builder`, returning a [`oneshot::Receiver`] for receiving the
+    /// `Response`.
+    fn send_request<Response>(
+        &self,
+        builder: impl FnOnce(oneshot::Sender<Response>) -> Request,
+    ) -> oneshot::Receiver<Response>
+    where
+        Response: Send;
+}
+
+impl<Request> SendRequestExt<Request> for mpsc::UnboundedSender<Request>
+where
+    Request: Send,
+{
+    fn send_request<Response>(
+        &self,
+        builder: impl FnOnce(oneshot::Sender<Response>) -> Request,
+    ) -> oneshot::Receiver<Response>
+    where
+        Response: Send,
+    {
+        let (response_sender, response_receiver) = oneshot::channel();
+        let request = builder(response_sender);
+
+        self.unbounded_send(request).unwrap_or_else(|error| {
+            panic!("Failed to send request because receiver has stopped listening: {error}")
+        });
+
+        response_receiver
     }
 }