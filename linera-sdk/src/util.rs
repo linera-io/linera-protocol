34a3dd4166504176fd1bde02205760baa8dd831e
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-11-10 18:32:30 +0000

    Test the behavior `yield_once`
    Check that the first poll returns pending and that the second returns
    ready.

--- linera-sdk/src/util.rs
+++ linera-sdk/src/util.rs
@@ -38,3 +38,30 @@ impl Future for YieldOnce {
         }
     }
 }
+
+/// Unit tests for the helpers defined in the `util` module.
+#[cfg(test)]
+mod tests {
+    use super::yield_once;
+    use futures::{task::noop_waker, FutureExt};
+    use std::task::Context;
+
+    /// Tests the behavior of the [`YieldOnce`] future.
+    ///
+    /// Checks the internal state before and after the first and second polls, and ensures that
+    /// only the first poll returns [`Poll::Pending`].
+    #[test]
+    #[allow(clippy::bool_assert_comparison)]
+    fn yield_once_returns_pending_only_on_first_call() {
+        let mut future = yield_once();
+
+        let waker = noop_waker();
+        let mut context = Context::from_waker(&waker);
+
+        assert_eq!(future.yielded, false);
+        assert!(future.poll_unpin(&mut context).is_pending());
+        assert_eq!(future.yielded, true);
+        assert!(future.poll_unpin(&mut context).is_ready());
+        assert_eq!(future.yielded, true);
+    }
+}
