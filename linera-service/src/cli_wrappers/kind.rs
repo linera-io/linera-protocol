8a19a11e618e7f3efa033dc6fcda5aadb113e722
Andre da Silva <andre.dasilva@linera.io>
2023-11-16 20:37:41 -0300

    linera net up --kubernetes multiple validators

--- linera-service/src/cli_wrappers/kind.rs
+++ linera-service/src/cli_wrappers/kind.rs
@@ -11,29 +11,25 @@ pub struct KindCluster {
 }
 
 impl KindCluster {
-    pub async fn new(cluster_id: Option<u32>) -> Result<Self> {
-        let cluster = match cluster_id {
-            Some(id) => Self { id },
-            None => Self {
-                id: Self::get_random_cluster_id(),
-            },
-        };
-
-        cluster.create().await?;
-        Ok(cluster)
-    }
-
     fn get_random_cluster_id() -> u32 {
         rand::thread_rng().gen_range(0..99999)
     }
 
-    async fn create(&self) -> Result<()> {
+    pub async fn create() -> Result<Self> {
+        let cluster = Self {
+            id: Self::get_random_cluster_id(),
+        };
+
         Command::new("kind")
             .args(["create", "cluster"])
-            .args(["--name", self.id.to_string().as_str()])
+            .args(["--name", cluster.id().to_string().as_str()])
             .spawn_and_wait_for_stdout()
             .await?;
-        Ok(())
+        Ok(cluster)
+    }
+
+    pub fn id(&self) -> u32 {
+        self.id
     }
 
     pub async fn delete(&self) -> Result<()> {
