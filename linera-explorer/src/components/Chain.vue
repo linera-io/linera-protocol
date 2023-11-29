3d873ac6ace2dd449ad2334167d1de81bcd1a397
James Kay <james.kay@linera.io>
2023-11-29 13:45:49 +0000

    `linera-explorer`: update for new GraphQL schema

--- linera-explorer/src/components/Chain.vue
+++ linera-explorer/src/components/Chain.vue
@@ -17,42 +17,42 @@ defineProps<{title: string, chain: ChainStateExtendedView}>()
           {{ chain.chainId }}
         </li>
 
-        <li v-if="chain.inboxes.values.length!==0" class="list-group-item d-flex justify-content-between" data-bs-toggle="collapse" :data-bs-target="'#chain-'+chain.chainId+'-inboxes-collapse'">
-          <span><strong>Inboxes</strong> ({{ chain.inboxes.values.length }})</span>
+        <li v-if="chain.inboxes.entries.length!==0" class="list-group-item d-flex justify-content-between" data-bs-toggle="collapse" :data-bs-target="'#chain-'+chain.chainId+'-inboxes-collapse'">
+          <span><strong>Inboxes</strong> ({{ chain.inboxes.entries.length }})</span>
           <i class="bi bi-caret-down-fill"></i>
         </li>
         <li v-else class="list-group-item d-flex justify-content-between">
           <span><strong>Inboxes</strong> (0)</span>
           <span></span>
         </li>
-        <div v-if="chain.inboxes.values.length!==0" class="collapse" :id="'chain-'+chain.chainId+'-inboxes-collapse'">
+        <div v-if="chain.inboxes.entries.length!==0" class="collapse" :id="'chain-'+chain.chainId+'-inboxes-collapse'">
           <ul class="list-group">
-            <li v-for="(inbox, i) in chain.inboxes.values" class="list-group-item p-0" key="'chain-'+chain.chainId+'-inbox-'+i">
+            <li v-for="(inbox, i) in chain.inboxes.entries" class="list-group-item p-0" key="'chain-'+chain.chainId+'-inbox-'+i">
               <div class="card">
                 <div class="card-header">Inbox {{ i+1 }}</div>
                 <div class="card-body">
-                  <Json :data="inbox"/>
+                  <Json :data="inbox.value"/>
                 </div>
               </div>
             </li>
           </ul>
         </div>
 
-        <li v-if="chain.outboxes.values.length!==0" class="list-group-item d-flex justify-content-between" data-bs-toggle="collapse" :data-bs-target="'#chain-'+chain.chainId+'-outboxes-collapse'">
-          <span><strong>Outboxes</strong> ({{ chain.outboxes.values.length }})</span>
+        <li v-if="chain.outboxes.entries.length!==0" class="list-group-item d-flex justify-content-between" data-bs-toggle="collapse" :data-bs-target="'#chain-'+chain.chainId+'-outboxes-collapse'">
+          <span><strong>Outboxes</strong> ({{ chain.outboxes.entries.length }})</span>
           <i class="bi bi-caret-down-fill"></i>
         </li>
         <li v-else class="list-group-item d-flex justify-content-between">
           <span><strong>Outboxes</strong> (0)</span>
           <span></span>
         </li>
-        <div v-if="chain.outboxes.values.length!==0" class="collapse" :id="'chain-'+chain.chainId+'-outboxes-collapse'">
+        <div v-if="chain.outboxes.entries.length!==0" class="collapse" :id="'chain-'+chain.chainId+'-outboxes-collapse'">
           <ul class="list-group">
-            <li v-for="(outbox, i) in chain.outboxes.values" class="list-group-item p-0" key="'chain-'+chain.chainId+'-outbox-'+i">
+            <li v-for="(outbox, i) in chain.outboxes.entries" class="list-group-item p-0" key="'chain-'+chain.chainId+'-outbox-'+i">
               <div class="card">
                 <div class="card-header">Outbox {{ i+1 }}</div>
                 <div class="card-body">
-                  <Json :data="outbox"/>
+                  <Json :data="outbox.value"/>
                 </div>
               </div>
             </li>
