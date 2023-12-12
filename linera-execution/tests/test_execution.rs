de0349546a19b03eccb424a12d2d91a4832bae2b
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2023-12-12 02:19:37 +0000

    Fix panic on user application errors (#1350)
    * Pop app. call stack before propagating errors
    
    Avoid reaching a situation where an assertion fails because the stack
    was not updated when propagating an error, but the error is then
    ignored.
    
    * Fix ignoring of custom user errors
    
    Now the error can occur either in the runtime actor first (if it happens
    in a cross-application call, and then the execution fails because the
    runtime actor doesn't respond) or in normal execution (if it happens in
    while handling the incoming message or the operation).
    
    * Refactor to create a `TestOperation` type
    
    Make it more explicit what the test application should do.
    
    * Forward operation to other methods
    
    Pass the original operation bytes along the call, preparing for a test
    with an empty operation which should fail.
    
    * Test a cross-application call that fails
    
    Ensure the custom user application error is handled correctly.

--- linera-execution/tests/test_execution.rs
+++ linera-execution/tests/test_execution.rs
@@ -9,6 +9,7 @@ use self::utils::create_dummy_user_application_description;
 use linera_base::{
     crypto::PublicKey,
     data_types::BlockHeight,
+    ensure,
     identifiers::{ChainDescription, ChainId, Owner, SessionId},
 };
 use linera_execution::{
@@ -70,6 +71,15 @@ struct TestApplication {
     owner: Owner,
 }
 
+/// A fake operation for the [`TestApplication`] which can be easily "serialized" into a single
+/// byte.
+#[repr(u8)]
+enum TestOperation {
+    Completely,
+    LeakingSession,
+    FailingCrossApplicationCall,
+}
+
 impl UserContract for TestApplication {
     /// Nothing needs to be done during initialization.
     fn initialize(
@@ -85,13 +95,14 @@ impl UserContract for TestApplication {
     /// Extend the application state with the `operation` bytes.
     ///
     /// Calls itself during the operation, opening a session. The session is intentionally
-    /// leaked if the operation is empty.
+    /// leaked if the test operation is [`TestOperation::LeakingSession`].
     fn execute_operation(
         &self,
         context: OperationContext,
         mut runtime_sender: ContractRuntimeSender,
         operation: Vec<u8>,
     ) -> Result<RawExecutionResult<Vec<u8>>, ExecutionError> {
+        assert_eq!(operation.len(), 1);
         // Who we are.
         assert_eq!(context.authenticated_signer, Some(self.owner));
         let app_id = runtime_sender.application_id()?;
@@ -111,12 +122,12 @@ impl UserContract for TestApplication {
         let call_result = runtime_sender.try_call_application(
             /* authenticated */ true,
             app_id,
-            vec![],
+            operation.clone(),
             vec![],
         )?;
         assert_eq!(call_result.value, Vec::<u8>::new());
         assert_eq!(call_result.sessions.len(), 1);
-        if !operation.is_empty() {
+        if operation[0] != TestOperation::LeakingSession as u8 {
             // Call the session to close it.
             let session_id = call_result.sessions[0];
             runtime_sender.try_call_session(
@@ -134,8 +145,9 @@ impl UserContract for TestApplication {
         &self,
         context: MessageContext,
         mut runtime_sender: ContractRuntimeSender,
-        _message: Vec<u8>,
+        message: Vec<u8>,
     ) -> Result<RawExecutionResult<Vec<u8>>, ExecutionError> {
+        assert_eq!(message.len(), 1);
         // Who we are.
         assert_eq!(context.authenticated_signer, Some(self.owner));
         let app_id = runtime_sender.application_id()?;
@@ -145,7 +157,7 @@ impl UserContract for TestApplication {
         runtime_sender.try_call_application(
             /* authenticated */ true,
             app_id,
-            vec![],
+            message,
             vec![],
         )?;
         runtime_sender.unlock()?;
@@ -158,10 +170,15 @@ impl UserContract for TestApplication {
         &self,
         context: CalleeContext,
         _runtime_sender: ContractRuntimeSender,
-        _argument: Vec<u8>,
+        argument: Vec<u8>,
         _forwarded_sessions: Vec<SessionId>,
     ) -> Result<ApplicationCallResult, ExecutionError> {
+        assert_eq!(argument.len(), 1);
         assert_eq!(context.authenticated_signer, Some(self.owner));
+        ensure!(
+            argument[0] != TestOperation::FailingCrossApplicationCall as u8,
+            ExecutionError::UserError("Cross-application call failed".to_owned())
+        );
         Ok(ApplicationCallResult {
             create_sessions: vec![vec![1]],
             ..ApplicationCallResult::default()
@@ -246,7 +263,7 @@ async fn test_simple_user_operation() -> anyhow::Result<()> {
             context,
             Operation::User {
                 application_id: app_id,
-                bytes: vec![1],
+                bytes: vec![TestOperation::Completely as u8],
             },
             &policy,
             &mut tracker,
@@ -281,7 +298,7 @@ async fn test_simple_user_operation() -> anyhow::Result<()> {
         )
         .await
         .unwrap(),
-        Response::User(vec![1])
+        Response::User(vec![TestOperation::Completely as u8])
     );
     Ok(())
 }
@@ -319,7 +336,7 @@ async fn test_simple_user_operation_with_leaking_session() -> anyhow::Result<()>
             context,
             Operation::User {
                 application_id: app_id,
-                bytes: vec![],
+                bytes: vec![TestOperation::LeakingSession as u8],
             },
             &policy,
             &mut tracker,
@@ -327,5 +344,65 @@ async fn test_simple_user_operation_with_leaking_session() -> anyhow::Result<()>
         .await;
 
     assert!(matches!(result, Err(ExecutionError::SessionWasNotClosed)));
+    Ok(())
+}
+
+/// Tests if user application errors when handling cross-application calls are handled correctly.
+///
+/// Sends an operation to the [`TestApplication`] requesting it to fail a cross-application call.
+/// It is then forwarded to the reentrant call, where the cross-application call handler fails and
+/// the execution error should be handled correctly.
+#[tokio::test]
+async fn test_cross_application_error() -> anyhow::Result<()> {
+    let owner = Owner::from(PublicKey::debug(0));
+    let mut state = SystemExecutionState::default();
+    state.description = Some(ChainDescription::Root(0));
+    let mut view =
+        ExecutionStateView::<MemoryContext<TestExecutionRuntimeContext>>::from_system_state(state)
+            .await;
+    let app_desc = create_dummy_user_application_description();
+    let app_id = view
+        .system
+        .registry
+        .register_application(app_desc.clone())
+        .await?;
+    view.context()
+        .extra()
+        .user_contracts()
+        .insert(app_id, Arc::new(TestApplication { owner }));
+    view.context()
+        .extra()
+        .user_services()
+        .insert(app_id, Arc::new(TestApplication { owner }));
+
+    let context = OperationContext {
+        chain_id: ChainId::root(0),
+        height: BlockHeight(0),
+        index: 0,
+        authenticated_signer: Some(owner),
+        next_message_index: 0,
+    };
+    let mut tracker = ResourceTracker::default();
+    let policy = ResourceControlPolicy::default();
+    let result = view
+        .execute_operation(
+            context,
+            Operation::User {
+                application_id: app_id,
+                bytes: vec![TestOperation::FailingCrossApplicationCall as u8],
+            },
+            &policy,
+            &mut tracker,
+        )
+        .await
+        .unwrap();
+    assert_eq!(
+        result,
+        vec![ExecutionResult::User(
+            app_id,
+            RawExecutionResult::default().with_authenticated_signer(Some(owner))
+        ),]
+    );
+
     Ok(())
 }
