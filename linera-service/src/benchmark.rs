e481442b54bbbf0f40dbf26697e088e6c95e4db5
Andreas Fackler <andreas.fackler@linera.io>
2024-01-03 12:24:54 +0100

    Publish bytecode only once for benchmarks.

--- linera-service/src/benchmark.rs
+++ linera-service/src/benchmark.rs
@@ -1,17 +1,21 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use anyhow::Result;
 use clap::Parser as _;
-use fungible::{Account, AccountOwner, FungibleTokenAbi, InitialState};
+use fungible::{Account, AccountOwner, FungibleTokenAbi, InitialState, Parameters};
 use futures::future::join_all;
 use linera_base::{
     async_graphql::InputType,
     data_types::Amount,
     identifiers::{ApplicationId, ChainId, Owner},
 };
+use linera_execution::system::SystemChannel;
 use linera_service::cli_wrappers::{ApplicationWrapper, ClientWrapper, Network};
 use port_selector::random_free_tcp_port;
 use rand::{seq::IteratorRandom as _, SeedableRng};
 use serde_json::Value;
-use std::{collections::BTreeMap, iter, path::Path, sync::Arc};
+use std::{collections::BTreeMap, path::Path, sync::Arc};
 use tempfile::tempdir;
 use tokio::time::Instant;
 
@@ -61,91 +65,91 @@ async fn benchmark_with_fungible(
     seed: u64,
 ) -> Result<()> {
     // Create the clients and initialize the wallets
+    let publisher = ClientWrapper::new(Arc::new(tempdir().unwrap()), Network::Grpc, None, n_users);
+    publisher.wallet_init(&[], Some(&faucet)).await.unwrap();
     let clients = (0..n_users)
         .map(|n| ClientWrapper::new(Arc::new(tempdir().unwrap()), Network::Grpc, None, n))
         .collect::<Vec<_>>();
     join_all(clients.iter().map(|client| async {
         client.wallet_init(&[], Some(&faucet)).await.unwrap();
-        println!("wallet created");
     }))
     .await;
 
     // Sync their balances (sanity check)
     join_all(clients.iter().map(|user| async move {
-        println!(
-            "User {:?} has {}",
-            user.get_owner(),
-            user.synchronize_balance(user.default_chain().unwrap())
-                .await
-                .unwrap()
-        )
+        let chain = user.default_chain().unwrap();
+        let balance = user.synchronize_balance(chain).await.unwrap();
+        println!("User {:?} has {}", user.get_owner(), balance)
     }))
     .await;
 
-    struct BenchmarkContext<'a> {
+    // Start the node services and subscribe to the publisher chain.
+    let publisher_chain_id = publisher.default_chain().unwrap();
+    let services = join_all(clients.iter().map(|client| async move {
+        let free_port = random_free_tcp_port().unwrap();
+        let node_service = client.run_node_service(free_port).await.unwrap();
+        let chain_id = client.default_chain().unwrap();
+        let channel = SystemChannel::PublishedBytecodes;
+        node_service
+            .subscribe(chain_id, publisher_chain_id, channel)
+            .await
+            .unwrap();
+        node_service
+    }))
+    .await;
+
+    // Publish the fungible application bytecode.
+    let path = Path::new("../examples/fungible").canonicalize().unwrap();
+    let (contract, service) = publisher.build_application(&path, "fungible", true).await?;
+    let bytecode_id = publisher.publish_bytecode(contract, service, None).await?;
+
+    struct BenchmarkContext {
         application_id: ApplicationId<FungibleTokenAbi>,
-        client: &'a ClientWrapper,
         owner: Owner,
         default_chain: ChainId,
     }
 
-    // Publish and create the fungible applications
-    let contexts = join_all(clients.iter().enumerate().map(|(i, client)| async move {
-        let initial_state = InitialState {
-            accounts: BTreeMap::from([(
-                AccountOwner::User(client.get_owner().unwrap()),
-                Amount::from_tokens(1_000_000),
-            )]),
-        };
-        let path = Path::new("../examples/fungible").canonicalize().unwrap();
-        let (contract, service) = client
-            .build_application(&path, "fungible", true)
-            .await
-            .unwrap();
-        let parameters = fungible::Parameters::new(format!("FUN{}", i).leak());
-        let application_id = client
-            .publish_and_create::<FungibleTokenAbi>(
-                contract,
-                service,
-                &parameters,
-                &initial_state,
-                &[],
-                None,
-            )
-            .await
-            .unwrap();
-        BenchmarkContext {
-            application_id,
-            client,
-            owner: client.get_owner().unwrap(),
-            default_chain: client.default_chain().unwrap(),
-        }
-    }))
-    .await;
-    for context in &contexts {
-        println!(
-            "User {:?} has published application {:?}",
-            context.owner, context.application_id
-        );
-    }
-
-    // create node services
-    let apps = join_all(contexts.iter().map(|context| async move {
-        let free_port = random_free_tcp_port().unwrap();
-        let node_service = context.client.run_node_service(free_port).await.unwrap();
-        let app = FungibleApp(
-            node_service
-                .make_application(&context.default_chain, &context.application_id)
+    // Create the fungible applications
+    let apps = join_all(clients.iter().zip(services).enumerate().map(
+        |(i, (client, node_service))| async move {
+            let initial_state = InitialState {
+                accounts: BTreeMap::from([(
+                    AccountOwner::User(client.get_owner().unwrap()),
+                    Amount::from_tokens(n_transactions as u128),
+                )]),
+            };
+            let parameters = Parameters::new(format!("FUN{}", i).leak());
+            let application_id = client
+                .create_application::<FungibleTokenAbi>(
+                    &bytecode_id,
+                    &parameters,
+                    &initial_state,
+                    &[],
+                    None,
+                )
                 .await
-                .unwrap(),
-        );
-        (app, context, node_service)
-    }))
+                .unwrap();
+            let owner = client.get_owner().unwrap();
+            let default_chain = client.default_chain().unwrap();
+            let context = BenchmarkContext {
+                application_id,
+                owner,
+                default_chain,
+            };
+            let app = FungibleApp(
+                node_service
+                    .make_application(&context.default_chain, &context.application_id)
+                    .await
+                    .unwrap(),
+            );
+            (app, context, node_service)
+        },
+    ))
     .await;
 
     // create transaction futures
     let total_transactions = n_users * n_transactions;
-    let mut expected_balances = vec![vec![Amount::ZERO; contexts.len()]; clients.len()];
+    let mut expected_balances = vec![vec![Amount::ZERO; apps.len()]; apps.len()];
     let mut rng = rand::rngs::SmallRng::seed_from_u64(seed);
 
     let transaction_futures = (0..n_transactions).flat_map(|_| {
@@ -196,11 +200,10 @@ async fn benchmark_with_fungible(
                             .await
                             .unwrap(),
                     );
-                    app.assert_balances(iter::once((
-                        AccountOwner::User(context.owner),
-                        expected_balance,
-                    )))
-                    .await;
+                    assert_eq!(
+                        app.get_amount(&AccountOwner::User(context.owner)).await,
+                        expected_balance
+                    );
                 },
             ))
         },
@@ -221,16 +224,6 @@ impl FungibleApp {
         let response_body = self.0.query(&query).await.unwrap();
         serde_json::from_value(response_body["accounts"]["entry"]["value"].clone())
             .unwrap_or_default()
-    }
-
-    async fn assert_balances(
-        &self,
-        accounts: impl IntoIterator<Item = (fungible::AccountOwner, Amount)>,
-    ) {
-        for (account_owner, amount) in accounts {
-            let value = self.get_amount(&account_owner).await;
-            assert_eq!(value, amount);
-        }
     }
 
     async fn transfer(
