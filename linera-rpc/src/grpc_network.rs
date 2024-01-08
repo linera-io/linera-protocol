8ecbd608bad0f7883ae21bfa9602d83bb93d89be
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2024-01-08 07:16:19 -0800

    performance fix: do not call RNG for simulating failures when the feature is unused (#1445)

--- linera-rpc/src/grpc_network.rs
+++ linera-rpc/src/grpc_network.rs
@@ -356,7 +356,9 @@ where
         let pool = ConnectionPool::default();
 
         while let Some((cross_chain_request, shard_id)) = receiver.next().await {
-            if rand::thread_rng().gen::<f32>() < cross_chain_sender_failure_rate {
+            if cross_chain_sender_failure_rate > 0.0
+                && rand::thread_rng().gen::<f32>() < cross_chain_sender_failure_rate
+            {
                 warn!("Dropped 1 cross-chain message intentionally.");
                 continue;
             }
