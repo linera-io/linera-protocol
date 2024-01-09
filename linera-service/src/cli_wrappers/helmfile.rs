908689eb868a9172e522d390b3426e4fba89e796
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2024-01-09 00:58:12 +0000

    Create new temporary directory for every Helmfile deployment

--- linera-service/src/cli_wrappers/helmfile.rs
+++ linera-service/src/cli_wrappers/helmfile.rs
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::util::CommandExt;
-use anyhow::{Context, Result};
-use pathdiff::diff_paths;
+use anyhow::Result;
+use fs_extra::dir::CopyOptions;
 use std::path::Path;
 use tokio::process::Command;
 
@@ -11,29 +11,23 @@ pub struct HelmFile;
 
 impl HelmFile {
     pub async fn sync(
-        config_dir: &Path,
         server_config_id: usize,
         github_root: &Path,
         num_shards: usize,
         cluster_id: u32,
     ) -> Result<()> {
-        let execution_dir = format!("{}/kubernetes/linera-validator", github_root.display());
-        let config_dir = diff_paths(config_dir, execution_dir.clone())
-            .context("failed to get relative path")?
-            .to_str()
-            .context("failed to convert relative path to string")?
-            .to_string();
+        let chart_dir = format!("{}/kubernetes/linera-validator", github_root.display());
+
+        let temp_dir = tempfile::tempdir()?;
+        fs_extra::copy_items(&[&chart_dir], temp_dir.path(), &CopyOptions::new())?;
 
         Command::new("helmfile")
-            .current_dir(&execution_dir)
+            .current_dir(&temp_dir.path().join("linera-validator"))
             .env(
                 "LINERA_HELMFILE_SET_SERVER_CONFIG",
-                &format!("{config_dir}/server_{server_config_id}.json"),
+                &format!("working/server_{server_config_id}.json"),
             )
-            .env(
-                "LINERA_HELMFILE_SET_GENESIS_CONFIG",
-                &format!("{config_dir}/genesis.json"),
-            )
+            .env("LINERA_HELMFILE_SET_GENESIS_CONFIG", "working/genesis.json")
             .env("LINERA_HELMFILE_SET_NUM_SHARDS", num_shards.to_string())
             .arg("sync")
             .arg("--wait")
