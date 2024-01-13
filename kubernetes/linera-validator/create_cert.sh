423594e043b5a087098722de237c761e23f1c5c0
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2024-01-13 17:26:59 +0000

    Removed create_cert script

--- kubernetes/linera-validator/create_cert.sh
+++
@@ -1,7 +0,0 @@
- openssl req -x509 \
-  -newkey rsa:4096 \
-  -nodes \
-  -keyout key.pem \
-  -out cert.pem \
-  -days 365 \
-  -addext "subjectAltName = DNS:scylla-operator-webhook.scylla-operator.svc"
