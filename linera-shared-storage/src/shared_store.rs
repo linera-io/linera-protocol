b86e2195975f68c40870ff112973b5852bc2eb5f
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-01-14 21:50:36 +0100

    Some corrections.

--- linera-shared-storage/src/shared_store.rs
+++ linera-shared-storage/src/shared_store.rs
@@ -4,6 +4,21 @@ use linera_views::common::CommonStoreConfig;
 //use std::process;
 use linera_views::memory::MemoryStore;
 use linera_views::memory::create_memory_store;
+use crate::key_value_store::store_processor_server::StoreProcessorServer;
+use crate::key_value_store::store_processor_server::StoreProcessor;
+use crate::key_value_store::StoreRequest;
+use crate::key_value_store::StoreResponse;
+use crate::key_value_store::store_request::Pattern::ReadKey;
+use crate::key_value_store::store_request::Pattern::FindKeysByPrefix;
+use crate::key_value_store::store_request::Pattern::FindKeyValuesByPrefix;
+use crate::key_value_store::store_request::Pattern::ReadMultiKeys;
+use crate::key_value_store::store_request::Pattern::WriteBatch;
+use crate::key_value_store::store_request::Pattern::ContainsKey;
+use crate::key_value_store::store_request::Pattern::ClearJournal;
+
+use crate::key_value_store::store_response::Pattern::Unit;
+
+
 
 #[allow(clippy::derive_partial_eq_without_eq)]
 // https://github.com/hyperium/tonic/issues/1056
@@ -11,9 +26,9 @@ pub mod key_value_store {
     tonic::include_proto!("key_value_store.v1");
 }
 
-use key_value_store::{
-    StoreProcessorServer,
-};
+//use key_value_store::{
+//    StoreProcessorServer,
+//};
 
 pub struct Storage {
     memory_store: MemoryStore,
@@ -31,31 +46,31 @@ struct SharedStoreOptions {
 
 #[tonic::async_trait]
 impl StoreProcessor for Storage {
-    async fn process(
+    async fn store_process(
         &self,
         request: Request<StoreRequest>,
     ) -> Result<Response<StoreResponse>, Status> {
         let response = match request {
             ReadKey(read_key) => {
-                StoreResponse { clear_journal: ClearJournal { } }
+                StoreResponse { clear_journal: Unit { } }
             },
             ContainsKey(contains_key) => {
-                StoreResponse { clear_journal: ClearJournal { } }
+                StoreResponse { clear_journal: Unit { } }
             },
             ReadMultiKeys(read_multi_keys) => {
-                StoreResponse { clear_journal: ClearJournal { } }
+                StoreResponse { clear_journal: Unit { } }
             },
             FindKeysByPrefix(find_keys_by_prefix) => {
-                StoreResponse { clear_journal: ClearJournal { } }
+                StoreResponse { clear_journal: Unit { } }
             },
             FindKeyValuesByPrefix(find_key_values_by_prefix) => {
-                StoreResponse { clear_journal: ClearJournal { } }
+                StoreResponse { clear_journal: Unit { } }
             },
             WriteBatch(write_batch) => {
-                StoreResponse { clear_journal: ClearJournal { } }
+                StoreResponse { clear_journal: Unit { } }
             },
             ClearJournal(clear_journal) => {
-                StoreResponse { clear_journal: ClearJournal { } }
+                StoreResponse { clear_journal: Unit { } }
             }
         };
         Ok(Response::new(response))
@@ -93,7 +108,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Server::builder()
         .add_service(StoreProcessorServer::new(storage))
-        .serve(addr)
+        .serve(endpoint)
         .await?;
 
     Ok(())
