71d109604ae883a71898eab8a52dd10c2a010430
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-01-16 14:21:11 +0100

    Addition of the "try_load_entry" to the test code.

--- linera-views/tests/collection_tests.rs
+++ linera-views/tests/collection_tests.rs
@@ -48,7 +48,7 @@ async fn classic_collection_view_check() {
         let count_oper = rng.gen_range(0..25);
         let mut new_map = map.clone();
         for _ in 0..count_oper {
-            let choice = rng.gen_range(0..7);
+            let choice = rng.gen_range(0..6);
             if choice == 0 {
                 // deleting random stuff
                 let pos = rng.gen_range(0..nmax);
@@ -84,12 +84,12 @@ async fn classic_collection_view_check() {
                     new_map.insert(pos, 0);
                 }
             }
-            if choice == 5 {
+            if choice == 4 {
                 // Doing the clearing
                 view.clear();
                 new_map.clear();
             }
-            if choice == 6 {
+            if choice == 5 {
                 // Doing the rollback
                 view.rollback();
                 new_map = map.clone();
@@ -100,6 +100,13 @@ async fn classic_collection_view_check() {
                 assert_eq!(new_hash, hash);
             } else {
                 assert_ne!(new_hash, hash);
+            }
+            // Checking the behavior of "try_load_entry"
+            for _ in 0..10 {
+                let pos = rng.gen::<u8>();
+                let test_view = !view.v.try_load_entry(&pos).await.unwrap().is_none();
+                let test_map = new_map.contains_key(&pos);
+                assert_eq!(test_view, test_map);
             }
             // Checking the keys
             let key_values = view.key_values().await;
