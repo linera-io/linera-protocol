db784d495e7db6d7e846c3cf64d63e33246299b0
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-01-17 11:46:46 +0100

    Some changes.

--- linera-shared-storage/src/common.rs
+++ linera-shared-storage/src/common.rs
@@ -1,9 +1,22 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use tonic::Status;
 
+use tonic::Status;
 use thiserror::Error;
+use linera_views::common::CommonStoreConfig;
+
+
+/// The shared store is potentially handling an infinite number of connections.
+/// However, for testing or some other purpose we really need to decrease the number of
+/// connections.
+#[cfg(any(test, feature = "test"))]
+const TEST_SHARED_STORE_MAX_CONCURRENT_QUERIES: usize = 10;
+
+/// The number of entries in a stream of the tests can be controlled by this parameter for tests.
+#[cfg(any(test, feature = "test"))]
+const TEST_SHARED_STORE_MAX_STREAM_QUERIES: usize = 10;
+
 
 #[derive(Debug, Error)]
 pub enum SharedContextError {
@@ -18,4 +31,13 @@ pub enum SharedContextError {
     /// Transport error
     #[error(transparent)]
     TransportError(#[from] tonic::transport::Error),
+}
+
+#[cfg(any(test, feature = "test"))]
+pub fn create_shared_store_common_config() -> CommonStoreConfig {
+    CommonStoreConfig {
+        max_concurrent_queries: Some(TEST_SHARED_STORE_MAX_CONCURRENT_QUERIES),
+        max_stream_queries: TEST_SHARED_STORE_MAX_STREAM_QUERIES,
+        cache_size: usize::MAX,
+    }
 }
