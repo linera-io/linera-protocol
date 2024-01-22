f5baf9257b6fd227d457648dee42581d35a23774
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-01-22 09:57:18 +0100

    Handle the cargo rdme error.

--- linera-shared-storage/src/lib.rs
+++ linera-shared-storage/src/lib.rs
@@ -1,6 +1,8 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+//! This module provides the executables needed to provides a shared RocksDB/memory KeyValueStore server. It also provides the client functionality and tests them.
+
 #[allow(clippy::derive_partial_eq_without_eq)]
 // https://github.com/hyperium/tonic/issues/1056
 pub mod key_value_store {
