479297f91ff617b6f776e317a9f7b15dcaeaf89c
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2024-01-23 23:19:41 -0800

    support funding from authenticated accounts and native tokens possibly changed from user apps

--- linera-execution/src/resources.rs
+++ linera-execution/src/resources.rs
@@ -57,11 +57,9 @@ pub struct ResourceTracker {
 pub trait BalanceHolder {
     fn as_amount(&self) -> Amount;
 
-    fn as_amount_mut(&mut self) -> &mut Amount;
+    fn try_add_assign(&mut self, other: Amount) -> Result<(), ArithmeticError>;
 
-    fn try_sub_assign(&mut self, other: Amount) -> Result<(), ArithmeticError> {
-        self.as_amount_mut().try_sub_assign(other)
-    }
+    fn try_sub_assign(&mut self, other: Amount) -> Result<(), ArithmeticError>;
 }
 
 // The main accounting functions for a ResourceController.
@@ -71,20 +69,31 @@ where
     Tracker: AsMut<ResourceTracker>,
 {
     /// Obtains the balance of the account.
-    pub fn balance(&mut self) -> Amount {
+    pub fn balance(&self) -> Amount {
         self.account.as_amount()
     }
 
-    /// Obtains a mutable reference on the balance of the account.
-    pub fn balance_mut(&mut self) -> &mut Amount {
-        self.account.as_amount_mut()
+    /// Operates a 3-way merge by transferring the difference between `initial`
+    /// and `other` to `self`.
+    pub fn merge_balance(&mut self, initial: Amount, other: Amount) -> Result<(), ExecutionError> {
+        if other <= initial {
+            self.account
+                .try_sub_assign(initial.try_sub(other).expect("other <= initial"))
+                .map_err(|_| SystemExecutionError::InsufficientFunding {
+                    current_balance: self.balance(),
+                })?;
+        } else {
+            self.account
+                .try_add_assign(other.try_sub(initial).expect("other > initial"))?;
+        }
+        Ok(())
     }
 
     /// Subtracts an amount from a balance and reports an error if that is impossible.
     fn update_balance(&mut self, fees: Amount) -> Result<(), ExecutionError> {
         self.account.try_sub_assign(fees).map_err(|_| {
             SystemExecutionError::InsufficientFunding {
-                current_balance: self.account.as_amount(),
+                current_balance: self.balance(),
             }
         })?;
         Ok(())
@@ -92,7 +101,7 @@ where
 
     /// Obtains the amount of fuel that could be spent by consuming the entire balance.
     pub(crate) fn remaining_fuel(&self) -> u64 {
-        self.policy.remaining_fuel(self.account.as_amount())
+        self.policy.remaining_fuel(self.balance())
     }
 
     /// Tracks the creation of a block.
@@ -239,8 +248,12 @@ impl BalanceHolder for Amount {
         *self
     }
 
-    fn as_amount_mut(&mut self) -> &mut Amount {
-        self
+    fn try_add_assign(&mut self, other: Amount) -> Result<(), ArithmeticError> {
+        self.try_add_assign(other)
+    }
+
+    fn try_sub_assign(&mut self, other: Amount) -> Result<(), ArithmeticError> {
+        self.try_sub_assign(other)
     }
 }
 
@@ -267,6 +280,34 @@ pub struct OwnedView<'a, C> {
     view: &'a mut ExecutionStateView<C>,
 }
 
+impl<'a, C> OwnedView<'a, C>
+where
+    C: Context + Clone + Send + Sync + 'static,
+    ViewError: From<C::Error>,
+{
+    fn get_owner_balance(&self, owner: &Owner) -> Amount {
+        self.view
+            .system
+            .balances
+            .get(owner)
+            .now_or_never()
+            .expect("The map entry was previously loaded by OwnedView::with")
+            .expect("Account was created there as well")
+            .expect("No I/O can fail here")
+    }
+
+    fn get_owner_balance_mut(&mut self, owner: &Owner) -> &mut Amount {
+        self.view
+            .system
+            .balances
+            .get_mut(owner)
+            .now_or_never()
+            .expect("The map entry was previously loaded by OwnedView::with")
+            .expect("Account was created there as well")
+            .expect("No I/O can fail here")
+    }
+}
+
 impl<C> BalanceHolder for OwnedView<'_, C>
 where
     C: Context + Clone + Send + Sync + 'static,
@@ -278,27 +319,44 @@ where
             Some(owner) => self
                 .view
                 .system
-                .balances
-                .get(owner)
-                .now_or_never()
-                .expect("The map entry was previously loaded by OwnedView::with")
-                .expect("Account was created there as well")
-                .expect("No I/O can fail here"),
+                .balance
+                .get()
+                .try_add(self.get_owner_balance(owner))
+                .expect("Overflow was tested in `ResourceController::with` and `add_assign`"),
         }
     }
 
-    fn as_amount_mut(&mut self) -> &mut Amount {
-        match &self.owner {
-            None => self.view.system.balance.get_mut(),
-            Some(owner) => self
-                .view
-                .system
-                .balances
-                .get_mut(owner)
-                .now_or_never()
-                .expect("The map entry was previously loaded by OwnedView::with")
-                .expect("Account was created there as well")
-                .expect("No I/O can fail here"),
+    fn try_add_assign(&mut self, other: Amount) -> Result<(), ArithmeticError> {
+        match self.owner {
+            None => self.view.system.balance.get_mut().try_add_assign(other),
+            Some(owner) => {
+                let balance = self.get_owner_balance_mut(&owner);
+                balance.try_add_assign(other)?;
+                // Safety check. (See discussion below in `ResourceController::with`).
+                balance.try_add(*self.view.system.balance.get())?;
+                Ok(())
+            }
+        }
+    }
+
+    fn try_sub_assign(&mut self, other: Amount) -> Result<(), ArithmeticError> {
+        match self.owner {
+            None => self.view.system.balance.get_mut().try_sub_assign(other),
+            Some(owner) => {
+                // Charge the owner's account first, then the chain's account for the
+                // reminder.
+                if self
+                    .get_owner_balance_mut(&owner)
+                    .try_sub_assign(other)
+                    .is_err()
+                {
+                    let balance = self.get_owner_balance(&owner);
+                    let delta = other.try_sub(balance).expect("balance < other");
+                    self.view.system.balance.get_mut().try_sub_assign(delta)?;
+                    *self.get_owner_balance_mut(&owner) = Amount::ZERO;
+                }
+                Ok(())
+            }
         }
     }
 }
@@ -317,7 +375,11 @@ impl ResourceController<Option<Owner>, ResourceTracker> {
         if let Some(owner) = &self.account {
             // Make sure `owner` has an account and that the account is loaded in memory.
             let balance = view.system.balances.get_mut(owner).await?;
-            if balance.is_none() {
+            if let Some(balance) = balance {
+                // Making sure the sum doesn't overflow. In practice, though, the total
+                // supply of tokens is known so this should never happen.
+                view.system.balance.get().try_add(*balance)?;
+            } else {
                 view.system.balances.insert(owner, Amount::ZERO)?;
             }
         }
