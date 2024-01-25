jvff/linera-protocol-archive

Use `MeasureLatency` to measure latencies
Replace the `run_with_execution_time_metric` helper function.

linera-views/src/metering.rs
@@ -7,9 +7,8 @@ use crate::{
 };
 use async_trait::async_trait;
 use convert_case::{Case, Casing};
-use linera_base::sync::Lazy;
+use linera_base::{prometheus_util::MeasureLatency, sync::Lazy};
 use prometheus::{register_histogram_vec, HistogramVec};
-use std::{future::Future, time::Instant};
 
 #[derive(Clone)]
 /// The implementation of the `KeyValueStoreMetrics` for the `KeyValueStore`.
@@ -113,18 +112,6 @@ pub struct MeteredStore<K> {
     pub store: K,
 }
 
-async fn run_with_execution_time_metric<F, O>(f: F, hist: &HistogramVec) -> O
-where
-    F: Future<Output = O>,
-{
-    let start = Instant::now();
-    let out = f.await;
-    let duration = start.elapsed();
-    hist.with_label_values(&[])
-        .observe(duration.as_micros() as f64);
-    out
-}
-
 #[async_trait]
 impl<K, E> ReadableKeyValueStore<E> for MeteredStore<K>
 where
@@ -139,40 +126,28 @@ where
     }
 
     async fn read_value_bytes(&self, key: &[u8]) -> Result<Option<Vec<u8>>, E> {
-        run_with_execution_time_metric(
-            self.store.read_value_bytes(key),
-            &self.counter.read_value_bytes,
-        )
-        .await
+        let _latency = self.counter.read_value_bytes.measure_latency();
+        self.store.read_value_bytes(key).await
     }
 
     async fn contains_key(&self, key: &[u8]) -> Result<bool, E> {
-        run_with_execution_time_metric(self.store.contains_key(key), &self.counter.contains_key)
-            .await
+        let _latency = self.counter.contains_key.measure_latency();
+        self.store.contains_key(key).await
     }
 
     async fn read_multi_values_bytes(&self, keys: Vec<Vec<u8>>) -> Result<Vec<Option<Vec<u8>>>, E> {
-        run_with_execution_time_metric(
-            self.store.read_multi_values_bytes(keys),
-            &self.counter.read_multi_values_bytes,
-        )
-        .await
+        let _latency = self.counter.read_multi_values_bytes.measure_latency();
+        self.store.read_multi_values_bytes(keys).await
     }
 
     async fn find_keys_by_prefix(&self, key_prefix: &[u8]) -> Result<Self::Keys, E> {
-        run_with_execution_time_metric(
-            self.store.find_keys_by_prefix(key_prefix),
-            &self.counter.find_keys_by_prefix,
-        )
-        .await
+        let _latency = self.counter.find_keys_by_prefix.measure_latency();
+        self.store.find_keys_by_prefix(key_prefix).await
     }
 
     async fn find_key_values_by_prefix(&self, key_prefix: &[u8]) -> Result<Self::KeyValues, E> {
-        run_with_execution_time_metric(
-            self.store.find_key_values_by_prefix(key_prefix),
-            &self.counter.find_key_values_by_prefix,
-        )
-        .await
+        let _latency = self.counter.find_key_values_by_prefix.measure_latency();
+        self.store.find_key_values_by_prefix(key_prefix).await
     }
 }
 
@@ -184,19 +159,13 @@ where
     const MAX_VALUE_SIZE: usize = K::MAX_VALUE_SIZE;
 
     async fn write_batch(&self, batch: Batch, base_key: &[u8]) -> Result<(), E> {
-        run_with_execution_time_metric(
-            self.store.write_batch(batch, base_key),
-            &self.counter.write_batch,
-        )
-        .await
+        let _latency = self.counter.write_batch.measure_latency();
+        self.store.write_batch(batch, base_key).await
     }
 
     async fn clear_journal(&self, base_key: &[u8]) -> Result<(), E> {
-        run_with_execution_time_metric(
-            self.store.clear_journal(base_key),
-            &self.counter.clear_journal,
-        )
-        .await
+        let _latency = self.counter.clear_journal.measure_latency();
+        self.store.clear_journal(base_key).await
     }
 }
 

linera-io/linera-protocol

linera-views/src/metering.rs
@@ -7,9 +7,8 @@ use crate::{
 };
 use async_trait::async_trait;
 use convert_case::{Case, Casing};
-use linera_base::sync::Lazy;
+use linera_base::{prometheus_util::MeasureLatency, sync::Lazy};
 use prometheus::{register_histogram_vec, HistogramVec};
-use std::{future::Future, time::Instant};
 
 #[derive(Clone)]
 /// The implementation of the `KeyValueStoreMetrics` for the `KeyValueStore`.
@@ -113,18 +112,6 @@ pub struct MeteredStore<K> {
     pub store: K,
 }
 
-async fn run_with_execution_time_metric<F, O>(f: F, hist: &HistogramVec) -> O
-where
-    F: Future<Output = O>,
-{
-    let start = Instant::now();
-    let out = f.await;
-    let duration = start.elapsed();
-    hist.with_label_values(&[])
-        .observe(duration.as_micros() as f64);
-    out
-}
-
 #[async_trait]
 impl<K, E> ReadableKeyValueStore<E> for MeteredStore<K>
 where
@@ -139,40 +126,28 @@ where
     }
 
     async fn read_value_bytes(&self, key: &[u8]) -> Result<Option<Vec<u8>>, E> {
-        run_with_execution_time_metric(
-            self.store.read_value_bytes(key),
-            &self.counter.read_value_bytes,
-        )
-        .await
+        let _metric = self.counter.read_value_bytes.measure_latency();
+        self.store.read_value_bytes(key).await
     }
 
     async fn contains_key(&self, key: &[u8]) -> Result<bool, E> {
-        run_with_execution_time_metric(self.store.contains_key(key), &self.counter.contains_key)
-            .await
+        let _metric = self.counter.contains_key.measure_latency();
+        self.store.contains_key(key).await
     }
 
     async fn read_multi_values_bytes(&self, keys: Vec<Vec<u8>>) -> Result<Vec<Option<Vec<u8>>>, E> {
-        run_with_execution_time_metric(
-            self.store.read_multi_values_bytes(keys),
-            &self.counter.read_multi_values_bytes,
-        )
-        .await
+        let _metric = self.counter.read_multi_values_bytes.measure_latency();
+        self.store.read_multi_values_bytes(keys).await
     }
 
     async fn find_keys_by_prefix(&self, key_prefix: &[u8]) -> Result<Self::Keys, E> {
-        run_with_execution_time_metric(
-            self.store.find_keys_by_prefix(key_prefix),
-            &self.counter.find_keys_by_prefix,
-        )
-        .await
+        let _metric = self.counter.find_keys_by_prefix.measure_latency();
+        self.store.find_keys_by_prefix(key_prefix).await
     }
 
     async fn find_key_values_by_prefix(&self, key_prefix: &[u8]) -> Result<Self::KeyValues, E> {
-        run_with_execution_time_metric(
-            self.store.find_key_values_by_prefix(key_prefix),
-            &self.counter.find_key_values_by_prefix,
-        )
-        .await
+        let _metric = self.counter.find_key_values_by_prefix.measure_latency();
+        self.store.find_key_values_by_prefix(key_prefix).await
     }
 }
 
@@ -184,19 +159,13 @@ where
     const MAX_VALUE_SIZE: usize = K::MAX_VALUE_SIZE;
 
     async fn write_batch(&self, batch: Batch, base_key: &[u8]) -> Result<(), E> {
-        run_with_execution_time_metric(
-            self.store.write_batch(batch, base_key),
-            &self.counter.write_batch,
-        )
-        .await
+        let _metric = self.counter.write_batch.measure_latency();
+        self.store.write_batch(batch, base_key).await
     }
 
     async fn clear_journal(&self, base_key: &[u8]) -> Result<(), E> {
-        run_with_execution_time_metric(
-            self.store.clear_journal(base_key),
-            &self.counter.clear_journal,
-        )
-        .await
+        let _metric = self.counter.clear_journal.measure_latency();
+        self.store.clear_journal(base_key).await
     }
 }
 