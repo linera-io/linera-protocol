6aded63f2bc2135ed12b455338fd14c3366ca416
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-02-01 16:29:41 +0100

    Update linera-views/src/rocks_db.rs
    Co-authored-by: Andreas Fackler <afck@users.noreply.github.com>
    Signed-off-by: Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>

--- linera-views/src/rocks_db.rs
+++ linera-views/src/rocks_db.rs
@@ -372,7 +372,7 @@ pub fn create_rocks_db_common_config() -> CommonStoreConfig {
 }
 
 /// Creates a RocksDB database client to be used for tests.
-/// The tempoorary directory has to be carried because if it goes
+/// The temporary directory has to be carried because if it goes
 /// out of scope then the RocksDB client can become unstable.
 #[cfg(any(test, feature = "test"))]
 pub async fn create_rocks_db_test_store() -> (RocksDbStore, TempDir) {
