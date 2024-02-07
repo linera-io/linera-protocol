fe4bc229b1cacf321033ff26a59a08ad345a7665
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-02-07 20:55:10 +0100

    Simplification of the initialize code.

--- linera-storage/src/db_storage.rs
+++ linera-storage/src/db_storage.rs
@@ -467,7 +467,14 @@ impl<Client> DbStorage<Client, WallClock>
 where
     Client: KeyValueStore + AdminKeyValueStore<<Client as KeyValueStore>::Error> + Clone + Send + Sync + 'static,
     ViewError: From<<Client as KeyValueStore>::Error>,
-    <Client as KeyValueStore>::Error: From<bcs::Error> + Send + Sync + serde::ser::StdError,
+    <Client as KeyValueStore>::Error: From<bcs::Error> + From<DatabaseConsistencyError> + Send + Sync + serde::ser::StdError,
 {
-
+    pub async fn initialize(
+	store_config: Client::Config,
+	namespace: &str,
+	wasm_runtime: Option<WasmRuntime>,
+    ) -> Result<Self, <Client as KeyValueStore>::Error> {
+	let storage = DbStorageInner::<Client>::initialize(store_config, namespace, wasm_runtime).await?;
+	Ok(Self::create(storage, WallClock))
+    }
 }
