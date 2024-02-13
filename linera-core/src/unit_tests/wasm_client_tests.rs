8464d2ac8c1f64e78a326f6579a89cc1e7327498
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2024-02-12 21:15:33 -0800

    activate fuel grant in test

--- linera-core/src/unit_tests/wasm_client_tests.rs
+++ linera-core/src/unit_tests/wasm_client_tests.rs
@@ -270,7 +270,8 @@ where
         .unwrap()
         .unwrap();
 
-    let operation = meta_counter::Operation::increment(receiver_id, 5);
+    let mut operation = meta_counter::Operation::increment(receiver_id, 5);
+    operation.fuel_grant = 1000000;
     let cert = creator
         .execute_operation(Operation::user(application_id2, &operation)?)
         .await
@@ -336,9 +337,21 @@ where
     assert_eq!(certs.len(), 1);
     let cert = certs.pop().unwrap();
     let incoming_messages = &cert.value().block().unwrap().incoming_messages;
-    assert_eq!(incoming_messages.len(), 1);
+    assert_eq!(incoming_messages.len(), 2);
+    // First message is the grant refund for the successful message sent before.
     assert_eq!(incoming_messages[0].action, MessageAction::Accept);
-    assert_eq!(incoming_messages[0].event.kind, MessageKind::Bouncing);
+    assert_eq!(incoming_messages[0].event.kind, MessageKind::Tracked);
+    assert!(matches!(
+        incoming_messages[0].event.message,
+        Message::System(SystemMessage::Credit { .. })
+    ));
+    // Second message is the bounced message.
+    assert_eq!(incoming_messages[1].action, MessageAction::Accept);
+    assert_eq!(incoming_messages[1].event.kind, MessageKind::Bouncing);
+    assert!(matches!(
+        incoming_messages[1].event.message,
+        Message::User { .. }
+    ));
 
     Ok(())
 }
