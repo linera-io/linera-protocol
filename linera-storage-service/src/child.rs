e47752b28c63c2da334322b1ee3d2790789508c1
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-02-15 09:46:21 +0100

    Change the loop structure to the one of other such iteration in the code.

--- linera-storage-service/src/child.rs
+++ linera-storage-service/src/child.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::client::storage_service_check_endpoint;
-use anyhow::Result;
+use anyhow::{bail, Result};
 use linera_service::util::CommandExt;
 use std::time::Duration;
 use tokio::process::{Child, Command};
@@ -27,26 +27,25 @@ impl StorageServiceSpanner {
         Self { endpoint, binary }
     }
 
-    async fn command(&self) -> Result<Command> {
+    async fn command(&self) -> Command {
         let mut command = Command::new(&self.binary);
         command.args(["memory", "--endpoint", &self.endpoint]);
         command.kill_on_drop(true);
-        Ok(command)
+        command
     }
     pub async fn run_service(&self) -> Result<StorageServiceGuard> {
-        let mut command = self.command().await?;
+        let mut command = self.command().await;
         let _child = command.spawn_into()?;
         let guard = StorageServiceGuard { _child };
         // We iterate until the child is spanned and can be accessed.
         // We add an additional waiting period to avoid problems.
-        let mut wait_period = 1;
-        loop {
+        for i in 1..10 {
             let result = storage_service_check_endpoint(self.endpoint.clone()).await;
             if result.is_ok() {
                 return Ok(guard);
             }
-            tokio::time::sleep(Duration::from_secs(wait_period)).await;
-            wait_period += 1;
+            tokio::time::sleep(Duration::from_secs(i)).await;
         }
+        bail!("Failed to start child server");
     }
 }
