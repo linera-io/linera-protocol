528de1f7756c04c5e8370ce43a0f0ed2c19a4f6c
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-03-09 14:29:41 +0000

    Remove `SessionId` type
    Complete removal of sessions.

--- linera-sdk/src/lib.rs
+++ linera-sdk/src/lib.rs
@@ -66,7 +66,7 @@ pub use self::{
     log::{ContractLogger, ServiceLogger},
     service::{ServiceRuntime, ServiceStateStorage},
 };
-pub use linera_base::{abi, data_types::Resources, ensure, identifiers::SessionId};
+pub use linera_base::{abi, data_types::Resources, ensure};
 #[doc(hidden)]
 pub use wit_bindgen_guest_rust;
 
