4d4c55279e4f287e731552f30feb81a5284d3358
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-03-11 18:01:04 +0100

    Formatting corrections.

--- linera-service/src/cli_wrappers/local_net.rs
+++ linera-service/src/cli_wrappers/local_net.rs
@@ -29,18 +29,15 @@ use tonic_health::pb::{
 };
 use tracing::{info, warn};
 
-use linera_base::sync::Lazy;
 use async_lock::RwLock;
+use linera_base::sync::Lazy;
+use linera_storage_service::{
+    child::get_free_port, client::service_config_from_endpoint, common::ServiceStoreConfig,
+};
 use std::ops::Deref;
-use linera_storage_service::child::get_free_port;
-use linera_storage_service::client::service_config_from_endpoint;
-use linera_storage_service::common::ServiceStoreConfig;
 
 #[cfg(feature = "rocksdb")]
-use {
-    linera_views::rocks_db::{create_rocks_db_test_config, RocksDbStoreConfig},
-};
-
+use linera_views::rocks_db::{create_rocks_db_test_config, RocksDbStoreConfig};
 
 struct LocalServerInternal {
     pub service_config: ServiceStoreConfig,
@@ -71,20 +68,31 @@ impl LocalServerInternal {
     pub async fn new() -> Result<Self> {
         let (service_config, _service_guard) = Self::service_info().await?;
         let (rocks_db_config, _temp_dir) = create_rocks_db_test_config().await;
-        Ok(Self { service_config, _service_guard, rocks_db_config, _temp_dir })
+        Ok(Self {
+            service_config,
+            _service_guard,
+            rocks_db_config,
+            _temp_dir,
+        })
     }
 
     #[cfg(not(feature = "rocksdb"))]
     pub async fn new() -> Result<Self> {
         let (service_config, _service_guard) = Self::service_info().await?;
-        Ok(Self { service_config, _service_guard })
+        Ok(Self {
+            service_config,
+            _service_guard,
+        })
     }
 
     #[cfg(feature = "rocksdb")]
     pub fn get_config(&self) -> LocalServerConfig {
         let service_config = self.service_config.clone();
         let rocks_db_config = self.rocks_db_config.clone();
-        LocalServerConfig { service_config, rocks_db_config }
+        LocalServerConfig {
+            service_config,
+            rocks_db_config,
+        }
     }
 
     #[cfg(not(feature = "rocksdb"))]
@@ -98,9 +106,17 @@ pub struct LocalServer {
     internal_server: RwLock<Option<LocalServerInternal>>,
 }
 
+impl Default for LocalServer {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl LocalServer {
     pub fn new() -> Self {
-        Self { internal_server: RwLock::new(None) }
+        Self {
+            internal_server: RwLock::new(None),
+        }
     }
 
     pub async fn get_config(&self) -> LocalServerConfig {
@@ -116,7 +132,7 @@ impl LocalServer {
 }
 
 // A static data to store the integration test server
-pub static LOCAL_SERVER: Lazy<LocalServer> = Lazy::new(|| LocalServer::new());
+pub static LOCAL_SERVER: Lazy<LocalServer> = Lazy::new(LocalServer::new);
 
 /// The information needed to start a [`LocalNet`].
 pub struct LocalNetConfig {
@@ -477,24 +493,17 @@ impl LocalNet {
             Database::Service => {
                 let endpoint = &self.server_config.service_config.endpoint;
                 let endpoint = endpoint.strip_prefix("http://").unwrap();
-                (format!("service:http://{}:{}", endpoint, namespace),
-                 (validator, 0))
-            },
-            Database::RocksDb => (
-                format!("rocksdb:{}", namespace),
-                (validator, shard),
-            ),
+                (
+                    format!("service:http://{}:{}", endpoint, namespace),
+                    (validator, 0),
+                )
+            }
+            Database::RocksDb => (format!("rocksdb:{}", namespace), (validator, shard)),
             Database::DynamoDb => (
-                format!(
-                    "dynamodb:{}:localstack",
-                    namespace,
-                ),
+                format!("dynamodb:{}:localstack", namespace,),
                 (validator, 0),
             ),
-            Database::ScyllaDb => (
-                format!("scylladb:{}", namespace),
-                (validator, 0),
-            ),
+            Database::ScyllaDb => (format!("scylladb:{}", namespace), (validator, 0)),
         };
         if !self.set_init.contains(&key) {
             let max_try = 4;
