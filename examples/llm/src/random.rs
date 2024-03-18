b977a64872c595bf88bc033eae987fe79337c8e2
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2024-03-18 18:59:29 +0000

    RNG is seeded by current_system_time api

--- examples/llm/src/random.rs
+++ examples/llm/src/random.rs
@@ -1,13 +1,16 @@
 use rand::{rngs::StdRng, Rng, SeedableRng};
-use std::{
-    cell::OnceCell,
-    sync::{Arc, Mutex, OnceLock},
-};
+use std::sync::{Mutex, OnceLock};
 
 static RNG: OnceLock<Mutex<StdRng>> = OnceLock::new();
 
 fn custom_getrandom(buf: &mut [u8]) -> Result<(), getrandom::Error> {
-    RNG.get_or_init(|| Mutex::new(StdRng::from_seed([0; 32])))
+    let mut seed = linera_sdk::service::system_api::current_system_time()
+        .micros()
+        .to_be_bytes()
+        .to_vec();
+    // Timestamp is 8 bytes and RNG seed requires 32 bytes.
+    seed.extend(std::iter::repeat(0).take(32 - 8));
+    RNG.get_or_init(|| Mutex::new(StdRng::from_seed(seed.try_into().expect("wrong length"))))
         .lock()
         .expect("failed to get RNG lock")
         .fill(buf);
