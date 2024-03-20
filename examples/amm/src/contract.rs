jvff/linera-protocol-archive

Refactor `check_account_authentication` methods
Simplify their signatures to have less parameters.

examples/amm/src/contract.rs
@@ -85,11 +85,7 @@ impl Contract for AmmContract {
                 input_token_idx,
                 input_amount,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.execute_swap(owner, input_token_idx, input_amount)?;
             }
         }
@@ -108,11 +104,7 @@ impl Contract for AmmContract {
                 input_token_idx,
                 input_amount,
             } => {
-                Self::check_account_authentication(
-                    self.runtime.authenticated_caller_id(),
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 if self.runtime.chain_id() == self.runtime.application_id().creation.chain_id {
                     self.execute_swap(owner, input_token_idx, input_amount)?;
                 } else {
@@ -130,16 +122,23 @@ impl Contract for AmmContract {
 
 impl AmmContract {
     /// authenticate the originator of the message
-    fn check_account_authentication(
-        authenticated_application_id: Option<ApplicationId>,
-        authenticated_signer: Option<Owner>,
-        owner: AccountOwner,
-    ) -> Result<(), AmmError> {
+    fn check_account_authentication(&mut self, owner: AccountOwner) -> Result<(), AmmError> {
         match owner {
-            AccountOwner::User(address) if authenticated_signer == Some(address) => Ok(()),
-            AccountOwner::Application(id) if authenticated_application_id == Some(id) => Ok(()),
-            _ => Err(AmmError::IncorrectAuthentication),
+            AccountOwner::User(address) => {
+                ensure!(
+                    self.runtime.authenticated_signer() == Some(address),
+                    AmmError::IncorrectAuthentication
+                )
+            }
+            AccountOwner::Application(id) => {
+                ensure!(
+                    self.runtime.authenticated_application_id() == Some(id),
+                    AmmError::IncorrectAuthentication
+                )
+            }
         }
+
+        Ok(())
     }
 
     fn execute_order_local(&mut self, operation: Operation) -> Result<(), AmmError> {

examples/fungible/src/contract.rs
@@ -75,11 +75,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 target_account,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.state.debit(owner, amount).await?;
                 Ok(self
                     .finish_transfer_to_account(amount, target_account, owner)
@@ -91,11 +87,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 target_account,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    source_account.owner,
-                )?;
+                self.check_account_authentication(source_account.owner)?;
                 self.claim(source_account, amount, target_account).await
             }
         }
@@ -124,11 +116,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 target_account,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.state.debit(owner, amount).await?;
                 Ok(self
                     .finish_transfer_to_account(amount, target_account, owner)
@@ -154,11 +142,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 destination,
             } => {
-                Self::check_account_authentication(
-                    self.runtime.authenticated_caller_id(),
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.state.debit(owner, amount).await?;
                 let execution_outcome = self
                     .finish_transfer_to_account(amount, destination, owner)
@@ -174,11 +158,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 target_account,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    source_account.owner,
-                )?;
+                self.check_account_authentication(source_account.owner)?;
                 let execution_outcome = self.claim(source_account, amount, target_account).await?;
                 Ok(ApplicationCallOutcome {
                     execution_outcome,
@@ -198,16 +178,23 @@ impl Contract for FungibleTokenContract {
 
 impl FungibleTokenContract {
     /// Verifies that a transfer is authenticated for this local account.
-    fn check_account_authentication(
-        authenticated_application_id: Option<ApplicationId>,
-        authenticated_signer: Option<Owner>,
-        owner: AccountOwner,
-    ) -> Result<(), Error> {
+    fn check_account_authentication(&mut self, owner: AccountOwner) -> Result<(), Error> {
         match owner {
-            AccountOwner::User(address) if authenticated_signer == Some(address) => Ok(()),
-            AccountOwner::Application(id) if authenticated_application_id == Some(id) => Ok(()),
-            _ => Err(Error::IncorrectAuthentication),
+            AccountOwner::User(address) => {
+                ensure!(
+                    self.runtime.authenticated_signer() == Some(address),
+                    AmmError::IncorrectAuthentication
+                )
+            }
+            AccountOwner::Application(id) => {
+                ensure!(
+                    self.runtime.authenticated_application_id() == Some(id),
+                    AmmError::IncorrectAuthentication
+                )
+            }
         }
+
+        Ok(())
     }
 
     async fn claim(

examples/matching-engine/src/contract.rs
@@ -91,11 +91,7 @@ impl Contract for MatchingEngineContract {
             Operation::ExecuteOrder { order } => {
                 let owner = Self::get_owner(&order);
                 let chain_id = self.runtime.chain_id();
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 if chain_id == self.runtime.application_id().creation.chain_id {
                     self.execute_order_local(order, chain_id).await?;
                 } else {
@@ -133,11 +129,7 @@ impl Contract for MatchingEngineContract {
                     .runtime
                     .message_id()
                     .expect("Incoming message ID has to be available when executing a message");
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.execute_order_local(order, message_id.chain_id).await?;
             }
         }
@@ -155,11 +147,7 @@ impl Contract for MatchingEngineContract {
             ApplicationCall::ExecuteOrder { order } => {
                 let owner = Self::get_owner(&order);
                 let chain_id = self.runtime.chain_id();
-                Self::check_account_authentication(
-                    self.runtime.authenticated_caller_id(),
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 if chain_id == self.runtime.application_id().creation.chain_id {
                     self.execute_order_local(order, chain_id).await?;
                 } else {
@@ -192,15 +180,25 @@ impl MatchingEngineContract {
 
     /// authenticate the originator of the message
     fn check_account_authentication(
-        authenticated_application_id: Option<ApplicationId>,
-        authenticated_signer: Option<Owner>,
+        &mut self,
         owner: AccountOwner,
     ) -> Result<(), MatchingEngineError> {
         match owner {
-            AccountOwner::User(address) if authenticated_signer == Some(address) => Ok(()),
-            AccountOwner::Application(id) if authenticated_application_id == Some(id) => Ok(()),
-            _ => Err(MatchingEngineError::IncorrectAuthentication),
+            AccountOwner::User(address) => {
+                ensure!(
+                    self.runtime.authenticated_signer() == Some(address),
+                    AmmError::IncorrectAuthentication
+                )
+            }
+            AccountOwner::Application(id) => {
+                ensure!(
+                    self.runtime.authenticated_application_id() == Some(id),
+                    AmmError::IncorrectAuthentication
+                )
+            }
         }
+
+        Ok(())
     }
 
     /// The application engine is trading between two tokens. Those tokens are the parameters of the

linera-io/linera-protocol

examples/amm/src/contract.rs
@@ -10,7 +10,7 @@ use amm::{AmmError, ApplicationCall, Message, Operation};
 use async_trait::async_trait;
 use fungible::{Account, FungibleTokenAbi};
 use linera_sdk::{
-    base::{AccountOwner, Amount, ApplicationId, Owner, WithContractAbi},
+    base::{AccountOwner, Amount, ApplicationId, WithContractAbi},
     contract::system_api,
     ensure, ApplicationCallOutcome, Contract, ContractRuntime, ExecutionOutcome, OutgoingMessage,
     Resources, ViewStateStorage,
@@ -82,11 +82,7 @@ impl Contract for AmmContract {
                 input_token_idx,
                 input_amount,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.execute_swap(owner, input_token_idx, input_amount)?;
             }
         }
@@ -105,11 +101,7 @@ impl Contract for AmmContract {
                 input_token_idx,
                 input_amount,
             } => {
-                Self::check_account_authentication(
-                    self.runtime.authenticated_caller_id(),
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 if self.runtime.chain_id() == self.runtime.application_id().creation.chain_id {
                     self.execute_swap(owner, input_token_idx, input_amount)?;
                 } else {
@@ -127,16 +119,23 @@ impl Contract for AmmContract {
 
 impl AmmContract {
     /// authenticate the originator of the message
-    fn check_account_authentication(
-        authenticated_application_id: Option<ApplicationId>,
-        authenticated_signer: Option<Owner>,
-        owner: AccountOwner,
-    ) -> Result<(), AmmError> {
+    fn check_account_authentication(&mut self, owner: AccountOwner) -> Result<(), AmmError> {
         match owner {
-            AccountOwner::User(address) if authenticated_signer == Some(address) => Ok(()),
-            AccountOwner::Application(id) if authenticated_application_id == Some(id) => Ok(()),
-            _ => Err(AmmError::IncorrectAuthentication),
+            AccountOwner::User(address) => {
+                ensure!(
+                    self.runtime.authenticated_signer() == Some(address),
+                    AmmError::IncorrectAuthentication
+                )
+            }
+            AccountOwner::Application(id) => {
+                ensure!(
+                    self.runtime.authenticated_caller_id() == Some(id),
+                    AmmError::IncorrectAuthentication
+                )
+            }
         }
+
+        Ok(())
     }
 
     fn execute_order_local(&mut self, operation: Operation) -> Result<(), AmmError> {

examples/fungible/src/contract.rs
@@ -9,9 +9,9 @@ use self::state::FungibleToken;
 use async_trait::async_trait;
 use fungible::{Account, ApplicationCall, FungibleResponse, Message, Operation};
 use linera_sdk::{
-    base::{AccountOwner, Amount, ApplicationId, Owner, WithContractAbi},
+    base::{AccountOwner, Amount, WithContractAbi},
     contract::system_api,
-    ApplicationCallOutcome, Contract, ContractRuntime, ExecutionOutcome, ViewStateStorage,
+    ensure, ApplicationCallOutcome, Contract, ContractRuntime, ExecutionOutcome, ViewStateStorage,
 };
 use std::str::FromStr;
 use thiserror::Error;
@@ -72,11 +72,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 target_account,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.state.debit(owner, amount).await?;
                 Ok(self
                     .finish_transfer_to_account(amount, target_account, owner)
@@ -88,11 +84,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 target_account,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    source_account.owner,
-                )?;
+                self.check_account_authentication(source_account.owner)?;
                 self.claim(source_account, amount, target_account).await
             }
         }
@@ -121,11 +113,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 target_account,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.state.debit(owner, amount).await?;
                 Ok(self
                     .finish_transfer_to_account(amount, target_account, owner)
@@ -151,11 +139,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 destination,
             } => {
-                Self::check_account_authentication(
-                    self.runtime.authenticated_caller_id(),
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.state.debit(owner, amount).await?;
                 let execution_outcome = self
                     .finish_transfer_to_account(amount, destination, owner)
@@ -171,11 +155,7 @@ impl Contract for FungibleTokenContract {
                 amount,
                 target_account,
             } => {
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    source_account.owner,
-                )?;
+                self.check_account_authentication(source_account.owner)?;
                 let execution_outcome = self.claim(source_account, amount, target_account).await?;
                 Ok(ApplicationCallOutcome {
                     execution_outcome,
@@ -195,16 +175,23 @@ impl Contract for FungibleTokenContract {
 
 impl FungibleTokenContract {
     /// Verifies that a transfer is authenticated for this local account.
-    fn check_account_authentication(
-        authenticated_application_id: Option<ApplicationId>,
-        authenticated_signer: Option<Owner>,
-        owner: AccountOwner,
-    ) -> Result<(), Error> {
+    fn check_account_authentication(&mut self, owner: AccountOwner) -> Result<(), Error> {
         match owner {
-            AccountOwner::User(address) if authenticated_signer == Some(address) => Ok(()),
-            AccountOwner::Application(id) if authenticated_application_id == Some(id) => Ok(()),
-            _ => Err(Error::IncorrectAuthentication),
+            AccountOwner::User(address) => {
+                ensure!(
+                    self.runtime.authenticated_signer() == Some(address),
+                    Error::IncorrectAuthentication
+                )
+            }
+            AccountOwner::Application(id) => {
+                ensure!(
+                    self.runtime.authenticated_caller_id() == Some(id),
+                    Error::IncorrectAuthentication
+                )
+            }
         }
+
+        Ok(())
     }
 
     async fn claim(

examples/matching-engine/src/contract.rs
@@ -5,20 +5,19 @@
 
 mod state;
 use crate::state::{KeyBook, OrderEntry};
+use async_trait::async_trait;
+use fungible::{Account, FungibleTokenAbi};
+use linera_sdk::{
+    base::{AccountOwner, Amount, ApplicationId, ChainId, WithContractAbi},
+    contract::system_api,
+    ensure, ApplicationCallOutcome, Contract, ContractRuntime, ExecutionOutcome, OutgoingMessage,
+    Resources, ViewStateStorage,
+};
 use matching_engine::{
     product_price_amount, ApplicationCall, Message, Operation, Order, OrderId, OrderNature, Price,
 };
 use state::{LevelView, MatchingEngine, MatchingEngineError};
 use std::cmp::min;
-
-use async_trait::async_trait;
-use fungible::{Account, FungibleTokenAbi};
-use linera_sdk::{
-    base::{AccountOwner, Amount, ApplicationId, ChainId, Owner, WithContractAbi},
-    contract::system_api,
-    ensure, ApplicationCallOutcome, Contract, ContractRuntime, ExecutionOutcome, OutgoingMessage,
-    Resources, ViewStateStorage,
-};
 
 pub struct MatchingEngineContract {
     state: MatchingEngine,
@@ -88,11 +87,7 @@ impl Contract for MatchingEngineContract {
             Operation::ExecuteOrder { order } => {
                 let owner = Self::get_owner(&order);
                 let chain_id = self.runtime.chain_id();
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 if chain_id == self.runtime.application_id().creation.chain_id {
                     self.execute_order_local(order, chain_id).await?;
                 } else {
@@ -130,11 +125,7 @@ impl Contract for MatchingEngineContract {
                     .runtime
                     .message_id()
                     .expect("Incoming message ID has to be available when executing a message");
-                Self::check_account_authentication(
-                    None,
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 self.execute_order_local(order, message_id.chain_id).await?;
             }
         }
@@ -152,11 +143,7 @@ impl Contract for MatchingEngineContract {
             ApplicationCall::ExecuteOrder { order } => {
                 let owner = Self::get_owner(&order);
                 let chain_id = self.runtime.chain_id();
-                Self::check_account_authentication(
-                    self.runtime.authenticated_caller_id(),
-                    self.runtime.authenticated_signer(),
-                    owner,
-                )?;
+                self.check_account_authentication(owner)?;
                 if chain_id == self.runtime.application_id().creation.chain_id {
                     self.execute_order_local(order, chain_id).await?;
                 } else {
@@ -189,15 +176,25 @@ impl MatchingEngineContract {
 
     /// authenticate the originator of the message
     fn check_account_authentication(
-        authenticated_application_id: Option<ApplicationId>,
-        authenticated_signer: Option<Owner>,
+        &mut self,
         owner: AccountOwner,
     ) -> Result<(), MatchingEngineError> {
         match owner {
-            AccountOwner::User(address) if authenticated_signer == Some(address) => Ok(()),
-            AccountOwner::Application(id) if authenticated_application_id == Some(id) => Ok(()),
-            _ => Err(MatchingEngineError::IncorrectAuthentication),
+            AccountOwner::User(address) => {
+                ensure!(
+                    self.runtime.authenticated_signer() == Some(address),
+                    MatchingEngineError::IncorrectAuthentication
+                )
+            }
+            AccountOwner::Application(id) => {
+                ensure!(
+                    self.runtime.authenticated_caller_id() == Some(id),
+                    MatchingEngineError::IncorrectAuthentication
+                )
+            }
         }
+
+        Ok(())
     }
 
     /// The application engine is trading between two tokens. Those tokens are the parameters of the