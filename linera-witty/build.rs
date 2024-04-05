965b84c6ef02e2070c22f84a8c0d961a991a956d
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-04-05 15:26:41 +0000

    Add a `log` feature to `linera-witty`
    Prepare to implement the Witty traits for a type from the `log` crate,
    but only if requested by the user.

--- linera-witty/build.rs
+++ linera-witty/build.rs
@@ -3,6 +3,7 @@
 
 fn main() {
     cfg_aliases::cfg_aliases! {
+        with_log: { feature = "log" },
         with_testing: { any(test, feature = "test") },
         with_wasmer: { feature = "wasmer" },
         with_wasmtime: { feature = "wasmtime" },
