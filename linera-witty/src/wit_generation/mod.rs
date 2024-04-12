2f60d59ac012021ad770391891d6a04dc816ddce
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-04-12 17:08:05 +0000

    Generate the contents of a WIT world file
    Create a `WitWorldWriter` helper type that returns an iterator to
    generate the contents of the file.

--- linera-witty/src/wit_generation/mod.rs
+++ linera-witty/src/wit_generation/mod.rs
@@ -52,29 +52,104 @@ impl WitInterfaceWriter {
     }
 
     /// Returns an [`Iterator`] with the file contents of the WIT interface file.
-    pub fn generate_file_contents(&self) -> impl Iterator<Item = &[u8]> {
+    pub fn generate_file_contents(&self) -> impl Iterator<Item = &str> {
         gen!({
-            yield_!(b"package ".as_slice());
-            yield_!(self.package.as_bytes());
-            yield_!(b";\n\n");
+            yield_!("package ");
+            yield_!(self.package);
+            yield_!(";\n\n");
 
-            yield_!(b"interface ");
-            yield_!(self.name.as_bytes());
-            yield_!(b" {\n");
+            yield_!("interface ");
+            yield_!(self.name);
+            yield_!(" {\n");
 
             for function in &self.functions {
-                yield_!(function.as_bytes());
-                yield_!(b"\n");
+                yield_!(&function);
+                yield_!("\n");
             }
 
             for type_declaration in self.types.values() {
                 if !type_declaration.is_empty() {
-                    yield_!(b"\n");
-                    yield_!(type_declaration.as_bytes());
+                    yield_!("\n");
+                    yield_!(&type_declaration);
                 }
             }
 
-            yield_!(b"}\n");
+            yield_!("}\n");
+        })
+        .into_iter()
+    }
+}
+
+/// Helper type to write a WIT file declaring a
+/// [world](https://github.com/WebAssembly/component-model/blob/main/design/mvp/WIT.md#wit-worlds).
+#[derive(Clone, Debug)]
+pub struct WitWorldWriter {
+    package: Option<&'static str>,
+    name: String,
+    imports: Vec<&'static str>,
+    exports: Vec<&'static str>,
+}
+
+impl WitWorldWriter {
+    /// Creates a new [`WitWorldWriter`] to write a world with the provided `name`.
+    pub fn new(package: impl Into<Option<&'static str>>, name: impl Into<String>) -> Self {
+        WitWorldWriter {
+            package: package.into(),
+            name: name.into(),
+            imports: Vec::new(),
+            exports: Vec::new(),
+        }
+    }
+
+    /// Registers a [`WitInterface`] to be imported into this world.
+    pub fn import<Interface>(mut self) -> Self
+    where
+        Interface: WitInterface,
+    {
+        self.imports.push(Interface::wit_name());
+        self
+    }
+
+    /// Registers a [`WitInterface`] to be exported from this world.
+    pub fn export<Interface>(mut self) -> Self
+    where
+        Interface: WitInterface,
+    {
+        self.exports.push(Interface::wit_name());
+        self
+    }
+
+    /// Returns an [`Iterator`] with the file contents of the WIT world file, optionally including
+    /// a package header.
+    pub fn generate_file_contents(&self) -> impl Iterator<Item = &str> {
+        gen!({
+            if let Some(package) = &self.package {
+                yield_!("package ");
+                yield_!(package);
+                yield_!(";\n\n");
+            }
+
+            yield_!("world ");
+            yield_!(&self.name);
+            yield_!(" {\n");
+
+            for import in &self.imports {
+                yield_!("    import ");
+                yield_!(import);
+                yield_!(";\n");
+            }
+
+            if !self.imports.is_empty() {
+                yield_!("\n");
+            }
+
+            for export in &self.exports {
+                yield_!("    export ");
+                yield_!(export);
+                yield_!(";\n");
+            }
+
+            yield_!("}\n");
         })
         .into_iter()
     }
