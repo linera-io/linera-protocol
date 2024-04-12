b03373ca1ea95cf784c77737607cd773a41efadf
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-04-12 16:57:37 +0000

    Generate the contents of a WIT interface file
    Create a `WitInterfaceWriter` helper type that returns an iterator to
    generate the contents of the file.

--- linera-witty/src/wit_generation/mod.rs
+++ linera-witty/src/wit_generation/mod.rs
@@ -3,6 +3,10 @@
 
 //! Generation of WIT files.
 
+use std::collections::BTreeMap;
+
+use genawaiter::{rc::gen, yield_};
+
 pub use crate::type_traits::RegisterWitTypes;
 
 /// Generates WIT snippets for an interface.
@@ -18,4 +22,60 @@ pub trait WitInterface {
 
     /// The WIT definitions of each function in this interface.
     fn wit_functions() -> Vec<String>;
+}
+
+/// Helper type to write a [`WitInterface`] to a file.
+#[derive(Clone, Debug)]
+pub struct WitInterfaceWriter {
+    package: &'static str,
+    name: &'static str,
+    types: BTreeMap<String, String>,
+    functions: Vec<String>,
+}
+
+impl WitInterfaceWriter {
+    /// Prepares a new [`WitInterfaceWriter`] to write the provided `Interface`.
+    pub fn new<Interface>() -> Self
+    where
+        Interface: WitInterface,
+    {
+        let mut types = BTreeMap::new();
+
+        Interface::Dependencies::register_wit_types(&mut types);
+
+        WitInterfaceWriter {
+            package: Interface::wit_package(),
+            name: Interface::wit_name(),
+            types,
+            functions: Interface::wit_functions(),
+        }
+    }
+
+    /// Returns an [`Iterator`] with the file contents of the WIT interface file.
+    pub fn generate_file_contents(&self) -> impl Iterator<Item = &[u8]> {
+        gen!({
+            yield_!(b"package ".as_slice());
+            yield_!(self.package.as_bytes());
+            yield_!(b";\n\n");
+
+            yield_!(b"interface ");
+            yield_!(self.name.as_bytes());
+            yield_!(b" {\n");
+
+            for function in &self.functions {
+                yield_!(function.as_bytes());
+                yield_!(b"\n");
+            }
+
+            for type_declaration in self.types.values() {
+                if !type_declaration.is_empty() {
+                    yield_!(b"\n");
+                    yield_!(type_declaration.as_bytes());
+                }
+            }
+
+            yield_!(b"}\n");
+        })
+        .into_iter()
+    }
 }
