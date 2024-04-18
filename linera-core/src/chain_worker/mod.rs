1a421dc3bb4d1c7a4585b072c1322ca4b7b1c90e
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-04-18 18:37:02 +0000

    Create a `ChainWorkerState` type
    A separate worker type responsible for a single chain.

---
+++ linera-core/src/chain_worker/mod.rs
@@ -0,0 +1,8 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! A worker to handle a single chain.
+
+mod state;
+
+pub use self::state::ChainWorkerState;
--- linera-core/src/lib.rs
+++ linera-core/src/lib.rs
@@ -4,6 +4,7 @@
 
 //! This module defines the core Linera protocol.
 
+pub mod chain_worker;
 pub mod client;
 pub mod data_types;
 pub mod local_node;
