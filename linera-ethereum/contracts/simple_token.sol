035eba827949d297b7f190ba40c1a348d0654d71
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-04-22 21:23:52 +0200

    Add some spaces between lines.

--- linera-ethereum/contracts/simple_token.sol
+++ linera-ethereum/contracts/simple_token.sol
@@ -4,10 +4,12 @@ contract SimpleToken {
     mapping(address => uint256) public balances;
     uint256 public totalSupply;
     event Transfer(address indexed from, address indexed to, uint256 value);
+
     constructor(uint256 initialSupply) {
         totalSupply = initialSupply;
         balances[msg.sender] = initialSupply;
     }
+
     function transfer(address to, uint256 value) public returns (bool) {
         require(value > 0, "Value must be greater than 0");
         require(balances[msg.sender] >= value, "Insufficient balance");
@@ -16,6 +18,7 @@ contract SimpleToken {
         emit Transfer(msg.sender, to, value);
         return true;
     }
+
     function balanceOf(address account) public view returns (uint256) {
         return balances[account];
     }
