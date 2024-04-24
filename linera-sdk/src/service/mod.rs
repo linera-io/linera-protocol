b48f8842b6481746ef926bc506f61e429ef2bee9
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-04-24 13:16:20 +0000

    Use `State` instead of `ServiceStateStorage`
    Prepare to remove the entire `storage` sub-module from the `service`
    module.

--- linera-sdk/src/service/mod.rs
+++ linera-sdk/src/service/mod.rs
@@ -24,14 +24,20 @@ macro_rules! service {
     ($application:ty) => {
         #[doc(hidden)]
         #[no_mangle]
-        fn __service_handle_query(
-            argument: Vec<u8>,
-        ) -> Result<Vec<u8>, String> {
-            $crate::service::run_async_entrypoint(
-                <
-                    <$application as $crate::Service>::Storage as $crate::ServiceStateStorage
-                >::handle_query(argument),
-            )
+        fn __service_handle_query(argument: Vec<u8>) -> Result<Vec<u8>, String> {
+            let request = serde_json::from_slice(&argument).map_err(|error| error.to_string())?;
+            let response = $crate::service::run_async_entrypoint(move |runtime| async move {
+                let state =
+                    <<$application as $crate::Service>::State as $crate::State>::load().await;
+                let application = <$application as $crate::Service>::new(state, runtime)
+                    .await
+                    .map_err(|error| error.to_string())?;
+                application
+                    .handle_query(request)
+                    .await
+                    .map_err(|error| error.to_string())
+            })?;
+            serde_json::to_vec(&response).map_err(|error| error.to_string())
         }
 
         /// Stub of a `main` entrypoint so that the binary doesn't fail to compile on targets other
@@ -67,16 +73,17 @@ macro_rules! service {
 
 /// Runs an asynchronous entrypoint in a blocking manner, by repeatedly polling the entrypoint
 /// future.
-pub fn run_async_entrypoint<Entrypoint, Output, Error>(
-    entrypoint: Entrypoint,
+pub fn run_async_entrypoint<Service, Entrypoint, Output, Error>(
+    entrypoint: impl FnOnce(ServiceRuntime<Service>) -> Entrypoint,
 ) -> Result<Output, String>
 where
+    Service: crate::Service,
     Entrypoint: Future<Output = Result<Output, Error>>,
     Error: ToString + 'static,
 {
     ServiceLogger::install();
 
-    entrypoint
+    entrypoint(ServiceRuntime::new())
         .blocking_wait()
         .map_err(|error| error.to_string())
 }
