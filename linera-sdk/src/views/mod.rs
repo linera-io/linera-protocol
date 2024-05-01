085b632f264bec133e78acb91698d1951a9e01c9
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-05-01 14:14:44 +0000

    Remove test-specific `ViewStorageContext`
    Replaced by using the `KeyValueStore` with a mock backend.

--- linera-sdk/src/views/mod.rs
+++ linera-sdk/src/views/mod.rs
@@ -8,8 +8,6 @@ mod conversions_to_wit;
 #[cfg(with_testing)]
 mod mock_key_value_store;
 mod system_api;
-#[cfg(with_testing)]
-mod test_context;
 
 pub use linera_views::{
     self,
@@ -17,15 +15,11 @@ pub use linera_views::{
     views::{RootView, View, ViewError},
 };
 
-#[cfg(not(with_testing))]
-pub use self::system_api::ViewStorageContext;
-#[cfg(with_testing)]
-pub use self::test_context::ViewStorageContext;
 pub use self::{
     aliases::{
         ByteCollectionView, ByteMapView, ByteSetView, CollectionView, CustomCollectionView,
         CustomMapView, CustomSetView, LogView, MapView, QueueView, ReadGuardedView, RegisterView,
         SetView,
     },
-    system_api::KeyValueStore,
+    system_api::{KeyValueStore, ViewStorageContext},
 };
