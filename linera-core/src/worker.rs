1d09273f340ce0bce52aea424eeb346919862e19
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-05-07 18:51:10 +0000

    Use chain actor for `read_bytecode_location`
    Prepare to have the actor run for a longer term.

--- linera-core/src/worker.rs
+++ linera-core/src/worker.rs
@@ -479,13 +479,12 @@ where
         chain_id: ChainId,
         bytecode_id: BytecodeId,
     ) -> Result<Option<BytecodeLocation>, WorkerError> {
-        ChainWorkerState::new(
-            self.chain_worker_config.clone(),
-            self.storage.clone(),
-            chain_id,
-        )
-        .await?
-        .read_bytecode_location(bytecode_id)
+        self.query_chain_worker(chain_id, move |callback| {
+            ChainWorkerRequest::ReadBytecodeLocation {
+                bytecode_id,
+                callback,
+            }
+        })
         .await
     }
 
