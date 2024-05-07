56345026c33dc81607ac693a2df6c8ae40ec6232
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-05-07 12:07:34 +0000

    Add `ChainWorkerState::read_certificate`
    Refactor to move the code from the `WorkerState` into the
    `ChainWorkerState`.

--- linera-core/src/worker.rs
+++ linera-core/src/worker.rs
@@ -1016,13 +1016,10 @@ where
         chain_id: ChainId,
         height: BlockHeight,
     ) -> Result<Option<Certificate>, WorkerError> {
-        let chain = self.storage.load_active_chain(chain_id).await?;
-        let certificate_hash = match chain.confirmed_log.get(height.try_into()?).await? {
-            Some(hash) => hash,
-            None => return Ok(None),
-        };
-        let certificate = self.storage.read_certificate(certificate_hash).await?;
-        Ok(Some(certificate))
+        self.create_chain_worker(chain_id)
+            .await?
+            .read_certificate(height)
+            .await
     }
 
     /// Returns the application registry for a specific chain.
