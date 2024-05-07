2ad087b0f8d36a93a9147f67d9f5a47f2f5a303e
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-05-07 12:23:10 +0000

    Add `FindEventInInbox` request
    Handle the request in the `ChainWorkerActor`.

--- linera-core/src/chain_worker/actor.rs
+++ linera-core/src/chain_worker/actor.rs
@@ -3,8 +3,8 @@
 
 //! An actor that runs a chain worker.
 
-use linera_base::{data_types::BlockHeight, identifiers::ChainId};
-use linera_chain::data_types::{Block, ExecutedBlock, MessageBundle, Origin, Target};
+use linera_base::{crypto::CryptoHash, data_types::BlockHeight, identifiers::ChainId};
+use linera_chain::data_types::{Block, Event, ExecutedBlock, MessageBundle, Origin, Target};
 use linera_execution::{Query, Response, UserApplicationDescription, UserApplicationId};
 use linera_storage::Storage;
 use linera_views::views::ViewError;
@@ -26,6 +26,16 @@ pub enum ChainWorkerRequest {
     ReadCertificate {
         height: BlockHeight,
         callback: oneshot::Sender<Result<Option<Certificate>, WorkerError>>,
+    },
+
+    /// Search for an event in one of the chain's inboxes.
+    #[cfg(with_testing)]
+    FindEventInInbox {
+        inbox_id: Origin,
+        certificate_hash: CryptoHash,
+        height: BlockHeight,
+        index: u32,
+        callback: oneshot::Sender<Result<Option<Event>, WorkerError>>,
     },
 
     /// Query an application's state.
@@ -112,6 +122,20 @@ where
                 #[cfg(with_testing)]
                 ChainWorkerRequest::ReadCertificate { height, callback } => {
                     let _ = callback.send(self.worker.read_certificate(height).await);
+                }
+                #[cfg(with_testing)]
+                ChainWorkerRequest::FindEventInInbox {
+                    inbox_id,
+                    certificate_hash,
+                    height,
+                    index,
+                    callback,
+                } => {
+                    let _ = callback.send(
+                        self.worker
+                            .find_event_in_inbox(inbox_id, certificate_hash, height, index)
+                            .await,
+                    );
                 }
                 ChainWorkerRequest::QueryApplication { query, callback } => {
                     let _ = callback.send(self.worker.query_application(query).await);
