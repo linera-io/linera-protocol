7f8c073cc8e13a653e3a84ad79dffaca225d55d8
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-05-07 21:54:02 +0200

    Restructuration of the code.

--- linera-ethereum/src/client.rs
+++ linera-ethereum/src/client.rs
@@ -5,7 +5,7 @@
 
 use alloy::{
     primitives::{Address, U256},
-    providers::{Provider, ProviderBuilder},
+    providers::{Provider, RootProvider, ProviderBuilder},
     rpc::types::eth::{
         request::{TransactionInput, TransactionRequest},
         BlockId, BlockNumberOrTag, Filter,
@@ -17,18 +17,19 @@ use eyre::Result;
 
 use crate::common::{event_name_from_expanded, parse_log, EthereumEvent, EthereumServiceError};
 
+use reqwest::Client;
+pub type HttpProvider = RootProvider<alloy::transports::http::Http<Client>>;
+
 /// The Ethereum endpoint and its provider used for accessing the ethereum node.
 pub struct EthereumEndpoint {
-    url: String,
+    provider: HttpProvider,
 }
 
 impl EthereumEndpoint {
     /// Lists all the accounts of the Ethereum node.
     pub async fn get_accounts(&self) -> Result<Vec<String>, EthereumServiceError> {
-        let url = reqwest::Url::parse(&self.url)?;
-        let provider = ProviderBuilder::new().on_http(url);
         let mut accounts = Vec::new();
-        for account in provider.get_accounts().await? {
+        for account in self.provider.get_accounts().await? {
             let account = format!("{:?}", account);
             accounts.push(account);
         }
@@ -37,9 +38,7 @@ impl EthereumEndpoint {
 
     /// Gets the latest block number of the Ethereum node.
     pub async fn get_block_number(&self) -> Result<u64, EthereumServiceError> {
-        let url = reqwest::Url::parse(&self.url)?;
-        let provider = ProviderBuilder::new().on_http(url);
-        Ok(provider.get_block_number().await?)
+        Ok(self.provider.get_block_number().await?)
     }
 
     /// Gets the balance of the specified address at the specified block number.
@@ -50,15 +49,13 @@ impl EthereumEndpoint {
         address: &str,
         block_nr: Option<u64>,
     ) -> Result<U256, EthereumServiceError> {
-        let url = reqwest::Url::parse(&self.url)?;
-        let provider = ProviderBuilder::new().on_http(url);
         let address = address.parse::<Address>()?;
         let number = match block_nr {
             None => BlockNumberOrTag::Latest,
             Some(val) => BlockNumberOrTag::Number(val),
         };
         let block_id = BlockId::Number(number);
-        let balance = provider.get_balance(address, block_id).await?;
+        let balance = self.provider.get_balance(address, block_id).await?;
         Ok(balance)
     }
 
@@ -72,15 +69,13 @@ impl EthereumEndpoint {
         event_name_expanded: &str,
         starting_block: u64,
     ) -> Result<Vec<EthereumEvent>, EthereumServiceError> {
-        let url = reqwest::Url::parse(&self.url)?;
-        let provider = ProviderBuilder::new().on_http(url);
         let contract_address = contract_address.parse::<Address>()?;
         let event_name = event_name_from_expanded(event_name_expanded);
         let filter = Filter::new()
             .address(contract_address)
             .event(&event_name)
             .from_block(starting_block);
-        let events = provider.get_logs(&filter).await?;
+        let events = self.provider.get_logs(&filter).await?;
         let events = events
             .into_iter()
             .map(|x| parse_log(event_name_expanded, x))
@@ -98,8 +93,6 @@ impl EthereumEndpoint {
         data: Bytes,
         from: &str,
     ) -> Result<Bytes, EthereumServiceError> {
-        let url = reqwest::Url::parse(&self.url)?;
-        let provider = ProviderBuilder::new().on_http(url);
         let contract_address = contract_address.parse::<Address>()?;
         let from = from.parse::<Address>()?;
         let input = TransactionInput::new(data);
@@ -107,17 +100,17 @@ impl EthereumEndpoint {
             .from(from)
             .to(contract_address)
             .input(input);
-        let eth_call = provider.call(&tx).await?;
+        let eth_call = self.provider.call(&tx).await?;
         Ok(eth_call)
     }
 }
 
-/*
-impl EthereumEndpoint<Http> {
+impl EthereumEndpoint {
     /// Connects to an existing Ethereum node and creates an `EthereumEndpoint`
     /// if successful.
     pub fn new(url: String) -> Result<Self, EthereumServiceError> {
-        Ok(Self { url })
+        let rpc_url = reqwest::Url::parse(&url)?;
+        let provider = ProviderBuilder::new().on_http(rpc_url);
+        Ok(Self { provider })
     }
 }
-*/
