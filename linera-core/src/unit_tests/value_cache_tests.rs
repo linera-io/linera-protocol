58525d244aaa155c2d9ab25a5c561fd136b4ae7d
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-05-09 13:28:15 +0000

    Test inserting many values individually
    Check that they are all retrievable from the cache.

--- linera-core/src/unit_tests/value_cache_tests.rs
+++ linera-core/src/unit_tests/value_cache_tests.rs
@@ -7,7 +7,7 @@ use linera_base::{crypto::CryptoHash, data_types::BlockHeight, identifiers::Chai
 use linera_chain::data_types::{CertificateValue, HashedCertificateValue};
 use linera_execution::committee::Epoch;
 
-use super::CertificateValueCache;
+use super::{CertificateValueCache, DEFAULT_VALUE_CACHE_SIZE};
 
 /// Tests attempt to retrieve non-existent value.
 #[tokio::test]
@@ -30,6 +30,36 @@ async fn test_insert_single_value() {
     assert!(cache.contains(&hash).await);
     assert_eq!(cache.get(&hash).await, Some(value));
     assert_eq!(cache.keys::<BTreeSet<_>>().await, BTreeSet::from([hash]));
+}
+
+/// Tests inserting many values in the cache, one-by-one.
+#[tokio::test]
+async fn test_insert_many_values_individually() {
+    let cache = CertificateValueCache::default();
+    let values = create_dummy_values(0..(DEFAULT_VALUE_CACHE_SIZE as u64)).collect::<Vec<_>>();
+
+    for value in &values {
+        assert!(cache.insert(Cow::Borrowed(value)).await);
+    }
+
+    for value in &values {
+        assert!(cache.contains(&value.hash()).await);
+        assert_eq!(cache.get(&value.hash()).await.as_ref(), Some(value));
+    }
+
+    assert_eq!(
+        cache.keys::<BTreeSet<_>>().await,
+        BTreeSet::from_iter(values.iter().map(HashedCertificateValue::hash))
+    );
+}
+
+/// Creates multiple dummy [`HashedCertificateValue`]s to use in the tests.
+fn create_dummy_values<Heights>(heights: Heights) -> impl Iterator<Item = HashedCertificateValue>
+where
+    Heights: IntoIterator,
+    Heights::Item: Into<BlockHeight>,
+{
+    heights.into_iter().map(create_dummy_value)
 }
 
 /// Creates a new dummy [`HashedCertificateValue`] to use in the tests.
