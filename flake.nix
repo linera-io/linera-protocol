dd41104b9c133c12cfb6cb4317e75bb63de888db
James Kay <james.kay@linera.io>
2024-05-12 22:54:31 +0100

    `flake.nix`: use a Nix package to facilitate sharing (#2016)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+
+    # Rust
     rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
 
     # Dev tools
     treefmt-nix.url = "github:numtide/treefmt-nix";
@@ -14,62 +17,43 @@
     imports = [
       inputs.treefmt-nix.flakeModule
     ];
-    perSystem = { config, self', pkgs, lib, system, rust-overlay, ... }:
+    perSystem = { config, self', inputs', pkgs, lib, system, ... }:
       let
         cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
         nonRustDeps = with pkgs; [
-          # for building
-          clang
-          libclang.lib
-          libiconv
-          nodejs
-          openssl
-          protobuf
-          pkg-config
-          rocksdb
-
-          # for testing
-          jq
-          kubernetes-helm
-          kind
-          kubectl
-
-          # for Wasm testing
-          chromium
-          chromedriver
-          wasm-pack
         ];
-        rustBuildToolchain = (pkgs.rust-bin.fromRustupToolchainFile
-          ./toolchains/build/rust-toolchain.toml);
-        rustLintToolchain = (pkgs.rust-bin.fromRustupToolchainFile
-          ./toolchains/lint/rust-toolchain.toml);
+        linera = pkgs.callPackage ./. { inherit (inputs) crane; };
       in {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
           overlays = [ (import inputs.rust-overlay) ];
         };
 
+        packages.default = linera;
+
         # Rust dev environment
         devShells.default = pkgs.mkShell {
           inputsFrom = [
             config.treefmt.build.devShell
+            linera
           ];
           shellHook = ''
             # For rust-analyzer 'hover' tooltips to work.
-            export RUST_SRC_PATH=${rustBuildToolchain.availableComponents.rust-src}
+            export RUST_SRC_PATH=${linera.rust-toolchain.availableComponents.rust-src}
             export LIBCLANG_PATH=${pkgs.libclang.lib}/lib
             export PATH=$PWD/target/debug:~/.cargo/bin:$PATH
             export ROCKSDB_LIB_DIR=${pkgs.rocksdb}/lib
           '';
-          buildInputs = nonRustDeps;
           nativeBuildInputs = with pkgs; [
-            rustBuildToolchain
             rust-analyzer
           ];
         };
 
         devShells.lint = pkgs.mkShell {
-          nativeBuildInputs = [ rustLintToolchain ];
+          nativeBuildInputs = [
+            (pkgs.rust-bin.fromRustupToolchainFile
+              ./toolchains/lint/rust-toolchain.toml)
+          ];
         };
 
         # Add your auto-formatters here.
