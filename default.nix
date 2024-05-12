8e81a5859a618296bbefdf3460cd630db8605c1c
James Kay <james.kay@linera.io>
2024-05-12 17:38:28 +0100

    `flake.nix`: use a Nix package to facilitate sharing

---
+++ default.nix
@@ -0,0 +1,35 @@
+{ crane, pkgs }:
+let
+  rust-toolchain = (pkgs.rust-bin.fromRustupToolchainFile
+    ./toolchains/build/rust-toolchain.toml);
+in ((crane.mkLib pkgs).overrideToolchain rust-toolchain).buildPackage {
+  pname = "linera";
+  src = ./.;
+  cargoExtraArgs = "-p linera-service";
+  nativeBuildInputs = with pkgs; [
+    clang
+    pkg-config
+    rocksdb
+    protobufc
+  ];
+  buildInputs = with pkgs; [
+    clang.cc.lib
+    libiconv
+    nodejs
+    openssl
+    protobuf
+  ];
+  checkInputs = with pkgs; [
+    # for native testing
+    jq
+    kubernetes-helm
+    kind
+    kubectl
+
+    # for Wasm testing
+    chromium
+    chromedriver
+    wasm-pack
+  ];
+  passthru = { inherit rust-toolchain; };
+}
