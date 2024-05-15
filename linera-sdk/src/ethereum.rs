2c085d3b1f914c43e4ecd4d38ede24bcee13c72e
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-05-15 15:03:43 +0200

    Add the linera-ethereum test code.

--- linera-sdk/src/ethereum.rs
+++ linera-sdk/src/ethereum.rs
@@ -5,7 +5,7 @@
 
 use std::fmt::Debug;
 
-use ethers::providers::{JsonRpcClient, ProviderError};
+use linera_ethereum::{client::JsonRpcClient, common::EthereumServiceError};
 use serde::{de::DeserializeOwned, Serialize};
 
 use crate::contract::wit::contract_system_api;
@@ -15,21 +15,22 @@ use crate::contract::wit::contract_system_api;
 pub struct EthereumClient {
     /// The URL of the JSON-RPC server, without the method or parameters.
     pub url: String,
+    pub id: Mutex<u64>,
 }
 
 #[cfg_attr(target_arch = "wasm32", async_trait::async_trait(?Send))]
 #[cfg_attr(not(target_arch = "wasm32"), async_trait::async_trait)]
 impl JsonRpcClient for EthereumClient {
-    type Error = ProviderError;
+    type Error = EthereumServiceError;
 
-    async fn request<T, R>(&self, method: &str, params: T) -> Result<R, Self::Error>
-    where
-        T: Debug + Serialize + Send + Sync,
-        R: DeserializeOwned + Send,
+    async fn get_id(&self) -> u64 {
+        let mut id = self.id.lock().await;
+        *id += 1;
+        *id
+    }
+
+    async fn request_inner(&self, payload: Vec<u8>) -> Result<Vec<u8>, Self::Error>
     {
-        let params = serde_json::to_string(&params).expect("Failed to serialize parameters");
-        let url = format!("{}?method={method}&params={params}", self.url);
-        let json = contract_system_api::fetch_json(&url);
-        Ok(serde_json::from_str(&json).expect("Failed to deserialize JSON response"))
+        contract_system_api::fetch_json(&self.url, payload)
     }
 }
