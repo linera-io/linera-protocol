5187e57ce08d2ec35f94d0c09b179b267c9bca56
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-05-16 13:26:01 +0000

    Add `retry_pending_cross_chain_requests` method
    Prepare to allow handling pending network actions for newly added
    tracked chains.

--- linera-core/src/local_node.rs
+++ linera-core/src/local_node.rs
@@ -2,7 +2,7 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::{borrow::Cow, sync::Arc};
+use std::{borrow::Cow, collections::VecDeque, sync::Arc};
 
 use futures::{future, lock::Mutex};
 use linera_base::{
@@ -538,5 +538,23 @@ where
             }
         }
         None
+    }
+
+    /// Handles any pending local cross-chain requests.
+    pub async fn retry_pending_cross_chain_requests(
+        &mut self,
+        sender_chain: ChainId,
+    ) -> Result<(), LocalNodeError> {
+        let mut node = self.node.lock().await;
+        let (_response, actions) = node
+            .state
+            .handle_chain_info_query(ChainInfoQuery::new(sender_chain))
+            .await?;
+        let mut requests = VecDeque::from_iter(actions.cross_chain_requests);
+        while let Some(request) = requests.pop_front() {
+            let new_actions = node.state.handle_cross_chain_request(request).await?;
+            requests.extend(new_actions.cross_chain_requests);
+        }
+        Ok(())
     }
 }
