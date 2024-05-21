jvff/linera-protocol-archive

Refactor to add an `insert_all` method
Avoid obtaining the cache lock repeatedly in order to insert multiple
values.

linera-core/src/worker.rs
@@ -648,9 +648,9 @@ where
         // Verify that all required bytecode blobs are available, and no unrelated ones provided.
         self.check_no_missing_bytecode(block, blobs).await?;
         // Persist certificate and blobs.
-        for value in blobs {
-            self.cache_recent_value(Cow::Borrowed(value)).await;
-        }
+        self.recent_values
+            .insert_all(blobs.iter().map(Cow::Borrowed))
+            .await;
         let (result_blob, result_certificate) = tokio::join!(
             self.storage.write_hashed_certificate_values(blobs),
             self.storage.write_certificate(&certificate)

linera-io/linera-protocol

linera-core/src/worker.rs
@@ -656,9 +656,9 @@ where
         self.check_no_missing_bytecode(block, hashed_certificate_values)
             .await?;
         // Persist certificate and hashed certificate values.
-        for value in hashed_certificate_values {
-            self.cache_recent_value(Cow::Borrowed(value)).await;
-        }
+        self.recent_values
+            .insert_all(hashed_certificate_values.iter().map(Cow::Borrowed))
+            .await;
         let (result_hashed_certificate_value, result_certificate) = tokio::join!(
             self.storage
                 .write_hashed_certificate_values(hashed_certificate_values),