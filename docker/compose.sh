0b1f758f92f336cb0064036a3a67355cd5fe1b5b
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2024-06-05 00:41:42 +0100

    Added cleanup for compose and better namespacing

--- docker/compose.sh
+++ docker/compose.sh
@@ -1,9 +1,37 @@
 #!/bin/sh
 
-rm -r wallet.json linera.db
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-CONF_DIR=../configuration/compose
+ROOT_DIR=$SCRIPT_DIR/..
+CONF_DIR=$ROOT_DIR/configuration/compose
+
+cleanup_started=false
+
+# Clean up hanging volumes when the script is terminated.
+cleanup() {
+  if [ "$cleanup_started" = true ]; then
+      exit 0
+    fi
+  cleanup_started=true
+  rm committee.json
+  rm genesis.json
+  rm -r linera.db
+  rm server.json
+  rm wallet.json
+  SCYLLA_VOLUME=docker_linera-scylla-data
+  SHARED_VOLUME=docker_linera-shared
+  docker rm -f $(docker ps -a -q --filter volume=$SCYLLA_VOLUME)
+  docker volume rm $SCYLLA_VOLUME
+  docker rm -f $(docker ps -a -q --filter volume=$SHARED_VOLUME)
+  docker volume rm $SHARED_VOLUME
+}
+
+trap cleanup EXIT INT
+
+cd "$ROOT_DIR"
+docker build -f docker/Dockerfile . -t linera-test
+
+cd "$SCRIPT_DIR"
+
 set -xe
 
 # Create configuration files.
@@ -16,7 +44,5 @@ linera-server generate --validators "$CONF_DIR/validator.toml" --committee commi
 # * `genesis.json` will contain the initial balances of chains as well as the initial committee.
 
 linera --wallet wallet.json --storage rocksdb:linera.db create-genesis-config 10 --genesis genesis.json --initial-funding 10 --committee committee.json --testing-prng-seed 2
-
-mv server_1.json server.json
 
 docker compose up
