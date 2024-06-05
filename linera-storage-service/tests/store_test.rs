16fb251633c361eabe12cacffc8287b13faaabbd
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-06-05 13:36:20 +0200

    Remove the entries about obtaining the storage_service.

--- linera-storage-service/tests/store_test.rs
+++ linera-storage-service/tests/store_test.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use linera_storage_service::{
-    child::{get_free_endpoint, StorageService},
     client::{create_service_test_store, service_config_from_endpoint, ServiceStoreClient},
 };
 use linera_views::{
@@ -14,52 +13,35 @@ use linera_views::{
     },
 };
 
-/// The endpoint used for the storage service tests.
-#[cfg(test)]
-fn get_storage_service_guard(endpoint: &str) -> StorageService {
-    let binary = env!("CARGO_BIN_EXE_storage_service_server").to_string();
-    StorageService::new(endpoint, binary)
-}
-
 #[tokio::test]
 async fn test_reads_service_store() {
-    let endpoint = get_free_endpoint().await.unwrap();
     for scenario in get_random_test_scenarios() {
-        let _guard = get_storage_service_guard(&endpoint).run().await;
-        let key_value_store = create_service_test_store(&endpoint).await.unwrap();
+        let key_value_store = create_service_test_store("127.0.0.1:1235").await.unwrap();
         run_reads(key_value_store, scenario).await;
     }
 }
 
 #[tokio::test]
 async fn test_service_store_writes_from_blank() {
-    let endpoint = get_free_endpoint().await.unwrap();
-    let _guard = get_storage_service_guard(&endpoint).run().await;
-    let key_value_store = create_service_test_store(&endpoint).await.unwrap();
+    let key_value_store = create_service_test_store("127.0.0.1:1235").await.unwrap();
     run_writes_from_blank(&key_value_store).await;
 }
 
 #[tokio::test]
 async fn test_service_store_writes_from_state() {
-    let endpoint = get_free_endpoint().await.unwrap();
-    let _guard = get_storage_service_guard(&endpoint).run().await;
-    let key_value_store = create_service_test_store(&endpoint).await.unwrap();
+    let key_value_store = create_service_test_store("127.0.0.1:1235").await.unwrap();
     run_writes_from_state(&key_value_store).await;
 }
 
 #[tokio::test]
 async fn test_service_admin() {
-    let endpoint = get_free_endpoint().await.unwrap();
-    let _guard = get_storage_service_guard(&endpoint).run().await;
-    let config = service_config_from_endpoint(&endpoint).expect("config");
+    let config = service_config_from_endpoint("127.0.0.1:1235").expect("config");
     admin_test::<ServiceStoreClient>(&config).await;
 }
 
 #[tokio::test]
 async fn test_service_big_raw_write() {
-    let endpoint = get_free_endpoint().await.unwrap();
-    let _guard = get_storage_service_guard(&endpoint).run().await;
-    let key_value_store = create_service_test_store(&endpoint).await.unwrap();
+    let key_value_store = create_service_test_store("127.0.0.1:1235").await.unwrap();
     let n = 5000000;
     let mut rng = test_utils::make_deterministic_rng();
     let vector = get_random_byte_vector(&mut rng, &[], n);
