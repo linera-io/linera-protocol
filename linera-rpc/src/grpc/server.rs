jvff/linera-protocol-archive

Receive `JoinSet` to spawn tasks as a parameter
Allow the caller to create the `JoinSet` to control the spawned tasks.

linera-rpc/src/grpc/server.rs
@@ -112,7 +112,6 @@ pub struct GrpcServer<S> {
 
 pub struct GrpcServerHandle {
     handle: oneshot::Receiver<Result<(), GrpcError>>,
-    _tasks: JoinSet<()>,
 }
 
 impl GrpcServerHandle {
@@ -188,6 +187,7 @@ where
         cross_chain_config: CrossChainConfig,
         notification_config: NotificationConfig,
         shutdown_signal: CancellationToken,
+        tasks: &mut JoinSet<()>,
     ) -> GrpcServerHandle {
         info!(
             "spawning gRPC server on {}:{} for shard {}",
@@ -199,8 +199,6 @@ where
 
         let (notification_sender, notification_receiver) =
             mpsc::channel(notification_config.notification_queue_size);
-
-        let mut tasks = JoinSet::new();
 
         tasks.spawn_task({
             info!(
@@ -272,10 +270,7 @@ where
             Ok(())
         });
 
-        GrpcServerHandle {
-            handle,
-            _tasks: tasks,
-        }
+        GrpcServerHandle { handle }
     }
 
     /// Continuously waits for receiver to receive a notification which is then sent to

linera-rpc/src/simple/server.rs
@@ -136,7 +136,11 @@ where
         }
     }
 
-    pub fn spawn(self, shutdown_signal: CancellationToken) -> ServerHandle {
+    pub fn spawn(
+        self,
+        shutdown_signal: CancellationToken,
+        tasks: &mut JoinSet<()>,
+    ) -> ServerHandle {
         info!(
             "Listening to {:?} traffic on {}:{}",
             self.network.protocol, self.host, self.port
@@ -146,7 +150,6 @@ where
         let (cross_chain_sender, cross_chain_receiver) =
             mpsc::channel(self.cross_chain_config.queue_size);
 
-        let mut tasks = JoinSet::new();
         tasks.spawn_task(Self::forward_cross_chain_queries(
             self.state.nickname().to_string(),
             self.network.clone(),

linera-rpc/src/simple/transport.rs
@@ -85,7 +85,6 @@ pub trait MessageHandler: Clone {
 /// executing tasks.
 pub struct ServerHandle {
     pub handle: oneshot::Receiver<Result<(), std::io::Error>>,
-    _tasks: JoinSet<()>,
 }
 
 impl ServerHandle {
@@ -162,7 +161,7 @@ impl TransportProtocol {
         address: impl ToSocketAddrs + Send + 'static,
         state: S,
         shutdown_signal: CancellationToken,
-        mut tasks: JoinSet<()>,
+        tasks: &mut JoinSet<()>,
     ) -> ServerHandle
     where
         S: MessageHandler + Send + 'static,
@@ -171,11 +170,7 @@ impl TransportProtocol {
             Self::Udp => tasks.spawn_task(UdpServer::run(address, state, shutdown_signal)),
             Self::Tcp => tasks.spawn_task(TcpServer::run(address, state, shutdown_signal)),
         };
-
-        ServerHandle {
-            handle,
-            _tasks: tasks,
-        }
+        ServerHandle { handle }
     }
 }
 

linera-service/src/proxy.rs
@@ -6,7 +6,7 @@ use std::{net::SocketAddr, path::PathBuf, time::Duration};
 use anyhow::{bail, Result};
 use async_trait::async_trait;
 use futures::{SinkExt, StreamExt};
-use linera_core::node::NodeError;
+use linera_core::{node::NodeError, JoinSetExt as _};
 use linera_rpc::{
     config::{
         NetworkProtocol, ShardConfig, ValidatorInternalNetworkPreConfig,
@@ -233,6 +233,7 @@ where
     #[instrument(skip_all, fields(port = self.public_config.port, metrics_port = self.internal_config.metrics_port), err)]
     async fn run(self, shutdown_signal: CancellationToken) -> Result<()> {
         info!("Starting simple server");
+        let mut tasks = JoinSet::new();
         let address = self.get_listen_address(self.public_config.port);
 
         #[cfg(with_metrics)]
@@ -243,9 +244,12 @@ where
 
         self.public_config
             .protocol
-            .spawn_server(address, self, shutdown_signal, JoinSet::new())
+            .spawn_server(address, self, shutdown_signal, &mut tasks)
             .join()
             .await?;
+
+        tasks.await_all_tasks().await;
+
         Ok(())
     }
 

linera-service/src/server.rs
@@ -8,7 +8,7 @@ use anyhow::bail;
 use async_trait::async_trait;
 use futures::{stream::FuturesUnordered, FutureExt, StreamExt, TryFutureExt};
 use linera_base::crypto::{CryptoRng, KeyPair};
-use linera_core::worker::WorkerState;
+use linera_core::{worker::WorkerState, JoinSetExt as _};
 use linera_execution::{committee::ValidatorName, WasmRuntime, WithWasmDefault};
 use linera_rpc::{
     config::{
@@ -29,6 +29,7 @@ use linera_service::{
 use linera_storage::Storage;
 use linera_views::{common::CommonStoreConfig, views::ViewError};
 use serde::Deserialize;
+use tokio::task::JoinSet;
 use tokio_util::sync::CancellationToken;
 use tracing::{error, info};
 
@@ -73,6 +74,7 @@ impl ServerContext {
         S: Storage + Clone + Send + Sync + 'static,
         ViewError: From<S::ContextError>,
     {
+        let mut tasks = JoinSet::new();
         let handles = FuturesUnordered::new();
 
         let internal_network = self
@@ -98,7 +100,7 @@ impl ServerContext {
                 shard_id,
                 cross_chain_config,
             )
-            .spawn(shutdown_signal.clone());
+            .spawn(shutdown_signal.clone(), &mut tasks);
 
             handles.push(
                 server_handle
@@ -111,6 +113,7 @@ impl ServerContext {
         }
 
         handles.collect::<()>().await;
+        tasks.await_all_tasks().await;
     }
 
     async fn spawn_grpc<S>(
@@ -122,7 +125,9 @@ impl ServerContext {
         S: Storage + Clone + Send + Sync + 'static,
         ViewError: From<S::ContextError>,
     {
+        let mut tasks = JoinSet::new();
         let handles = FuturesUnordered::new();
+
         for (state, shard_id, shard) in states {
             #[cfg(with_metrics)]
             if let Some(port) = shard.metrics_port {
@@ -138,6 +143,7 @@ impl ServerContext {
                 self.cross_chain_config.clone(),
                 self.notification_config.clone(),
                 shutdown_signal.clone(),
+                &mut tasks,
             );
 
             handles.push(
@@ -151,6 +157,7 @@ impl ServerContext {
         }
 
         handles.collect::<()>().await;
+        tasks.await_all_tasks().await;
     }
 
     #[cfg(with_metrics)]

linera-io/linera-protocol

linera-rpc/src/grpc/server.rs
@@ -112,7 +112,6 @@ pub struct GrpcServer<S> {
 
 pub struct GrpcServerHandle {
     handle: TaskHandle<Result<(), GrpcError>>,
-    _join_set: JoinSet<()>,
 }
 
 impl GrpcServerHandle {
@@ -188,6 +187,7 @@ where
         cross_chain_config: CrossChainConfig,
         notification_config: NotificationConfig,
         shutdown_signal: CancellationToken,
+        join_set: &mut JoinSet<()>,
     ) -> GrpcServerHandle {
         info!(
             "spawning gRPC server on {}:{} for shard {}",
@@ -199,8 +199,6 @@ where
 
         let (notification_sender, notification_receiver) =
             mpsc::channel(notification_config.notification_queue_size);
-
-        let mut join_set = JoinSet::new();
 
         join_set.spawn_task({
             info!(
@@ -272,10 +270,7 @@ where
             Ok(())
         });
 
-        GrpcServerHandle {
-            handle,
-            _join_set: join_set,
-        }
+        GrpcServerHandle { handle }
     }
 
     /// Continuously waits for receiver to receive a notification which is then sent to

linera-rpc/src/simple/server.rs
@@ -136,7 +136,11 @@ where
         }
     }
 
-    pub fn spawn(self, shutdown_signal: CancellationToken) -> ServerHandle {
+    pub fn spawn(
+        self,
+        shutdown_signal: CancellationToken,
+        join_set: &mut JoinSet<()>,
+    ) -> ServerHandle {
         info!(
             "Listening to {:?} traffic on {}:{}",
             self.network.protocol, self.host, self.port
@@ -146,7 +150,6 @@ where
         let (cross_chain_sender, cross_chain_receiver) =
             mpsc::channel(self.cross_chain_config.queue_size);
 
-        let mut join_set = JoinSet::new();
         join_set.spawn_task(Self::forward_cross_chain_queries(
             self.state.nickname().to_string(),
             self.network.clone(),

linera-rpc/src/simple/transport.rs
@@ -85,7 +85,6 @@ pub trait MessageHandler: Clone {
 /// executing tasks.
 pub struct ServerHandle {
     pub handle: TaskHandle<Result<(), std::io::Error>>,
-    _join_set: JoinSet<()>,
 }
 
 impl ServerHandle {
@@ -162,7 +161,7 @@ impl TransportProtocol {
         address: impl ToSocketAddrs + Send + 'static,
         state: S,
         shutdown_signal: CancellationToken,
-        mut join_set: JoinSet<()>,
+        join_set: &mut JoinSet<()>,
     ) -> ServerHandle
     where
         S: MessageHandler + Send + 'static,
@@ -171,11 +170,7 @@ impl TransportProtocol {
             Self::Udp => join_set.spawn_task(UdpServer::run(address, state, shutdown_signal)),
             Self::Tcp => join_set.spawn_task(TcpServer::run(address, state, shutdown_signal)),
         };
-
-        ServerHandle {
-            handle,
-            _join_set: join_set,
-        }
+        ServerHandle { handle }
     }
 }
 

linera-service/src/proxy.rs
@@ -6,7 +6,7 @@ use std::{net::SocketAddr, path::PathBuf, time::Duration};
 use anyhow::{bail, Result};
 use async_trait::async_trait;
 use futures::{SinkExt, StreamExt};
-use linera_core::node::NodeError;
+use linera_core::{node::NodeError, JoinSetExt as _};
 use linera_rpc::{
     config::{
         NetworkProtocol, ShardConfig, ValidatorInternalNetworkPreConfig,
@@ -233,6 +233,7 @@ where
     #[instrument(skip_all, fields(port = self.public_config.port, metrics_port = self.internal_config.metrics_port), err)]
     async fn run(self, shutdown_signal: CancellationToken) -> Result<()> {
         info!("Starting simple server");
+        let mut join_set = JoinSet::new();
         let address = self.get_listen_address(self.public_config.port);
 
         #[cfg(with_metrics)]
@@ -243,9 +244,12 @@ where
 
         self.public_config
             .protocol
-            .spawn_server(address, self, shutdown_signal, JoinSet::new())
+            .spawn_server(address, self, shutdown_signal, &mut join_set)
             .join()
             .await?;
+
+        join_set.await_all_tasks().await;
+
         Ok(())
     }
 

linera-service/src/server.rs
@@ -8,7 +8,7 @@ use anyhow::bail;
 use async_trait::async_trait;
 use futures::{stream::FuturesUnordered, FutureExt, StreamExt, TryFutureExt};
 use linera_base::crypto::{CryptoRng, KeyPair};
-use linera_core::worker::WorkerState;
+use linera_core::{worker::WorkerState, JoinSetExt as _};
 use linera_execution::{committee::ValidatorName, WasmRuntime, WithWasmDefault};
 use linera_rpc::{
     config::{
@@ -29,6 +29,7 @@ use linera_service::{
 use linera_storage::Storage;
 use linera_views::{common::CommonStoreConfig, views::ViewError};
 use serde::Deserialize;
+use tokio::task::JoinSet;
 use tokio_util::sync::CancellationToken;
 use tracing::{error, info};
 
@@ -73,6 +74,7 @@ impl ServerContext {
         S: Storage + Clone + Send + Sync + 'static,
         ViewError: From<S::ContextError>,
     {
+        let mut join_set = JoinSet::new();
         let handles = FuturesUnordered::new();
 
         let internal_network = self
@@ -98,7 +100,7 @@ impl ServerContext {
                 shard_id,
                 cross_chain_config,
             )
-            .spawn(shutdown_signal.clone());
+            .spawn(shutdown_signal.clone(), &mut join_set);
 
             handles.push(
                 server_handle
@@ -111,6 +113,7 @@ impl ServerContext {
         }
 
         handles.collect::<()>().await;
+        join_set.await_all_tasks().await;
     }
 
     async fn spawn_grpc<S>(
@@ -122,7 +125,9 @@ impl ServerContext {
         S: Storage + Clone + Send + Sync + 'static,
         ViewError: From<S::ContextError>,
     {
+        let mut join_set = JoinSet::new();
         let handles = FuturesUnordered::new();
+
         for (state, shard_id, shard) in states {
             #[cfg(with_metrics)]
             if let Some(port) = shard.metrics_port {
@@ -138,6 +143,7 @@ impl ServerContext {
                 self.cross_chain_config.clone(),
                 self.notification_config.clone(),
                 shutdown_signal.clone(),
+                &mut join_set,
             );
 
             handles.push(
@@ -151,6 +157,7 @@ impl ServerContext {
         }
 
         handles.collect::<()>().await;
+        join_set.await_all_tasks().await;
     }
 
     #[cfg(with_metrics)]