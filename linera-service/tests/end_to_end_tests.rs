b0aa700890e01190cf75132fc26aaece8ac1bb81
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-06-13 16:29:23 +0200

    Change the position of the request applications.

--- linera-service/tests/end_to_end_tests.rs
+++ linera-service/tests/end_to_end_tests.rs
@@ -1511,6 +1511,7 @@ async fn test_wasm_end_to_end_matching_engine(config: impl LineraNetConfig) -> R
     node_service_a
         .request_application(&chain_a, &application_id_matching)
         .await?;
+    node_service_a.process_inbox(&chain_a).await?;
     let app_matching_a = MatchingEngineApp(
         node_service_a
             .make_application(&chain_a, &application_id_matching)
@@ -1519,14 +1520,12 @@ async fn test_wasm_end_to_end_matching_engine(config: impl LineraNetConfig) -> R
     node_service_b
         .request_application(&chain_b, &application_id_matching)
         .await?;
+    node_service_b.process_inbox(&chain_b).await?;
     let app_matching_b = MatchingEngineApp(
         node_service_b
             .make_application(&chain_b, &application_id_matching)
             .await?,
     );
-    node_service_admin.process_inbox(&chain_admin).await?;
-    node_service_a.process_inbox(&chain_a).await?;
-    node_service_b.process_inbox(&chain_b).await?;
 
     // Now creating orders
     for price in [1, 2] {
