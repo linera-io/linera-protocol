40c333c542374bbd8c8402a7f9d5c9d78c6ef5af
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-06-17 11:07:26 +0200

    Relevant changes to the docker stuff.

--- docker/compose.sh
+++ docker/compose.sh
@@ -53,10 +53,14 @@ set -xe
 # * `committee.json` is the public description of the Linera committee.
 linera-server generate --validators "$CONF_DIR/validator.toml" --committee committee.json --testing-prng-seed 1
 
+# creating the linera-storage-service
+storage_service_server memory --endpoint 127.0.0.1:1235 &
+sleep 2
+
 # Create configuration files for 10 user chains.
 # * Private chain states are stored in one local wallet `wallet.json`.
 # * `genesis.json` will contain the initial balances of chains as well as the initial committee.
 
-linera --wallet wallet.json --storage rocksdb:linera.db create-genesis-config 10 --genesis genesis.json --initial-funding 10 --committee committee.json --testing-prng-seed 2
+linera --wallet wallet.json --storage service:tcp:127.0.0.1:1235:linera_docker_db create-genesis-config 10 --genesis genesis.json --initial-funding 10 --committee committee.json --testing-prng-seed 2
 
 docker compose up
