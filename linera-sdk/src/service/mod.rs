48fac7d2f225a30f73b557d086df30981548d33b
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-06-21 13:24:14 +0000

    Store application service type in global variable
    Keep it running while the Wasm module is running.

--- linera-sdk/src/service/mod.rs
+++ linera-sdk/src/service/mod.rs
@@ -12,8 +12,6 @@ mod test_runtime;
 #[doc(hidden)]
 pub mod wit;
 
-use std::future::Future;
-
 #[cfg(not(with_testing))]
 pub use self::runtime::ServiceRuntime;
 #[cfg(with_testing)]
@@ -34,18 +32,22 @@ pub type ServiceRuntime<Application> = MockServiceRuntime<Application>;
 #[macro_export]
 macro_rules! service {
     ($service:ident) => {
+        #[doc(hidden)]
+        static mut SERVICE: Option<$service> = None;
+
         /// Export the service interface.
         $crate::export_service!($service with_types_in $crate::service::wit);
 
         /// Mark the service type to be exported.
         impl $crate::service::wit::exports::linera::app::service_entrypoints::Guest for $service {
             fn handle_query(argument: Vec<u8>) -> Vec<u8> {
+                use $crate::util::BlockingWait;
                 let request = $crate::serde_json::from_slice(&argument)
                     .expect("Query is invalid and could not be deserialized");
-                let response = $crate::service::run_async_entrypoint(move |runtime| async move {
-                    let service = <$service as $crate::Service>::new(runtime).await;
-                    service.handle_query(request).await
-                });
+                let response = $crate::service::run_async_entrypoint(
+                    unsafe { &mut SERVICE },
+                    move |service| service.handle_query(request).blocking_wait(),
+                );
                 $crate::serde_json::to_vec(&response)
                     .expect("Failed to deserialize query response")
             }
@@ -60,14 +62,17 @@ macro_rules! service {
 
 /// Runs an asynchronous entrypoint in a blocking manner, by repeatedly polling the entrypoint
 /// future.
-pub fn run_async_entrypoint<Service, Entrypoint, Output>(
-    entrypoint: impl FnOnce(ServiceRuntime<Service>) -> Entrypoint,
+pub fn run_async_entrypoint<Service, Output>(
+    service: &mut Option<Service>,
+    entrypoint: impl FnOnce(&mut Service) -> Output,
 ) -> Output
 where
     Service: crate::Service,
-    Entrypoint: Future<Output = Output>,
 {
     ServiceLogger::install();
 
-    entrypoint(ServiceRuntime::new()).blocking_wait()
+    let service =
+        service.get_or_insert_with(|| Service::new(ServiceRuntime::new()).blocking_wait());
+
+    entrypoint(service)
 }
