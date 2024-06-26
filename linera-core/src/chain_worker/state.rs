4b617c4f6aca9333bd6227dbcc78e02547be3784
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-06-26 14:40:09 -0300

    Fix further incorrect leftover changes to `ChainStateView` (#2189)
    * Move `read_certificate` method
    
    Ensure that if it changes the `ChainStateView` in any way, the changes
    are rolled back at the end of the method's execution.
    
    * Move `find_event_in_inbox` method
    
    Ensure that if it changes the `ChainStateView` in any way, the changes
    are rolled back at the end of the method's execution.
    
    * Move `query_application` method
    
    Ensure that if it changes the `ChainStateView` in any way, the changes
    are rolled back at the end of the method's execution.
    
    * Move `read_bytecode_location` method
    
    Ensure that if it changes the `ChainStateView` in any way, the changes
    are rolled back at the end of the method's execution.
    
    * Move `describe_application` method
    
    Ensure that if it changes the `ChainStateView` in any way, the changes
    are rolled back at the end of the method's execution.
    
    * Move out new `prepare_chain_info_response` method
    
    Ensure that if it changes the `ChainStateView` in any way, the changes
    are rolled back at the end of the method's execution.
    
    * Change named field with unnamed field
    
    Reduce visual noise, since the type only has one field.

--- linera-core/src/chain_worker/state.rs
+++ linera-core/src/chain_worker/state.rs
@@ -117,13 +117,9 @@ where
         &mut self,
         height: BlockHeight,
     ) -> Result<Option<Certificate>, WorkerError> {
-        self.ensure_is_active()?;
-        let certificate_hash = match self.chain.confirmed_log.get(height.try_into()?).await? {
-            Some(hash) => hash,
-            None => return Ok(None),
-        };
-        let certificate = self.storage.read_certificate(certificate_hash).await?;
-        Ok(Some(certificate))
+        ChainWorkerStateWithTemporaryChanges(self)
+            .read_certificate(height)
+            .await
     }
 
     /// Searches for an event in one of the chain's inboxes.
@@ -135,26 +131,16 @@ where
         height: BlockHeight,
         index: u32,
     ) -> Result<Option<Event>, WorkerError> {
-        self.ensure_is_active()?;
-
-        let mut inbox = self.chain.inboxes.try_load_entry_mut(&inbox_id).await?;
-        let mut events = inbox.added_events.iter_mut().await?;
-
-        Ok(events
-            .find(|event| {
-                event.certificate_hash == certificate_hash
-                    && event.height == height
-                    && event.index == index
-            })
-            .cloned())
+        ChainWorkerStateWithTemporaryChanges(self)
+            .find_event_in_inbox(inbox_id, certificate_hash, height, index)
+            .await
     }
 
     /// Queries an application's state on the chain.
     pub async fn query_application(&mut self, query: Query) -> Result<Response, WorkerError> {
-        self.ensure_is_active()?;
-        let local_time = self.storage.clock().current_time();
-        let response = self.chain.query_application(local_time, query).await?;
-        Ok(response)
+        ChainWorkerStateWithTemporaryChanges(self)
+            .query_application(query)
+            .await
     }
 
     /// Returns the [`BytecodeLocation`] for the requested [`BytecodeId`], if it is known by the
@@ -164,9 +150,9 @@ where
         &mut self,
         bytecode_id: BytecodeId,
     ) -> Result<Option<BytecodeLocation>, WorkerError> {
-        self.ensure_is_active()?;
-        let response = self.chain.read_bytecode_location(bytecode_id).await?;
-        Ok(response)
+        ChainWorkerStateWithTemporaryChanges(self)
+            .read_bytecode_location(bytecode_id)
+            .await
     }
 
     /// Returns an application's description.
@@ -174,9 +160,9 @@ where
         &mut self,
         application_id: UserApplicationId,
     ) -> Result<UserApplicationDescription, WorkerError> {
-        self.ensure_is_active()?;
-        let response = self.chain.describe_application(application_id).await?;
-        Ok(response)
+        ChainWorkerStateWithTemporaryChanges(self)
+            .describe_application(application_id)
+            .await
     }
 
     /// Executes a block without persisting any changes to the state.
@@ -184,7 +170,7 @@ where
         &mut self,
         block: Block,
     ) -> Result<(ExecutedBlock, ChainInfoResponse), WorkerError> {
-        ChainWorkerStateWithTemporaryChanges { state: self }
+        ChainWorkerStateWithTemporaryChanges(self)
             .stage_block_execution(block)
             .await
     }
@@ -204,7 +190,7 @@ where
         &mut self,
         proposal: BlockProposal,
     ) -> Result<(ChainInfoResponse, NetworkActions), WorkerError> {
-        let maybe_validation_outcome = ChainWorkerStateWithTemporaryChanges { state: self }
+        let maybe_validation_outcome = ChainWorkerStateWithTemporaryChanges(self)
             .validate_block_proposal(&proposal)
             .await?;
 
@@ -281,70 +267,9 @@ where
                 .vote_for_fallback()
                 .await?;
         }
-        let mut info = ChainInfo::from(&self.chain);
-        if query.request_committees {
-            info.requested_committees =
-                Some(self.chain.execution_state.system.committees.get().clone());
-        }
-        if let Some(owner) = query.request_owner_balance {
-            info.requested_owner_balance = self
-                .chain
-                .execution_state
-                .system
-                .balances
-                .get(&owner)
-                .await?;
-        }
-        if let Some(next_block_height) = query.test_next_block_height {
-            ensure!(
-                self.chain.tip_state.get().next_block_height == next_block_height,
-                WorkerError::UnexpectedBlockHeight {
-                    expected_block_height: next_block_height,
-                    found_block_height: self.chain.tip_state.get().next_block_height
-                }
-            );
-        }
-        if query.request_pending_messages {
-            let mut messages = Vec::new();
-            let origins = self.chain.inboxes.indices().await?;
-            let inboxes = self.chain.inboxes.try_load_entries(&origins).await?;
-            let action = if *self.chain.execution_state.system.closed.get() {
-                MessageAction::Reject
-            } else {
-                MessageAction::Accept
-            };
-            for (origin, inbox) in origins.into_iter().zip(inboxes) {
-                for event in inbox.added_events.elements().await? {
-                    messages.push(IncomingMessage {
-                        origin: origin.clone(),
-                        event: event.clone(),
-                        action,
-                    });
-                }
-            }
-
-            info.requested_pending_messages = messages;
-        }
-        if let Some(range) = query.request_sent_certificate_hashes_in_range {
-            let start: usize = range.start.try_into()?;
-            let end = match range.limit {
-                None => self.chain.confirmed_log.count(),
-                Some(limit) => start
-                    .checked_add(usize::try_from(limit).map_err(|_| ArithmeticError::Overflow)?)
-                    .ok_or(ArithmeticError::Overflow)?
-                    .min(self.chain.confirmed_log.count()),
-            };
-            let keys = self.chain.confirmed_log.read(start..end).await?;
-            info.requested_sent_certificate_hashes = keys;
-        }
-        if let Some(start) = query.request_received_log_excluding_first_nth {
-            let start = usize::try_from(start).map_err(|_| ArithmeticError::Overflow)?;
-            info.requested_received_log = self.chain.received_log.read(start..).await?;
-        }
-        if query.request_manager_values {
-            info.manager.add_values(self.chain.manager.get());
-        }
-        let response = ChainInfoResponse::new(info, self.config.key_pair());
+        let response = ChainWorkerStateWithTemporaryChanges(self)
+            .prepare_chain_info_response(query)
+            .await?;
         // Trigger any outgoing cross-chain messages that haven't been confirmed yet.
         let actions = self.create_network_actions().await?;
         Ok((response, actions))
@@ -552,37 +477,104 @@ where
 }
 
 /// Wrapper type that rolls back changes to the `chain` state when dropped.
-pub struct ChainWorkerStateWithTemporaryChanges<'state, StorageClient>
+pub struct ChainWorkerStateWithTemporaryChanges<'state, StorageClient>(
+    &'state mut ChainWorkerState<StorageClient>,
+)
 where
     StorageClient: Storage + Clone + Send + Sync + 'static,
-    ViewError: From<StorageClient::ContextError>,
-{
-    state: &'state mut ChainWorkerState<StorageClient>,
-}
+    ViewError: From<StorageClient::ContextError>;
 
 impl<StorageClient> ChainWorkerStateWithTemporaryChanges<'_, StorageClient>
 where
     StorageClient: Storage + Clone + Send + Sync + 'static,
     ViewError: From<StorageClient::ContextError>,
 {
+    /// Returns a stored [`Certificate`] for the chain's block at the requested [`BlockHeight`].
+    #[cfg(with_testing)]
+    pub async fn read_certificate(
+        &mut self,
+        height: BlockHeight,
+    ) -> Result<Option<Certificate>, WorkerError> {
+        self.0.ensure_is_active()?;
+        let certificate_hash = match self.0.chain.confirmed_log.get(height.try_into()?).await? {
+            Some(hash) => hash,
+            None => return Ok(None),
+        };
+        let certificate = self.0.storage.read_certificate(certificate_hash).await?;
+        Ok(Some(certificate))
+    }
+
+    /// Searches for an event in one of the chain's inboxes.
+    #[cfg(with_testing)]
+    pub async fn find_event_in_inbox(
+        &mut self,
+        inbox_id: Origin,
+        certificate_hash: CryptoHash,
+        height: BlockHeight,
+        index: u32,
+    ) -> Result<Option<Event>, WorkerError> {
+        self.0.ensure_is_active()?;
+
+        let mut inbox = self.0.chain.inboxes.try_load_entry_mut(&inbox_id).await?;
+        let mut events = inbox.added_events.iter_mut().await?;
+
+        Ok(events
+            .find(|event| {
+                event.certificate_hash == certificate_hash
+                    && event.height == height
+                    && event.index == index
+            })
+            .cloned())
+    }
+
+    /// Queries an application's state on the chain.
+    pub async fn query_application(&mut self, query: Query) -> Result<Response, WorkerError> {
+        self.0.ensure_is_active()?;
+        let local_time = self.0.storage.clock().current_time();
+        let response = self.0.chain.query_application(local_time, query).await?;
+        Ok(response)
+    }
+
+    /// Returns the [`BytecodeLocation`] for the requested [`BytecodeId`], if it is known by the
+    /// chain.
+    #[cfg(with_testing)]
+    pub async fn read_bytecode_location(
+        &mut self,
+        bytecode_id: BytecodeId,
+    ) -> Result<Option<BytecodeLocation>, WorkerError> {
+        self.0.ensure_is_active()?;
+        let response = self.0.chain.read_bytecode_location(bytecode_id).await?;
+        Ok(response)
+    }
+
+    /// Returns an application's description.
+    pub async fn describe_application(
+        &mut self,
+        application_id: UserApplicationId,
+    ) -> Result<UserApplicationDescription, WorkerError> {
+        self.0.ensure_is_active()?;
+        let response = self.0.chain.describe_application(application_id).await?;
+        Ok(response)
+    }
+
     /// Executes a block without persisting any changes to the state.
     pub async fn stage_block_execution(
         &mut self,
         block: Block,
     ) -> Result<(ExecutedBlock, ChainInfoResponse), WorkerError> {
-        self.state.ensure_is_active()?;
+        self.0.ensure_is_active()?;
 
-        let local_time = self.state.storage.clock().current_time();
+        let local_time = self.0.storage.clock().current_time();
         let signer = block.authenticated_signer;
 
-        let executed_block = Box::pin(self.state.chain.execute_block(&block, local_time, None))
+        let executed_block = Box::pin(self.0.chain.execute_block(&block, local_time, None))
             .await?
             .with(block);
 
-        let mut response = ChainInfoResponse::new(&self.state.chain, None);
+        let mut response = ChainInfoResponse::new(&self.0.chain, None);
         if let Some(signer) = signer {
             response.info.requested_owner_balance = self
-                .state
+                .0
                 .chain
                 .execution_state
                 .system
@@ -618,10 +610,10 @@ where
                 "Must contain a certificate if and only if it contains oracle records".to_string()
             )
         );
-        self.state.ensure_is_active()?;
+        self.0.ensure_is_active()?;
         // Check the epoch.
         let (epoch, committee) = self
-            .state
+            .0
             .chain
             .execution_state
             .system
@@ -630,7 +622,7 @@ where
         check_block_epoch(epoch, block)?;
         // Check the authentication of the block.
         let public_key = self
-            .state
+            .0
             .chain
             .manager
             .get()
@@ -646,47 +638,32 @@ where
         }
         // Check if the chain is ready for this new block proposal.
         // This should always pass for nodes without voting key.
-        self.state
-            .chain
-            .tip_state
-            .get()
-            .verify_block_chaining(block)?;
-        if self
-            .state
-            .chain
-            .manager
-            .get()
-            .check_proposed_block(proposal)?
-            == manager::Outcome::Skip
-        {
+        self.0.chain.tip_state.get().verify_block_chaining(block)?;
+        if self.0.chain.manager.get().check_proposed_block(proposal)? == manager::Outcome::Skip {
             return Ok(None);
         }
         // Update the inboxes so that we can verify the provided hashed certificate values are
         // legitimately required.
         // Actual execution happens below, after other validity checks.
-        self.state.chain.remove_events_from_inboxes(block).await?;
+        self.0.chain.remove_events_from_inboxes(block).await?;
         // Verify that all required bytecode hashed certificate values are available, and no
         // unrelated ones provided.
-        self.state
+        self.0
             .check_no_missing_blobs(block, hashed_certificate_values, hashed_blobs)
             .await?;
         // Write the values so that the bytecode is available during execution.
-        self.state
+        self.0
             .storage
             .write_hashed_certificate_values(hashed_certificate_values)
             .await?;
-        let local_time = self.state.storage.clock().current_time();
+        let local_time = self.0.storage.clock().current_time();
         ensure!(
-            block.timestamp.duration_since(local_time) <= self.state.config.grace_period,
+            block.timestamp.duration_since(local_time) <= self.0.config.grace_period,
             WorkerError::InvalidTimestamp
         );
-        self.state
-            .storage
-            .clock()
-            .sleep_until(block.timestamp)
-            .await;
-        let local_time = self.state.storage.clock().current_time();
-        let outcome = Box::pin(self.state.chain.execute_block(
+        self.0.storage.clock().sleep_until(block.timestamp).await;
+        let local_time = self.0.storage.clock().current_time();
+        let outcome = Box::pin(self.0.chain.execute_block(
             block,
             local_time,
             oracle_records.clone(),
@@ -709,14 +686,80 @@ where
             );
         }
         // Check if the counters of tip_state would be valid.
-        self.state
+        self.0
             .chain
             .tip_state
             .get()
             .verify_counters(block, &outcome)?;
         // Verify that the resulting chain would have no unconfirmed incoming messages.
-        self.state.chain.validate_incoming_messages().await?;
+        self.0.chain.validate_incoming_messages().await?;
         Ok(Some((outcome, local_time)))
+    }
+
+    /// Prepares a [`ChainInfoResponse`] for a [`ChainInfoQuery`].
+    pub async fn prepare_chain_info_response(
+        &mut self,
+        query: ChainInfoQuery,
+    ) -> Result<ChainInfoResponse, WorkerError> {
+        let chain = &self.0.chain;
+        let mut info = ChainInfo::from(chain);
+        if query.request_committees {
+            info.requested_committees = Some(chain.execution_state.system.committees.get().clone());
+        }
+        if let Some(owner) = query.request_owner_balance {
+            info.requested_owner_balance =
+                chain.execution_state.system.balances.get(&owner).await?;
+        }
+        if let Some(next_block_height) = query.test_next_block_height {
+            ensure!(
+                chain.tip_state.get().next_block_height == next_block_height,
+                WorkerError::UnexpectedBlockHeight {
+                    expected_block_height: next_block_height,
+                    found_block_height: chain.tip_state.get().next_block_height
+                }
+            );
+        }
+        if query.request_pending_messages {
+            let mut messages = Vec::new();
+            let origins = chain.inboxes.indices().await?;
+            let inboxes = chain.inboxes.try_load_entries(&origins).await?;
+            let action = if *chain.execution_state.system.closed.get() {
+                MessageAction::Reject
+            } else {
+                MessageAction::Accept
+            };
+            for (origin, inbox) in origins.into_iter().zip(inboxes) {
+                for event in inbox.added_events.elements().await? {
+                    messages.push(IncomingMessage {
+                        origin: origin.clone(),
+                        event: event.clone(),
+                        action,
+                    });
+                }
+            }
+
+            info.requested_pending_messages = messages;
+        }
+        if let Some(range) = query.request_sent_certificate_hashes_in_range {
+            let start: usize = range.start.try_into()?;
+            let end = match range.limit {
+                None => chain.confirmed_log.count(),
+                Some(limit) => start
+                    .checked_add(usize::try_from(limit).map_err(|_| ArithmeticError::Overflow)?)
+                    .ok_or(ArithmeticError::Overflow)?
+                    .min(chain.confirmed_log.count()),
+            };
+            let keys = chain.confirmed_log.read(start..end).await?;
+            info.requested_sent_certificate_hashes = keys;
+        }
+        if let Some(start) = query.request_received_log_excluding_first_nth {
+            let start = usize::try_from(start).map_err(|_| ArithmeticError::Overflow)?;
+            info.requested_received_log = chain.received_log.read(start..).await?;
+        }
+        if query.request_manager_values {
+            info.manager.add_values(chain.manager.get());
+        }
+        Ok(ChainInfoResponse::new(info, self.0.config.key_pair()))
     }
 }
 
@@ -726,7 +769,7 @@ where
     ViewError: From<StorageClient::ContextError>,
 {
     fn drop(&mut self) {
-        self.state.chain.rollback();
+        self.0.chain.rollback();
     }
 }
 
