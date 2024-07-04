ea4cbb40c9d57012f26e43d85376c9a3414ed87a
James Kay <james.kay@linera.io>
2024-07-04 21:35:58 +0100

    `linera-client`: fix block height race (#2221)
    * Move `highest_validated_block` from `ChainClient` to `ChainManagerInfo`
    
    * `linera-core`: take a mutex whilst building a block

--- linera-chain/src/manager.rs
+++ linera-chain/src/manager.rs
@@ -85,7 +85,7 @@ use tracing::error;
 
 use crate::{
     data_types::{
-        BlockExecutionOutcome, BlockProposal, Certificate, CertificateValue,
+        Block, BlockExecutionOutcome, BlockProposal, Certificate, CertificateValue,
         HashedCertificateValue, LiteVote, ProposalContent, Vote,
     },
     ChainError,
@@ -613,5 +613,23 @@ impl ChainManagerInfo {
             .as_ref()
             .map(|vote| Box::new(vote.value.clone()));
         self.pending_blobs = manager.pending_blobs.clone();
+    }
+
+    /// Gets the highest validated block.
+    pub fn highest_validated_block(&self) -> Option<&Block> {
+        if let Some(certificate) = &self.requested_locked {
+            let block = certificate.value().block();
+            if block.is_some() {
+                return block;
+            }
+        }
+
+        if let Some(proposal) = &self.requested_proposed {
+            if proposal.content.round.is_fast() {
+                return Some(&proposal.content.block);
+            }
+        }
+
+        None
     }
 }
