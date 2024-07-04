cd11532b89d689d953d51607518652104040014c
Andreas Fackler <andreas.fackler@linera.io>
2024-07-04 13:44:13 +0200

    Extend the hex-game README with detailed usage instructions.

--- linera-service/tests/readme_test.rs
+++ linera-service/tests/readme_test.rs
@@ -16,6 +16,7 @@ use tokio::{process::Command, time::Duration};
 #[test_case::test_case("../examples/crowd-funding" ; "crowd funding")]
 #[test_case::test_case("../examples/fungible" ; "fungible")]
 #[test_case::test_case("../examples/gen-nft" ; "gen-nft")]
+#[test_case::test_case("../examples/hex-game" ; "hex-game")]
 #[test_case::test_case("../examples/native-fungible" ; "native-fungible")]
 #[test_case::test_case("../examples/non-fungible" ; "non-fungible")]
 #[test_case::test_case("../examples/matching-engine" ; "matching engine")]
