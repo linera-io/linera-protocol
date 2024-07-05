6d7e89818907b081c43f4c9572c7c8e27075d8d6
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-07-05 20:32:47 +0200

    Set the `self.hash` to `None` in the `hashable_wrapper` (#2208)
    * Reset the hash to `None` in case of `delete_view = true`.

--- linera-views/tests/hashable_tests.rs
+++ linera-views/tests/hashable_tests.rs
@@ -1,11 +1,12 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use anyhow::Result;
 use linera_views::{
     common::HasherOutput,
     hashable_wrapper::WrappedHashableContainerView,
     memory::create_memory_context,
-    register_view::RegisterView,
+    register_view::{HashedRegisterView, RegisterView},
     views::{HashableView, View},
 };
 use linera_views_derive::CryptoHashRootView;
@@ -18,10 +19,25 @@ struct TestType<C> {
 
 // TODO(#560): Implement the same for CryptoHash
 #[tokio::test]
-async fn check_hashable_container_hash() {
+async fn check_hashable_container_hash() -> Result<()> {
     let context = create_memory_context();
-    let test = TestType::load(context).await.unwrap();
-    let hash1 = test.inner.hash().await.unwrap();
-    let hash2 = test.wrap.hash().await.unwrap();
+    let test = TestType::load(context).await?;
+    let hash1 = test.inner.hash().await?;
+    let hash2 = test.wrap.hash().await?;
     assert_eq!(hash1, hash2);
+    Ok(())
+}
+
+#[tokio::test]
+async fn check_hashable_hash() -> Result<()> {
+    let context = create_memory_context();
+    let mut view = HashedRegisterView::<_, u32>::load(context).await?;
+    let hash0 = view.hash().await?;
+    let val = view.get_mut();
+    *val = 32;
+    let hash32 = view.hash().await?;
+    assert_ne!(hash0, hash32);
+    view.clear();
+    assert_eq!(hash0, view.hash().await?);
+    Ok(())
 }
