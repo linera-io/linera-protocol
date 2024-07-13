108fbbf91c1ee95d810428a032ce65c800198c86
Nirajsah <nirajsahy4aj@gmail.com>
2024-07-13 20:51:52 +0530

    chore: adding user chain

--- examples/social/web-frontend/src/components/Navbar.js
+++ examples/social/web-frontend/src/components/Navbar.js
@@ -1,9 +1,23 @@
-export default function Navbar() {
+import Logo from '../assets/linera.png'
+export default function Navbar({ chainId }) {
   return (
-    <div className="w-full flex justify-center bg-transparent fixed backdrop-blur-xl z-10">
+    <div className="w-full h-[80px] flex justify-center bg-transparent fixed backdrop-blur-xl z-10">
       <div className="w-full px-10 py-5 flex max-w-[1380px] justify-between">
-        <div>Navbar</div>
-        <div>Wallet Address</div>
+        <div className="w-10 h-10 flex items-center gap-1">
+          <img src={Logo} className="w-full h-full object-contain" />
+          <h1 className="text-xl font-bold font-sans">Casual</h1>
+        </div>
+        <div className="flex items-center gap-1">
+          <h1 className="font-semibold text-sm">{chainId}</h1>
+          <div className="w-12 h-12 rounded-full">
+            <img
+              width="full"
+              height="full"
+              src="https://img.icons8.com/fluency/48/test-account--v1.png"
+              alt="test-account--v1"
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
