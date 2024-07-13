3b81b875becd6f99e5b7abba61b3a92c5df503de
Nirajsah <nirajsahy4aj@gmail.com>
2024-07-13 20:54:55 +0530

    chore: fetching data and subscriptions

--- examples/social/web-frontend/src/App.js
+++ examples/social/web-frontend/src/App.js
@@ -1,17 +1,65 @@
+import React from 'react'
 import Navbar from './components/Navbar'
 import NewPost from './components/NewPost'
 import PostCard from './components/PostCard'
+import LeftSideMenu from './components/LeftSideMenu'
+import RightSideMenu from './components/RightSideMenu'
+import { gql, useSubscription, useQuery, useLazyQuery } from '@apollo/client'
 
-function App({ chainId, onwer }) {
+const NOTIFICATION_SUBSCRIPTION = gql`
+  subscription Notifications($chainId: ID!) {
+    notifications(chainId: $chainId)
+  }
+`
+export default function App({ chainId, onwer }) {
+  const [posts, setPosts] = React.useState([])
+
+  const [receivedPosts, { called }] = useLazyQuery(
+    gql`
+      query {
+        receivedPosts {
+          entries {
+            value
+          }
+        }
+      }
+    `,
+    {
+      onCompleted: (data) => {
+        console.log('data', data)
+        setPosts(data.receivedPosts.entries)
+      },
+      fetchPolicy: 'network-only',
+    }
+  )
+  useSubscription(NOTIFICATION_SUBSCRIPTION, {
+    variables: { chainId },
+    onData: () => receivedPosts(),
+  })
+  if (!called) {
+    void receivedPosts()
+  }
+
   return (
     <div className="w-full flex items-center flex-col">
-      <Navbar />
-      <div className="mt-20 flex flex-col gap-3">
-        <NewPost />
-        <PostCard />
+      <Navbar chainId={chainId} />
+      <div className="mt-20 p-2 w-full max-w-[1320px] relative min-h-screen flex gap-3 justify-center mb-20">
+        <div className="h-full fixed left-[180px]">
+          <LeftSideMenu />
+        </div>
+        <div className="flex flex-col gap-3">
+          <NewPost chainId={chainId} />
+          {posts &&
+            posts?.map((post, index) => (
+              <div key={index}>
+                <PostCard post={post} />
+              </div>
+            ))}
+        </div>
+        <div className="h-fit w-[410px] fixed right-[70px]">
+          <RightSideMenu />
+        </div>
       </div>
     </div>
   )
 }
-
-export default App
