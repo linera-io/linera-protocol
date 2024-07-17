8582c2c8e8acd6142cf0b073b7e1767e8f3096d2
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-07-17 02:45:22 +0000

    Unfreeze time in long-lived service tests
    The test uses real network connections, so pausing time causes all
    timeouts to fire while waiting for a response.

--- linera-core/src/unit_tests/worker_tests.rs
+++ linera-core/src/unit_tests/worker_tests.rs
@@ -3676,7 +3676,7 @@ where
 #[cfg_attr(feature = "rocksdb", test_case(RocksDbStorageBuilder::new().await; "rocks_db"))]
 #[cfg_attr(feature = "dynamodb", test_case(DynamoDbStorageBuilder::default(); "dynamo_db"))]
 #[cfg_attr(feature = "scylladb", test_case(ScyllaDbStorageBuilder::default(); "scylla_db"))]
-#[test_log::test(tokio::test(start_paused = true))]
+#[test_log::test(tokio::test)]
 async fn test_long_lived_service<B>(mut storage_builder: B) -> anyhow::Result<()>
 where
     B: StorageBuilder,
@@ -3755,7 +3755,7 @@ where
 #[cfg_attr(feature = "rocksdb", test_case(RocksDbStorageBuilder::new().await; "rocks_db"))]
 #[cfg_attr(feature = "dynamodb", test_case(DynamoDbStorageBuilder::default(); "dynamo_db"))]
 #[cfg_attr(feature = "scylladb", test_case(ScyllaDbStorageBuilder::default(); "scylla_db"))]
-#[test_log::test(tokio::test(start_paused = true))]
+#[test_log::test(tokio::test)]
 async fn test_new_block_causes_service_restart<B>(mut storage_builder: B) -> anyhow::Result<()>
 where
     B: StorageBuilder,
