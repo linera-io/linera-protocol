jvff/linera-protocol-archive

Test if adding new entry leads to pending changes
The view should be considered to have pending changes, even if the new
entry has no pending changes.

linera-views/src/unit_tests/views.rs
@@ -404,6 +404,28 @@ async fn test_reentrant_collection_view_has_no_pending_changes_after_try_load_en
     Ok(())
 }
 
+/// Check if a [`ReentrantCollectionView`] has pending changes after adding an entry.
+#[tokio::test]
+async fn test_reentrant_collection_view_has_pending_changes_after_new_entry() -> anyhow::Result<()>
+{
+    let context = create_memory_context();
+    let values = [(1, "first".to_owned()), (2, "second".to_owned())];
+    let mut view =
+        ReentrantCollectionView::<_, u8, RegisterView<_, String>>::load(context.clone()).await?;
+
+    populate_reentrant_collection_view(&mut view, values.clone()).await?;
+    save_view(&context, &mut view).await?;
+    assert!(!view.has_pending_changes().await);
+
+    let entry = view.try_load_entry_or_insert(&3).await?;
+    assert_eq!(entry.get(), "");
+    assert!(!entry.has_pending_changes().await);
+
+    assert!(view.has_pending_changes().await);
+
+    Ok(())
+}
+
 /// Saves a [`View`] into the [`MemoryContext<()>`] storage simulation.
 async fn save_view<C>(context: &C, view: &mut impl View<C>) -> anyhow::Result<()>
 where

linera-io/linera-protocol

linera-views/src/unit_tests/views.rs
@@ -406,6 +406,30 @@ async fn test_reentrant_collection_view_has_no_pending_changes_after_try_load_en
     Ok(())
 }
 
+/// Check if a [`ReentrantCollectionView`] has pending changes after adding an entry.
+#[tokio::test]
+async fn test_reentrant_collection_view_has_pending_changes_after_new_entry() -> anyhow::Result<()>
+{
+    let context = create_memory_context();
+    let values = [(1, "first".to_owned()), (2, "second".to_owned())];
+    let mut view =
+        ReentrantCollectionView::<_, u8, RegisterView<_, String>>::load(context.clone()).await?;
+
+    populate_reentrant_collection_view(&mut view, values.clone()).await?;
+    save_view(&context, &mut view).await?;
+    assert!(!view.has_pending_changes().await);
+
+    {
+        let entry = view.try_load_entry_mut(&3).await?;
+        assert_eq!(entry.get(), "");
+        assert!(!entry.has_pending_changes().await);
+    }
+
+    assert!(view.has_pending_changes().await);
+
+    Ok(())
+}
+
 /// Saves a [`View`] into the [`MemoryContext<()>`] storage simulation.
 async fn save_view<C>(context: &C, view: &mut impl View<C>) -> anyhow::Result<()>
 where