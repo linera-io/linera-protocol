ee59f2fb368feb823950a7161bb8ba1fbc4b765e
Andre da Silva <andre.dasilva@linera.io>
2024-08-06 11:11:25 -0300

    Align NFT UI with using blobs

--- examples/non-fungible/web-frontend/src/GraphQLProvider.js
+++ examples/non-fungible/web-frontend/src/GraphQLProvider.js
@@ -5,17 +5,35 @@ import {
   InMemoryCache,
   split,
 } from "@apollo/client";
-import React from "react";
+import React, { createContext, useContext } from "react";
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 
-function GraphQLProvider({ chainId, applicationId, port, children }) {
-  let client = apolloClient(chainId, applicationId, port);
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+// Create a context for selecting the client
+const ClientContext = createContext();
+
+export function useClient() {
+  return useContext(ClientContext);
 }
 
-function apolloClient(chainId, applicationId, port) {
+function GraphQLProvider({ chainId, applicationId, port, children }) {
+  const appClient = createAppClient(chainId, applicationId, port);
+  const nodeServiceClient = createNodeServiceClient(port);
+
+  const value = {
+    appClient,
+    nodeServiceClient,
+  };
+
+  return (
+    <ClientContext.Provider value={value}>
+      <ApolloProvider client={appClient}>{children}</ApolloProvider>
+    </ClientContext.Provider>
+  );
+}
+
+function createAppClient(chainId, applicationId, port) {
   const wsLink = new GraphQLWsLink(
     createClient({
       url: `ws://localhost:${port}/ws`,
@@ -24,6 +42,35 @@ function apolloClient(chainId, applicationId, port) {
 
   const httpLink = new HttpLink({
     uri: `http://localhost:${port}/chains/${chainId}/applications/${applicationId}`,
+  });
+
+  const splitLink = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === "OperationDefinition" &&
+        definition.operation === "subscription"
+      );
+    },
+    wsLink,
+    httpLink
+  );
+
+  return new ApolloClient({
+    link: splitLink,
+    cache: new InMemoryCache(),
+  });
+}
+
+function createNodeServiceClient(port) {
+  const wsLink = new GraphQLWsLink(
+    createClient({
+      url: `ws://localhost:${port}/ws`,
+    })
+  );
+
+  const httpLink = new HttpLink({
+    uri: `http://localhost:${port}/`,
   });
 
   const splitLink = split(
