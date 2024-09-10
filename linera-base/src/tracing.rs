91dd8a3ce202c7bf6a3732d37e68a1613704d12d
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-09-10 21:50:55 +0000

    Remove items from inside the function
    Now that the file is specific for non-web targets, they don't need to be
    placed together in the function.

--- linera-base/src/tracing.rs
+++ linera-base/src/tracing.rs
@@ -3,30 +3,14 @@
 
 //! This module provides unified handling for tracing subscribers within Linera binaries.
 
+use is_terminal::IsTerminal as _;
+use tracing_subscriber::fmt::format::FmtSpan;
+
 /// Initializes tracing in a standard way.
 /// The environment variables `RUST_LOG`, `RUST_LOG_SPAN_EVENTS`, and `RUST_LOG_FORMAT`
 /// can be used to control the verbosity, the span event verbosity, and the output format,
 /// respectively.
 pub fn init() {
-    use is_terminal::IsTerminal as _;
-    use tracing_subscriber::fmt::format::FmtSpan;
-
-    fn fmt_span_from_str(events: &str) -> FmtSpan {
-        let mut fmt_span = FmtSpan::NONE;
-        for event in events.split(',') {
-            fmt_span |= match event {
-                "new" => FmtSpan::NEW,
-                "enter" => FmtSpan::ENTER,
-                "exit" => FmtSpan::EXIT,
-                "close" => FmtSpan::CLOSE,
-                "active" => FmtSpan::ACTIVE,
-                "full" => FmtSpan::FULL,
-                _ => FmtSpan::NONE,
-            };
-        }
-        fmt_span
-    }
-
     let span_events = std::env::var("RUST_LOG_SPAN_EVENTS")
         .ok()
         .map(|s| fmt_span_from_str(&s))
@@ -54,4 +38,20 @@ pub fn init() {
             panic!("Invalid RUST_LOG_FORMAT: `{format}`.  Valid values are `json` or `pretty`.")
         }
     }
+}
+
+fn fmt_span_from_str(events: &str) -> FmtSpan {
+    let mut fmt_span = FmtSpan::NONE;
+    for event in events.split(',') {
+        fmt_span |= match event {
+            "new" => FmtSpan::NEW,
+            "enter" => FmtSpan::ENTER,
+            "exit" => FmtSpan::EXIT,
+            "close" => FmtSpan::CLOSE,
+            "active" => FmtSpan::ACTIVE,
+            "full" => FmtSpan::FULL,
+            _ => FmtSpan::NONE,
+        };
+    }
+    fmt_span
 }
