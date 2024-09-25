710f2b52f5e1c386dc407051cd64318e6ed321f5
James Kay <james.kay@linera.io>
2024-09-25 18:24:43 +0100

    `linera-base`: add support for messaging the spawned thread using `postMessage`

--- linera-base/src/task.rs
+++ linera-base/src/task.rs
@@ -5,7 +5,8 @@
 Abstractions over tasks that can be used natively or on the Web.
  */
 
-use std::future::Future;
+use std::{future::Future, rc::Rc};
+use tokio::sync::mpsc;
 
 #[cfg(not(web))]
 mod implementation {
@@ -35,9 +36,65 @@ mod implementation {
 
 #[cfg(web)]
 mod implementation {
-    use futures::channel::oneshot;
+    use std::convert::TryFrom;
+    use futures::{channel::oneshot, future, stream, StreamExt as _};
+    use wasm_bindgen::prelude::*;
+    use web_sys::js_sys;
 
     use super::*;
+
+    /// A type that satisfies the send/receive bounds, but can never be sent or received.
+    pub enum NoInput { }
+
+    impl TryFrom<JsValue> for NoInput {
+        type Error = JsValue;
+        fn try_from(value: JsValue) -> Result<Self, JsValue> {
+            Err(value)
+        }
+    }
+
+    impl Into<JsValue> for NoInput {
+        fn into(self) -> JsValue {
+            match self { }
+        }
+    }
+
+    /// The type of errors that can result from sending a message to the spawned task.
+    pub type SendError = JsValue;
+
+    /// A channel that can be used to send messages to the spawned task.
+    pub struct InputSender<T> {
+        worker: Rc<web_sys::Worker>,
+        _phantom: std::marker::PhantomData<fn(T)>,
+    }
+
+    impl<T> From<Rc<web_sys::Worker>> for InputSender<T> {
+        fn from(worker: Rc<web_sys::Worker>) -> Self {
+            Self {
+                worker,
+                _phantom: Default::default(),
+            }
+        }
+    }
+
+    impl<T: Into<JsValue>> InputSender<T> {
+        /// Send a message to the task using
+        /// [`postMessage`](https://developer.mozilla.org/en-US/docs/Web/API/Worker/postMessage).
+        pub fn send(&self, message: T) -> Result<(), SendError> {
+            self.worker.post_message(&message.into())
+        }
+    }
+
+    /// The stream of inputs available to the spawned task.
+    pub type InputReceiver<T> = stream::FilterMap<
+        tokio_stream::wrappers::UnboundedReceiverStream<JsValue>,
+        future::Ready<Option<T>>,
+        fn(JsValue) -> future::Ready<Option<T>>,
+    >;
+
+    fn convert_or_discard<V, T: TryFrom<V>>(value: V) -> future::Ready<Option<T>> {
+        future::ready(T::try_from(value).ok())
+    }
 
     /// The type of errors that can result from awaiting a task to completion.
     pub type Error = oneshot::Canceled;
@@ -64,6 +121,25 @@ mod implementation {
             let _ = send.send(task());
         });
         recv
+    }
+
+    /// Spawns a blocking task on a new Web Worker with a stream of input messages.
+    pub fn spawn_blocking_with_input<T: Into<JsValue> + TryFrom<JsValue, Error: std::error::Error>, F: Future<Output: Send + 'static>>(
+        task: impl FnOnce(InputReceiver<T>) -> F + Send + 'static,
+    ) -> (InputSender<T>, BlockingFuture<F::Output>) {
+        let (done_sender, done_receiver) = oneshot::channel();
+        let (worker, _join_handle) = wasm_thread::Builder::new().spawn(|| async move {
+            let (input_sender, input_receiver) = mpsc::unbounded_channel::<JsValue>();
+            let input_receiver = tokio_stream::wrappers::UnboundedReceiverStream::new(input_receiver);
+            let onmessage = wasm_bindgen::closure::Closure::<dyn FnMut(JsValue) -> Result<(), JsError>>::new(move |v: JsValue| -> Result<(), JsError> {
+                input_sender.send(v)?;
+                Ok(())
+            });
+            js_sys::global().dyn_into::<web_sys::DedicatedWorkerGlobalScope>().unwrap().set_onmessage(Some(onmessage.as_ref().unchecked_ref()));
+            onmessage.forget(); // doesn't truly forget it, but lets the JS GC take care of it
+            let _ = done_sender.send(task(input_receiver.filter_map(convert_or_discard::<JsValue, T>)).await);
+        }).unwrap();
+        (worker.into(), done_receiver)
     }
 }
 
