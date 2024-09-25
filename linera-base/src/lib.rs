752d84fcdf477c93e39bd7861226e55c4f830fde
James Kay <james.kay@linera.io>
2024-09-25 18:24:43 +0100

    `linera-base`: add support for messaging the spawned thread using `postMessage`

--- linera-base/src/lib.rs
+++ linera-base/src/lib.rs
@@ -27,6 +27,8 @@ pub mod port;
 pub mod prometheus_util;
 #[cfg(not(chain))]
 pub mod task;
+#[cfg(not(chain))]
+pub use task::Blocking;
 pub mod time;
 #[cfg_attr(web, path = "tracing_web.rs")]
 pub mod tracing;
