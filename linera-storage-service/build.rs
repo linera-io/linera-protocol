cf7f3e625833a590daf354ceb5e7a56ee36b09be
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2024-09-26 19:07:34 -0400

    fix fatal warning for tonic_build in CI

--- linera-storage-service/build.rs
+++ linera-storage-service/build.rs
@@ -10,6 +10,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let no_includes: &[&str] = &[];
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(&["proto/key_value_store.proto"], no_includes)?;
+        .compile_protos(&["proto/key_value_store.proto"], no_includes)?;
     Ok(())
 }
