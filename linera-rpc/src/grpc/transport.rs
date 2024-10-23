3da0ff7136cc6f638563dcc64a588ea1238d0aaf
James Kay <james.kay@linera.io>
2024-10-23 21:51:51 +0100

    `linera-rpc`: manually configure TLS for the client as required by Tonic ≥ 0.12 (#2700)
    * `linera-rpc`: manually configure TLS for the client as required by Tonic ≥ 0.12
    
    * `linera-rpc`: add CA roots from `webpki-roots`

--- linera-rpc/src/grpc/transport.rs
+++ linera-rpc/src/grpc/transport.rs
@@ -33,7 +33,9 @@ cfg_if::cfg_if! {
             address: String,
             options: &Options,
         ) -> Result<Channel, Error> {
-            let mut endpoint = tonic::transport::Endpoint::from_shared(address)?;
+            let mut endpoint = tonic::transport::Endpoint::from_shared(address)?
+                .tls_config(tonic::transport::channel::ClientTlsConfig::default().with_webpki_roots())?;
+
             if let Some(timeout) = options.connect_timeout {
                 endpoint = endpoint.connect_timeout(timeout);
             }
