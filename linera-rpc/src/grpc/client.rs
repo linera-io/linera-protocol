41077d667f13ffd63120f16a7bd28c2689671e28
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-10-24 16:38:59 +0200

    Implement the pool effectively.

--- linera-rpc/src/grpc/client.rs
+++ linera-rpc/src/grpc/client.rs
@@ -17,6 +17,7 @@ use linera_core::{
 };
 use linera_version::VersionInfo;
 use tonic::{Code, IntoRequest, Request, Status};
+use tonic::transport::Channel;
 use tracing::{debug, error, info, instrument};
 #[cfg(not(web))]
 use {
@@ -63,6 +64,23 @@ impl GrpcClient {
             retry_delay: options.retry_delay,
             max_retries: options.max_retries,
         })
+    }
+
+    pub fn from_address_channel(
+        address: String,
+        channel: Channel,
+        retry_delay: Duration,
+        max_retries: u32,
+    ) -> Self {
+        let client = ValidatorNodeClient::new(channel)
+            .max_encoding_message_size(GRPC_MAX_MESSAGE_SIZE)
+            .max_decoding_message_size(GRPC_MAX_MESSAGE_SIZE);
+        Self {
+            address,
+            client,
+            retry_delay,
+            max_retries,
+        }
     }
 
     /// Returns whether this gRPC status means the server stream should be reconnected to, or not.
--- linera-rpc/src/grpc/node_provider.rs
+++ linera-rpc/src/grpc/node_provider.rs
@@ -1,43 +1,39 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::str::FromStr as _;
-
 use linera_core::node::{NodeError, ValidatorNodeProvider};
+use linera_base::time::Duration;
 
 use super::GrpcClient;
-use crate::{config::ValidatorPublicNetworkConfig, grpc::pool::GrpcConnectionPool, node_provider::NodeOptions};
+use crate::{grpc::pool::GrpcConnectionPool, node_provider::NodeOptions};
+use crate::grpc::transport::Options;
 
 #[derive(Clone)]
 pub struct GrpcNodeProvider {
     pool: GrpcConnectionPool,
-    options: NodeOptions,
+    retry_delay: Duration,
+    max_retries: u32,
 }
 
 impl GrpcNodeProvider {
     pub fn new(options: NodeOptions) -> Self {
-        let pool = GrpcConnectionPool::default();
-        Self{ pool, options }
+        let transport_options = Options::from(&options);
+        let retry_delay = options.retry_delay;
+        let max_retries = options.max_retries;
+        let pool = GrpcConnectionPool::new(transport_options);
+        Self{ pool, retry_delay, max_retries }
     }
 }
 
 impl ValidatorNodeProvider for GrpcNodeProvider {
     type Node = GrpcClient;
 
-    fn make_node(&self, address: &str) -> anyhow::Result<Self::Node, NodeError> {
-        let network = ValidatorPublicNetworkConfig::from_str(address).map_err(|_| {
-            NodeError::CannotResolveValidatorAddress {
-                address: address.to_string(),
-            }
-        })?;
+    fn make_node(&self, address: &str) -> Result<Self::Node, NodeError> {
+        let address = address.to_string();
+        let channel = self.pool.channel(address.clone()).map_err(
+            |error| NodeError::GrpcError { error: format!("error creating channel: {}", error) }
+        )?;
 
-        let client = GrpcClient::new(network, self.options).map_err(|e| NodeError::GrpcError {
-            error: format!(
-                "could not initialize gRPC client for address {} : {}",
-                address, e
-            ),
-        })?;
-
-        Ok(client)
+        Ok(GrpcClient::from_address_channel(address.to_string(), channel, self.retry_delay, self.max_retries))
     }
 }
