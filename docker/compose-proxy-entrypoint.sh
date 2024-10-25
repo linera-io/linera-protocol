cd678d002330fda48de04fe28b1335a6da9f6d61
Andre da Silva <andresilva91@users.noreply.github.com>
2024-10-25 18:39:08 -0300

    Depend on other services based on health checks for docker compose (#2715)
    ## Motivation
    
    When using Docker Compose, we're not currently properly waiting for the service dependencies.
    
    ## Proposal
    
    Add a health check, and remove the `init_done` file
    
    ## Test Plan
    
    CI: we can see from the Docker CI logs that both the `proxy` and `shard`s services only get created after `shard-init` exits:
    ```
    Network docker_default  Creating
     Network docker_default  Created
     Volume "docker_grafana-storage"  Creating
     Volume "docker_grafana-storage"  Created
     Volume "docker_linera-scylla-data"  Creating
     Volume "docker_linera-scylla-data"  Created
     Container scylla  Creating
     Container prometheus  Creating
     Container grafana  Creating
     Container prometheus  Created
     Container scylla  Created
     Container grafana  Created
     Container shard-init  Creating
     Container shard-init  Created
     Container proxy  Creating
     Container docker-shard-4  Creating
     Container docker-shard-2  Creating
     Container docker-shard-1  Creating
     Container docker-shard-3  Creating
     Container docker-shard-1  Created               <----  Creations
     Container proxy  Created                               <----
     Container docker-shard-4  Created              <----
     Container docker-shard-3  Created              <----
     Container docker-shard-2  Created              <----
     Container grafana  Starting
     Container prometheus  Starting
     Container scylla  Starting
     Container scylla  Started
     Container grafana  Started
     Container shard-init  Starting
     Container prometheus  Started
     Container shard-init  Started
     Container shard-init  Waiting
     Container shard-init  Waiting
     Container shard-init  Exited                           <----    Exits
     Container proxy  Starting                               <----   Starts
     Container shard-init  Exited
     Container docker-shard-4  Starting              <----
     Container proxy  Started
     Container docker-shard-4  Started
     Container docker-shard-1  Starting               <----
     Container docker-shard-1  Started
     Container docker-shard-3  Starting               <----
     Container docker-shard-3  Started
     Container docker-shard-2  Starting                <----
     Container docker-shard-2  Started
     Container prometheus  Waiting
     Container grafana  Waiting
     Container scylla  Waiting
     Container proxy  Waiting
     Container docker-shard-4  Waiting
     Container docker-shard-1  Waiting
     Container docker-shard-3  Waiting
     Container docker-shard-2  Waiting
     Container shard-init  Waiting
     Container docker-shard-4  Healthy
     Container docker-shard-1  Healthy
     Container docker-shard-3  Healthy
     Container docker-shard-2  Healthy
     Container proxy  Healthy
     Container shard-init  Exited
     Container scylla  Healthy
     Container grafana  Healthy
     Container prometheus  Healthy
    ```
    
    ## Release Plan
    
    - Nothing to do / These changes follow the usual release cycle.

--- docker/compose-proxy-entrypoint.sh
+++ docker/compose-proxy-entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-while [ ! -f /shared/init_done ]; do
-  echo "waiting for db init"
-  sleep 5
-done
-
 exec ./linera-proxy \
   --storage scylladb:tcp:scylla:9042 \
   --genesis /config/genesis.json \
