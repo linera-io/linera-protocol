fcc4e285b3c395abbd9cf30c5187b29ee50dc22d
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-10-25 01:58:13 +0000

    Tweak review suggestion
    Fix small typos to make it compile.

--- linera-service/src/cli_wrappers/remote_net.rs
+++ linera-service/src/cli_wrappers/remote_net.rs
@@ -29,10 +29,11 @@ impl RemoteNetTestingConfig {
     /// variable, or the default devnet faucet URL.
     pub fn new(faucet_url: Option<String>) -> Self {
         Self {
-            faucet: Faucet::new(faucet_url
-                .or_else(|| env::var("LINERA_FAUCET_URL"))
-                .unwrap_or_else(|_| "https://faucet.devnet.linera.net".to_owned())
-            )),
+            faucet: Faucet::new(
+                faucet_url
+                    .or_else(|| env::var("LINERA_FAUCET_URL").ok())
+                    .unwrap_or_else(|| "https://faucet.devnet.linera.net".to_owned()),
+            ),
         }
     }
 }
