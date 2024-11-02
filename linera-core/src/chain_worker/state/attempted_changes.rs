4363e6c16e0db944f413953e9afc8b6497b91b66
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-11-02 15:29:03 +0100

    Switch the "TimestampBundleInInbox" to BucketQueueView.

--- linera-core/src/chain_worker/state/attempted_changes.rs
+++ linera-core/src/chain_worker/state/attempted_changes.rs
@@ -566,7 +566,7 @@ where
         let chain = &mut self.state.chain;
         if let (Some(epoch), Some(entry)) = (
             chain.execution_state.system.epoch.get(),
-            chain.unskippable_bundles.front().await?,
+            chain.unskippable_bundles.front(),
         ) {
             let ownership = chain.execution_state.system.ownership.get();
             let elapsed = self
