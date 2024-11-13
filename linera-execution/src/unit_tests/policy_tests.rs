3a43211b13e3153f85f3953e0a63c0a76f68d706
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-11-13 21:14:10 +0000

    Test `ResourceLimit` JSON serialization round-trip
    Ensure that it does not break serialization because it ends up
    representing the value with a floating point number.

--- linera-execution/src/unit_tests/policy_tests.rs
+++ linera-execution/src/unit_tests/policy_tests.rs
@@ -10,3 +10,19 @@ use super::ResourceLimit;
 fn default_resource_limit() {
     assert_eq!(ResourceLimit::default(), ResourceLimit(u64::MAX));
 }
+
+/// Test the JSON serialization round-trip of the [`ResourceLimit`].
+///
+/// Checks if a large value is serialized as a string, instead of being serialized as a
+/// floating-point number which would lose precision.
+#[test]
+fn resource_limit_json_roundtrip() -> anyhow::Result<()> {
+    let initial_value = ResourceLimit::default();
+    let json = serde_json::to_string(&initial_value)?;
+    let recovered_value = serde_json::from_str::<ResourceLimit>(&json)?;
+
+    assert_eq!(json, format!("\"{}\"", u64::MAX));
+    assert_eq!(recovered_value, initial_value);
+
+    Ok(())
+}
