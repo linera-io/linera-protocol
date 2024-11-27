9cd3cb2e72a83553f886baaabd2de8ce3be9455b
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-11-27 18:36:15 +0000

    Test `read_owner_balance` service runtime API
    Ensure applications can read the balances of individual accounts.

--- linera-execution/tests/service_runtime_apis.rs
+++ linera-execution/tests/service_runtime_apis.rs
@@ -3,12 +3,16 @@
 
 #![allow(clippy::field_reassign_with_default)]
 
-use std::vec;
+use std::{collections::BTreeMap, vec};
 
-use linera_base::{data_types::Amount, identifiers::ChainDescription};
+use linera_base::{
+    data_types::Amount,
+    identifiers::{AccountOwner, ChainDescription},
+};
 use linera_execution::{
     test_utils::{
-        create_dummy_query_context, ExpectedCall, RegisterMockApplication, SystemExecutionState,
+        create_dummy_query_context, test_accounts_strategy, ExpectedCall, RegisterMockApplication,
+        SystemExecutionState,
     },
     BaseRuntime, Query,
 };
@@ -30,6 +34,40 @@ async fn test_read_chain_balance_system_api(chain_balance: Amount) {
     application.expect_call(ExpectedCall::handle_query(
         move |runtime, _context, _query| {
             assert_eq!(runtime.read_chain_balance().unwrap(), chain_balance);
+            Ok(vec![])
+        },
+    ));
+    application.expect_call(ExpectedCall::default_finalize());
+
+    let context = create_dummy_query_context();
+    let query = Query::User {
+        application_id,
+        bytes: vec![],
+    };
+
+    view.query_application(context, query, None).await.unwrap();
+}
+
+/// Tests the contract system API to read a single account balance.
+#[proptest(async = "tokio")]
+async fn test_read_owner_balance_system_api(
+    #[strategy(test_accounts_strategy())] accounts: BTreeMap<AccountOwner, Amount>,
+) {
+    let mut view = SystemExecutionState {
+        description: Some(ChainDescription::Root(0)),
+        balances: accounts.clone(),
+        ..SystemExecutionState::default()
+    }
+    .into_view()
+    .await;
+
+    let (application_id, application) = view.register_mock_application().await.unwrap();
+
+    application.expect_call(ExpectedCall::handle_query(
+        move |runtime, _context, _query| {
+            for (owner, balance) in accounts {
+                assert_eq!(runtime.read_owner_balance(owner).unwrap(), balance);
+            }
             Ok(vec![])
         },
     ));
