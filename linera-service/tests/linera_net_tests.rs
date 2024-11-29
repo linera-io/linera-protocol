d42c2b58d7d60c4ff2865d9561d27fe04305c370
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-11-29 11:48:35 +0100

    Add corresponding entries for the local_net and make the code compile.

--- linera-service/tests/linera_net_tests.rs
+++ linera-service/tests/linera_net_tests.rs
@@ -406,7 +406,7 @@ async fn test_wasm_end_to_end_ethereum_tracker(config: impl LineraNetConfig) -> 
     };
 
     // Setting up the validators
-    let (net, client) = config.instantiate().await?;
+    let (mut net, client) = config.instantiate().await?;
     let chain = client.load_wallet()?.default_chain().unwrap();
 
     // Change the ownership so that the blocks inserted are not
@@ -433,7 +433,7 @@ async fn test_wasm_end_to_end_ethereum_tracker(config: impl LineraNetConfig) -> 
         )
         .await?;
     let port = get_node_port().await;
-    let node_service = client.run_node_service(port, ProcessInbox::Skip).await?;
+    let mut node_service = client.run_node_service(port, ProcessInbox::Skip).await?;
 
     let app = EthereumTrackerApp(
         node_service
@@ -470,6 +470,7 @@ async fn test_wasm_end_to_end_ethereum_tracker(config: impl LineraNetConfig) -> 
 
     net.ensure_is_running().await?;
     net.terminate().await?;
+
     Ok(())
 }
 
