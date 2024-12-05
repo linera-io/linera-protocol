9ed3db6159f987ff56ea23f15a3866010e995275
Andre da Silva <andre.dasilva@linera.io>
2024-12-05 11:53:36 -0300

    Pass proper shard id on docker compose

--- docker/compose-server-entrypoint.sh
+++ docker/compose-server-entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# Check if the ordinal number is provided
+if [ -z "$1" ]; then
+  echo "Error: No ordinal number provided."
+  echo "Usage: $0 <ordinal_number>"
+  exit 1
+fi
+
 exec ./linera-server run \
   --storage scylladb:tcp:scylla:9042 \
   --server /config/server.json \
-  --shard 0 \
+  --shard $1 \
   --genesis /config/genesis.json
