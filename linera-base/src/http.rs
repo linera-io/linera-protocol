abe33bb0d9563782499eda50aacd0851cd44aa10
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-12-18 17:23:52 +0000

    Create an `http::Header` type
    Make it simpler to tweak how it is serialized and formatted for debug
    output.

--- linera-base/src/http.rs
+++ linera-base/src/http.rs
@@ -3,7 +3,11 @@
 
 //! Types used when performing HTTP requests.
 
+use custom_debug_derive::Debug;
 use linera_witty::{WitLoad, WitStore, WitType};
+use serde::{Deserialize, Serialize};
+
+use crate::hex_debug;
 
 /// The method used in an HTTP request.
 #[derive(Clone, Copy, Debug, Eq, PartialEq, WitLoad, WitStore, WitType)]
@@ -50,6 +54,29 @@ impl From<Method> for reqwest::Method {
             Method::Connect => reqwest::Method::CONNECT,
             Method::Patch => reqwest::Method::PATCH,
             Method::Trace => reqwest::Method::TRACE,
+        }
+    }
+}
+
+/// A header for a HTTP request or response.
+#[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize, WitLoad, WitStore, WitType)]
+#[witty(name = "http-header")]
+pub struct Header {
+    /// The header name.
+    pub name: String,
+
+    /// The value of the header.
+    #[debug(with = "hex_debug")]
+    #[serde(with = "serde_bytes")]
+    pub value: Vec<u8>,
+}
+
+impl Header {
+    /// Creates a new [`Header`] with the provided `name` and `value`.
+    pub fn new(name: impl Into<String>, value: impl Into<Vec<u8>>) -> Self {
+        Header {
+            name: name.into(),
+            value: value.into(),
         }
     }
 }
