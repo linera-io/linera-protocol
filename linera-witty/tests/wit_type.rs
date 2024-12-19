17aaabb4ee79d133acadf4b3b0f82e8a8f760b77
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-12-19 19:07:42 +0000

    Refactor `WitType` unit tests to use a helper
    Prepare to test more things in the helper function.

--- linera-witty/tests/wit_type.rs
+++ linera-witty/tests/wit_type.rs
@@ -18,106 +18,95 @@ use self::types::{
 /// Check the memory size, layout and WIT type declaration derived for a wrapper type.
 #[test]
 fn test_simple_bool_wrapper() {
-    assert_eq!(SimpleWrapper::SIZE, 1);
-    assert_eq!(<SimpleWrapper as WitType>::Layout::ALIGNMENT, 1);
-    assert_eq!(<<SimpleWrapper as WitType>::Layout as Layout>::Flat::LEN, 1);
-    assert_eq!(
-        wit_type_declaration_of::<SimpleWrapper>(),
-        "    record simple-wrapper {\n        inner0: bool,\n    }\n"
-    );
+    test_wit_type_implementation::<SimpleWrapper>(ExpectedMetadata {
+        size: 1,
+        alignment: 1,
+        flat_layout_length: 1,
+        declaration: concat!(
+            "    record simple-wrapper {\n",
+            "        inner0: bool,\n",
+            "    }\n"
+        ),
+    });
 }
 
 /// Check the memory size, layout and WIT type declaration derived for a type with multiple fields
 /// ordered in a way that doesn't require any padding.
 #[test]
 fn test_tuple_struct_without_padding() {
-    assert_eq!(TupleWithoutPadding::SIZE, 16);
-    assert_eq!(<TupleWithoutPadding as WitType>::Layout::ALIGNMENT, 8);
-    assert_eq!(
-        <<TupleWithoutPadding as WitType>::Layout as Layout>::Flat::LEN,
-        3
-    );
-    assert_eq!(
-        wit_type_declaration_of::<TupleWithoutPadding>(),
-        concat!(
+    test_wit_type_implementation::<TupleWithoutPadding>(ExpectedMetadata {
+        size: 16,
+        alignment: 8,
+        flat_layout_length: 3,
+        declaration: concat!(
             "    record tuple-without-padding {\n",
             "        inner0: u64,\n",
             "        inner1: s32,\n",
             "        inner2: s16,\n",
             "    }\n"
-        )
-    );
+        ),
+    });
 }
 
 /// Check the memory size, layout and WIT type declaration derived for a type with multiple fields
 /// ordered in a way that requires padding between all fields.
 #[test]
 fn test_tuple_struct_with_padding() {
-    assert_eq!(TupleWithPadding::SIZE, 16);
-    assert_eq!(<TupleWithPadding as WitType>::Layout::ALIGNMENT, 8);
-    assert_eq!(
-        <<TupleWithPadding as WitType>::Layout as Layout>::Flat::LEN,
-        3
-    );
-    assert_eq!(
-        wit_type_declaration_of::<TupleWithPadding>(),
-        concat!(
+    test_wit_type_implementation::<TupleWithPadding>(ExpectedMetadata {
+        size: 16,
+        alignment: 8,
+        flat_layout_length: 3,
+        declaration: concat!(
             "    record tuple-with-padding {\n",
             "        inner0: u16,\n",
             "        inner1: u32,\n",
             "        inner2: s64,\n",
             "    }\n"
-        )
-    );
+        ),
+    });
 }
 
 /// Check the memory size, layout and WIT type declaration derived for a type with multiple named
 /// fields ordered in a way that requires padding before two fields.
 #[test]
 fn test_named_struct_with_double_padding() {
-    assert_eq!(RecordWithDoublePadding::SIZE, 24);
-    assert_eq!(<RecordWithDoublePadding as WitType>::Layout::ALIGNMENT, 8);
-    assert_eq!(
-        <<RecordWithDoublePadding as WitType>::Layout as Layout>::Flat::LEN,
-        4
-    );
-    assert_eq!(
-        wit_type_declaration_of::<RecordWithDoublePadding>(),
-        concat!(
+    test_wit_type_implementation::<RecordWithDoublePadding>(ExpectedMetadata {
+        size: 24,
+        alignment: 8,
+        flat_layout_length: 4,
+        declaration: concat!(
             "    record record-with-double-padding {\n",
             "        first: u16,\n",
             "        second: u32,\n",
             "        third: s8,\n",
             "        fourth: s64,\n",
             "    }\n"
-        )
-    );
+        ),
+    });
 }
 
 /// Check the memory size, layout and WIT type declarations derived for a type that contains a
 /// field with a type that also has `WitType` derived for it.
 #[test]
 fn test_nested_types() {
-    assert_eq!(Leaf::SIZE, 24);
-    assert_eq!(<Leaf as WitType>::Layout::ALIGNMENT, 8);
-    assert_eq!(<<Leaf as WitType>::Layout as Layout>::Flat::LEN, 3);
-    assert_eq!(
-        wit_type_declaration_of::<Leaf>(),
-        concat!(
+    test_wit_type_implementation::<Leaf>(ExpectedMetadata {
+        size: 24,
+        alignment: 8,
+        flat_layout_length: 3,
+        declaration: concat!(
             "    record leaf {\n",
             "        first: bool,\n",
             "        second: u128,\n",
             "    }\n\n",
             "    type u128 = tuple<u64, u64>;\n"
-        )
-    );
+        ),
+    });
 
-    assert_eq!(Branch::SIZE, 56);
-    assert_eq!(<Branch as WitType>::Layout::ALIGNMENT, 8);
-    assert_eq!(<<Branch as WitType>::Layout as Layout>::Flat::LEN, 7);
-    assert_eq!(
-        wit_type_declaration_of::<Branch>(),
-        concat!(
+    test_wit_type_implementation::<Branch>(ExpectedMetadata {
+        size: 56,
+        alignment: 8,
+        flat_layout_length: 7,
+        declaration: concat!(
             "    record branch {\n",
             "        tag: u16,\n",
             "        first-leaf: leaf,\n",
@@ -128,19 +117,18 @@ fn test_nested_types() {
             "        second: u128,\n",
             "    }\n\n",
             "    type u128 = tuple<u64, u64>;\n"
-        )
-    );
+        ),
+    });
 }
 
 /// Check the memory size, layout and WIT type declaration derived for an `enum` type.
 #[test]
 fn test_enum_type() {
-    assert_eq!(Enum::SIZE, 24);
-    assert_eq!(<Enum as WitType>::Layout::ALIGNMENT, 8);
-    assert_eq!(<<Enum as WitType>::Layout as Layout>::Flat::LEN, 11);
-    assert_eq!(
-        wit_type_declaration_of::<Enum>(),
-        concat!(
+    test_wit_type_implementation::<Enum>(ExpectedMetadata {
+        size: 24,
+        alignment: 8,
+        flat_layout_length: 11,
+        declaration: concat!(
             "    variant enum {\n",
             "        empty,\n",
             "        large-variant-with-loose-alignment(\
@@ -148,58 +136,66 @@ fn test_enum_type() {
                      ),\n",
             "        smaller-variant-with-strict-alignment(u64),\n",
             "    }\n",
-        )
-    );
+        ),
+    });
 }
 
 /// Check the memory size, layout and WIT type declaration derived for a specialized generic
 /// `struct` type.
 #[test]
 fn test_specialized_generic_struct() {
-    assert_eq!(SpecializedGenericStruct::SIZE, 12);
-    assert_eq!(
-        <SpecializedGenericStruct<u8, i16> as WitType>::Layout::ALIGNMENT,
-        4
-    );
-    assert_eq!(
-        <<SpecializedGenericStruct<u8, i16> as WitType>::Layout as Layout>::Flat::LEN,
-        4
-    );
-    assert_eq!(
-        wit_type_declaration_of::<SpecializedGenericStruct<u8, i16>>(),
-        concat!(
+    test_wit_type_implementation::<SpecializedGenericStruct<u8, i16>>(ExpectedMetadata {
+        size: 12,
+        alignment: 4,
+        flat_layout_length: 4,
+        declaration: concat!(
             "    record specialized-generic-struct {\n",
             "        first: u8,\n",
             "        second: s16,\n",
             "        both: list<tuple<u8, s16>>,\n",
             "    }\n",
-        )
-    );
+        ),
+    });
 }
 
 /// Check the memory size, layout and WIT type declaration derived for a specialized generic `enum`
 /// type.
 #[test]
 fn test_specialized_generic_enum_type() {
-    assert_eq!(SpecializedGenericEnum::SIZE, 12);
-    assert_eq!(
-        <SpecializedGenericEnum<Option<bool>, u32> as WitType>::Layout::ALIGNMENT,
-        4
-    );
-    assert_eq!(
-        <<SpecializedGenericEnum<Option<bool>, u32> as WitType>::Layout as Layout>::Flat::LEN,
-        3
-    );
-    assert_eq!(
-        wit_type_declaration_of::<SpecializedGenericEnum<Option<bool>, u32>>(),
-        concat!(
+    test_wit_type_implementation::<SpecializedGenericEnum<Option<bool>, u32>>(ExpectedMetadata {
+        size: 12,
+        alignment: 4,
+        flat_layout_length: 3,
+        declaration: concat!(
             "    variant specialized-generic-enum {\n",
             "        none,\n",
             "        first(option<bool>),\n",
             "        maybe-second(option<u32>),\n",
             "    }\n",
-        )
+        ),
+    });
+}
+
+/// Helper type to make visible what each metadata value is.
+struct ExpectedMetadata {
+    size: u32,
+    alignment: u32,
+    flat_layout_length: usize,
+    declaration: &'static str,
+}
+
+/// Tests that a type `T` has the `expected` [`WitType`] metadata in its implementation.
+fn test_wit_type_implementation<T>(expected: ExpectedMetadata)
+where
+    T: WitType,
+{
+    assert_eq!(T::SIZE, expected.size);
+    assert_eq!(<T as WitType>::Layout::ALIGNMENT, expected.alignment);
+    assert_eq!(
+        <<T as WitType>::Layout as Layout>::Flat::LEN,
+        expected.flat_layout_length
     );
+    assert_eq!(wit_type_declaration_of::<T>(), expected.declaration);
 }
 
 /// Returns the WIT snippet with the type declarations for `T`.
