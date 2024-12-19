d3f80f3657ef8b5c19ace87d4965ae00806442e0
Andre da Silva <andre.dasilva@linera.io>
2024-12-19 19:34:34 -0300

    backport adaptation

--- linera-core/src/worker.rs
+++ linera-core/src/worker.rs
@@ -93,11 +93,12 @@ static NUM_BLOCKS: LazyLock<IntCounterVec> = LazyLock::new(|| {
 
 #[cfg(with_metrics)]
 static CERTIFICATES_SIGNED: LazyLock<IntCounterVec> = LazyLock::new(|| {
-    register_int_counter_vec(
+    prometheus_util::register_int_counter_vec(
         "certificates_signed",
         "Number of confirmed block certificates signed by each validator",
         &["validator_name"],
     )
+    .expect("Counter creation should not fail")
 });
 
 /// Instruct the networking layer to send cross-chain requests and/or push notifications.
@@ -771,6 +772,8 @@ where
             false,
         );
 
+        #[cfg(with_metrics)]
+        let certificate_signatures = certificate.signatures().clone();
         let (info, actions) = match certificate.value() {
             CertificateValue::ValidatedBlock { .. } => {
                 // Confirm the validated block.
@@ -816,7 +819,7 @@ where
                     .inc_by(confirmed_transactions);
             }
 
-            for (validator_name, _) in certificate.signatures() {
+            for (validator_name, _) in certificate_signatures {
                 CERTIFICATES_SIGNED
                     .with_label_values(&[&validator_name.to_string()])
                     .inc();
