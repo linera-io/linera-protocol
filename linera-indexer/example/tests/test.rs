c3ccffa8efb6decaaf5818828dc57a24b57f477f
deuszx <mateusz.gorski@linera.io>
2025-01-09 13:21:33 +0100

    fix linera-indexer/examples test

--- linera-indexer/example/tests/test.rs
+++ linera-indexer/example/tests/test.rs
@@ -149,8 +149,8 @@ async fn test_end_to_end_operations_indexer(config: impl LineraNetConfig) {
     );
 
     // checking indexer operation
-    let executed_block = last_block.value.executed_block;
-    let last_operation = executed_block.block.operations[0].clone();
+    let exeuted_block = last_block.value.block;
+    let last_operation = exeuted_block.body.operations[0].clone();
     let variables = get_operation::Variables {
         key: get_operation::OperationKeyKind::Last(chain0),
     };
@@ -172,7 +172,7 @@ async fn test_end_to_end_operations_indexer(config: impl LineraNetConfig) {
                 (
                     OperationKey {
                         chain_id: chain0,
-                        height: executed_block.block.height,
+                        height: exeuted_block.header.height,
                         index: 0
                     },
                     last_hash,
