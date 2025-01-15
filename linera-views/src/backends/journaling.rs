1a6fc7600d4b162e85bcca3e1dee4e92e82955d8
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-01-15 14:28:35 +0100

    Insert the code for the basic stores.

--- linera-views/src/backends/journaling.rs
+++ linera-views/src/backends/journaling.rs
@@ -200,6 +200,10 @@ where
         K::list_all(config).await
     }
 
+    async fn get_root_keys(config: &Self::Config, namespace: &str) -> Result<Vec<Vec<u8>>, Self::Error> {
+        K::get_root_keys(config, namespace).await
+    }
+
     async fn delete_all(config: &Self::Config) -> Result<(), Self::Error> {
         K::delete_all(config).await
     }
