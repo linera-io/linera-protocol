655d379c0cc1b95d69d715d08f8ba4b9eeb6b5cf
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-01-15 20:28:48 +0100

    Add more tests.

--- linera-views/tests/admin_tests.rs
+++ linera-views/tests/admin_tests.rs
@@ -7,7 +7,7 @@ use linera_views::dynamo_db::DynamoDbStore;
 use linera_views::rocks_db::RocksDbStore;
 #[cfg(with_scylladb)]
 use linera_views::scylla_db::ScyllaDbStore;
-use linera_views::{memory::MemoryStore, test_utils::namespace_admin_test};
+use linera_views::{memory::MemoryStore, test_utils::{namespace_admin_test, root_key_admin_test}};
 
 #[tokio::test]
 async fn namespace_admin_test_memory() {
@@ -30,4 +30,27 @@ async fn namespace_admin_test_dynamo_db() {
 #[tokio::test]
 async fn namespace_admin_test_scylla_db() {
     namespace_admin_test::<ScyllaDbStore>().await;
+}
+
+#[tokio::test]
+async fn root_key_admin_test_memory() {
+    root_key_admin_test::<MemoryStore>().await;
+}
+
+#[cfg(with_rocksdb)]
+#[tokio::test]
+async fn root_key_admin_test_rocks_db() {
+    root_key_admin_test::<RocksDbStore>().await;
+}
+
+#[cfg(with_dynamodb)]
+#[tokio::test]
+async fn root_key_admin_test_dynamo_db() {
+    root_key_admin_test::<DynamoDbStore>().await;
+}
+
+#[cfg(with_scylladb)]
+#[tokio::test]
+async fn root_key_admin_test_scylla_db() {
+    root_key_admin_test::<ScyllaDbStore>().await;
 }
