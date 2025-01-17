fa81c13d9129b40a1155b5e72ac4d8b26deda50c
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2025-01-17 02:25:08 +0000

    Expand GraphQL query implementation
    Prepare to add custom queries beyond the ones derived from
    `SimpleObject`, in a way that has access to the `EthereumTrackerService`
    type.

--- examples/ethereum-tracker/src/service.rs
+++ examples/ethereum-tracker/src/service.rs
@@ -8,9 +8,12 @@ mod state;
 use std::sync::Arc;
 
 use async_graphql::{EmptySubscription, Request, Response, Schema};
-use ethereum_tracker::Operation;
+use ethereum_tracker::{Operation, U256Cont};
 use linera_sdk::{
-    base::WithServiceAbi, graphql::GraphQLMutationRoot, views::View, Service, ServiceRuntime,
+    base::WithServiceAbi,
+    graphql::GraphQLMutationRoot,
+    views::{MapView, View},
+    Service, ServiceRuntime,
 };
 
 use self::state::EthereumTrackerState;
@@ -40,11 +43,37 @@ impl Service for EthereumTrackerService {
 
     async fn handle_query(&self, request: Request) -> Response {
         let schema = Schema::build(
-            self.state.clone(),
+            Query(self.clone()),
             Operation::mutation_root(),
             EmptySubscription,
         )
         .finish();
         schema.execute(request).await
+    }
+}
+
+/// The service handler for GraphQL queries.
+pub struct Query(EthereumTrackerService);
+
+#[async_graphql::Object]
+impl Query {
+    /// Returns the Ethereum endpoint the service is using.
+    async fn ethereum_endpoint(&self) -> String {
+        self.0.state.ethereum_endpoint.get().clone()
+    }
+
+    /// Returns the Ethereum contract address that the application is monitoring.
+    async fn contract_address(&self) -> String {
+        self.0.state.contract_address.get().clone()
+    }
+
+    /// Returns the Ethereum block height where monitoring started.
+    async fn start_block(&self) -> u64 {
+        *self.0.state.start_block.get()
+    }
+
+    /// Returns the map of known Ethereum addresses that have used the contract and their balances.
+    async fn accounts(&self) -> &MapView<String, U256Cont> {
+        &self.0.state.accounts
     }
 }
