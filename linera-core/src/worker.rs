098a87ec2c355aeb677717222966994bd50bb31c
Andreas Fackler <andreas.fackler@linera.io>
2025-01-30 11:45:29 +0100

    Remove unused error variants.

--- linera-core/src/worker.rs
+++ linera-core/src/worker.rs
@@ -209,10 +209,6 @@ pub enum WorkerError {
     MissingCertificateValue,
     #[error("The hash certificate doesn't match its value.")]
     InvalidLiteCertificate,
-    #[error("An additional blob was provided that is not required: {blob_id}.")]
-    UnneededBlob { blob_id: BlobId },
-    #[error("The blobs provided in the proposal were not the published ones, in order.")]
-    WrongBlobsInProposal,
     #[error("Fast blocks cannot query oracles")]
     FastBlockUsingOracles,
     #[error("Blobs not found: {0:?}")]
