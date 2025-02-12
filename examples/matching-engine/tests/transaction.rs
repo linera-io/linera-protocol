9c4f5b9eecc7d5d564e1d0f161959a4b3f587ddd
XxAlex74xX <30472093+XxAlex74xX@users.noreply.github.com>
2025-02-12 14:31:26 +0100

    update lib.rs / transaction.rs  (#3290)
    exchange of token - fix - exchange for token `2x`
    An message - fix - A message
    
    ---------
    
    Signed-off-by: XxAlex74xX <30472093+XxAlex74xX@users.noreply.github.com>

--- examples/matching-engine/tests/transaction.rs
+++ examples/matching-engine/tests/transaction.rs
@@ -49,11 +49,11 @@ pub async fn get_orders(
 ///   * user_a with 10 tokens A.
 ///   * user_b with 9 tokens B.
 /// * Then we create the following orders:
-///   * User_a: Offer to buy token B in exchange of token A for a price of 1 (or 2) with
+///   * User_a: Offer to buy token B in exchange for token A for a price of 1 (or 2) with
 ///     a quantity of 3 token B.
 ///     User_a thus commits 3 * 1 + 3 * 2 = 9 token A to the matching engine chain and is
 ///     left with 1 token A on chain A
-///   * User_b: Offer to sell token B in exchange of token A for a price of 2 (or 4) with
+///   * User_b: Offer to sell token B in exchange for token A for a price of 2 (or 4) with
 ///     a quantity of 4 token B
 ///     User_b thus commits 4 + 4 = 8 token B on the matching engine chain and is left
 ///     with 1 token B.
