5ccc3abb0f0d83cb8c6b7635b1485b4b9f2e1260
duin5 <116599379+duin5@users.noreply.github.com>
2025-02-13 15:40:32 +0530

    formatting improvements

--- linera-witty/src/imported_function_interface/mod.rs
+++ linera-witty/src/imported_function_interface/mod.rs
@@ -2,13 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! Generic representation of a function the host imports from a guest Wasm instance.
-//!
 //! This helps determining the actual signature of the imported guest Wasm function based on host
 //! types for the parameters and the results.
-//!
 //! The signature depends on the number of flat types used to represent the parameters and the
-//! results, as specified in the [canonical
-//! ABI](https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#flattening).
+//! results, as specified in the [canonical ABI](https://github.com/WebAssembly/component-model/blob/main/design/mvp/CanonicalABI.md#flattening).
 
 mod parameters;
 mod results;
@@ -20,7 +17,6 @@ use crate::{
 };
 
 /// Representation of an imported function's interface.
-///
 /// Implemented for a tuple pair of the host parameters type and the host results type, and then
 /// allows converting to the guest function's signature.
 pub trait ImportedFunctionInterface {
