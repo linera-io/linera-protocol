jvff/linera-protocol-archive

Replace `content_type` parameter with `headers`
Allow specifying multiple headers for the request.

linera-execution/src/wasm/system_api.rs
@@ -343,13 +343,13 @@ where
         caller: &mut Caller,
         method: http::Method,
         query: String,
-        content_type: String,
+        headers: Vec<(String, Vec<u8>)>,
         payload: Vec<u8>,
     ) -> Result<Vec<u8>, RuntimeError> {
         caller
             .user_data_mut()
             .runtime
-            .http_request(method, &query, content_type, payload)
+            .http_request(method, &query, headers, payload)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
@@ -547,13 +547,13 @@ where
         caller: &mut Caller,
         method: http::Method,
         query: String,
-        content_type: String,
+        headers: Vec<(String, Vec<u8>)>,
         payload: Vec<u8>,
     ) -> Result<Vec<u8>, RuntimeError> {
         caller
             .user_data_mut()
             .runtime
-            .http_request(method, &query, content_type, payload)
+            .http_request(method, &query, headers, payload)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 

linera-sdk/src/contract/runtime.rs
@@ -275,10 +275,10 @@ where
         &mut self,
         method: http::Method,
         url: &str,
-        content_type: &str,
+        headers: Vec<(String, Vec<u8>)>,
         payload: Vec<u8>,
     ) -> Vec<u8> {
-        wit::http_request(url, content_type, &payload)
+        wit::http_request(method.into(), url, &headers, &payload)
     }
 
     /// Panics if the current time at block validation is `>= timestamp`. Note that block

linera-io/linera-protocol

linera-execution/src/wasm/system_api.rs
@@ -383,13 +383,13 @@ where
         caller: &mut Caller,
         method: http::Method,
         query: String,
-        content_type: String,
+        headers: Vec<(String, Vec<u8>)>,
         payload: Vec<u8>,
     ) -> Result<Vec<u8>, RuntimeError> {
         caller
             .user_data_mut()
             .runtime
-            .perform_http_request(method, &query, content_type, payload)
+            .perform_http_request(method, &query, headers, payload)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
@@ -597,13 +597,13 @@ where
         caller: &mut Caller,
         method: http::Method,
         query: String,
-        content_type: String,
+        headers: Vec<(String, Vec<u8>)>,
         payload: Vec<u8>,
     ) -> Result<Vec<u8>, RuntimeError> {
         caller
             .user_data_mut()
             .runtime
-            .perform_http_request(method, &query, content_type, payload)
+            .perform_http_request(method, &query, headers, payload)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 

linera-sdk/src/contract/runtime.rs
@@ -316,10 +316,10 @@ where
         &mut self,
         method: http::Method,
         url: &str,
-        content_type: &str,
+        headers: Vec<(String, Vec<u8>)>,
         payload: Vec<u8>,
     ) -> Vec<u8> {
-        wit::perform_http_request(method.into(), url, content_type, &payload)
+        wit::perform_http_request(method.into(), url, &headers, &payload)
     }
 
     /// Panics if the current time at block validation is `>= timestamp`. Note that block