jvff/linera-protocol-archive

Add `ServiceRuntime::http_request` method
The API is already available in the WIT interface, so it makes sense to
expose it in a more discoverable way.

linera-sdk/src/service/runtime.rs
@@ -135,6 +135,17 @@ where
             .expect("Failed to deserialize query response from application")
     }
 
+    /// Makes an HTTP request to the given URL as an oracle and returns the answer, if any.
+    ///
+    /// Should only be used with queries where it is very likely that all validators will receive
+    /// the same response, otherwise most block proposals will fail.
+    ///
+    /// Cannot be used in fast blocks: A block using this call should be proposed by a regular
+    /// owner, not a super owner.
+    pub fn http_request(&mut self, request: http::Request) -> http::Response {
+        wit::http_request(&request.into()).into()
+    }
+
     /// Fetches a blob of bytes from a given URL.
     pub fn fetch_url(&self, url: &str) -> Vec<u8> {
         wit::fetch_url(url)

linera-io/linera-protocol

linera-sdk/src/service/runtime.rs
@@ -8,6 +8,7 @@ use std::sync::Mutex;
 use linera_base::{
     abi::ServiceAbi,
     data_types::{Amount, BlockHeight, Timestamp},
+    http,
     identifiers::{AccountOwner, ApplicationId, ChainId},
 };
 use serde::Serialize;
@@ -150,6 +151,17 @@ where
 
         serde_json::from_slice(&response_bytes)
             .expect("Failed to deserialize query response from application")
+    }
+
+    /// Makes an HTTP request to the given URL as an oracle and returns the answer, if any.
+    ///
+    /// Should only be used with queries where it is very likely that all validators will receive
+    /// the same response, otherwise most block proposals will fail.
+    ///
+    /// Cannot be used in fast blocks: A block using this call should be proposed by a regular
+    /// owner, not a super owner.
+    pub fn http_request(&mut self, request: http::Request) -> http::Response {
+        wit::perform_http_request(&request.into()).into()
     }
 
     /// Fetches a blob of bytes from a given URL.