jvff/linera-protocol-archive

Replace individual parameters with `http::Request`
Simplify the API a bit, and make it simpler to change the types in the
future if needed.

linera-execution/src/execution_state_actor.rs
@@ -289,21 +289,16 @@ where
                 callback.respond(bytes);
             }
 
-            HttpRequest {
-                method,
-                url,
-                headers,
-                payload,
-                callback,
-            } => {
-                let headers = headers
+            HttpRequest { request, callback } => {
+                let headers = request
+                    .headers
                     .into_iter()
                     .map(|(name, value)| Ok((name.parse()?, value.try_into()?)))
                     .collect::<Result<HeaderMap, ExecutionError>>()?;
 
                 let response = Client::new()
-                    .request(method.into(), url)
-                    .body(payload)
+                    .request(request.method.into(), request.url)
+                    .body(request.body)
                     .headers(headers)
                     .send()
                     .await?;
@@ -443,10 +438,7 @@ pub enum ExecutionRequest {
     },
 
     HttpRequest {
-        method: http::Method,
-        url: String,
-        headers: Vec<(String, Vec<u8>)>,
-        payload: Vec<u8>,
+        request: http::Request,
         callback: oneshot::Sender<http::Response>,
     },
 
@@ -584,16 +576,9 @@ impl Debug for ExecutionRequest {
                 .field("url", url)
                 .finish_non_exhaustive(),
 
-            ExecutionRequest::HttpRequest {
-                method,
-                url,
-                headers,
-                ..
-            } => formatter
+            ExecutionRequest::HttpRequest { request, .. } => formatter
                 .debug_struct("ExecutionRequest::HttpRequest")
-                .field("method", method)
-                .field("url", url)
-                .field("headers", headers)
+                .field("request", request)
                 .finish_non_exhaustive(),
 
             ExecutionRequest::ReadBlobContent { blob_id, .. } => formatter

linera-execution/src/wasm/system_api.rs
@@ -341,15 +341,12 @@ where
     /// Makes an HTTP request to the given URL and returns the response body.
     fn http_request(
         caller: &mut Caller,
-        method: http::Method,
-        query: String,
-        headers: Vec<(String, Vec<u8>)>,
-        payload: Vec<u8>,
+        request: http::Request,
     ) -> Result<http::Response, RuntimeError> {
         caller
             .user_data_mut()
             .runtime
-            .http_request(method, &query, headers, payload)
+            .http_request(request)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
@@ -545,15 +542,12 @@ where
     /// Makes an HTTP request to the given URL and returns the response body.
     fn http_request(
         caller: &mut Caller,
-        method: http::Method,
-        query: String,
-        headers: Vec<(String, Vec<u8>)>,
-        payload: Vec<u8>,
+        request: http::Request,
     ) -> Result<http::Response, RuntimeError> {
         caller
             .user_data_mut()
             .runtime
-            .http_request(method, &query, headers, payload)
+            .http_request(request)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 

linera-sdk/src/contract/runtime.rs
@@ -264,21 +264,15 @@ where
         serde_json::from_slice(&response).expect("Failed to deserialize service response")
     }
 
-    /// Makes an HTTP request to the given URL as an oracle and returns the answer, if any.
+    /// Makes an HTTP `request` as an oracle and returns the HTTP response.
     ///
     /// Should only be used with queries where it is very likely that all validators will receive
     /// the same response, otherwise most block proposals will fail.
     ///
     /// Cannot be used in fast blocks: A block using this call should be proposed by a regular
     /// owner, not a super owner.
-    pub fn http_request(
-        &mut self,
-        method: http::Method,
-        url: &str,
-        headers: Vec<(String, Vec<u8>)>,
-        payload: Vec<u8>,
-    ) -> http::Response {
-        wit::http_request(method.into(), url, &headers, &payload).into()
+    pub fn http_request(&mut self, request: http::Request) -> http::Response {
+        wit::http_request(&request.into()).into()
     }
 
     /// Panics if the current time at block validation is `>= timestamp`. Note that block

linera-io/linera-protocol

linera-execution/src/execution_state_actor.rs
@@ -329,21 +329,16 @@ where
                 callback.respond(bytes);
             }
 
-            PerformHttpRequest {
-                method,
-                url,
-                headers,
-                payload,
-                callback,
-            } => {
-                let headers = headers
+            PerformHttpRequest { request, callback } => {
+                let headers = request
+                    .headers
                     .into_iter()
-                    .map(|(name, value)| Ok((name.parse()?, value.try_into()?)))
+                    .map(|http::Header { name, value }| Ok((name.parse()?, value.try_into()?)))
                     .collect::<Result<HeaderMap, ExecutionError>>()?;
 
                 let response = Client::new()
-                    .request(method.into(), url)
-                    .body(payload)
+                    .request(request.method.into(), request.url)
+                    .body(request.body)
                     .headers(headers)
                     .send()
                     .await?;
@@ -529,11 +524,7 @@ pub enum ExecutionRequest {
     },
 
     PerformHttpRequest {
-        method: http::Method,
-        url: String,
-        headers: Vec<(String, Vec<u8>)>,
-        #[debug(with = hex_debug)]
-        payload: Vec<u8>,
+        request: http::Request,
         #[debug(skip)]
         callback: Sender<http::Response>,
     },

linera-execution/src/wasm/system_api.rs
@@ -381,15 +381,12 @@ where
     /// Makes an HTTP request to the given URL and returns the response body.
     fn perform_http_request(
         caller: &mut Caller,
-        method: http::Method,
-        query: String,
-        headers: Vec<(String, Vec<u8>)>,
-        payload: Vec<u8>,
+        request: http::Request,
     ) -> Result<http::Response, RuntimeError> {
         caller
             .user_data_mut()
             .runtime
-            .perform_http_request(method, &query, headers, payload)
+            .perform_http_request(request)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
@@ -595,15 +592,12 @@ where
     /// Makes an HTTP request to the given URL and returns the response body.
     fn perform_http_request(
         caller: &mut Caller,
-        method: http::Method,
-        query: String,
-        headers: Vec<(String, Vec<u8>)>,
-        payload: Vec<u8>,
+        request: http::Request,
     ) -> Result<http::Response, RuntimeError> {
         caller
             .user_data_mut()
             .runtime
-            .perform_http_request(method, &query, headers, payload)
+            .perform_http_request(request)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 

linera-sdk/src/contract/runtime.rs
@@ -305,21 +305,15 @@ where
         serde_json::from_slice(&response).expect("Failed to deserialize service response")
     }
 
-    /// Makes an HTTP request to the given URL as an oracle and returns the answer, if any.
+    /// Makes an HTTP `request` as an oracle and returns the HTTP response.
     ///
     /// Should only be used with queries where it is very likely that all validators will receive
     /// the same response, otherwise most block proposals will fail.
     ///
     /// Cannot be used in fast blocks: A block using this call should be proposed by a regular
     /// owner, not a super owner.
-    pub fn http_request(
-        &mut self,
-        method: http::Method,
-        url: &str,
-        headers: Vec<(String, Vec<u8>)>,
-        payload: Vec<u8>,
-    ) -> http::Response {
-        wit::perform_http_request(method.into(), url, &headers, &payload).into()
+    pub fn http_request(&mut self, request: http::Request) -> http::Response {
+        wit::perform_http_request(&request.into()).into()
     }
 
     /// Panics if the current time at block validation is `>= timestamp`. Note that block