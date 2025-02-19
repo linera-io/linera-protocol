jvff/linera-protocol-archive

Add a `http::Method` parameter to `http_request`
Allow specifying which HTTP method should be used for the request.

linera-execution/src/runtime.rs
@@ -15,7 +15,7 @@ use linera_base::{
         Amount, ApplicationPermissions, ArithmeticError, BlockHeight, OracleResponse, Resources,
         SendMessageRequest, Timestamp,
     },
-    ensure,
+    ensure, http,
     identifiers::{
         Account, ApplicationId, BlobId, BlobType, ChainId, ChannelName, MessageId, Owner,
         StreamName,
@@ -676,11 +676,13 @@ impl<UserInstance> BaseRuntime for SyncRuntimeHandle<UserInstance> {
 
     fn http_request(
         &mut self,
+        method: http::Method,
         url: &str,
         content_type: String,
         payload: Vec<u8>,
     ) -> Result<Vec<u8>, ExecutionError> {
-        self.inner().http_request(url, content_type, payload)
+        self.inner()
+            .http_request(method, url, content_type, payload)
     }
 
     fn assert_before(&mut self, timestamp: Timestamp) -> Result<(), ExecutionError> {
@@ -954,6 +956,7 @@ impl<UserInstance> BaseRuntime for SyncRuntimeInternal<UserInstance> {
 
     fn http_request(
         &mut self,
+        method: http::Method,
         url: &str,
         content_type: String,
         payload: Vec<u8>,
@@ -972,6 +975,7 @@ impl<UserInstance> BaseRuntime for SyncRuntimeInternal<UserInstance> {
                 let url = url.to_string();
                 self.execution_state_sender
                     .send_request(|callback| ExecutionRequest::HttpRequest {
+                        method,
                         url,
                         content_type,
                         payload,

linera-execution/src/wasm/system_api.rs
@@ -6,6 +6,7 @@ use std::{any::Any, collections::HashMap, marker::PhantomData};
 use linera_base::{
     crypto::CryptoHash,
     data_types::{Amount, ApplicationPermissions, BlockHeight, SendMessageRequest, Timestamp},
+    http,
     identifiers::{Account, ApplicationId, ChainId, ChannelName, MessageId, Owner, StreamName},
     ownership::{ChainOwnership, CloseChainError},
 };
@@ -337,9 +338,10 @@ where
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
-    /// Makes a POST request to the given URL and returns the response body.
+    /// Makes an HTTP request to the given URL and returns the response body.
     fn http_request(
         caller: &mut Caller,
+        method: http::Method,
         query: String,
         content_type: String,
         payload: Vec<u8>,
@@ -347,7 +349,7 @@ where
         caller
             .user_data_mut()
             .runtime
-            .http_request(&query, content_type, payload)
+            .http_request(method, &query, content_type, payload)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
@@ -540,9 +542,10 @@ where
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
-    /// Makes a POST request to the given URL and returns the response body.
+    /// Makes an HTTP request to the given URL and returns the response body.
     fn http_request(
         caller: &mut Caller,
+        method: http::Method,
         query: String,
         content_type: String,
         payload: Vec<u8>,
@@ -550,7 +553,7 @@ where
         caller
             .user_data_mut()
             .runtime
-            .http_request(&query, content_type, payload)
+            .http_request(method, &query, content_type, payload)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 

linera-sdk/src/contract/conversions_to_wit.rs
@@ -9,6 +9,7 @@ use linera_base::{
         Amount, ApplicationPermissions, BlockHeight, Resources, SendMessageRequest, TimeDelta,
         Timestamp,
     },
+    http,
     identifiers::{
         Account, ApplicationId, BytecodeId, ChainId, ChannelName, Destination, MessageId, Owner,
         StreamName,
@@ -166,6 +167,22 @@ impl From<Resources> for wit_system_api::Resources {
             messages: resources.messages,
             message_size: resources.message_size,
             storage_size_delta: resources.storage_size_delta,
+        }
+    }
+}
+
+impl From<http::Method> for wit_system_api::Method {
+    fn from(method: http::Method) -> Self {
+        match method {
+            http::Method::Get => wit_system_api::Method::Get,
+            http::Method::Post => wit_system_api::Method::Post,
+            http::Method::Put => wit_system_api::Method::Put,
+            http::Method::Delete => wit_system_api::Method::Delete,
+            http::Method::Head => wit_system_api::Method::Head,
+            http::Method::Options => wit_system_api::Method::Options,
+            http::Method::Connect => wit_system_api::Method::Connect,
+            http::Method::Patch => wit_system_api::Method::Patch,
+            http::Method::Trace => wit_system_api::Method::Trace,
         }
     }
 }

linera-io/linera-protocol

linera-execution/src/runtime.rs
@@ -15,7 +15,7 @@ use linera_base::{
         Amount, ApplicationPermissions, ArithmeticError, BlockHeight, OracleResponse, Resources,
         SendMessageRequest, Timestamp,
     },
-    ensure,
+    ensure, http,
     identifiers::{
         Account, AccountOwner, ApplicationId, BlobId, BlobType, ChainId, ChannelFullName,
         ChannelName, MessageId, Owner, StreamId, StreamName,
@@ -699,12 +699,13 @@ impl<UserInstance> BaseRuntime for SyncRuntimeHandle<UserInstance> {
 
     fn perform_http_request(
         &mut self,
+        method: http::Method,
         url: &str,
         content_type: String,
         payload: Vec<u8>,
     ) -> Result<Vec<u8>, ExecutionError> {
         self.inner()
-            .perform_http_request(url, content_type, payload)
+            .perform_http_request(method, url, content_type, payload)
     }
 
     fn assert_before(&mut self, timestamp: Timestamp) -> Result<(), ExecutionError> {
@@ -949,6 +950,7 @@ impl<UserInstance> BaseRuntime for SyncRuntimeInternal<UserInstance> {
 
     fn perform_http_request(
         &mut self,
+        method: http::Method,
         url: &str,
         content_type: String,
         payload: Vec<u8>,
@@ -967,6 +969,7 @@ impl<UserInstance> BaseRuntime for SyncRuntimeInternal<UserInstance> {
                 let url = url.to_string();
                 self.execution_state_sender
                     .send_request(|callback| ExecutionRequest::PerformHttpRequest {
+                        method,
                         url,
                         content_type,
                         payload,

linera-execution/src/wasm/system_api.rs
@@ -6,6 +6,7 @@ use std::{any::Any, collections::HashMap, marker::PhantomData};
 use linera_base::{
     crypto::CryptoHash,
     data_types::{Amount, ApplicationPermissions, BlockHeight, SendMessageRequest, Timestamp},
+    http,
     identifiers::{
         Account, AccountOwner, ApplicationId, ChainId, ChannelName, MessageId, Owner, StreamName,
     },
@@ -377,9 +378,10 @@ where
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
-    /// Makes a POST request to the given URL and returns the response body.
+    /// Makes an HTTP request to the given URL and returns the response body.
     fn perform_http_request(
         caller: &mut Caller,
+        method: http::Method,
         query: String,
         content_type: String,
         payload: Vec<u8>,
@@ -387,7 +389,7 @@ where
         caller
             .user_data_mut()
             .runtime
-            .perform_http_request(&query, content_type, payload)
+            .perform_http_request(method, &query, content_type, payload)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
@@ -590,9 +592,10 @@ where
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 
-    /// Makes a POST request to the given URL and returns the response body.
+    /// Makes an HTTP request to the given URL and returns the response body.
     fn perform_http_request(
         caller: &mut Caller,
+        method: http::Method,
         query: String,
         content_type: String,
         payload: Vec<u8>,
@@ -600,7 +603,7 @@ where
         caller
             .user_data_mut()
             .runtime
-            .perform_http_request(&query, content_type, payload)
+            .perform_http_request(method, &query, content_type, payload)
             .map_err(|error| RuntimeError::Custom(error.into()))
     }
 

linera-sdk/src/contract/conversions_to_wit.rs
@@ -9,6 +9,7 @@ use linera_base::{
         Amount, ApplicationPermissions, BlockHeight, Resources, SendMessageRequest, TimeDelta,
         Timestamp,
     },
+    http,
     identifiers::{
         Account, AccountOwner, ApplicationId, BytecodeId, ChainId, ChannelName, Destination,
         MessageId, Owner, StreamName,
@@ -177,6 +178,22 @@ impl From<Resources> for wit_system_api::Resources {
             messages: resources.messages,
             message_size: resources.message_size,
             storage_size_delta: resources.storage_size_delta,
+        }
+    }
+}
+
+impl From<http::Method> for wit_system_api::HttpMethod {
+    fn from(method: http::Method) -> Self {
+        match method {
+            http::Method::Get => wit_system_api::HttpMethod::Get,
+            http::Method::Post => wit_system_api::HttpMethod::Post,
+            http::Method::Put => wit_system_api::HttpMethod::Put,
+            http::Method::Delete => wit_system_api::HttpMethod::Delete,
+            http::Method::Head => wit_system_api::HttpMethod::Head,
+            http::Method::Options => wit_system_api::HttpMethod::Options,
+            http::Method::Connect => wit_system_api::HttpMethod::Connect,
+            http::Method::Patch => wit_system_api::HttpMethod::Patch,
+            http::Method::Trace => wit_system_api::HttpMethod::Trace,
         }
     }
 }