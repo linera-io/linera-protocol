c5c34a3a2f8ef651f6e3d3943a4845e756f3a062
Andre da Silva <2917611+ndr-ds@users.noreply.github.com>
2025-02-20 20:54:32 -0300

    Adjust latency metrics buckets (#3379)
    ## Motivation
    
    We don't need to track latencies below 1ms IMHO. Also, for the server latency, I noticed it would very often just shoot to 50ms and stay there the whole time. If you're using few shards (1, like me, for example), shard latency can actually be fairly high.
    
    ## Proposal
    
    Adjust things
    
    ## Test Plan
    
    Check the changes on a local network
    
    ## Release Plan
    
    - Nothing to do / These changes follow the usual release cycle.

--- linera-service/src/proxy/grpc.rs
+++ linera-service/src/proxy/grpc.rs
@@ -57,7 +57,7 @@ use tracing::{debug, info, instrument, Instrument as _, Level};
 #[cfg(with_metrics)]
 use {
     linera_base::prometheus_util::{
-        bucket_latencies, register_histogram_vec, register_int_counter_vec,
+        bucket_interval, register_histogram_vec, register_int_counter_vec,
     },
     prometheus::{HistogramVec, IntCounterVec},
 };
@@ -71,7 +71,7 @@ static PROXY_REQUEST_LATENCY: LazyLock<HistogramVec> = LazyLock::new(|| {
         "proxy_request_latency",
         "Proxy request latency",
         &[],
-        bucket_latencies(500.0),
+        bucket_interval(1.0, 500.0),
     )
 });
 
