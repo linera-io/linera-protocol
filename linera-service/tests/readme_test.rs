5bfec663679e09a2fd17a99a969b55606e7a3db8
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2025-02-22 15:02:49 -0500

    upgrade LLM readme and make it run in CI!

--- linera-service/tests/readme_test.rs
+++ linera-service/tests/readme_test.rs
@@ -26,6 +26,7 @@ use tokio::process::Command;
 #[test_case::test_case("../examples/fungible" ; "fungible")]
 #[test_case::test_case("../examples/gen-nft" ; "gen-nft")]
 #[test_case::test_case("../examples/hex-game" ; "hex-game")]
+#[test_case::test_case("../examples/lmm" ; "lmm")]
 #[test_case::test_case("../examples/native-fungible" ; "native-fungible")]
 #[test_case::test_case("../examples/non-fungible" ; "non-fungible")]
 #[test_case::test_case("../examples/matching-engine" ; "matching engine")]
