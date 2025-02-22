fadb489fc3c85535bdf45994664110cdb448a31e
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-02-22 09:33:30 +0100

    Further corrections based on CI.

--- linera-execution/src/lib.rs
+++ linera-execution/src/lib.rs
@@ -60,6 +60,8 @@ use thiserror::Error;
 pub use crate::applications::ApplicationRegistry;
 #[cfg(with_revm)]
 use crate::revm::EvmExecutionError;
+#[cfg(all(with_revm, not(with_wasmer), not(with_wasmtime)))]
+use linera_base::vm::EvmRuntime;
 use crate::runtime::ContractSyncRuntime;
 #[cfg(all(with_testing, with_wasm_runtime))]
 pub use crate::wasm::test as wasm_test;
