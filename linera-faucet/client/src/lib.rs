8066d89f9e11e33816960c42b464f42d63c7f090
James Kay <james.kay@linera.io>
2025-02-23 18:51:46 +0000

    `linera-faucet-client`: remove `anyhow` (#3365)
    ## Motivation
    
    `anyhow::Error` doesn't implement `std::error::Error`, and therefore
    can't be converted to a JavaScript error (`wasm_bindgen::JsError`).
    
    ## Proposal
    
    Give `linera-faucet-client` a dedicated error type, with help from
    `thiserror` and `thiserror-context`.
    
    ## Test Plan
    
    Refactor: should be tested by CI.
    
    ## Release Plan
    
    - Nothing to do / These changes follow the usual release cycle.
    
    ## Links
    
    - [reviewer
    checklist](https://github.com/linera-io/linera-protocol/blob/main/CONTRIBUTING.md#reviewer-checklist)

--- linera-faucet/client/src/lib.rs
+++ linera-faucet/client/src/lib.rs
@@ -3,30 +3,26 @@
 
 //! The client component of the Linera faucet.
 
-use anyhow::{bail, Context, Result};
-use linera_base::{crypto::ValidatorPublicKey, identifiers::Owner};
+// TODO(#3362): generate this code
+
+use linera_base::crypto::ValidatorPublicKey;
 use linera_client::config::GenesisConfig;
 use linera_faucet::ClaimOutcome;
 use linera_version::VersionInfo;
-use serde_json::{json, Value};
+use thiserror_context::Context;
 
-fn truncate_query_output(input: &str) -> String {
-    let max_len = 200;
-    if input.len() < max_len {
-        input.to_string()
-    } else {
-        format!("{} ...", input.get(..max_len).unwrap())
-    }
+#[derive(Debug, thiserror::Error)]
+#[non_exhaustive]
+pub enum ErrorInner {
+    #[error("JSON parsing error: {0:?}")]
+    Json(#[from] serde_json::Error),
+    #[error("GraphQL error: {0:?}")]
+    GraphQl(Vec<serde_json::Value>),
+    #[error("HTTP error: {0:?}")]
+    Http(#[from] reqwest::Error),
 }
 
-fn reqwest_client() -> reqwest::Client {
-    let builder = reqwest::ClientBuilder::new();
-
-    #[cfg(not(target_arch = "wasm32"))]
-    let builder = builder.timeout(std::time::Duration::from_secs(30));
-
-    builder.build().unwrap()
-}
+thiserror_context::impl_context!(Error(ErrorInner));
 
 /// A faucet instance that can be queried.
 #[derive(Debug, Clone)]
@@ -43,170 +39,104 @@ impl Faucet {
         &self.url
     }
 
-    pub async fn genesis_config(&self) -> Result<GenesisConfig> {
-        let query = "query { genesisConfig }";
-        let client = reqwest_client();
-        let response = client
+    async fn query<Response: serde::de::DeserializeOwned>(
+        &self,
+        query: &str,
+    ) -> Result<Response, Error> {
+        #[derive(serde::Deserialize)]
+        struct GraphQlResponse<T> {
+            data: Option<T>,
+            errors: Option<Vec<serde_json::Value>>,
+        }
+
+        let builder = reqwest::ClientBuilder::new();
+
+        #[cfg(not(target_arch = "wasm32"))]
+        let builder = builder.timeout(std::time::Duration::from_secs(30));
+
+        let response: GraphQlResponse<Response> = builder
+            .build()
+            .unwrap()
             .post(&self.url)
-            .json(&json!({ "query": query }))
+            .json(&serde_json::json!({
+                "query": query,
+            }))
             .send()
             .await
-            .context("genesis_config: failed to post query")?;
-        anyhow::ensure!(
-            response.status().is_success(),
-            "Query \"{}\" failed: {}",
-            query,
-            response
-                .text()
-                .await
-                .unwrap_or_else(|error| format!("Could not get response text: {error}"))
-        );
-        let mut value: Value = response.json().await.context("invalid JSON")?;
-        if let Some(errors) = value.get("errors") {
-            bail!("Query \"{}\" failed: {}", query, errors);
+            .with_context(|| format!("executing query {query:?}"))?
+            .error_for_status()?
+            .json()
+            .await?;
+
+        if let Some(errors) = response.errors {
+            Err(ErrorInner::GraphQl(errors).into())
+        } else {
+            Ok(response
+                .data
+                .expect("no errors present but no data returned"))
         }
-        serde_json::from_value(value["data"]["genesisConfig"].take())
-            .context("could not parse genesis config")
     }
 
-    pub async fn version_info(&self) -> Result<VersionInfo> {
-        let query =
-            "query { version { crateVersion gitCommit gitDirty rpcHash graphqlHash witHash } }";
-        let client = reqwest_client();
-        let response = client
-            .post(&self.url)
-            .json(&json!({ "query": query }))
-            .send()
-            .await
-            .context("version_info: failed to post query")?;
-        anyhow::ensure!(
-            response.status().is_success(),
-            "Query \"{}\" failed: {}",
-            query,
-            response
-                .text()
-                .await
-                .unwrap_or_else(|error| format!("Could not get response text: {error}"))
-        );
-        let mut value: Value = response.json().await.context("invalid JSON")?;
-        if let Some(errors) = value.get("errors") {
-            bail!("Query \"{}\" failed: {}", query, errors);
+    pub async fn genesis_config(&self) -> Result<GenesisConfig, Error> {
+        #[derive(serde::Deserialize)]
+        #[serde(rename_all = "camelCase")]
+        struct Response {
+            genesis_config: GenesisConfig,
         }
-        let crate_version = serde_json::from_value(value["data"]["version"]["crateVersion"].take())
-            .context("could not parse crate version")?;
-        let git_commit = serde_json::from_value(value["data"]["version"]["gitCommit"].take())
-            .context("could not parse git commit")?;
-        let git_dirty = serde_json::from_value(value["data"]["version"]["gitDirty"].take())
-            .context("could not parse git dirty")?;
-        let rpc_hash = serde_json::from_value(value["data"]["version"]["rpcHash"].take())
-            .context("could not parse rpc hash")?;
-        let graphql_hash = serde_json::from_value(value["data"]["version"]["graphqlHash"].take())
-            .context("could not parse graphql hash")?;
-        let wit_hash = serde_json::from_value(value["data"]["version"]["witHash"].take())
-            .context("could not parse wit hash")?;
-        Ok(VersionInfo {
-            crate_version,
-            git_commit,
-            git_dirty,
-            rpc_hash,
-            graphql_hash,
-            wit_hash,
-        })
+
+        Ok(self
+            .query::<Response>("query { genesisConfig }")
+            .await?
+            .genesis_config)
     }
 
-    pub async fn claim(&self, owner: &Owner) -> Result<ClaimOutcome> {
+    pub async fn version_info(&self) -> Result<VersionInfo, Error> {
+        #[derive(serde::Deserialize)]
+        struct Response {
+            version: VersionInfo,
+        }
+
+        Ok(self.query::<Response>("query { version }").await?.version)
+    }
+
+    pub async fn claim(
+        &self,
+        owner: &linera_base::identifiers::Owner,
+    ) -> Result<ClaimOutcome, Error> {
         let query = format!(
             "mutation {{ claim(owner: \"{owner}\") {{ \
                 messageId chainId certificateHash \
             }} }}"
         );
-        let client = reqwest_client();
-        let response = client
-            .post(&self.url)
-            .json(&json!({ "query": &query }))
-            .send()
-            .await
-            .with_context(|| {
-                format!(
-                    "claim: failed to post query={}",
-                    truncate_query_output(&query)
-                )
-            })?;
-        anyhow::ensure!(
-            response.status().is_success(),
-            "Query \"{}\" failed: {}",
-            query,
-            response
-                .text()
-                .await
-                .unwrap_or_else(|error| format!("Could not get response text: {error}"))
-        );
-        let value: Value = response.json().await.context("invalid JSON")?;
-        if let Some(errors) = value.get("errors") {
-            bail!("Query \"{}\" failed: {}", query, errors);
+
+        #[derive(serde::Deserialize)]
+        struct Response {
+            claim: ClaimOutcome,
         }
-        let data = &value["data"]["claim"];
-        let message_id = data["messageId"]
-            .as_str()
-            .context("message ID not found")?
-            .parse()
-            .context("could not parse message ID")?;
-        let chain_id = data["chainId"]
-            .as_str()
-            .context("chain ID not found")?
-            .parse()
-            .context("could not parse chain ID")?;
-        let certificate_hash = data["certificateHash"]
-            .as_str()
-            .context("Certificate hash not found")?
-            .parse()
-            .context("could not parse certificate hash")?;
-        let outcome = ClaimOutcome {
-            message_id,
-            chain_id,
-            certificate_hash,
-        };
-        Ok(outcome)
+
+        Ok(self.query::<Response>(&query).await?.claim)
     }
 
-    pub async fn current_validators(&self) -> Result<Vec<(ValidatorPublicKey, String)>> {
-        let query = "query { currentValidators { publicKey networkAddress } }";
-        let client = reqwest_client();
-        let response = client
-            .post(&self.url)
-            .json(&json!({ "query": query }))
-            .send()
-            .await
-            .context("current_validators: failed to post query")?;
-        anyhow::ensure!(
-            response.status().is_success(),
-            "Query \"{}\" failed: {}",
-            query,
-            response
-                .text()
-                .await
-                .unwrap_or_else(|error| format!("Could not get response text: {error}"))
-        );
-        let mut value: Value = response.json().await.context("invalid JSON")?;
-        if let Some(errors) = value.get("errors") {
-            bail!("Query \"{}\" failed: {}", query, errors);
+    pub async fn current_validators(&self) -> Result<Vec<(ValidatorPublicKey, String)>, Error> {
+        #[derive(serde::Deserialize)]
+        #[serde(rename_all = "camelCase")]
+        struct Validator {
+            public_key: ValidatorPublicKey,
+            network_address: String,
         }
-        let validators = match value["data"]["currentValidators"].take() {
-            serde_json::Value::Array(validators) => validators,
-            validators => bail!("{validators} is not an array"),
-        };
-        validators
+
+        #[derive(serde::Deserialize)]
+        #[serde(rename_all = "camelCase")]
+        struct Response {
+            current_validators: Vec<Validator>,
+        }
+
+        Ok(self
+            .query::<Response>("query { currentValidators { publicKey networkAddress } }")
+            .await?
+            .current_validators
             .into_iter()
-            .map(|mut validator| {
-                let name =
-                    serde_json::from_value::<ValidatorPublicKey>(validator["publicKey"].take())
-                        .context("could not parse current validators: invalid name")?;
-                let addr = validator["networkAddress"]
-                    .as_str()
-                    .context("could not parse current validators: invalid address")?
-                    .to_string();
-                Ok((name, addr))
-            })
-            .collect()
+            .map(|validator| (validator.public_key, validator.network_address))
+            .collect())
     }
 }
