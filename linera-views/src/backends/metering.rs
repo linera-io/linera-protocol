29d2748ad7880d7ef86d1411e52d622e356b4718
comfsrt <155266597+comfsrt@users.noreply.github.com>
2025-02-25 19:27:40 +0100

    spelling errors metering.rs (#3410)
    _read_value_number_none_cases - _read_value_none_cases
    
    ---------
    
    Signed-off-by: comfsrt <155266597+comfsrt@users.noreply.github.com>

--- linera-views/src/backends/metering.rs
+++ linera-views/src/backends/metering.rs
@@ -158,8 +158,8 @@ impl KeyValueStoreMetrics {
         let entry2 = format!("{} delete latency", title_name);
         let delete_latency = register_histogram_vec(&entry1, &entry2, &[], None);
 
-        let entry1 = format!("{}_read_value_number_none_cases", var_name);
-        let entry2 = format!("{} read value number none cases", title_name);
+        let entry1 = format!("{}_read_value_none_cases", var_name);
+        let entry2 = format!("{} read value none cases", title_name);
         let read_value_none_cases = register_int_counter_vec(&entry1, &entry2, &[]);
 
         let entry1 = format!("{}_read_value_key_size", var_name);
