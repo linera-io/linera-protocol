2e12134a18558fc946818c7ffb292f4495dd5d26
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2025-02-28 03:18:53 +0000

    Implement a service query to perform HTTP requests
    Allow the client to run the service in order to perform an HTTP request.
    The status code is checked to be OK, the response headers discarded and
    the response body returned to the caller.

--- examples/how-to/perform-http-requests/src/service.rs
+++ examples/how-to/perform-http-requests/src/service.rs
@@ -5,12 +5,11 @@
 
 use std::sync::Arc;
 
-use async_graphql::{
-    connection::EmptyFields, EmptyMutation, EmptySubscription, Request, Response, Schema,
-};
+use async_graphql::{EmptyMutation, EmptySubscription, Request, Response, Schema};
 use how_to_perform_http_requests::Abi;
-use linera_sdk::{linera_base_types::WithServiceAbi, Service as _, ServiceRuntime};
+use linera_sdk::{ensure, http, linera_base_types::WithServiceAbi, Service as _, ServiceRuntime};
 
+#[derive(Clone)]
 pub struct Service {
     runtime: Arc<ServiceRuntime<Self>>,
 }
@@ -31,7 +30,51 @@ impl linera_sdk::Service for Service {
     }
 
     async fn handle_query(&self, request: Request) -> Response {
-        let schema = Schema::build(EmptyFields, EmptyMutation, EmptySubscription).finish();
+        let schema = Schema::build(
+            Query {
+                service: self.clone(),
+            },
+            EmptyMutation,
+            EmptySubscription,
+        )
+        .finish();
         schema.execute(request).await
+    }
+}
+
+/// The handler for service queries.
+struct Query {
+    service: Service,
+}
+
+#[async_graphql::Object]
+impl Query {
+    /// Performs an HTTP query in the service, and returns the response body if the status
+    /// code is OK.
+    ///
+    /// Note that any headers in the response are discarded.
+    pub async fn perform_http_request(&self) -> async_graphql::Result<Vec<u8>> {
+        self.service.perform_http_request()
+    }
+}
+
+impl Service {
+    /// Performs an HTTP query in the service, and returns the response body if the status
+    /// code is OK.
+    ///
+    /// Note that any headers in the response are discarded.
+    pub fn perform_http_request(&self) -> async_graphql::Result<Vec<u8>> {
+        let url = self.runtime.application_parameters();
+        let response = self.runtime.http_request(http::Request::get(url));
+
+        ensure!(
+            response.status == 200,
+            async_graphql::Error::new(format!(
+                "HTTP request failed with status code {}",
+                response.status
+            ))
+        );
+
+        Ok(response.body)
     }
 }
