603bbf17d31c7b3ebf5e63ae81ab246942975f3e
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2025-03-03 20:58:38 +0000

    Test reading native balance of one owner account
    Add an integration test to test the new `owner_balance` getter method.

--- examples/native-fungible/tests/transfers.rs
+++ examples/native-fungible/tests/transfers.rs
@@ -7,7 +7,7 @@
 
 use fungible::{self, FungibleTokenAbi};
 use linera_sdk::{
-    linera_base_types::{Amount, ChainId},
+    linera_base_types::{Account, Amount, ChainId, CryptoHash, Owner},
     test::{Recipient, TestValidator},
 };
 
@@ -42,4 +42,42 @@ async fn chain_balance_transfers() {
         .await;
 
     assert_eq!(recipient_chain.chain_balance().await, transfer_amount);
+}
+
+/// Tests if an individual account can receive tokens.
+#[test_log::test(tokio::test)]
+async fn transfer_to_owner() {
+    let parameters = fungible::Parameters {
+        ticker_symbol: "NAT".to_owned(),
+    };
+    let initial_state = fungible::InitialStateBuilder::default().build();
+    let (validator, _application_id, recipient_chain) = TestValidator::with_current_application::<
+        FungibleTokenAbi,
+        _,
+        _,
+    >(parameters, initial_state)
+    .await;
+
+    let transfer_amount = Amount::from_tokens(2);
+    let funding_chain = validator.get_chain(&ChainId::root(0));
+    let owner = Owner(CryptoHash::test_hash("owner"));
+    let account = Account::owner(recipient_chain.id(), owner);
+    let recipient = Recipient::Account(account);
+
+    let transfer_certificate = funding_chain
+        .add_block(|block| {
+            block.with_native_token_transfer(None, recipient, transfer_amount);
+        })
+        .await;
+
+    recipient_chain
+        .add_block(|block| {
+            block.with_messages_from(&transfer_certificate);
+        })
+        .await;
+
+    assert_eq!(
+        recipient_chain.owner_balance(&owner.into()).await,
+        Some(transfer_amount)
+    );
 }
