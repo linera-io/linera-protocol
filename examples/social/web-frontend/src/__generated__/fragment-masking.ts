a0c91b1f8f35da23173431326f8f1902a74f56d5
Eric <eric.norberg@protonmail.com>
2025-03-03 11:01:52 +0300

    fix

--- examples/social/web-frontend/src/__generated__/fragment-masking.ts
+++ examples/social/web-frontend/src/__generated__/fragment-masking.ts
@@ -8,9 +8,9 @@ export type FragmentType<TDocumentType extends DocumentTypeDecoration<any, any>>
   infer TType,
   any
 >
-  ? [TType] extends [{ ' $fragmentName'?: infer TKey }]
+  ? [TType] extends [{ '$fragmentName'?: infer TKey }]
     ? TKey extends string
-      ? { ' $fragmentRefs'?: { [key in TKey]: TType } }
+      ? { '$fragmentRefs'?: { [key in TKey]: TType } }
       : never
     : never
   : never;
