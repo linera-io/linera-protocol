a58f3ea6858e3b2310eb570cc68fd194b0f1f6d5
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2025-03-07 03:25:48 +0000

    Test if contract rejects an invalid HTTP response
    Ensure that the contract panics if the off-chain HTTP response can't be
    trusted.

--- examples/how-to/perform-http-requests/src/unit_tests/contract.rs
+++ examples/how-to/perform-http-requests/src/unit_tests/contract.rs
@@ -25,6 +25,22 @@ fn accepts_valid_off_chain_response() {
         .blocking_wait();
 }
 
+/// Tests if the contract rejects an invalid HTTP response obtained off-chain.
+///
+/// The contract should panic if it receives a HTTP response that it can't trust. In
+/// this example application, that just means an HTTP response different from one it
+/// expects, but in most applications this would involve checking the signature of the
+/// response to see if it was signed by a trusted party that created the response.
+#[test]
+#[should_panic(expected = "assertion `left == right` failed")]
+fn rejects_invalid_off_chain_response() {
+    let mut contract = create_contract();
+
+    contract
+        .execute_operation(Operation::HandleHttpResponse(b"Fake response".to_vec()))
+        .blocking_wait();
+}
+
 /// Creates a [`Contract`] instance for testing.
 fn create_contract() -> Contract {
     let runtime = ContractRuntime::new();
