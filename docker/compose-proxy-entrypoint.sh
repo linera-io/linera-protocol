00680b193e9cfc127595e4432e5b357347366e10
Christos Hadjiaslanis <christos.hadjiaslanis@linera.io>
2025-04-01 17:56:06 +0100

    WIP

--- docker/compose-proxy-entrypoint.sh
+++ docker/compose-proxy-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Extract the ordinal number from the pod hostname
-ORDINAL="${HOSTNAME##*-}"
+#ORDINAL="${HOSTNAME##*-}"
+ORDINAL=$(echo "$( v="$( nslookup "$( hostname -i )" | head -n 1 )"; v="${v##* = }"; v="${v%%.*}"; v="${v##*-}"; v="${v##*_}"; echo "$v" )")
 
 exec ./linera-proxy \
   --storage scylladb:tcp:scylla:9042 \
