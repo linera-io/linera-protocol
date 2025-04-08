e386a44297c6fca1bf4b031e29149a8ade2f928d
Andreas Fackler <andreas.fackler@linera.io>
2025-04-08 17:32:07 +0200

    Fix build.

--- linera-storage/src/db_storage.rs
+++ linera-storage/src/db_storage.rs
@@ -892,6 +892,15 @@ where
         }
         Ok(blob_ids)
     }
+
+    pub async fn block_exporter_context(
+        &self,
+        block_exporter_id: u32,
+    ) -> Result<ViewContext<u32, Store>, ViewError> {
+        let root_key = bcs::to_bytes(&BaseKey::BlockExporterState(block_exporter_id))?;
+        let store = self.store.clone_with_root_key(&root_key)?;
+        Ok(ViewContext::create_root_context(store, block_exporter_id).await?)
+    }
 }
 
 impl<Store> DbStorage<Store, WallClock>
@@ -914,15 +923,6 @@ where
             }
         }
         Ok(chain_ids)
-    }
-
-    pub async fn block_exporter_context(
-        &self,
-        block_exporter_id: u32,
-    ) -> Result<ViewContext<u32, Store>, ViewError> {
-        let root_key = bcs::to_bytes(&BaseKey::BlockExporterState(block_exporter_id))?;
-        let store = self.store.clone_with_root_key(&root_key)?;
-        Ok(ViewContext::create_root_context(store, block_exporter_id).await?)
     }
 }
 
