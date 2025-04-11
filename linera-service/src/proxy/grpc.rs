445eef856f0084b6cd2dfc588591cf78e3374bdc
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2025-04-11 19:19:46 -0400

    use the network information in the proxy

--- linera-service/src/proxy/grpc.rs
+++ linera-service/src/proxy/grpc.rs
@@ -19,7 +19,6 @@ use anyhow::Result;
 use async_trait::async_trait;
 use futures::{future::BoxFuture, FutureExt as _};
 use linera_base::identifiers::ChainId;
-use linera_client::config::GenesisConfig;
 use linera_core::{notifier::ChannelNotifier, JoinSetExt as _};
 use linera_rpc::{
     config::{
@@ -150,7 +149,6 @@ pub struct GrpcProxy<S>(Arc<GrpcProxyInner<S>>);
 struct GrpcProxyInner<S> {
     public_config: ValidatorPublicNetworkConfig,
     internal_config: ValidatorInternalNetworkConfig,
-    genesis_config: GenesisConfig,
     worker_connection_pool: GrpcConnectionPool,
     notifier: ChannelNotifier<Result<Notification, Status>>,
     tls: TlsConfig,
@@ -164,7 +162,6 @@ where
     pub fn new(
         public_config: ValidatorPublicNetworkConfig,
         internal_config: ValidatorInternalNetworkConfig,
-        genesis_config: GenesisConfig,
         connect_timeout: Duration,
         timeout: Duration,
         tls: TlsConfig,
@@ -173,7 +170,6 @@ where
         Self(Arc::new(GrpcProxyInner {
             public_config,
             internal_config,
-            genesis_config,
             worker_connection_pool: GrpcConnectionPool::default()
                 .with_connect_timeout(connect_timeout)
                 .with_timeout(timeout),
@@ -474,7 +470,16 @@ where
         &self,
         _request: Request<()>,
     ) -> Result<Response<CryptoHash>, Status> {
-        Ok(Response::new(self.0.genesis_config.hash().into()))
+        let description = self
+            .0
+            .storage
+            .read_network_description()
+            .await
+            .map_err(Self::error_to_status)?
+            .ok_or(Status::not_found(
+                "Cannot find network description in the database",
+            ))?;
+        Ok(Response::new(description.genesis_config_hash.into()))
     }
 
     #[instrument(skip_all, err(Display))]
--- linera-service/src/proxy/main.rs
+++ linera-service/src/proxy/main.rs
@@ -5,7 +5,7 @@
 
 use std::{net::SocketAddr, path::PathBuf, time::Duration};
 
-use anyhow::{bail, ensure, Result};
+use anyhow::{anyhow, bail, ensure, Result};
 use async_trait::async_trait;
 use futures::{FutureExt as _, SinkExt, StreamExt};
 use linera_base::listen_for_shutdown_signals;
@@ -106,7 +106,6 @@ where
 
 struct ProxyContext {
     config: ValidatorServerConfig,
-    genesis_config: GenesisConfig,
     send_timeout: Duration,
     recv_timeout: Duration,
 }
@@ -114,12 +113,10 @@ struct ProxyContext {
 impl ProxyContext {
     pub fn from_options(options: &ProxyOptions) -> Result<Self> {
         let config = util::read_json(&options.config_path)?;
-        let genesis_config = util::read_json(&options.genesis_config_path)?;
         Ok(Self {
             config,
             send_timeout: options.send_timeout,
             recv_timeout: options.recv_timeout,
-            genesis_config,
         })
     }
 }
@@ -155,7 +152,6 @@ where
                 Self::Grpc(GrpcProxy::new(
                     context.config.validator.network,
                     context.config.internal_network,
-                    context.genesis_config,
                     context.send_timeout,
                     context.recv_timeout,
                     tls,
@@ -175,7 +171,6 @@ where
                     .validator
                     .network
                     .clone_with_protocol(public_transport),
-                genesis_config: context.genesis_config,
                 send_timeout: context.send_timeout,
                 recv_timeout: context.recv_timeout,
                 storage,
@@ -200,7 +195,6 @@ where
 {
     public_config: ValidatorPublicNetworkPreConfig<TransportProtocol>,
     internal_config: ValidatorInternalNetworkPreConfig<TransportProtocol>,
-    genesis_config: GenesisConfig,
     send_timeout: Duration,
     recv_timeout: Duration,
     storage: S,
@@ -312,9 +306,16 @@ where
                     linera_version::VersionInfo::default().into(),
                 )))
             }
-            GenesisConfigHashQuery => Ok(Some(RpcMessage::GenesisConfigHashResponse(Box::new(
-                self.genesis_config.hash(),
-            )))),
+            GenesisConfigHashQuery => {
+                let description = self
+                    .storage
+                    .read_network_description()
+                    .await?
+                    .ok_or(anyhow!("Cannot find network description in the database"))?;
+                Ok(Some(RpcMessage::GenesisConfigHashResponse(Box::new(
+                    description.genesis_config_hash,
+                ))))
+            }
             UploadBlob(content) => {
                 let blob = Blob::new(*content);
                 let id = blob.id();
