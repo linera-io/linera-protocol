1051bae3e9d8d9845a728deb8a01ab36408a2755
Andreas Fackler <afck@users.noreply.github.com>
2025-04-29 16:58:02 +0200

    Automatically rotate the faucet chain once it has a maximum length. (#3848)
    ## Motivation
    
    If the faucet chain gets very long, requesting a new chain becomes very
    slow for new wallets.
    
    ## Proposal
    
    Make the faucet switch chains every 100 blocks.
    
    ## Test Plan
    
    I extended `test_end_to_end_faucet_with_long_chains`.
    
    ## Release Plan
    
    - Nothing to do / These changes follow the usual release cycle.
    - These changes should be backported to the latest `testnet` branch,
    then
        - be deployed as the faucet.
    
    ## Links
    
    - Closes #3846.
    - [reviewer
    checklist](https://github.com/linera-io/linera-protocol/blob/main/CONTRIBUTING.md#reviewer-checklist)

--- linera-faucet/server/src/tests.rs
+++ linera-faucet/server/src/tests.rs
@@ -9,7 +9,7 @@ use async_trait::async_trait;
 use futures::lock::Mutex;
 use linera_base::{
     crypto::{AccountPublicKey, AccountSecretKey},
-    data_types::{Amount, Timestamp},
+    data_types::{Amount, BlockHeight, Timestamp},
     identifiers::ChainId,
 };
 use linera_client::{chain_listener, wallet::Wallet};
@@ -83,9 +83,10 @@ async fn test_faucet_rate_limiting() {
     };
     let context = Arc::new(Mutex::new(context));
     let root = MutationRoot {
-        chain_id,
+        chain_id: Arc::new(Mutex::new(chain_id)),
         context: context.clone(),
         amount: Amount::from_tokens(1),
+        end_block_height: BlockHeight::from(10),
         end_timestamp: Timestamp::from(6000),
         start_timestamp: Timestamp::from(0),
         start_balance: Amount::from_tokens(6),
