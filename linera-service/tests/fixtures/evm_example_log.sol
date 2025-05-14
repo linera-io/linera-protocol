2dfa74aeeac40f3c15d29ca81d00d676670b3bb1
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-05-14 11:21:17 +0200

    Complete the test.

--- linera-service/tests/fixtures/evm_example_log.sol
+++ linera-service/tests/fixtures/evm_example_log.sol
@@ -5,14 +5,15 @@ pragma solidity ^0.8.0;
 contract ExampleEvent {
     uint64 value;
     event Start(uint64 value);
-    event Increment(address from, uint64 input, uint64 value);
+    event Increment(uint64 input, uint64 value);
 
     constructor(uint64 start_value) {
+        value = start_value;
         emit Start(start_value);
     }
 
     function increment(uint64 input) external {
         value = value + input;
-        emit Increment(msg.sender, input, value);
+        emit Increment(input, value);
     }
 }
