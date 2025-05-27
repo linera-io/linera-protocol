5e23d7ee0541c9a92a03f857060d09deb6c239e0
deuszx <mateusz.gorski@linera.io>
2025-05-27 10:13:59 +0100

    Init version of EmbeddedSigner

--- linera-web/src/signer.rs
+++ linera-web/src/signer.rs
@@ -9,7 +9,6 @@ pub mod in_memory {
         identifiers::AccountOwner,
     };
     use linera_persistent::{self as persistent};
-
     use wasm_bindgen::prelude::*;
     use web_sys::wasm_bindgen;
 
@@ -49,6 +48,104 @@ pub mod in_memory {
             value: &CryptoHash,
         ) -> Result<AccountSignature, Self::Error> {
             self.0.sign(owner, value).await
+        }
+    }
+}
+
+pub mod embedded {
+    use std::fmt::Display;
+
+    use linera_base::{
+        crypto::{AccountPublicKey, AccountSignature, CryptoHash, Signer},
+        identifiers::AccountOwner,
+    };
+    use wasm_bindgen::prelude::*;
+    use web_sys::wasm_bindgen;
+
+    #[repr(u8)]
+    #[wasm_bindgen(js_name = "SignerError")]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub enum EmbeddedSignerError {
+        MissingKey = 0,
+        SigningError = 1,
+    }
+
+    impl Display for EmbeddedSignerError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                EmbeddedSignerError::MissingKey => write!(f, "No key found for the given owner"),
+                EmbeddedSignerError::SigningError => write!(f, "Error signing the value"),
+            }
+        }
+    }
+
+    impl std::error::Error for EmbeddedSignerError {}
+
+    unsafe impl Send for EmbeddedSignerError {}
+
+    #[wasm_bindgen]
+    pub struct EmbeddedSigner {
+        sign_handler: js_sys::Function,
+        get_public_key_handler: js_sys::Function,
+        contains_key_handler: js_sys::Function,
+    }
+
+    #[wasm_bindgen(js_class = "EmbeddedSigner")]
+    impl EmbeddedSigner {
+        /// Creates a new embedded signer with the provided handlers.
+        #[wasm_bindgen(constructor)]
+        pub fn new(
+            sign_handler: js_sys::Function,
+            get_public_key_handler: js_sys::Function,
+            contains_key_handler: js_sys::Function,
+        ) -> Self {
+            Self {
+                sign_handler,
+                get_public_key_handler,
+                contains_key_handler,
+            }
+        }
+    }
+
+    impl Signer for EmbeddedSigner {
+        type Error = EmbeddedSignerError;
+
+        async fn contains_key(&self, owner: &AccountOwner) -> Result<bool, Self::Error> {
+            let context = JsValue::null();
+            let js_owner = serde_wasm_bindgen::to_value(owner).unwrap();
+            let result = self
+                .contains_key_handler
+                .call1(&context, &js_owner)
+                .unwrap();
+            Ok(serde_wasm_bindgen::from_value(result).unwrap())
+        }
+
+        async fn get_public_key(
+            &self,
+            owner: &AccountOwner,
+        ) -> Result<AccountPublicKey, Self::Error> {
+            let context = JsValue::null();
+            let js_owner = serde_wasm_bindgen::to_value(owner).unwrap();
+            let result = self
+                .get_public_key_handler
+                .call1(&context, &js_owner)
+                .unwrap();
+            Ok(serde_wasm_bindgen::from_value(result).unwrap())
+        }
+
+        async fn sign(
+            &self,
+            owner: &AccountOwner,
+            value: &CryptoHash,
+        ) -> Result<AccountSignature, Self::Error> {
+            let context = JsValue::null();
+            let js_owner = serde_wasm_bindgen::to_value(owner).unwrap();
+            let js_cryptohash = serde_wasm_bindgen::to_value(value).unwrap();
+            let result = self
+                .sign_handler
+                .call2(&context, &js_owner, &js_cryptohash)
+                .unwrap();
+            Ok(serde_wasm_bindgen::from_value(result).unwrap())
         }
     }
 }
