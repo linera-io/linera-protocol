79e8a02ceea0da5b7aa486d1c537b6b7c7d2d098
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-06-03 15:24:07 +0200

    Correct the collection view.

--- linera-views/src/views/collection_view.rs
+++ linera-views/src/views/collection_view.rs
@@ -269,7 +269,10 @@ impl<W: View> ByteCollectionView<W::Context, W> {
         &self,
         short_key: &[u8],
     ) -> Result<Option<ReadGuardedView<W>>, ViewError> {
-        let mut updates = self.updates.write().await;
+        let mut updates = self
+            .updates
+            .try_write()
+            .ok_or(ViewError::CannotAcquireCollectionEntry)?;
         match updates.entry(short_key.to_vec()) {
             btree_map::Entry::Occupied(entry) => {
                 let entry = entry.into_mut();
