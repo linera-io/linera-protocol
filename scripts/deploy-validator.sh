b9353789c2baf526c6084b16d4afafc6fce366b2
Emanuele "Lele" Calo <emanuele.lele.calo@gmail.com>
2025-08-26 03:10:45 +0200

    working setup

--- scripts/deploy-validator.sh
+++ scripts/deploy-validator.sh
@@ -7,37 +7,40 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 # Function to check if a command exists
 command_exists() {
-    command -v "$1" >/dev/null 2>&1
+	command -v "$1" >/dev/null 2>&1
 }
 
 docker_compose_plugin_installed() {
-    docker compose version >/dev/null 2>&1
+	docker compose version >/dev/null 2>&1
 }
 
 if ! command_exists docker; then
-    echo "Error: Docker is not installed. Please install Docker before running this script."
-    exit 1
+	echo "Error: Docker is not installed. Please install Docker before running this script."
+	exit 1
 fi
 
 if ! docker_compose_plugin_installed; then
-    echo "Error: Docker Compose is not installed. Please install Docker Compose before running this script."
-    exit 1
+	echo "Error: Docker Compose is not installed. Please install Docker Compose before running this script."
+	exit 1
 fi
 
 HOST=$1
+export DOMAIN=$HOST
+
+export ACME_EMAIL="infra@linera.io"
 
 # Check if the host is provided as an argument
 if [ -z "$HOST" ]; then
-  echo "Usage: $0 <host> [--remote-image]"
-  exit 1
+	echo "Usage: $0 <host> [--remote-image]"
+	exit 1
 fi
 
 if [ -n "$2" ]; then
-  if [ "$2" != "--remote-image" ]; then
-    echo "Usage: $0 <host> [--remote-image]"
-    exit 1
-  fi
-  REMOTE_IMAGE="$2"
+	if [ "$2" != "--remote-image" ]; then
+		echo "Usage: $0 <host> [--remote-image]"
+		exit 1
+	fi
+	REMOTE_IMAGE="$2"
 fi
 
 # Get the current branch name and replace underscores with dashes
@@ -53,27 +56,32 @@ VALIDATOR_CONFIG="docker/validator-config.toml"
 GENESIS_CONFIG="docker/genesis.json"
 
 if [ -z "$REMOTE_IMAGE" ]; then
-  echo "Building local image from commit $GIT_COMMIT..."
-  docker build --build-arg git_commit="$GIT_COMMIT" -f  docker/Dockerfile . -t linera
-  export LINERA_IMAGE=linera
+	echo "Building local image from commit $GIT_COMMIT..."
+	docker build --build-arg git_commit="$GIT_COMMIT" -f docker/Dockerfile . -t linera
+	export LINERA_IMAGE=linera
 else
-  export LINERA_IMAGE="us-docker.pkg.dev/linera-io-dev/linera-public-registry/linera:${BRANCH_NAME}_release"
-  echo "Using remote image $LINERA_IMAGE..."
+	export LINERA_IMAGE="us-docker.pkg.dev/linera-io-dev/linera-public-registry/linera:${BRANCH_NAME}"
+	echo "Using remote image $LINERA_IMAGE..."
 fi
 
 # Create validator configuration file
 echo "Creating validator configuration..."
-cat > $VALIDATOR_CONFIG <<EOL
+cat >$VALIDATOR_CONFIG <<EOL
+
 server_config_path = "server.json"
 host = "$HOST"
-port = $PORT
-metrics_port = $METRICS_PORT
-internal_host = "proxy"
-internal_port = 20100
+port = 443
+
 [external_protocol]
-Grpc = "ClearText"
+Grpc = "Tls"
 [internal_protocol]
 Grpc = "ClearText"
+
+[[proxies]]
+host = "proxy"
+public_port = 443
+private_port = 20100
+metrics_port = $METRICS_PORT
 
 [[shards]]
 host = "docker-shard-1"
@@ -98,7 +106,7 @@ EOL
 
 # Download genesis configuration
 echo "Downloading genesis configuration..."
-wget -O $GENESIS_CONFIG $GENESIS_URL
+#wget -O $GENESIS_CONFIG $GENESIS_URL
 
 cd docker
 
