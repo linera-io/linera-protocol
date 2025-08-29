230e438b629411e3ce9cab05907bc5ed9c6c8879
deuszx <mateusz.gorski@linera.io>
2025-08-29 17:23:01 +0100

    Set fetch timeout for web client

--- linera-rpc/src/grpc/transport.rs
+++ linera-rpc/src/grpc/transport.rs
@@ -20,11 +20,16 @@ impl From<&'_ NodeOptions> for Options {
 
 cfg_if::cfg_if! {
     if #[cfg(web)] {
-        pub use tonic_web_wasm_client::{Client as Channel, Error};
+        pub use tonic_web_wasm_client::{Client as Channel, Error, options::FetchOptions};
 
-        pub fn create_channel(address: String, _options: &Options) -> Result<Channel, Error> {
-            // TODO(#1817): this should respect `options`
-            Ok(tonic_web_wasm_client::Client::new(address))
+        pub fn create_channel(address: String, options: &Options) -> Result<Channel, Error> {
+            if let Some(timeout) = options.timeout {
+                let mut options = FetchOptions::new();
+                options.timeout(timeout);
+                Ok(tonic_web_wasm_client::Client::new_with_options(address, options))
+            } else {
+                Ok(tonic_web_wasm_client::Client::new(address))
+            }
         }
     } else {
         pub use tonic::transport::{Channel, Error};
