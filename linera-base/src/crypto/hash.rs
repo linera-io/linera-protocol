e0abfd04f29e6986313b9add6f246434721453c1
Andreas Fackler <andreas.fackler@linera.io>
2025-09-01 16:43:32 +0200

    Limit both variants to 8 bytes.

--- linera-base/src/crypto/hash.rs
+++ linera-base/src/crypto/hash.rs
@@ -168,7 +168,7 @@ impl fmt::Display for CryptoHash {
 
 impl fmt::Debug for CryptoHash {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Display::fmt(self, f)
+        write!(f, "{}", hex::encode(&self.0[..8]))
     }
 }
 
