651cc812e49a1af2680d8792cf08f06ad9fabf1a
deuszx <95355183+deuszx@users.noreply.github.com>
2025-09-04 18:35:23 +0200

    [testnet] Add new DownloadRawCertificatesByHeight endpoint and use it. (#4483)
    ## Motivation
    
    During our performance tests it was found that proxies' bottleneck is
    (de)serialization of certificates when responding to
    `DownloadCertificatesByHeights` request.
    
    ## Proposal
    
    It is a waste of CPU cycles to deserialize data (after loading from the
    DB) only to serialize it for transporting over gRPC protocol.
    
    This PR:
    - adds a new method on the storage `read_certificates_raw` which returns
    raw bytes of the requested certificates
    - adds a new endpoint to `rpc.proto/ValidatorNode` –
    `DownloadRawCertificatesByHeight` that responds with (bcs) bytes of the
    requested certificates
    - updates proxy code to serve the new endpoint
    - the gRPC _client_ does NOT get a new method, instead the old
    `download_certificates_by_height` is modified to use the new endpoint.
    **This is safe:** b/c we will release this as a new SDK version meaning
    old clients still use old code paths while next version uses new.
    
    ## Test Plan
    
    CI
    
    ## Release Plan
    
    - These changes should be backported to the latest `devnet` branch, then
        - be released in a new SDK,
        - be released in a validator hotfix.
    - These changes should be backported to the latest `testnet` branch,
    then
        - be released in a new SDK,
        - be released in a validator hotfix.
    
    ## Links
    
    Testnet backport of
    https://github.com/linera-io/linera-protocol/pull/4478
    - [reviewer
    checklist](https://github.com/linera-io/linera-protocol/blob/main/CONTRIBUTING.md#reviewer-checklist)

--- linera-storage/src/lib.rs
+++ linera-storage/src/lib.rs
@@ -142,6 +142,17 @@ pub trait Storage: Sized {
         hashes: I,
     ) -> Result<Vec<Option<ConfirmedBlockCertificate>>, ViewError>;
 
+    /// Reads certificates by hashes.
+    ///
+    /// Returns a vector of tuples where the first element is a lite certificate
+    /// and the second element is confirmed block.
+    ///
+    /// It does not check if all hashes all returned.
+    async fn read_certificates_raw<I: IntoIterator<Item = CryptoHash> + Send>(
+        &self,
+        hashes: I,
+    ) -> Result<Vec<(Vec<u8>, Vec<u8>)>, ViewError>;
+
     /// Reads the event with the given ID.
     async fn read_event(&self, id: EventId) -> Result<Option<Vec<u8>>, ViewError>;
 
