9e39e2736a9513e17a23bfe9ccc6646956dedfce
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-09-09 17:22:41 +0200

    Some corrections.

--- linera-sdk/src/contract/runtime.rs
+++ linera-sdk/src/contract/runtime.rs
@@ -164,6 +164,11 @@ where
     pub fn assert_data_blob_exists(&mut self, hash: DataBlobHash) {
         base_wit::assert_data_blob_exists(hash.into())
     }
+
+    /// Returns true if the corresponding contract uses a non-zero amount of storage.
+    pub fn has_non_trivial_storage(&mut self, application: ApplicationId) -> bool {
+        base_wit::has_non_trivial_storage(application.into())
+    }
 }
 
 impl<Application> ContractRuntime<Application>
