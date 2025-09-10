ba1fc741013871e9f5d0ee2c50bb6aab3f0975ed
Andreas Fackler <afck@users.noreply.github.com>
2025-09-10 17:22:54 +0200

    Always fetch the chain description on `follow-chain`. (#4532)
    ## Motivation
    
    `follow-chain` synchronizes the new chain only if `--sync` is specified.
    Otherwise it doesn't even verify that the chain exists.
    
    ## Proposal
    
    Without `--sync`, still download the chain description. If that fails,
    don't add the chain to the wallet.
    
    ## Test Plan
    
    An end-to-end test was extended.
    
    ## Release Plan
    
    - These changes should be backported to the latest `testnet` branch,
    then
        - be released in a new SDK.
    
    ## Links
    
    - Closes https://github.com/linera-io/linera-protocol/issues/4529.
    - [reviewer
    checklist](https://github.com/linera-io/linera-protocol/blob/main/CONTRIBUTING.md#reviewer-checklist)

--- linera-service/tests/linera_net_tests.rs
+++ linera-service/tests/linera_net_tests.rs
@@ -4080,6 +4080,13 @@ async fn test_end_to_end_assign_greatgrandchild_chain(config: impl LineraNetConf
     // Verify that a third party can also follow the chain.
     client3.follow_chain(chain2, true).await?;
     assert!(client3.local_balance(account2).await? > Amount::ZERO);
+    assert!(client3.load_wallet()?.chain_ids().contains(&chain2));
+
+    // Verify that trying to follow a chain that does not exist will fail, even without --sync.
+    let wrong_id = ChainId(CryptoHash::test_hash("wrong chain ID"));
+    let result = client3.follow_chain(wrong_id, false).await;
+    assert!(result.is_err());
+    assert!(!client3.load_wallet()?.chain_ids().contains(&wrong_id));
 
     net.ensure_is_running().await?;
     net.terminate().await?;
