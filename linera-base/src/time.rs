5957976c3a9fd09c66b7f5fa96ea3c6e6836409f
James Kay <james.kay@linera.io>
2025-09-13 18:55:35 +0100

    `linera-base`: expunge `wasmtimer` in favour of `kywasmtime`

--- linera-base/src/time.rs
+++ linera-base/src/time.rs
@@ -7,8 +7,10 @@ Abstractions over time that can be used natively or on the Web.
 
 cfg_if::cfg_if! {
     if #[cfg(web)] {
+        // This must remain conditional as otherwise it pulls in JavaScript symbols
+        // on-chain (on any Wasm target).
         pub use web_time::*;
-        pub use wasmtimer::tokio as timer;
+        pub use kywasmtime as timer;
     } else {
         pub use std::time::*;
         pub use tokio::time as timer;
