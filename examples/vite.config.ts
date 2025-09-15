cd0b44a9466fb3c859fa3cc50c2d07e99a887b10
James Kay <james.kay@linera.io>
2025-09-15 19:07:53 +0100

    `examples/vite.config.ts`: check for required environment variables

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 
+const checkEnvironment = envs => {
+  const missing = envs.filter(env => !(env in process.env));
+  if (missing.length > 0)
+    throw new Error(`required environment variables missing: ${missing.toString()}`);
+
+  return {
+    name: 'checkEnvironment',
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
   envPrefix: 'LINERA_',
+  plugins: [
+    checkEnvironment([
+      'LINERA_FAUCET_URL',
+      'LINERA_APPLICATION_ID',
+    ]),
+  ],
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
