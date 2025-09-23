c6e3e26961602f4725a029f2427714a21897fe5d
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2025-09-24 00:50:42 +0900

    create option certificate_download_batch_size (#4629)
    ## Motivation
    
    Avoid timeouts and/or help experiment with better parameters when needed
    
    ## Proposal
    
    * Create CLI option `--certificate-download-batch-size`
    
    ## Test Plan
    
    CI
    
    ## Release Plan
    
    - These changes should be backported to the latest `testnet` branch,
    then
        - be released in a new SDK,
        - be released in a validator hotfix.

--- linera-web/src/lib.rs
+++ linera-web/src/lib.rs
@@ -85,6 +85,7 @@ pub const OPTIONS: ClientContextOptions = ClientContextOptions {
     restrict_chain_ids_to: None,
     long_lived_services: false,
     blob_download_timeout: linera_base::time::Duration::from_millis(1000),
+    certificate_download_batch_size: linera_core::client::DEFAULT_CERTIFICATE_DOWNLOAD_BATCH_SIZE,
     chain_worker_ttl: Duration::from_secs(30),
     grace_period: linera_core::DEFAULT_GRACE_PERIOD,
 
