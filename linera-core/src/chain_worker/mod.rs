57bc720a7ec23ed79b841fd405f34cff0f8d1c81
Bartłomiej Kamiński <bartlomiej.kaminski@linera.io>
2025-09-25 18:34:03 +0200

    Add an env var, set default to 1000

--- linera-core/src/chain_worker/mod.rs
+++ linera-core/src/chain_worker/mod.rs
@@ -14,5 +14,8 @@ pub(crate) use self::state::CrossChainUpdateHelper;
 pub(crate) use self::{
     actor::{ChainWorkerActor, ChainWorkerRequest},
     config::ChainWorkerConfig,
-    state::{BlockOutcome, CHAIN_INFO_MAX_RECEIVED_LOG_ENTRIES},
+    state::{
+        BlockOutcome, CHAIN_INFO_RECEIVED_LOG_MAX_ENTRIES_DEFAULT,
+        CHAIN_INFO_RECEIVED_LOG_MAX_ENTRIES_VAR,
+    },
 };
