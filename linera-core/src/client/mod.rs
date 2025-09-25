fa05504dcb5e18f73110e10f49a317e2e07a000c
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2025-09-26 05:56:30 +0900

    Revert "Add an env var, set default to 1000"
    This reverts commit 8827bddc42b4f461990eebf8b3baadef4b549ce3.

--- linera-core/src/client/mod.rs
+++ linera-core/src/client/mod.rs
@@ -65,9 +65,7 @@ use tokio_stream::wrappers::UnboundedReceiverStream;
 use tracing::{debug, error, info, instrument, trace, warn, Instrument as _};
 
 use crate::{
-    chain_worker::{
-        CHAIN_INFO_RECEIVED_LOG_MAX_ENTRIES_DEFAULT, CHAIN_INFO_RECEIVED_LOG_MAX_ENTRIES_VAR,
-    },
+    chain_worker::CHAIN_INFO_MAX_RECEIVED_LOG_ENTRIES,
     data_types::{ChainInfo, ChainInfoQuery, ChainInfoResponse, ClientOutcome, RoundTimeout},
     environment::Environment,
     local_node::{LocalChainInfoExt as _, LocalNodeClient, LocalNodeError},
@@ -841,17 +839,13 @@ impl<Env: Environment> Client<Env> {
         // Retrieve the list of newly received certificates from this validator.
         let mut remote_log = Vec::new();
         let mut offset = tracker;
-        let max_entries = std::env::var(CHAIN_INFO_RECEIVED_LOG_MAX_ENTRIES_VAR)
-            .ok()
-            .and_then(|var| var.parse().ok())
-            .unwrap_or(CHAIN_INFO_RECEIVED_LOG_MAX_ENTRIES_DEFAULT);
         loop {
             let query = ChainInfoQuery::new(chain_id).with_received_log_excluding_first_n(offset);
             let info = remote_node.handle_chain_info_query(query).await?;
             let received_entries = info.requested_received_log.len();
             offset += received_entries as u64;
             remote_log.extend(info.requested_received_log);
-            if received_entries < max_entries {
+            if received_entries < CHAIN_INFO_MAX_RECEIVED_LOG_ENTRIES {
                 break;
             }
         }
