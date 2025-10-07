ad2aa02dfc87fd0ea504f2d0ee792445a478ad9a
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-10-07 15:01:42 +0200

    Clean up the sizes of the keys.

--- linera-base/src/crypto/mod.rs
+++ linera-base/src/crypto/mod.rs
@@ -37,12 +37,13 @@ pub type ValidatorSignature = secp256k1::Secp256k1Signature;
 pub type ValidatorKeypair = secp256k1::Secp256k1KeyPair;
 
 /// Size constants for Allocative implementations
-const ED25519_PUBLIC_KEY_SIZE: usize = 32;
-const SECP256K1_PUBLIC_KEY_SIZE: usize = 32;
-const EVM_PUBLIC_KEY_SIZE: usize = 20;
-const ED25519_SIGNATURE_SIZE: usize = 96;
-const SECP256K1_SIGNATURE_SIZE: usize = 97;
-const EVM_SECP256K1_SIGNATURE_SIZE: usize = 85;
+const ED25519_PUBLIC_KEY_SIZE: usize = std::mem::size_of::<ed25519::Ed25519PublicKey>();
+const SECP256K1_PUBLIC_KEY_SIZE: usize = std::mem::size_of::<secp256k1::Secp256k1PublicKey>();
+const EVM_PUBLIC_KEY_SIZE: usize = std::mem::size_of::<secp256k1::evm::EvmPublicKey>();
+
+const ED25519_ACCOUNT_SIGNATURE_SIZE: usize = ED25519_PUBLIC_KEY_SIZE + std::mem::size_of::<ed25519::Ed25519Signature>();
+const SECP256K1_ACCOUNT_SIGNATURE_SIZE: usize = SECP256K1_PUBLIC_KEY_SIZE + std::mem::size_of::<secp256k1::Secp256k1Signature>();
+const EVM_SECP256K1_ACCOUNT_SIGNATURE_SIZE: usize = EVM_PUBLIC_KEY_SIZE + std::mem::size_of::<secp256k1::evm::EvmSignature>();
 
 /// Signature scheme used for the public key.
 #[derive(Serialize, Deserialize, Debug, Copy, Clone, Eq, PartialEq)]
@@ -143,19 +144,19 @@ impl Allocative for AccountSignature {
                 signature: _,
                 public_key: _,
             } => {
-                visitor.visit_simple(Key::new("Ed25529_signature"), ED25519_SIGNATURE_SIZE);
+                visitor.visit_simple(Key::new("Ed25529_signature"), ED25519_ACCOUNT_SIGNATURE_SIZE);
             }
             AccountSignature::Secp256k1 {
                 signature: _,
                 public_key: _,
             } => {
-                visitor.visit_simple(Key::new("Secp256k1_signature"), SECP256K1_SIGNATURE_SIZE);
+                visitor.visit_simple(Key::new("Secp256k1_signature"), SECP256K1_ACCOUNT_SIGNATURE_SIZE);
             }
             AccountSignature::EvmSecp256k1 {
                 signature: _,
                 address: _,
             } => {
-                visitor.visit_simple(Key::new("EvmSecp256k1_signature"), EVM_SECP256K1_SIGNATURE_SIZE);
+                visitor.visit_simple(Key::new("EvmSecp256k1_signature"), EVM_SECP256K1_ACCOUNT_SIGNATURE_SIZE);
             }
         }
     }
