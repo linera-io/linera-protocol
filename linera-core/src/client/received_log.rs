cba0091d51cab8e4049328005f747a51ccd7c37c
Bartłomiej Kamiński <bartlomiej.kaminski@linera.io>
2025-10-09 12:11:25 +0200

    Improve batching

--- linera-core/src/client/received_log.rs
+++ linera-core/src/client/received_log.rs
@@ -60,16 +60,7 @@ impl ReceivedLogs {
     /// Splits this `ReceivedLogs` into batches of size `batch_size`. Batches are sorted
     /// by chain ID and height.
     pub(super) fn into_batches(self, batch_size: usize) -> impl Iterator<Item = ReceivedLogs> {
-        LazyBatch {
-            iterator: self.0.into_iter().flat_map(|(chain_id, heights)| {
-                heights.into_iter().flat_map(move |(height, validators)| {
-                    validators
-                        .into_iter()
-                        .map(move |validator| (ChainAndHeight { chain_id, height }, validator))
-                })
-            }),
-            batch_size,
-        }
+        BatchingHelper::new(self.0, batch_size)
     }
 
     fn from_iterator<I: IntoIterator<Item = (ChainAndHeight, ValidatorPublicKey)>>(
@@ -92,21 +83,75 @@ impl ReceivedLogs {
 
 /// Iterator adapter lazily yielding batches of size `self.batch_size` from
 /// `self.iterator`.
-struct LazyBatch<I: Iterator<Item = (ChainAndHeight, ValidatorPublicKey)>> {
-    iterator: I,
+struct BatchingHelper {
+    keys: Vec<ChainId>,
+    heights: BTreeMap<ChainId, BTreeMap<BlockHeight, BTreeSet<ValidatorPublicKey>>>,
     batch_size: usize,
+    current_chain: usize,
 }
 
-impl<I: Iterator<Item = (ChainAndHeight, ValidatorPublicKey)>> Iterator for LazyBatch<I> {
+impl BatchingHelper {
+    fn new(
+        heights: BTreeMap<ChainId, BTreeMap<BlockHeight, BTreeSet<ValidatorPublicKey>>>,
+        batch_size: usize,
+    ) -> Self {
+        let keys = heights.keys().copied().collect();
+        Self {
+            keys,
+            heights,
+            batch_size,
+            current_chain: 0,
+        }
+    }
+
+    fn next_chain_and_height(&mut self) -> Option<(ChainAndHeight, BTreeSet<ValidatorPublicKey>)> {
+        if self.keys.is_empty() {
+            return None;
+        }
+
+        let (chain_id, maybe_heights) = loop {
+            let chain_id = self.keys[self.current_chain];
+            if self.heights[&chain_id].is_empty() {
+                self.heights.remove(&chain_id);
+                self.keys.remove(self.current_chain);
+                if self.current_chain >= self.keys.len() {
+                    self.current_chain = 0;
+                }
+                if self.keys.is_empty() {
+                    return None;
+                }
+            } else {
+                break (chain_id, self.heights.get_mut(&chain_id));
+            }
+        };
+
+        if self.current_chain < self.keys.len() - 1 {
+            self.current_chain += 1;
+        } else {
+            self.current_chain = 0;
+        }
+
+        maybe_heights
+            .and_then(|heights| heights.pop_first())
+            .map(|(height, validators)| (ChainAndHeight { chain_id, height }, validators))
+    }
+}
+
+impl Iterator for BatchingHelper {
     type Item = ReceivedLogs;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.iterator.next().map(|next_item| {
-            ReceivedLogs::from_iterator(
-                std::iter::once(next_item)
-                    .chain((&mut self.iterator).take(self.batch_size.saturating_sub(1))),
-            )
-        })
+        let batch_size = self.batch_size;
+        let result = ReceivedLogs::from_iterator(
+            std::iter::from_fn(|| self.next_chain_and_height())
+                .take(batch_size)
+                .flat_map(|(chain_and_height, validators)| {
+                    validators
+                        .into_iter()
+                        .map(move |validator| (chain_and_height, validator))
+                }),
+        );
+        (result.num_chains() > 0).then_some(result)
     }
 }
 
@@ -122,8 +167,16 @@ mod tests {
 
     #[test]
     fn test_received_log_batching() {
-        let chain1 = ChainId(CryptoHash::test_hash("chain1"));
-        let chain2 = ChainId(CryptoHash::test_hash("chain2"));
+        let (chain1, chain2) = {
+            // make sure that chain1 is lexicographically earlier than chain2
+            let chain_a = ChainId(CryptoHash::test_hash("chain_a"));
+            let chain_b = ChainId(CryptoHash::test_hash("chain_b"));
+            if chain_a < chain_b {
+                (chain_a, chain_b)
+            } else {
+                (chain_b, chain_a)
+            }
+        };
         let validator = ValidatorKeypair::generate().public_key;
         let test_log = ReceivedLogs::from_iterator(
             vec![
@@ -134,8 +187,8 @@ mod tests {
                 (chain2, 2),
                 (chain2, 3),
                 (chain1, 4),
-                (chain2, 4),
                 (chain1, 5),
+                (chain2, 4),
             ]
             .into_iter()
             .map(|(chain_id, height)| {
@@ -157,7 +210,7 @@ mod tests {
                 .iter()
                 .map(|batch| batch.num_chains())
                 .collect::<Vec<_>>(),
-            vec![1, 2, 1, 2, 1]
+            vec![2, 2, 2, 2, 1]
         );
 
         let chains_heights = batches
@@ -176,11 +229,84 @@ mod tests {
         assert_eq!(
             chains_heights,
             vec![
-                vec![(chain1, 1), (chain1, 2)],
-                vec![(chain2, 1), (chain2, 2)],
-                vec![(chain2, 2), (chain2, 3)],
+                vec![(chain1, 1), (chain2, 1)],
+                vec![(chain1, 2), (chain2, 2)],
+                vec![(chain1, 3), (chain2, 3)],
                 vec![(chain1, 4), (chain2, 4)],
                 vec![(chain1, 5)]
+            ]
+        );
+    }
+
+    #[test]
+    fn test_received_log_batching_unbalanced() {
+        let (chain1, chain2) = {
+            // make sure that chain1 is lexicographically earlier than chain2
+            let chain_a = ChainId(CryptoHash::test_hash("chain_a"));
+            let chain_b = ChainId(CryptoHash::test_hash("chain_b"));
+            if chain_a < chain_b {
+                (chain_a, chain_b)
+            } else {
+                (chain_b, chain_a)
+            }
+        };
+        let validator = ValidatorKeypair::generate().public_key;
+        let test_log = ReceivedLogs::from_iterator(
+            vec![
+                (chain1, 1),
+                (chain1, 2),
+                (chain2, 1),
+                (chain1, 3),
+                (chain2, 2),
+                (chain1, 4),
+                (chain1, 5),
+                (chain1, 6),
+                (chain1, 7),
+            ]
+            .into_iter()
+            .map(|(chain_id, height)| {
+                (
+                    ChainAndHeight {
+                        chain_id,
+                        height: height.into(),
+                    },
+                    validator,
+                )
+            }),
+        );
+
+        let batches = test_log.into_batches(2).collect::<Vec<_>>();
+
+        assert_eq!(batches.len(), 5);
+        assert_eq!(
+            batches
+                .iter()
+                .map(|batch| batch.num_chains())
+                .collect::<Vec<_>>(),
+            vec![2, 2, 1, 1, 1]
+        );
+
+        let chains_heights = batches
+            .into_iter()
+            .map(|batch| {
+                batch
+                    .heights_per_chain()
+                    .into_iter()
+                    .flat_map(|(chain_id, heights)| {
+                        heights.into_iter().map(move |height| (chain_id, height.0))
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        assert_eq!(
+            chains_heights,
+            vec![
+                vec![(chain1, 1), (chain2, 1)],
+                vec![(chain1, 2), (chain2, 2)],
+                vec![(chain1, 3), (chain1, 4)],
+                vec![(chain1, 5), (chain1, 6)],
+                vec![(chain1, 7)]
             ]
         );
     }
