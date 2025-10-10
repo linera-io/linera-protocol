97598dca6ac017d493f4604e7354f03300b89f81
Bartłomiej Kamiński <bartlomiej.kaminski@linera.io>
2025-10-10 15:54:13 +0200

    Move ValidatorTracker to the bottom of the file

--- linera-core/src/client/validator_trackers.rs
+++ linera-core/src/client/validator_trackers.rs
@@ -6,53 +6,6 @@ use std::collections::{BTreeMap, BTreeSet, HashMap, VecDeque};
 use linera_base::{crypto::ValidatorPublicKey, data_types::BlockHeight, identifiers::ChainId};
 use linera_chain::data_types::ChainAndHeight;
 
-/// Manages a "tracker"  of a single validator.
-/// The received log is the list of chains and heights of blocks sending messages to a
-/// particular receiver chain. The tracker is the index of the first entry in that log
-/// that corresponds to a block that hasn't been processed yet.
-/// In order to keep the tracker value up to date, we keep the part of the log starting
-/// with the first entry corresponding to a not-yet-processed block, and a record of which
-/// certificates we have already downloaded. Whenever we download a certificate corresponding to
-/// the first block in the log, we increase the tracker and pop the blocks off the log,
-/// until we hit one we haven't downloaded yet.
-struct ValidatorTracker {
-    current_tracker_value: u64,
-    to_be_downloaded: VecDeque<ChainAndHeight>,
-    already_downloaded: BTreeSet<ChainAndHeight>,
-}
-
-impl ValidatorTracker {
-    /// Creates a new `ValidatorTracker`.
-    fn new(tracker: u64, validator_log: Vec<ChainAndHeight>) -> Self {
-        Self {
-            current_tracker_value: tracker,
-            to_be_downloaded: validator_log.into_iter().collect(),
-            already_downloaded: BTreeSet::new(),
-        }
-    }
-
-    /// Marks a certificate at a particular height in a particular chain as downloaded,
-    /// and updates the tracker accordingly.
-    fn downloaded_cert(&mut self, chain_and_height: ChainAndHeight) {
-        self.already_downloaded.insert(chain_and_height);
-        self.maximize_tracker();
-    }
-
-    /// Matches the downloaded certificates with the log and increases the tracker value
-    /// to the first index that hasn't been downloaded yet.
-    fn maximize_tracker(&mut self) {
-        while self
-            .to_be_downloaded
-            .front()
-            .is_some_and(|first_cert| self.already_downloaded.contains(first_cert))
-        {
-            let first_cert = self.to_be_downloaded.pop_front().unwrap();
-            self.already_downloaded.remove(&first_cert);
-            self.current_tracker_value += 1;
-        }
-    }
-}
-
 /// Keeps multiple `ValidatorTracker`s for multiple validators.
 pub(super) struct ValidatorTrackers(BTreeMap<ValidatorPublicKey, ValidatorTracker>);
 
@@ -119,5 +72,52 @@ impl ValidatorTrackers {
         }
 
         remote_heights
+    }
+}
+
+/// Manages a "tracker"  of a single validator.
+/// The received log is the list of chains and heights of blocks sending messages to a
+/// particular receiver chain. The tracker is the index of the first entry in that log
+/// that corresponds to a block that hasn't been processed yet.
+/// In order to keep the tracker value up to date, we keep the part of the log starting
+/// with the first entry corresponding to a not-yet-processed block, and a record of which
+/// certificates we have already downloaded. Whenever we download a certificate corresponding to
+/// the first block in the log, we increase the tracker and pop the blocks off the log,
+/// until we hit one we haven't downloaded yet.
+struct ValidatorTracker {
+    current_tracker_value: u64,
+    to_be_downloaded: VecDeque<ChainAndHeight>,
+    already_downloaded: BTreeSet<ChainAndHeight>,
+}
+
+impl ValidatorTracker {
+    /// Creates a new `ValidatorTracker`.
+    fn new(tracker: u64, validator_log: Vec<ChainAndHeight>) -> Self {
+        Self {
+            current_tracker_value: tracker,
+            to_be_downloaded: validator_log.into_iter().collect(),
+            already_downloaded: BTreeSet::new(),
+        }
+    }
+
+    /// Marks a certificate at a particular height in a particular chain as downloaded,
+    /// and updates the tracker accordingly.
+    fn downloaded_cert(&mut self, chain_and_height: ChainAndHeight) {
+        self.already_downloaded.insert(chain_and_height);
+        self.maximize_tracker();
+    }
+
+    /// Matches the downloaded certificates with the log and increases the tracker value
+    /// to the first index that hasn't been downloaded yet.
+    fn maximize_tracker(&mut self) {
+        while self
+            .to_be_downloaded
+            .front()
+            .is_some_and(|first_cert| self.already_downloaded.contains(first_cert))
+        {
+            let first_cert = self.to_be_downloaded.pop_front().unwrap();
+            self.already_downloaded.remove(&first_cert);
+            self.current_tracker_value += 1;
+        }
     }
 }
