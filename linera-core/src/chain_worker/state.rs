4cd130e546dbe5ffb712bda53b66c01ea27bc779
Bartłomiej Kamiński <bartlomiej.kaminski@linera.io>
2025-10-22 15:50:40 +0200

    Return a Block from ChainWorkerState::execute_block

--- linera-core/src/chain_worker/state.rs
+++ linera-core/src/chain_worker/state.rs
@@ -1301,7 +1301,7 @@ where
         let (_, committee) = self.chain.current_committee()?;
         block.check_proposal_size(committee.policy().maximum_block_proposal_size)?;
 
-        let outcome = self
+        let executed_block = self
             .execute_block(&block, local_time, round, published_blobs)
             .await?;
 
@@ -1317,7 +1317,7 @@ where
                 .await?;
         }
 
-        Ok((outcome.with(block), response))
+        Ok((executed_block, response))
     }
 
     /// Validates and executes a block proposed to extend this chain.
@@ -1426,15 +1426,15 @@ where
         self.chain
             .remove_bundles_from_inboxes(block.timestamp, true, block.incoming_bundles())
             .await?;
-        let outcome = if let Some(outcome) = outcome {
-            outcome.clone()
+        let block = if let Some(outcome) = outcome {
+            outcome.clone().with(proposal.content.block.clone())
         } else {
             self.execute_block(block, local_time, round.multi_leader(), &published_blobs)
                 .await?
         };
 
         ensure!(
-            !round.is_fast() || !outcome.has_oracle_responses(),
+            !round.is_fast() || !block.has_oracle_responses(),
             WorkerError::FastBlockUsingOracles
         );
         let chain = &mut self.chain;
@@ -1442,12 +1442,11 @@ where
         chain
             .tip_state
             .get_mut()
-            .update_counters(&block.transactions, &outcome.messages)?;
+            .update_counters(&block.body.transactions, &block.body.messages)?;
         // Don't save the changes since the block is not confirmed yet.
         chain.rollback();
 
         // Create the vote and store it in the chain state.
-        let block = outcome.with(proposal.content.block.clone());
         let created_blobs: BTreeMap<_, _> = block.iter_created_blobs().collect();
         let blobs = self
             .get_required_blobs(proposal.expected_blob_ids(), &created_blobs)
@@ -1553,14 +1552,14 @@ where
         local_time: Timestamp,
         round: Option<u32>,
         published_blobs: &[Blob],
-    ) -> Result<BlockExecutionOutcome, WorkerError> {
+    ) -> Result<Block, WorkerError> {
         let outcome =
             Box::pin(
                 self.chain
                     .execute_block(block, local_time, round, published_blobs, None),
             )
             .await?;
-        let block = Block::new(block.clone(), outcome.clone());
+        let block = Block::new(block.clone(), outcome);
         let block_hash = CryptoHash::new(&block);
         self.execution_state_cache.insert_owned(
             &block_hash,
@@ -1569,7 +1568,7 @@ where
                 .with_context(|ctx| InactiveContext(ctx.base_key().clone()))
                 .await,
         );
-        Ok(outcome)
+        Ok(block)
     }
 
     /// Initializes and saves the current chain if it is not active yet.
