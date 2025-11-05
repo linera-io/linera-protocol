e92fd4f87d1ffc5415fd70c584696c94330968c3
Andreas Fackler <afck@users.noreply.github.com>
2025-11-05 16:55:00 +0100

    Sync if we learn about a new round while requesting a timeout. (#4912)
    ## Motivation
    
    If a client requests a timeout certificate and the validators return a
    `WrongRound` error with a _higher_ round, the client currently fails to
    update its local node about the new round, and thus cannot propose a
    block.
    
    ## Proposal
    
    Synchronize the chain state if we get a `WrongRound` error with a round
    higher than the one we see locally.
    
    ## Test Plan
    
    A regression test was added (by Claude).
    
    ## Release Plan
    
    - These changes should be ported to `testnet_conway` and
      * released in a new SDK.
    
    ## Links
    
    - Fixes #4909.
    - [reviewer
    checklist](https://github.com/linera-io/linera-protocol/blob/main/CONTRIBUTING.md#reviewer-checklist)

--- linera-core/src/client/mod.rs
+++ linera-core/src/client/mod.rs
@@ -259,7 +259,7 @@ pub struct Client<Env: Environment> {
     environment: Env,
     /// Local node to manage the execution state and the local storage of the chains that we are
     /// tracking.
-    local_node: LocalNodeClient<Env::Storage>,
+    pub local_node: LocalNodeClient<Env::Storage>,
     /// Manages the requests sent to validator nodes.
     requests_scheduler: RequestsScheduler<Env>,
     /// The admin chain ID.
@@ -673,7 +673,7 @@ impl<Env: Environment> Client<Env> {
     /// Submits a validated block for finalization and returns the confirmed block certificate.
     #[instrument(level = "trace", skip_all)]
     async fn finalize_block(
-        &self,
+        self: &Arc<Self>,
         committee: &Committee,
         certificate: ValidatedBlockCertificate,
     ) -> Result<ConfirmedBlockCertificate, chain_client::Error> {
@@ -694,7 +694,7 @@ impl<Env: Environment> Client<Env> {
     /// Submits a block proposal to the validators.
     #[instrument(level = "trace", skip_all)]
     async fn submit_block_proposal<T: ProcessableCertificate>(
-        &self,
+        self: &Arc<Self>,
         committee: &Committee,
         proposal: Box<BlockProposal>,
         value: T,
@@ -718,7 +718,7 @@ impl<Env: Environment> Client<Env> {
     /// Broadcasts certified blocks to validators.
     #[instrument(level = "trace", skip_all, fields(chain_id, block_height, delivery))]
     async fn communicate_chain_updates(
-        &self,
+        self: &Arc<Self>,
         committee: &Committee,
         chain_id: ChainId,
         height: BlockHeight,
@@ -732,7 +732,7 @@ impl<Env: Environment> Client<Env> {
             |remote_node| {
                 let mut updater = ValidatorUpdater {
                     remote_node,
-                    local_node: self.local_node.clone(),
+                    client: self.clone(),
                     admin_id: self.admin_id,
                 };
                 Box::pin(async move {
@@ -754,7 +754,7 @@ impl<Env: Environment> Client<Env> {
     /// and returns a certificate.
     #[instrument(level = "trace", skip_all)]
     async fn communicate_chain_action<T: CertificateValue>(
-        &self,
+        self: &Arc<Self>,
         committee: &Committee,
         action: CommunicateAction,
         value: T,
@@ -767,7 +767,7 @@ impl<Env: Environment> Client<Env> {
             |remote_node| {
                 let mut updater = ValidatorUpdater {
                     remote_node,
-                    local_node: self.local_node.clone(),
+                    client: self.clone(),
                     admin_id: self.admin_id,
                 };
                 let action = action.clone();
@@ -1202,7 +1202,7 @@ impl<Env: Environment> Client<Env> {
     /// Downloads any certificates from the specified validator that we are missing for the given
     /// chain, and processes them.
     #[instrument(level = "trace", skip(self, remote_node, chain_id))]
-    async fn synchronize_chain_state_from(
+    pub(crate) async fn synchronize_chain_state_from(
         &self,
         remote_node: &RemoteNode<Env::ValidatorNode>,
         chain_id: ChainId,
