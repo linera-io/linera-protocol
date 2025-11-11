412390dc68ae7f5c5558b89e405fa956e1973890
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2025-11-11 20:12:54 +0100

    Change the MissingEntries error.

--- linera-views/src/views/log_view.rs
+++ linera-views/src/views/log_view.rs
@@ -316,8 +316,7 @@ where
         for entry in self.context.store().read_multi_values(&keys).await? {
             match entry {
                 None => {
-                    let root_key = self.context.store().root_key()?;
-                    return Err(ViewError::MissingEntries(root_key));
+                    return Err(ViewError::MissingEntries("LogView".into()));
                 }
                 Some(value) => values.push(value),
             }
