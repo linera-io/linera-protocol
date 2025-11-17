5305071a2340f9cdf9f31cec0fc76671317135aa
deuszx <mateusz.gorski@linera.io>
2025-11-17 18:26:00 +0000

    Add Postgres support

---
+++ linera-indexer/lib/src/db/postgres/tests.rs
@@ -0,0 +1,9 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! Tests for PostgreSQL database implementation.
+
+#[cfg(test)]
+mod tests {
+    // TODO: Add tests for PostgreSQL database implementation
+}
