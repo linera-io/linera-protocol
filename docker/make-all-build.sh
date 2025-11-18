91d1107b27404b943d97a38b20dd091034197339
deuszx <mateusz.gorski@linera.io>
2025-11-18 15:12:57 +0000

    Update docker scripts

--- docker/make-all-build.sh
+++ docker/make-all-build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # NOTE: This script assumes to be called from within the `/docker` directory.
 echo "Building linera images..."
-docker build -f ./Dockerfile.indexer-test -t linera-all-test ..
+docker build -f ./Dockerfile.indexer -t linera-all-test ..
 
 echo "Building linera explorer image..."
 docker build -f ./Dockerfile.explorer -t linera-explorer-new ..
-
 
 echo "Done building images"
