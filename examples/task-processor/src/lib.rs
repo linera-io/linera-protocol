5b7d4d492488ae449bdcf58101ad129788f92617
Andreas Fackler <andreas.fackler@linera.io>
2025-12-02 11:48:25 +0100

    Add an example app to test the task processor.

---
+++ examples/task-processor/src/lib.rs
@@ -0,0 +1,33 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+//! ABI of the Task Processor Example Application.
+//!
+//! This application demonstrates the off-chain task processor functionality.
+//! It requests tasks to be executed by an external "echo" operator and stores
+//! the results in its state.
+
+use async_graphql::{Request, Response};
+use linera_sdk::linera_base_types::{ContractAbi, ServiceAbi};
+use serde::{Deserialize, Serialize};
+
+pub struct TaskProcessorAbi;
+
+/// Operations that can be executed on the contract.
+#[derive(Debug, Deserialize, Serialize)]
+pub enum TaskProcessorOperation {
+    /// Request a task to be processed by the given operator with the given input.
+    RequestTask { operator: String, input: String },
+    /// Store the result of a completed task.
+    StoreResult { result: String },
+}
+
+impl ContractAbi for TaskProcessorAbi {
+    type Operation = TaskProcessorOperation;
+    type Response = ();
+}
+
+impl ServiceAbi for TaskProcessorAbi {
+    type Query = Request;
+    type QueryResponse = Response;
+}
