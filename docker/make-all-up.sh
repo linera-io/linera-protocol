3702e40e222a1775355735931e19f1e30dbdc5b2
deuszx <95355183+deuszx@users.noreply.github.com>
2025-12-05 18:02:12 +0100

    Switch to ScyllaDB when testing the exporter stack locally (#5066) (#5087)
    Backport of #5066 
    
    ## Motivation
    
    Our testing setup should resemble production one as close as possible.
    
    ## Proposal
    
    For testing block exporter stack, replace `linera-storage-service`
    docker container w/ Scylla.
    
    Setup is mimicing what was done in `docker-compose.yml`.
    
    ## Test Plan
    
    Manual.
    
    ## Release Plan
    
    - Nothing to do. This is test only.
    
    ## Links
    
    - [reviewer
    checklist](https://github.com/linera-io/linera-protocol/blob/main/CONTRIBUTING.md#reviewer-checklist)

--- docker/make-all-up.sh
+++ docker/make-all-up.sh
@@ -5,5 +5,10 @@ echo "Removing exporter data..."
 rm -rf ./exporter-data/
 echo "Removing indexer data..."
 rm -rf ./indexer-data/
+echo "Removing scylla data..."
+rm -rf ./scylla-data/
+echo "Clear docker volumes..."
+docker system prune -f
+docker volume prune -f
 echo "All data removed"
 docker-compose -f ./docker-compose.indexer-test.yml up --force-recreate --remove-orphans
