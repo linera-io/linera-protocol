78ada45bca8d694bc1be4a334f7e573d57cae513
Bartłomiej Kamiński <bartlomiej.kaminski@linera.io>
2026-01-12 14:05:17 +0000

    Fix the open-multi-owner-chain wrapper

--- linera-service/src/cli_wrappers/wallet.rs
+++ linera-service/src/cli_wrappers/wallet.rs
@@ -902,12 +902,12 @@ impl ClientWrapper {
             .arg("open-multi-owner-chain")
             .args(["--from", &from.to_string()])
             .arg("--owners")
-            .args(owners.iter().map(AccountOwner::to_string))
+            .arg(serde_json::to_string(&owners)?)
             .args(["--base-timeout-ms", &base_timeout_ms.to_string()]);
         if !weights.is_empty() {
             command
                 .arg("--owner-weights")
-                .args(weights.iter().map(u64::to_string));
+                .arg(serde_json::to_string(&weights)?);
         };
         command
             .args(["--multi-leader-rounds", &multi_leader_rounds.to_string()])
