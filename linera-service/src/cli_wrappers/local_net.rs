9eee9a7f2c8e09de2d3863d8bbd089ac225b10b0
deuszx <mateusz.gorski@linera.io>
2026-01-21 13:05:13 +0000

    Restructure exporter CLI to support destinations subcommands

--- linera-service/src/cli_wrappers/local_net.rs
+++ linera-service/src/cli_wrappers/local_net.rs
@@ -770,7 +770,7 @@ impl LocalNet {
         let child = self
             .command_for_binary("linera-exporter")
             .await?
-            .args(["--config-path", &config_path])
+            .args(["run", "--config-path", &config_path])
             .args(["--storage", &storage.to_string()])
             .spawn_into()?;
 
