f47c7d430a3cab213a1b59f783989283c8360ffe
James Kay <james.kay@linera.io>
2026-01-22 20:12:52 +0000

    Web storage (#5265) (#5308)
    We'd like to not recompute chains on page load, especially for upcoming
    apps where chains can become quite long.
    
    Store the ephemeral chain data in IndexedDB on the page.
    
    I thought this would be relatively straightforward, as we already had an
    IndexedDB storage implementation that was tested in CI. Unfortunately,
    it turns out that there were some problems not caught by those tests:
    - the `create` implementation for the `linera_views::KeyValueDatabase`
    trait was nonsensical, as it could only be executed inside a
    `versionchange` transaction, i.e. when connecting to the database
    - the library we were using for IndexedDB, `indexed_db_futures`, has [a
    bug](https://github.com/Alorel/rust-indexed-db/issues/33) that manifests
    in multithreaded `wasm-bindgen-futures` executors; since we use
    multithreading for chain execution, this bug is activated
    
    In practice, this means this PR:
    - relaxes some `Send` and `Sync` bounds throughout the codebase
    - rethinks the way that `linera_views::backends::indexed_db` does
    namespacing, using the database name for namespacing instead of the
    object store
    - removes the IndexedDB `linera-persistent` implementation, since we
    aren't using it right now and if we do start using it it will need to be
    rewritten anyway
    - replaces `indexed_db_futures` in `linera-views` with
    [`indexed-db`](https://docs.rs/indexed-db/)
    - adds some functionality to the counter demo to reüse private keys by
    providing a mnemonic in the URL parameters (for testing purposes only!)
    
    CI tests should cover basic store functionality. Additionally, I tested
    the functionality locally with the counter app (which I extended with
    functionality to read the wallet mnemonic from the URL query
    parameters).
    
    - These changes should be backported to the latest `testnet` branch,
    then
        - be released in a new SDK,
    
    - [reviewer
    checklist](https://github.com/linera-io/linera-protocol/blob/main/CONTRIBUTING.md#reviewer-checklist)
    - fixes https://github.com/linera-io/linera-protocol/issues/5064

--- web/@linera/client/src/storage.rs
+++ web/@linera/client/src/storage.rs
@@ -1,9 +1,10 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-// TODO(#4637): convert to IndexedDbStore once we refactor Context
-pub type Storage =
-    linera_storage::DbStorage<linera_views::memory::MemoryDatabase, linera_storage::WallClock>;
+pub type Storage = linera_storage::DbStorage<
+    linera_views::indexed_db::IndexedDbDatabase,
+    linera_storage::WallClock,
+>;
 
 /// Create and return the storage implementation.
 ///
@@ -11,9 +12,8 @@ pub type Storage =
 /// If the storage can't be initialized.
 pub async fn get_storage() -> Result<Storage, linera_views::ViewError> {
     Ok(linera_storage::DbStorage::maybe_create_and_connect(
-        &linera_views::memory::MemoryStoreConfig {
+        &linera_views::indexed_db::IndexedDbStoreConfig {
             max_stream_queries: 1,
-            kill_on_drop: false,
         },
         "linera",
         Some(linera_execution::WasmRuntime::Wasmer),
