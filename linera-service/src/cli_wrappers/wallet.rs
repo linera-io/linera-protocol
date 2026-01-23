4578071f16e9e4268dd4c05251cddbf9e8209b33
Bartłomiej Kamiński <bartlomiej.kaminski@linera.io>
2026-01-23 15:12:43 +0000

    [testnet] Add a controller end-to-end test; fix an issue in the controller (backport: #5347) (#5391)
    == Motivation ==
    
    An issue with task processors being unable to propose blocks was discovered on the testnet.
    
    == Proposal ==
    
    This fixes the issue by setting a preferred owner in the chain client passed to the TaskProcessor. Also, a controller end-to-end test, extended by steps exercising the code which exhibited the issue, is backported.
    
    == Test Plan ==
    
    CI

--- linera-service/src/cli_wrappers/wallet.rs
+++ linera-service/src/cli_wrappers/wallet.rs
@@ -526,6 +526,47 @@ impl ClientWrapper {
         bail!("Failed to start node service");
     }
 
+    /// Runs `linera service` with a controller application.
+    pub async fn run_node_service_with_controller(
+        &self,
+        port: impl Into<Option<u16>>,
+        process_inbox: ProcessInbox,
+        controller_id: &ApplicationId,
+        operators: &[(String, PathBuf)],
+    ) -> Result<NodeService> {
+        let port = port.into().unwrap_or(8080);
+        let mut command = self.command().await?;
+        command.arg("service");
+        if let ProcessInbox::Skip = process_inbox {
+            command.arg("--listener-skip-process-inbox");
+        }
+        if let Ok(var) = env::var(CLIENT_SERVICE_ENV) {
+            command.args(var.split_whitespace());
+        }
+        command.args(["--controller-id", &controller_id.to_string()]);
+        for (name, path) in operators {
+            command.args(["--operators", &format!("{}={}", name, path.display())]);
+        }
+        let child = command
+            .args(["--port".to_string(), port.to_string()])
+            .spawn_into()?;
+        let client = reqwest_client();
+        for i in 0..10 {
+            linera_base::time::timer::sleep(Duration::from_secs(i)).await;
+            let request = client
+                .get(format!("http://localhost:{}/", port))
+                .send()
+                .await;
+            if request.is_ok() {
+                tracing::info!("Node service has started");
+                return Ok(NodeService::new(port, child));
+            } else {
+                tracing::warn!("Waiting for node service to start");
+            }
+        }
+        bail!("Failed to start node service");
+    }
+
     /// Runs `linera validator query`
     pub async fn query_validator(&self, address: &str) -> Result<CryptoHash> {
         let mut command = self.command().await?;
