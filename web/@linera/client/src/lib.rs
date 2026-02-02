bb2c9a211cf7d695e875147566abb9acf7d2d525
James Kay <james.kay@linera.io>
2026-02-02 19:46:36 +0000

    `@linera/client`: runtime-configurable logging

--- web/@linera/client/src/lib.rs
+++ web/@linera/client/src/lib.rs
@@ -22,14 +22,13 @@ key directly in memory and uses it to sign.
 #![allow(clippy::unused_async)]
 #![recursion_limit = "256"]
 
-use std::{rc::Rc, sync::Arc};
+use std::rc::Rc;
 
-use futures::{future::FutureExt as _, lock::Mutex as AsyncMutex};
-use linera_base::identifiers::ChainId;
-use linera_client::chain_listener::{ChainListener, ClientContext as _};
 use wasm_bindgen::prelude::*;
 use web_sys::wasm_bindgen;
 
+pub mod client;
+pub use client::Client;
 pub mod chain;
 pub use chain::Chain;
 pub mod faucet;
@@ -44,108 +43,36 @@ pub use wallet::Wallet;
 pub type Network = linera_rpc::node_provider::NodeProvider;
 pub type Environment =
     linera_core::environment::Impl<Storage, Network, Signer, Rc<linera_core::wallet::Memory>>;
+
 type JsResult<T> = Result<T, JsError>;
 
-/// The full client API, exposed to the wallet implementation. Calls
-/// to this API can be trusted to have originated from the user's
-/// request.
-#[wasm_bindgen]
-#[derive(Clone)]
-pub struct Client(
-    // This use of `futures::lock::Mutex` is safe because we only
-    // expose concurrency to the browser, which must always run all
-    // futures on the global task queue.
-    // It does nothing here in this single-threaded context, but is
-    // hard-coded by `ChainListener`.
-    Arc<AsyncMutex<linera_client::ClientContext<Environment>>>,
-);
-
-#[wasm_bindgen]
-impl Client {
-    /// Creates a new client and connects to the network.
-    ///
-    /// # Errors
-    /// On transport or protocol error, if persistent storage is
-    /// unavailable, or if `options` is incorrectly structured.
-    #[wasm_bindgen(constructor)]
-    pub async fn new(
-        wallet: &Wallet,
-        signer: Signer,
-        options: Option<linera_client::Options>,
-    ) -> Result<Client, JsError> {
-        const BLOCK_CACHE_SIZE: usize = 5000;
-        const EXECUTION_STATE_CACHE_SIZE: usize = 10000;
-
-        let options = options.unwrap_or_default();
-
-        let mut storage = storage::get_storage().await?;
-        wallet
-            .genesis_config
-            .initialize_storage(&mut storage)
-            .await?;
-
-        let client = linera_client::ClientContext::new(
-            storage.clone(),
-            wallet.chains.clone(),
-            signer,
-            &options,
-            wallet.default,
-            wallet.genesis_config.clone(),
-            BLOCK_CACHE_SIZE,
-            EXECUTION_STATE_CACHE_SIZE,
-        )
-        .await?;
-        // The `Arc` here is useless, but it is required by the `ChainListener` API.
-        #[expect(clippy::arc_with_non_send_sync)]
-        let client = Arc::new(AsyncMutex::new(client));
-        let client_clone = client.clone();
-        let chain_listener = ChainListener::new(
-            options.chain_listener_config,
-            client_clone,
-            storage,
-            tokio_util::sync::CancellationToken::new(),
-            tokio::sync::mpsc::unbounded_channel().1,
-            true, // Enable background sync
-        )
-        .run()
-        .boxed_local()
-        .await?
-        .boxed_local();
-        wasm_bindgen_futures::spawn_local(
-            async move {
-                if let Err(error) = chain_listener.await {
-                    tracing::error!("ChainListener error: {error:?}");
-                }
-            }
-            .boxed_local(),
-        );
-        log::info!("Linera Web client successfully initialized");
-        Ok(Self(client))
-    }
-
-    /// Connect to a chain on the Linera network.
-    ///
-    /// # Errors
-    ///
-    /// If the wallet could not be read or chain synchronization fails.
-    #[wasm_bindgen]
-    pub async fn chain(&self, chain: ChainId) -> JsResult<Chain> {
-        Ok(Chain {
-            chain_client: self.0.lock().await.make_chain_client(chain).await?,
-            client: self.clone(),
-        })
-    }
+#[derive(serde::Deserialize, Default, tsify::Tsify)]
+#[tsify(from_wasm_abi)]
+#[serde(default, rename_all = "camelCase")]
+pub struct InitializeOptions {
+    /// A log filter string, in the format of [`tracing_subscriber::EnvFilter`].
+    /// Defaults to `INFO`.
+    pub log: String,
+    /// Whether to enable performance reporting through the Performance API.
+    pub profiling: bool,
 }
 
-#[wasm_bindgen(start)]
-pub fn main() {
+#[wasm_bindgen]
+pub fn initialize(options: Option<InitializeOptions>) {
     use tracing_subscriber::{
-        prelude::__tracing_subscriber_SubscriberExt as _, util::SubscriberInitExt as _,
+        prelude::__tracing_subscriber_SubscriberExt as _, util::SubscriberInitExt as _, EnvFilter,
     };
+
+    let options = options.unwrap_or_default();
 
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
     tracing_subscriber::registry()
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(tracing_subscriber::filter::LevelFilter::INFO.into())
+                .parse_lossy(options.log),
+        )
         .with(
             tracing_subscriber::fmt::layer()
                 .with_ansi(false)
@@ -153,8 +80,11 @@ pub fn main() {
                 .with_writer(tracing_web::MakeWebConsoleWriter::new()),
         )
         .with(
-            tracing_web::performance_layer()
-                .with_details_from_fields(tracing_subscriber::fmt::format::Pretty::default()),
+            Some(
+                tracing_web::performance_layer()
+                    .with_details_from_fields(tracing_subscriber::fmt::format::Pretty::default()),
+            )
+            .filter(|_| options.profiling),
         )
         .init();
 }
