1759d24f627332b9d50239fe49da612a68c37316
deuszx <95355183+deuszx@users.noreply.github.com>
2026-02-05 13:42:04 +0000

    Add metamask fungible example (#5389)
    ## Motivation
    
    <!--
    Briefly describe the goal(s) of this PR.
    -->
    
    ## Proposal
    
    <!--
    Summarize the proposed changes and how they address the goal(s) stated
    above.
    -->
    
    ## Test Plan
    
    <!--
    Explain how you made sure that the changes are correct and that they
    perform as intended.
    
    Please describe testing protocols (CI, manual tests, benchmarks, etc) in
    a way that others
    can reproduce the results.
    -->
    
    ## Release Plan
    
    <!--
    If this PR targets the `main` branch, **keep the applicable lines** to
    indicate if you
    recommend the changes to be picked in release branches, SDKs, and
    hotfixes.
    
    This generally concerns only bug fixes.
    
    Note that altering the public protocol (e.g. transaction format, WASM
    syscalls) or storage
    formats requires a new deployment.
    -->
    - Nothing to do / These changes follow the usual release cycle.
    - These changes should be backported to the latest `devnet` branch, then
        - be released in a new SDK,
        - be released in a validator hotfix.
    - These changes should be backported to the latest `testnet` branch,
    then
        - be released in a new SDK,
        - be released in a validator hotfix.
    - Backporting is not possible but we may want to deploy a new `devnet`
    and release a new
          SDK soon.
    
    ## Links
    
    <!--
    Optional section for related PRs, related issues, and other references.
    
    If needed, please create issues to track future improvements and link
    them here.
    -->
    - [reviewer
    checklist](https://github.com/linera-io/linera-protocol/blob/main/CONTRIBUTING.md#reviewer-checklist)

---
+++ examples/native-fungible/metamask/vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
+
+const checkEnvironment = required => {
+  return {
+    name: 'checkEnvironment',
+    config(config, { mode }) {
+      const available = {...process.env, ...loadEnv(mode, process.cwd(), config.envPrefix)};
+      const missing = required.filter(env => !available[env]);
+      if (missing.length > 0)
+        throw new Error(`required environment variables missing: ${missing.toString()}`);
+    }
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '',
+  envPrefix: 'LINERA_',
+  plugins: [
+    checkEnvironment([
+      'LINERA_FAUCET_URL',
+      'LINERA_APPLICATION_ID',
+    ]),
+  ],
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    },
+    fs: {
+      allow: [
+        path.resolve(__dirname, '.'),
+        path.resolve(__dirname, '../../../web'),
+      ],
+    },
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true,
+    },
+  },
+})
