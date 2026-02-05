18d142dc9fe01976831d190c34a768b248064f75
deuszx <95355183+deuszx@users.noreply.github.com>
2026-02-05 11:01:57 +0000

    Testnet backport of conditional logging in the linera web client (#5383)
    #5373
    
    ## Motivation
    
    Performance profiling, while useful, causes the JavaScript memory usage
    to expand greatly (and grow over time!).
    
    
    ## Proposal
    
    Disable profiling and logs below `INFO` by default, and add a couple of
    different ways of enabling them:
    
    The `initialize` function now takes an `InitializeOptions` that can be
    used to set two options:
    
    - `log` takes a log filter string à la `RUST_LOG`;
    - `profiling` is a boolean that can be used to enable performance
    reporting via the Web Performance API.
    
    If not set explicitly, and `window.location` is available, defaults for
    these can also be provided in the URL search parameters as `LINERA_LOG`
    and `LINERA_PROFILING` respectively.
    
    ## Test Plan
    
    Tested locally.
    
    ## Release Plan
    
    -  These changes should be released in the next SDK release.
    
    ## Links
    
    #5373 
    
    <!--
    Optional section for related PRs, related issues, and other references.
    
    If needed, please create issues to track future improvements and link
    them here.
    -->
    - [reviewer
    checklist](https://github.com/linera-io/linera-protocol/blob/main/CONTRIBUTING.md#reviewer-checklist)
    
    ---------
    
    Co-authored-by: James Kay <james.kay@linera.io>

---
+++ web/@linera/client/src/client.rs
@@ -0,0 +1,105 @@
+// Copyright (c) Zefchain Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::sync::Arc;
+
+use futures::{future::FutureExt as _, lock::Mutex as AsyncMutex};
+use linera_base::identifiers::ChainId;
+use linera_client::chain_listener::{ChainListener, ClientContext as _};
+use wasm_bindgen::prelude::*;
+use web_sys::wasm_bindgen;
+
+use crate::{chain::Chain, signer::Signer, storage, wallet::Wallet, Environment, JsResult};
+
+/// The full client API, exposed to the wallet implementation. Calls
+/// to this API can be trusted to have originated from the user's
+/// request.
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct Client {
+    // This use of `futures::lock::Mutex` is safe because we only
+    // expose concurrency to the browser, which must always run all
+    // futures on the global task queue.
+    // It does nothing here in this single-threaded context, but is
+    // hard-coded by `ChainListener`.
+    pub(crate) client_context: Arc<AsyncMutex<linera_client::ClientContext<Environment>>>,
+}
+
+#[wasm_bindgen]
+impl Client {
+    /// Creates a new client and connects to the network.
+    ///
+    /// # Errors
+    /// On transport or protocol error, if persistent storage is
+    /// unavailable, or if `options` is incorrectly structured.
+    #[wasm_bindgen(constructor)]
+    pub async fn new(
+        wallet: &Wallet,
+        signer: Signer,
+        options: Option<linera_client::Options>,
+    ) -> Result<Client, JsError> {
+        let options = options.unwrap_or_default();
+
+        let mut storage = storage::get_storage().await?;
+        wallet
+            .genesis_config
+            .initialize_storage(&mut storage)
+            .await?;
+
+        let client = linera_client::ClientContext::new(
+            storage.clone(),
+            wallet.chains.clone(),
+            signer,
+            &options,
+            wallet.default,
+            wallet.genesis_config.clone(),
+        )
+        .await?;
+        // The `Arc` here is useless, but it is required by the `ChainListener` API.
+        #[expect(clippy::arc_with_non_send_sync)]
+        let client = Arc::new(AsyncMutex::new(client));
+        let client_clone = client.clone();
+        let chain_listener = ChainListener::new(
+            options.chain_listener_config,
+            client_clone,
+            storage,
+            tokio_util::sync::CancellationToken::new(),
+            tokio::sync::mpsc::unbounded_channel().1,
+            true, // Enable background sync
+        )
+        .run()
+        .boxed_local()
+        .await?
+        .boxed_local();
+        wasm_bindgen_futures::spawn_local(
+            async move {
+                if let Err(error) = chain_listener.await {
+                    tracing::error!("ChainListener error: {error:?}");
+                }
+            }
+            .boxed_local(),
+        );
+        log::info!("Linera Web client successfully initialized");
+        Ok(Self {
+            client_context: client,
+        })
+    }
+
+    /// Connect to a chain on the Linera network.
+    ///
+    /// # Errors
+    ///
+    /// If the wallet could not be read or chain synchronization fails.
+    #[wasm_bindgen]
+    pub async fn chain(&self, chain: ChainId) -> JsResult<Chain> {
+        Ok(Chain {
+            chain_client: self
+                .client_context
+                .lock()
+                .await
+                .make_chain_client(chain)
+                .await?,
+            client: self.clone(),
+        })
+    }
+}
--- web/@linera/client/src/index.ts
+++ web/@linera/client/src/index.ts
@@ -1,4 +1,18 @@
 export * from './wasm/index.js';
-export { default as initialize } from './wasm/index.js';
 export * as signer from './signer/index.js';
 export type { Signer } from './signer/Signer.js';
+
+import * as wasm from './wasm/index.js';
+
+export async function initialize(options?: wasm.InitializeOptions) {
+  if (window.location) {
+    const params = new URL(window.location.href).searchParams;
+    const defaults: wasm.InitializeOptions = {};
+    defaults.profiling = params.get('LINERA_PROFILING') !== null;
+    defaults.log = params.get('LINERA_LOG') || '';
+    options = { ...defaults, ...options };
+  }
+
+  await wasm.default();
+  wasm.initialize(options);
+}
