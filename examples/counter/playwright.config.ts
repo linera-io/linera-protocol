ea5741d5f53e3661f2a96cfb7bfb6127185d8dae
James Kay <james.kay@linera.io>
2026-02-23 14:52:02 +0000

    try without `workers` setting

--- examples/counter/playwright.config.ts
+++ examples/counter/playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
