f67b26eaa6500eee88be5cd6b6d91bedabc2632c
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2026-03-11 12:28:33 +0100

    Introduce the "save_and_drop".

--- examples/gen-nft/src/contract.rs
+++ examples/gen-nft/src/contract.rs
@@ -124,8 +124,11 @@ impl Contract for GenNftContract {
         }
     }
 
-    async fn store(mut self) {
-        self.state.save().await.expect("Failed to save state");
+    async fn store(self) {
+        self.state
+            .save_and_drop()
+            .await
+            .expect("Failed to save state");
     }
 }
 
