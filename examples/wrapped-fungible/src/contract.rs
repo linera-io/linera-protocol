75ff2fc9be0bc1ddb88107990004ff378f945b67
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2026-03-12 10:07:38 +0100

    Backport the save_and_drop from main.

--- examples/wrapped-fungible/src/contract.rs
+++ examples/wrapped-fungible/src/contract.rs
@@ -153,8 +153,11 @@ impl Contract for WrappedFungibleTokenContract {
         }
     }
 
-    async fn store(mut self) {
-        self.state.save().await.expect("Failed to save state");
+    async fn store(self) {
+        self.state
+            .save_and_drop()
+            .await
+            .expect("Failed to save state");
     }
 }
 
