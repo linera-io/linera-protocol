488bf6f2ca46a165c306591fdb3bc21f890be4ea
Andreas Fackler <andreas.fackler@linera.io>
2026-05-06 10:03:41 +0000

    cargo fmt

--- linera-execution/src/system.rs
+++ linera-execution/src/system.rs
@@ -458,17 +458,12 @@ where
                         };
                         let stream_id = StreamId::system(EPOCH_STREAM_NAME);
                         let next_index = epoch.0.checked_add(1).ok_or(ArithmeticError::Overflow)?;
-                        self.stream_event_counts
-                            .insert(&stream_id, next_index)?;
+                        self.stream_event_counts.insert(&stream_id, next_index)?;
                         txn_tracker.add_event(stream_id, epoch.0, bcs::to_bytes(&event_data)?);
                     }
                     AdminOperation::RemoveCommittee { epoch } => {
                         let stream_id = StreamId::system(REMOVED_EPOCH_STREAM_NAME);
-                        let count = self
-                            .stream_event_counts
-                            .get(&stream_id)
-                            .await?
-                            .unwrap_or(0);
+                        let count = self.stream_event_counts.get(&stream_id).await?.unwrap_or(0);
                         // Revocations must happen in increasing epoch order, so the stream's
                         // indices stay sequential.
                         ensure!(
@@ -476,8 +471,7 @@ where
                             ExecutionError::InvalidCommitteeRemoval
                         );
                         let next_index = epoch.0.checked_add(1).ok_or(ArithmeticError::Overflow)?;
-                        self.stream_event_counts
-                            .insert(&stream_id, next_index)?;
+                        self.stream_event_counts.insert(&stream_id, next_index)?;
                         txn_tracker.add_event(stream_id, epoch.0, vec![]);
                     }
                 }
