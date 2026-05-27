7c699ad935a4b9a498ec116083e2d457cfe915da
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2026-05-26 22:06:33 -0400

    linera-persistent: require docs with #![deny(missing_docs)]

--- linera-persistent/src/memory.rs
+++ linera-persistent/src/memory.rs
@@ -1,8 +1,11 @@
 // Copyright (c) Zefchain Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+//! An in-memory [`Persist`] backend that holds the value without writing it anywhere.
+
 use super::Persist;
 
+/// The error type of the in-memory backend: persisting can never fail.
 pub type Error = std::convert::Infallible;
 
 /// A dummy [`Persist`] implementation that doesn't persist anything, but holds the value
@@ -14,6 +17,7 @@ pub struct Memory<T> {
 }
 
 impl<T> Memory<T> {
+    /// Wraps a value in an in-memory backend.
     pub fn new(value: T) -> Self {
         Self { value }
     }
